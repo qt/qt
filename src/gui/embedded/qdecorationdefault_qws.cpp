154804f762a7cb6633b802a0c6c55c88368f6500
Jason McDonald <jason.mcdonald@nokia.com>
2009-04-08 17:18:16 +1000

    Replace license headers with 'release' versions
    This change was performed by a script that was written by the
    previous release manager and reviewed by me.  Thus
    Reviewed-by: Trust Me

--- src/gui/embedded/qdecorationdefault_qws.cpp
+++ src/gui/embedded/qdecorationdefault_qws.cpp
@@ -6,11 +6,11 @@
 ** This file is part of the QtGui module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
-** No Commercial Usage
-** This file contains pre-release code and may not be distributed.
-** You may use this file in accordance with the terms and conditions
-** contained in the either Technology Preview License Agreement or the
-** Beta Release License Agreement.
+** Commercial Usage
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Nokia.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
