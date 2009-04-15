qt/qt

commit 52f87de53328c661049acf09d5fedc1850aa9bfa

Fix docs of QPainter::initFrom.

src/gui/painting/qpainter.cpp
@@ -1465,8 +1465,8 @@ bool QPainter::isActive() const
 
 /*!
     Initializes the painters pen, background and font to the same as
-    the given \a widget. Call this function after begin() while the
-    painter is active.
+    the given \a widget. This function is automatically when the
+    painter is opened on a QWidget.
 
     \sa begin(), {QPainter#Settings}{Settings}
 */

commit e3462ae7868c679bc88dc01de85ca39454651ff7

Fix typo in documentation.
Error was introduced in change 52f87de53328c661049acf09d5fedc1850aa9bfa.

Reviewed-by: Trust Me

src/gui/painting/qpainter.cpp
@@ -1465,7 +1465,7 @@ bool QPainter::isActive() const
 
 /*!
     Initializes the painters pen, background and font to the same as
-    the given \a widget. This function is automatically when the
+    the given \a widget. This function is called automatically when the
     painter is opened on a QWidget.
 
     \sa begin(), {QPainter#Settings}{Settings}