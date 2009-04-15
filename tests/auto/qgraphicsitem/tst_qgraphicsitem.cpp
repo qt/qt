qt/qt

commit c1909321a486621ef196dd4bb3cf354406d86d46

Fixes:    Clever invalidating of the cached clip path.
RevBy:    Andreas
AutoTest: still pass
Details:  Adds emptyClipPath bit, a cut-off to test if the item
	has an empty clip path (i.e., is clipped away). Also adds
	code for invalidating the cache from inside setPos().

src/gui/graphicsview/qgraphicsitem.cpp
@@ -2368,15 +2368,18 @@ void QGraphicsItemPrivate::setPosHelper(const QPointF &pos)
         return;
 
     // Update and repositition.
+    inSetPosHelper = 1;
     if (scene) {
         fullUpdateHelper(true);
         q->prepareGeometryChange();
     }
     this->pos = newPos;
     invalidateSceneTransformCache();
+    updateCachedClipPathFromSetPosHelper();
 
     // Send post-notification.
     q->itemChange(QGraphicsItem::ItemPositionHasChanged, newPos);
+    inSetPosHelper = 0;
 }
 
 /*!
@@ -3141,7 +3144,7 @@ QPainterPath QGraphicsItem::clipPath() const
 {
     Q_D(const QGraphicsItem);
     if (!d->dirtyClipPath)
-        return d->cachedClipPath;
+        return d->emptyClipPath ? QPainterPath() : d->cachedClipPath;
 
     if (!isClipped()) {
         d_ptr->setCachedClipPath(QPainterPath());
@@ -3163,6 +3166,13 @@ QPainterPath QGraphicsItem::clipPath() const
                 // Map clip to the current parent and intersect with its shape/clipPath
                 clip = lastParent->itemTransform(parent).map(clip);
                 if ((foundValidClipPath = !parent->d_ptr->dirtyClipPath && parent->isClipped())) {
+                    if (parent->d_ptr->emptyClipPath) {
+                        if (d_ptr->flags & ItemClipsChildrenToShape)
+                            d_ptr->setEmptyCachedClipPathRecursively();
+                        else
+                            d_ptr->setEmptyCachedClipPath();
+                        return QPainterPath();
+                    }
                     clip = clip.intersected(parent->d_ptr->cachedClipPath);
                     if (!(parent->d_ptr->flags & ItemClipsToShape))
                         clip = clip.intersected(parent->shape());
@@ -3171,7 +3181,10 @@ QPainterPath QGraphicsItem::clipPath() const
                 }
 
                 if (clip.isEmpty()) {
-                    d_ptr->setCachedClipPath(clip);
+                    if (d_ptr->flags & ItemClipsChildrenToShape)
+                        d_ptr->setEmptyCachedClipPathRecursively();
+                    else
+                        d_ptr->setEmptyCachedClipPath();
                     return clip;
                 }
                 lastParent = parent;
@@ -3749,13 +3762,95 @@ void QGraphicsItemPrivate::removeExtraItemCache()
     unsetExtra(ExtraCacheData);
 }
 
-void QGraphicsItemPrivate::invalidateCachedClipPathRecursively(bool childrenOnly)
+void QGraphicsItemPrivate::setEmptyCachedClipPathRecursively(const QRectF &emptyIfOutsideThisRect)
+{
+    setEmptyCachedClipPath();
+
+    const bool checkRect = !emptyIfOutsideThisRect.isNull()
+                           && !(flags & QGraphicsItem::ItemClipsChildrenToShape);
+    for (int i = 0; i < children.size(); ++i) {
+        if (!checkRect) {
+            children.at(i)->d_ptr->setEmptyCachedClipPathRecursively();
+            continue;
+        }
+
+        QGraphicsItem *child = children.at(i);
+        const QRectF rect = child->mapRectFromParent(emptyIfOutsideThisRect);
+        if (rect.intersects(child->boundingRect()))
+            child->d_ptr->invalidateCachedClipPathRecursively(false, rect);
+        else
+            child->d_ptr->setEmptyCachedClipPathRecursively(rect);
+    }
+}
+
+void QGraphicsItemPrivate::invalidateCachedClipPathRecursively(bool childrenOnly, const QRectF &emptyIfOutsideThisRect)
 {
     if (!childrenOnly)
         invalidateCachedClipPath();
-    // ### Return if this item doesn't clip its children?
-    for (int i = 0; i < children.size(); ++i)
-        children.at(i)->d_ptr->invalidateCachedClipPathRecursively(false);
+
+    const bool checkRect = !emptyIfOutsideThisRect.isNull();
+    for (int i = 0; i < children.size(); ++i) {
+        if (!checkRect) {
+            children.at(i)->d_ptr->invalidateCachedClipPathRecursively(false);
+            continue;
+        }
+
+        QGraphicsItem *child = children.at(i);
+        const QRectF rect = child->mapRectFromParent(emptyIfOutsideThisRect);
+        if (rect.intersects(child->boundingRect()))
+            child->d_ptr->invalidateCachedClipPathRecursively(false, rect);
+        else
+            child->d_ptr->setEmptyCachedClipPathRecursively(rect);
+    }
+}
+
+void QGraphicsItemPrivate::updateCachedClipPathFromSetPosHelper()
+{
+    Q_ASSERT(inSetPosHelper);
+
+    if (!(ancestorFlags & QGraphicsItemPrivate::AncestorClipsChildren))
+        return; // Not clipped by any ancestor.
+
+    // Find closest clip ancestor.
+    QGraphicsItem *clipParent = parent;
+    while (clipParent && !(clipParent->d_ptr->flags & QGraphicsItem::ItemClipsChildrenToShape))
+        clipParent = clipParent->d_ptr->parent;
+
+    Q_ASSERT(clipParent);
+    Q_Q(QGraphicsItem);
+
+    // From here everything is calculated in clip parent's coordinates.
+    const QTransform thisToParentTransform(q->itemTransform(clipParent));
+    const QRectF parentBoundingRect(clipParent->boundingRect());
+    const QRectF thisBoundingRect(thisToParentTransform.mapRect(q->boundingRect()));
+
+    if (!parentBoundingRect.intersects(thisBoundingRect)) {
+        // Item is moved outside the clip parent's bounding rect,
+        // i.e. it is fully clipped and the clip path is empty.
+        if (flags & QGraphicsItem::ItemClipsChildrenToShape)
+            setEmptyCachedClipPathRecursively();
+        else
+            setEmptyCachedClipPathRecursively(thisToParentTransform.inverted().mapRect(parentBoundingRect));
+        return;
+    }
+
+    const QPainterPath parentClip(clipParent->isClipped() ? clipParent->clipPath() : clipParent->shape());
+    if (parentClip.contains(thisBoundingRect))
+        return; // Item is inside the clip parent's shape. No update required.
+
+    const QRectF parentClipRect(parentClip.controlPointRect());
+    if (!parentClipRect.intersects(thisBoundingRect)) {
+        // Item is moved outside the clip parent's shape,
+        // i.e. it is fully clipped and the clip path is empty.
+        if (flags & QGraphicsItem::ItemClipsChildrenToShape)
+            setEmptyCachedClipPathRecursively();
+        else
+            setEmptyCachedClipPathRecursively(thisToParentTransform.inverted().mapRect(parentClipRect));
+    } else {
+        // Item is partially inside the clip parent's shape,
+        // i.e. the cached clip path must be invalidated.
+        invalidateCachedClipPathRecursively(false, thisToParentTransform.inverted().mapRect(parentClipRect));
+    }
 }
 
 /*!
@@ -5532,13 +5627,20 @@ void QGraphicsItem::removeFromIndex()
 */
 void QGraphicsItem::prepareGeometryChange()
 {
-    if (d_ptr->scene) {
-        d_ptr->updateHelper();
+    if (!d_ptr->scene)
+        return;
 
-        QGraphicsScenePrivate *scenePrivate = d_ptr->scene->d_func();
-        scenePrivate->removeFromIndex(this);
+    d_ptr->updateHelper();
+    QGraphicsScenePrivate *scenePrivate = d_ptr->scene->d_func();
+    scenePrivate->removeFromIndex(this);
+
+    if (d_ptr->inSetPosHelper)
+        return;
+
+    if (d_ptr->flags & ItemClipsChildrenToShape)
         d_ptr->invalidateCachedClipPathRecursively();
-    }
+    else
+        d_ptr->invalidateCachedClipPath();
 }
 
 /*!

src/gui/graphicsview/qgraphicsitem_p.h
@@ -139,6 +139,8 @@ public:
         dirtyChildren(0),
         localCollisionHack(0),
         dirtyClipPath(1),
+        emptyClipPath(0),
+        inSetPosHelper(0),
         globalStackingOrder(-1),
         sceneTransformIndex(-1),
         q_ptr(0)
@@ -239,12 +241,22 @@ public:
     {
         cachedClipPath = path;
         dirtyClipPath = 0;
+        emptyClipPath = 0;
     }
 
-    inline void invalidateCachedClipPath()
-    { dirtyClipPath = 1; }
+    inline void setEmptyCachedClipPath()
+    {
+        emptyClipPath = 1;
+        dirtyClipPath = 0;
+    }
 
-    void invalidateCachedClipPathRecursively(bool childrenOnly = false);
+    void setEmptyCachedClipPathRecursively(const QRectF &emptyIfOutsideThisRect = QRectF());
+
+    inline void invalidateCachedClipPath()
+    { /*static int count = 0 ;qWarning("%i", ++count);*/ dirtyClipPath = 1; emptyClipPath = 0; }
+
+    void invalidateCachedClipPathRecursively(bool childrenOnly = false, const QRectF &emptyIfOutsideThisRect = QRectF());
+    void updateCachedClipPathFromSetPosHelper();
 
     inline bool isInvisible() const
     {
@@ -288,6 +300,8 @@ public:
     quint32 dirtyChildren : 1;    
     quint32 localCollisionHack : 1;
     quint32 dirtyClipPath : 1;
+    quint32 emptyClipPath : 1;
+    quint32 inSetPosHelper : 1;
 
     // Optional stacking order
     int globalStackingOrder;

commit 7a0f9ae94ca7a47dc285431d31f6839c07870194

BT: Fix a regression to 4.4 in Graphics View's handling of child clippers
Regression caused by optimizations in QGraphicsItem and QGraphicsScene.
The changes in QGraphicsItem fix bugs in QGraphicsItem::mapToParent
functions, which did the translation before applying the transformation,
instead of the other way (transform, then translate). This bug caused
almost all mapToParent and mapRectToParent functions to behave wrongly.
Unfortunately the new helper functions in QGraphicsScene for discovering
items made use of these functions, which introduced a regression. Fixing
these functions also fixes item discovery.

The other part of this change fixes a regression caused by c1909321,
which luckily happened after 4.5.0 and never saw the light of day. The
fix is to also invalidate the cached clip path even if there is no scene,
which is necessary if you build your scene graph outside the scene, and
finish off by adding the root item to the scene.

Task-number: 250680
Reviewed-by: Alexis

src/gui/graphicsview/qgraphicsitem.cpp
@@ -4223,9 +4223,9 @@ QPolygonF QGraphicsItem::mapToItem(const QGraphicsItem *item, const QRectF &rect
 */
 QPolygonF QGraphicsItem::mapToParent(const QRectF &rect) const
 {
-    if (!d_ptr->hasTransform)
-        return QPolygonF(rect.translated(d_ptr->pos));
-    return transform().map(rect.translated(d_ptr->pos));
+    QPolygonF p = !d_ptr->hasTransform ? rect : transform().map(rect);
+    p.translate(d_ptr->pos);
+    return p;
 }
 
 /*!
@@ -4292,8 +4292,8 @@ QRectF QGraphicsItem::mapRectToItem(const QGraphicsItem *item, const QRectF &rec
 */
 QRectF QGraphicsItem::mapRectToParent(const QRectF &rect) const
 {
-    QRectF r = rect.translated(d_ptr->pos.x(), d_ptr->pos.y());
-    return !d_ptr->hasTransform ? r : transform().mapRect(r);
+    QRectF r = !d_ptr->hasTransform ? rect : transform().mapRect(rect);
+    return r.translated(d_ptr->pos);
 }
 
 /*!
@@ -4424,9 +4424,9 @@ QPolygonF QGraphicsItem::mapToItem(const QGraphicsItem *item, const QPolygonF &p
 */
 QPolygonF QGraphicsItem::mapToParent(const QPolygonF &polygon) const
 {
-    QPolygonF p = polygon;
+    QPolygonF p = !d_ptr->hasTransform ? polygon : transform().map(polygon);
     p.translate(d_ptr->pos);
-    return d_ptr->hasTransform ? transform().map(p) : p;
+    return p;
 }
 
 /*!
@@ -4468,7 +4468,10 @@ QPainterPath QGraphicsItem::mapToItem(const QGraphicsItem *item, const QPainterP
 */
 QPainterPath QGraphicsItem::mapToParent(const QPainterPath &path) const
 {
-    return d_ptr->parent ? itemTransform(d_ptr->parent).map(path) : mapToScene(path);
+    QTransform x = QTransform::fromTranslate(d_ptr->pos.x(), d_ptr->pos.y());
+    if (d_ptr->hasTransform)
+        x = transform() * x;
+    return x.map(path);
 }
 
 /*!
@@ -5697,12 +5700,11 @@ void QGraphicsItem::removeFromIndex()
 */
 void QGraphicsItem::prepareGeometryChange()
 {
-    if (!d_ptr->scene)
-        return;
-
-    d_ptr->updateHelper(QRectF(), false, /*maybeDirtyClipPath=*/!d_ptr->inSetPosHelper);
-    QGraphicsScenePrivate *scenePrivate = d_ptr->scene->d_func();
-    scenePrivate->removeFromIndex(this);
+    if (d_ptr->scene) {
+        d_ptr->updateHelper(QRectF(), false, /*maybeDirtyClipPath=*/!d_ptr->inSetPosHelper);
+        QGraphicsScenePrivate *scenePrivate = d_ptr->scene->d_func();
+        scenePrivate->removeFromIndex(this);
+    }
 
     if (d_ptr->inSetPosHelper)
         return;

tests/auto/qgraphicsitem/tst_qgraphicsitem.cpp
@@ -162,6 +162,8 @@ private slots:
     void mapFromToParent();
     void mapFromToScene();
     void mapFromToItem();
+    void mapRectFromToParent_data();
+    void mapRectFromToParent();
     void isAncestorOf();
     void commonAncestorItem();
     void data();
@@ -2513,6 +2515,87 @@ void tst_QGraphicsItem::mapFromToItem()
     delete item2;
     delete item3;
     delete item4;
+}
+
+void tst_QGraphicsItem::mapRectFromToParent_data()
+{
+    QTest::addColumn<bool>("parent");
+    QTest::addColumn<QPointF>("parentPos");
+    QTest::addColumn<QTransform>("parentTransform");
+    QTest::addColumn<QPointF>("pos");
+    QTest::addColumn<QTransform>("transform");
+    QTest::addColumn<QRectF>("inputRect");
+    QTest::addColumn<QRectF>("outputRect");
+
+    QTest::newRow("nil") << false << QPointF() << QTransform() << QPointF() << QTransform() << QRectF() << QRectF();
+    QTest::newRow("simple") << false << QPointF() << QTransform() << QPointF() << QTransform()
+                            << QRectF(0, 0, 10, 10) << QRectF(0, 0, 10, 10);
+    QTest::newRow("simple w/parent") << true
+                                     << QPointF() << QTransform()
+                                     << QPointF() << QTransform()
+                                     << QRectF(0, 0, 10, 10) << QRectF(0, 0, 10, 10);
+    QTest::newRow("simple w/parent parentPos") << true
+                                               << QPointF(50, 50) << QTransform()
+                                               << QPointF() << QTransform()
+                                               << QRectF(0, 0, 10, 10) << QRectF(0, 0, 10, 10);
+    QTest::newRow("simple w/parent parentPos parentRotation") << true
+                                                              << QPointF(50, 50) << QTransform().rotate(45)
+                                                              << QPointF() << QTransform()
+                                                              << QRectF(0, 0, 10, 10) << QRectF(0, 0, 10, 10);
+    QTest::newRow("pos w/parent") << true
+                                  << QPointF() << QTransform()
+                                  << QPointF(50, 50) << QTransform()
+                                  << QRectF(0, 0, 10, 10) << QRectF(50, 50, 10, 10);
+    QTest::newRow("rotation w/parent") << true
+                                       << QPointF() << QTransform()
+                                       << QPointF() << QTransform().rotate(90)
+                                       << QRectF(0, 0, 10, 10) << QRectF(-10, 0, 10, 10);
+    QTest::newRow("pos rotation w/parent") << true
+                                           << QPointF() << QTransform()
+                                           << QPointF(50, 50) << QTransform().rotate(90)
+                                           << QRectF(0, 0, 10, 10) << QRectF(40, 50, 10, 10);
+    QTest::newRow("pos rotation w/parent parentPos parentRotation") << true
+                                                                    << QPointF(-170, -190) << QTransform().rotate(90)
+                                                                    << QPointF(50, 50) << QTransform().rotate(90)
+                                                                    << QRectF(0, 0, 10, 10) << QRectF(40, 50, 10, 10);
+}
+
+void tst_QGraphicsItem::mapRectFromToParent()
+{
+    QFETCH(bool, parent);
+    QFETCH(QPointF, parentPos);
+    QFETCH(QTransform, parentTransform);
+    QFETCH(QPointF, pos);
+    QFETCH(QTransform, transform);
+    QFETCH(QRectF, inputRect);
+    QFETCH(QRectF, outputRect);
+
+    QGraphicsRectItem *rect = new QGraphicsRectItem;
+    rect->setPos(pos);
+    rect->setTransform(transform);
+
+    if (parent) {
+        QGraphicsRectItem *rectParent = new QGraphicsRectItem;
+        rect->setParentItem(rectParent);
+        rectParent->setPos(parentPos);
+        rectParent->setTransform(parentTransform);
+    }
+
+    // Make sure we use non-destructive transform operations (e.g., 90 degree
+    // rotations).
+    QCOMPARE(rect->mapRectToParent(inputRect), outputRect);
+    QCOMPARE(rect->mapRectFromParent(outputRect), inputRect);
+    QCOMPARE(rect->itemTransform(rect->parentItem()).mapRect(inputRect), outputRect);
+    QCOMPARE(rect->mapToParent(inputRect).boundingRect(), outputRect);
+    QCOMPARE(rect->mapToParent(QPolygonF(inputRect)).boundingRect(), outputRect);
+    QCOMPARE(rect->mapFromParent(outputRect).boundingRect(), inputRect);
+    QCOMPARE(rect->mapFromParent(QPolygonF(outputRect)).boundingRect(), inputRect);
+    QPainterPath inputPath;
+    inputPath.addRect(inputRect);
+    QPainterPath outputPath;
+    outputPath.addRect(outputRect);
+    QCOMPARE(rect->mapToParent(inputPath).boundingRect(), outputPath.boundingRect());
+    QCOMPARE(rect->mapFromParent(outputPath).boundingRect(), inputPath.boundingRect());
 }
 
 void tst_QGraphicsItem::isAncestorOf()

tests/auto/qgraphicsscene/tst_qgraphicsscene.cpp
@@ -245,6 +245,7 @@ private slots:
     void task139782_containsItemBoundingRect();
     void task176178_itemIndexMethodBreaksSceneRect();
     void task160653_selectionChanged();
+    void task250680_childClip();
 };
 
 void tst_QGraphicsScene::initTestCase()
@@ -3382,6 +3383,31 @@ void tst_QGraphicsScene::task160653_selectionChanged()
     QTest::mouseClick(
         view.viewport(), Qt::LeftButton, 0, view.mapFromScene(scene.items().first()->scenePos()));
     QCOMPARE(spy.count(), 1);
+}
+
+void tst_QGraphicsScene::task250680_childClip()
+{
+    QGraphicsRectItem *clipper = new QGraphicsRectItem;
+    clipper->setFlag(QGraphicsItem::ItemClipsChildrenToShape);
+    clipper->setPen(QPen(Qt::green));
+    clipper->setRect(200, 200, 640, 480);
+
+    QGraphicsRectItem *rect = new QGraphicsRectItem(clipper);
+    rect->setPen(QPen(Qt::red));
+    rect->setBrush(QBrush(QColor(255, 0, 0, 75)));
+    rect->setPos(320, 240);
+    rect->setRect(-25, -25, 50, 50);
+
+    QGraphicsScene scene;
+    scene.addItem(clipper);
+
+    QPainterPath path;
+    path.addRect(-25, -25, 50, 50);
+    QCOMPARE(rect->clipPath(), path);
+
+    QCOMPARE(scene.items(QRectF(320, 240, 5, 5)).size(), 2);
+    rect->rotate(45);
+    QCOMPARE(scene.items(QRectF(320, 240, 5, 5)).size(), 2);
 }
 
 void tst_QGraphicsScene::sorting_data()