qt/qt

commit a8d14ae567c7e847c6dbba644c36fbc6c6afc468

Work on getting unified toolbars to look more native.
Basically we try to get the toggled look correct and we've shrunk the
size of the toolbar by a good 10 pixels. We still look a bit "off" for
toggled on Tiger, but frankely that look is a bit odd.

We are a bit taller than the pure Cocoa toolbar (2 px), but given that
we are embedding our QToolbar, that's probably the best we can do.

All-in-all, it looks much better.

src/gui/styles/qstylehelper_p.h
@@ -32,6 +32,9 @@ namespace QStyleHelper
     int calcBigLineSize(int radius);
     void drawDial(const QStyleOptionSlider *dial, QPainter *painter);
 #endif //QT_NO_DIAL
+    void drawBorderPixmap(const QPixmap &pixmap, QPainter *painter, const QRect &rect,
+                     int left = 0, int top = 0, int right = 0,
+                     int bottom = 0);
 }
 
 QT_END_NAMESPACE

src/gui/kernel/kernel.pri
@@ -193,7 +193,7 @@ embedded {
         MENU_NIB.path = Resources 
         MENU_NIB.version = Versions
         QMAKE_BUNDLE_DATA += MENU_NIB 
-        RESOURCES += mac/maccursors.qrc
+        RESOURCES += mac/macresources.qrc
 
         LIBS += -framework AppKit
 }

src/gui/mac/macresources.qrc
@@ -6,4 +6,7 @@
 <file>images/waitcursor.png</file>
 <file>images/pluscursor.png</file>
 </qresource>
+<qresource prefix="/trolltech/mac/style">
+<file>images/leopard-unified-toolbar-on.png</file>
+</qresource>
 </RCC>

src/gui/styles/qmacstyle_mac.mm
@@ -2649,6 +2649,9 @@ int QMacStyle::pixelMetric(PixelMetric metric, const QStyleOption *opt, const QW
     case PM_MenuHMargin:
         ret = 0;
         break;
+    case PM_ToolBarFrameWidth:
+        ret = 0;
+        break;
     default:
         ret = QWindowsStyle::pixelMetric(metric, opt, widget);
         break;
@@ -3679,8 +3682,8 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
                         if (tb->toolButtonStyle != Qt::ToolButtonIconOnly) {
                             needText = true;
                             if (tb->toolButtonStyle == Qt::ToolButtonTextUnderIcon) {
-                                pr.setHeight(pixmap.size().height() + 6);
-                                cr.adjust(0, pr.bottom(), 0, -3);
+                                pr.setHeight(pixmap.size().height());
+                                cr.adjust(0, pr.bottom() + 1, 0, 1);
                                 alignment |= Qt::AlignCenter;
                             } else {
                                 pr.setWidth(pixmap.width() + 8);
@@ -3688,7 +3691,7 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
                                 alignment |= Qt::AlignLeft | Qt::AlignVCenter;
                             }
                         }
-                        if (down) {
+                        if (opt->state & State_Sunken) {
                             pr.translate(shiftX, shiftY);
                             pixmap = darkenPixmap(pixmap);
                         }
@@ -3718,7 +3721,8 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
                         }
                         drawItemText(p, cr, alignment, pal,
                                      tb->state & State_Enabled, tb->text, role);
-                        if (QSysInfo::MacintoshVersion < QSysInfo::MV_10_5 && down) {
+                        if (QSysInfo::MacintoshVersion < QSysInfo::MV_10_5 &&
+                            (tb->state & State_Sunken)) {
                             // Draw a "drop shadow" in earlier versions.
                             drawItemText(p, cr.adjusted(0, 1, 0, 1), alignment,
                                          tb->palette, tb->state & State_Enabled, tb->text);
@@ -4544,9 +4548,10 @@ void QMacStyle::drawControl(ControlElement ce, const QStyleOption *opt, QPainter
     case CE_ToolBar: {
         // For unified tool bars, draw nothing.
         if (w) {
-            if (QMainWindow * mainWindow = qobject_cast<QMainWindow *>(w->window()))
+            if (QMainWindow * mainWindow = qobject_cast<QMainWindow *>(w->window())) {
                 if (mainWindow->unifiedTitleAndToolBarOnMac())
                     break;
+                }
         }
 
         // draw background gradient
@@ -5270,6 +5275,7 @@ void QMacStyle::drawComplexControl(ComplexControl cc, const QStyleOptionComplex 
         if (const QStyleOptionToolButton *tb
                 = qstyleoption_cast<const QStyleOptionToolButton *>(opt)) {
             if (widget && qobject_cast<QToolBar *>(widget->parentWidget())) {
+//                p->fillRect(tb->rect, QColor(155, 0, 155, 155));
                 if (tb->subControls & SC_ToolButtonMenu) {
                     QStyleOption arrowOpt(0);
                     arrowOpt.rect = subControlRect(cc, tb, SC_ToolButtonMenu, widget);
@@ -5282,16 +5288,22 @@ void QMacStyle::drawComplexControl(ComplexControl cc, const QStyleOptionComplex 
                     drawToolbarButtonArrow(tb->rect, tds, cg);
                 }
                 if (tb->state & State_On) {
-                    QPen oldPen = p->pen();
-                    p->setPen(QColor(0, 0, 0, 0x3a));
-                    p->fillRect(tb->rect.adjusted(1, 1, -1, -1), QColor(0, 0, 0, 0x12));
-                    p->drawLine(tb->rect.left() + 1, tb->rect.top(),
-                                tb->rect.right() - 1, tb->rect.top());
-                    p->drawLine(tb->rect.left() + 1, tb->rect.bottom(),
-                                tb->rect.right() - 1, tb->rect.bottom());
-                    p->drawLine(tb->rect.topLeft(), tb->rect.bottomLeft());
-                    p->drawLine(tb->rect.topRight(), tb->rect.bottomRight());
-                    p->setPen(oldPen);
+                    if (QSysInfo::MacintoshVersion >= QSysInfo::MV_10_5) {
+                        static QPixmap pm(QLatin1String(":/trolltech/mac/style/images/leopard-unified-toolbar-on.png"));
+                        p->setRenderHint(QPainter::SmoothPixmapTransform);
+                        QStyleHelper::drawBorderPixmap(pm, p, tb->rect, 2, 2, 2, 2);
+                    } else {
+                        QPen oldPen = p->pen();
+                        p->setPen(QColor(0, 0, 0, 0x3a));
+                        p->fillRect(tb->rect.adjusted(1, 1, -1, -1), QColor(0, 0, 0, 0x12));
+                        p->drawLine(tb->rect.left() + 1, tb->rect.top(),
+                                    tb->rect.right() - 1, tb->rect.top());
+                        p->drawLine(tb->rect.left() + 1, tb->rect.bottom(),
+                                    tb->rect.right() - 1, tb->rect.bottom());
+                        p->drawLine(tb->rect.topLeft(), tb->rect.bottomLeft());
+                        p->drawLine(tb->rect.topRight(), tb->rect.bottomRight());
+                        p->setPen(oldPen);
+                    }
                 }
                 drawControl(CE_ToolButtonLabel, opt, p, widget);
             } else {
@@ -6009,6 +6021,14 @@ QSize QMacStyle::sizeFromContents(ContentsType ct, const QStyleOption *opt,
         }
         break;
     case CT_ToolButton:
+        if (widget && qobject_cast<const QToolBar *>(widget->parentWidget())) {
+            sz.rwidth() += 4;
+            if (sz.height() <= 32) {
+                // Workaround strange HIToolBar bug when getting constraints.
+                sz.rheight() += 1;
+            }
+            return sz;
+        }
         sz.rwidth() += 10;
         sz.rheight() += 10;
         return sz;

src/gui/styles/qstylehelper.cpp
@@ -50,9 +50,10 @@
 
 QT_BEGIN_NAMESPACE
 
-const bool QStyleHelper::UsePixmapCache = true;
+namespace QStyleHelper {
+const bool UsePixmapCache = true;
 
-QString QStyleHelper::uniqueName(const QString &key, const QStyleOption *option, const QSize &size)
+QString uniqueName(const QString &key, const QStyleOption *option, const QSize &size)
 {
     QString tmp;
     const QStyleOptionComplex *complexOption = qstyleoption_cast<const QStyleOptionComplex *>(option);
@@ -74,7 +75,7 @@ QString QStyleHelper::uniqueName(const QString &key, const QStyleOption *option,
 
 #ifndef QT_NO_DIAL
 
-int QStyleHelper::calcBigLineSize(int radius)
+int calcBigLineSize(int radius)
 {
     int bigLineSize = radius / 6;
     if (bigLineSize < 4)
@@ -107,7 +108,7 @@ static QPointF calcRadialPos(const QStyleOptionSlider *dial, qreal offset)
     return pos;
 }
 
-qreal QStyleHelper::angle(const QPointF &p1, const QPointF &p2)
+qreal angle(const QPointF &p1, const QPointF &p2)
 {
     static const qreal rad_factor = 180 / Q_PI;
     qreal _angle = 0;
@@ -139,7 +140,7 @@ qreal QStyleHelper::angle(const QPointF &p1, const QPointF &p2)
     return _angle;
 }
 
-QPolygonF QStyleHelper::calcLines(const QStyleOptionSlider *dial)
+QPolygonF calcLines(const QStyleOptionSlider *dial)
 {
     QPolygonF poly;
     int width = dial->rect.width();
@@ -182,7 +183,7 @@ QPolygonF QStyleHelper::calcLines(const QStyleOptionSlider *dial)
 // This will draw a nice and shiny QDial for us. We don't want
 // all the shinyness in QWindowsStyle, hence we place it here
 
-void QStyleHelper::drawDial(const QStyleOptionSlider *option, QPainter *painter)
+void drawDial(const QStyleOptionSlider *option, QPainter *painter)
 {
     QPalette pal = option->palette;
     QColor buttonColor = pal.button().color();
@@ -292,4 +293,61 @@ void QStyleHelper::drawDial(const QStyleOptionSlider *option, QPainter *painter)
 }
 #endif //QT_NO_DIAL
 
+void drawBorderPixmap(const QPixmap &pixmap, QPainter *painter, const QRect &rect,
+                     int left, int top, int right,
+                     int bottom)
+{
+    QSize size = pixmap.size();
+    //painter->setRenderHint(QPainter::SmoothPixmapTransform);
+
+    //top
+    if (top > 0) {
+        painter->drawPixmap(QRect(rect.left() + left, rect.top(), rect.width() -right - left, top), pixmap,
+                            QRect(left, 0, size.width() -right - left, top));
+
+        //top-left
+        if(left > 0)
+            painter->drawPixmap(QRect(rect.left(), rect.top(), left, top), pixmap,
+                                QRect(0, 0, left, top));
+
+        //top-right
+        if (right > 0)
+            painter->drawPixmap(QRect(rect.left() + rect.width() - right, rect.top(), right, top), pixmap,
+                                QRect(size.width() - right, 0, right, top));
+    }
+
+    //left
+    if (left > 0)
+        painter->drawPixmap(QRect(rect.left(), rect.top()+top, left, rect.height() - top - bottom), pixmap,
+                            QRect(0, top, left, size.height() - bottom - top));
+
+    //center
+    painter->drawPixmap(QRect(rect.left() + left, rect.top()+top, rect.width() -right - left,
+                             rect.height() - bottom - top), pixmap,
+                       QRect(left, top, size.width() -right -left,
+                             size.height() - bottom - top));
+    //right
+    if (right > 0)
+        painter->drawPixmap(QRect(rect.left() +rect.width() - right, rect.top()+top, right, rect.height() - top - bottom), pixmap,
+                            QRect(size.width() - right, top, right, size.height() - bottom - top));
+
+    //bottom
+    if (bottom > 0) {
+        painter->drawPixmap(QRect(rect.left() +left, rect.top() + rect.height() - bottom,
+                                 rect.width() - right - left, bottom), pixmap,
+                            QRect(left, size.height() - bottom,
+                                 size.width() - right - left, bottom));
+        //bottom-left
+        if (left > 0)
+            painter->drawPixmap(QRect(rect.left(), rect.top() + rect.height() - bottom, left, bottom), pixmap,
+                                QRect(0, size.height() - bottom, left, bottom));
+
+        //bottom-right
+        if (right > 0)
+            painter->drawPixmap(QRect(rect.left() + rect.width() - right, rect.top() + rect.height() - bottom, right, bottom), pixmap,
+                                QRect(size.width() - right, size.height() - bottom, right, bottom));
+
+    }
+}
+}
 QT_END_NAMESPACE

src/gui/widgets/qmainwindowlayout_mac.mm
@@ -502,11 +502,11 @@ void QMainWindowLayout::fixSizeInUnifiedToolbar(QToolBar *tb) const
         QMacCocoaAutoReleasePool pool;
         QWidgetItem layoutItem(tb);
         QSize size = layoutItem.maximumSize();
-        NSSize nssize = NSMakeSize(size.width(), size.height());
+        NSSize nssize = NSMakeSize(size.width(), size.height() - 2);
         [item setMaxSize:nssize];
         size = layoutItem.minimumSize();
         nssize.width = size.width();
-        nssize.height = size.height();
+        nssize.height = size.height() - 2;
         [item setMinSize:nssize];
     }
 }

commit ed24cd82444faeeda0a171a9082808ebb667e092

Fixes compile on linux-g++-32 and linux-g++-64.
Broken by a8d14ae567c7e847c6dbba644c36fbc6c6afc468

src/gui/styles/qstylehelper_p.h
@@ -19,6 +19,7 @@
 QT_BEGIN_NAMESPACE
 
 class QPainter;
+class QPixmap;
 class QStyleOptionSlider;
 class QStyleOption;
 