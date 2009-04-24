af4f0170810efeb071e6e9dac0df6a84962e9806
Eskil Abrahamsen Blomfeldt <eblomfel@trolltech.com>
2009-04-24 17:36:48 +0200

    Really broken plugin for errorState. Don't try to use this yet, it doesn't work at all.

---
+++ examples/statemachine/errorstateplugins/random_ai/random_ai_plugin.cpp
@@ -0,0 +1,40 @@
+#include "random_ai_plugin.h"
+
+#include <errorstate/tank.h>
+
+#include <QState>
+#include <QtPlugin>
+
+#include <time.h>
+
+QState *RandomAiPlugin::create(QState *parentState, Tank *tank)
+{
+    qsrand(uint(time(NULL)));
+
+    QState *topLevel = new QState(parentState);
+
+    QState *selectNextActionState = new SelectActionState(topLevel);
+    topLevel->setInitialState(selectNextActionState);
+
+    QState *fireState = new RandomDistanceState(topLevel);
+    connect(fireState, SIGNAL(distanceComputed(qreal)), tank, SLOT(fireCannon(qreal)));
+    selectNextActionState->addTransition(selectNextActionState, SIGNAL(fireSelected()), fireState);
+
+    QState *moveForwardsState = new RandomDistanceState(topLevel);
+    connect(moveForwardsState, SIGNAL(distanceComputed(qreal)), tank, SLOT(moveForwards(qreal)));
+    selectNextActionState->addTransition(selectNextActionState, SIGNAL(moveForwardsSelected()), moveForwardsState);
+
+    QState *moveBackwardsState = new RandomDistanceState(topLevel);
+    connect(moveBackwardsState, SIGNAL(distanceComputed(qreal)), tank, SLOT(moveBackwards(qreal)));
+    selectNextActionState->addTransition(selectNextActionState, SIGNAL(moveBackwardsSelected()), moveBackwardsState);
+
+    QState *turnState = new RandomDistanceState(topLevel);
+    connect(turnState, SIGNAL(distanceComputed(qreal)), tank, SLOT(turn(qreal)));
+    selectNextActionState->addTransition(selectNextActionState, SIGNAL(turnSelected()), turnState);
+
+    topLevel->addTransition(tank, SIGNAL(actionCompleted()), selectNextActionState);
+
+    return topLevel;
+}
+
+Q_EXPORT_PLUGIN2(random_ai, RandomAiPlugin)
