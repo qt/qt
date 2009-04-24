10fa7986020c75da0ce68ec051a1086bd6bf5bf4
Thiago Macieira <thiago.macieira@nokia.com>
2009-04-24 11:52:34 +0200

    Change Qt version number to 4.5.2
    Reviewed-By: TrustMe

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -44,11 +44,11 @@
 
 #include <stddef.h>
 
-#define QT_VERSION_STR   "4.5.1"
+#define QT_VERSION_STR   "4.5.2"
 /*
    QT_VERSION is (major << 16) + (minor << 8) + patch.
 */
-#define QT_VERSION 0x040501
+#define QT_VERSION 0x040502
 /*
    can be used like #if (QT_VERSION >= QT_VERSION_CHECK(4, 4, 0))
 */
