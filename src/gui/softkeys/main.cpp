2e76d2bfedce77064fde85573f5267032ce7979e
Markku Luukkainen <markku.luukkainen@digia.com>
2009-05-04 16:17:15 +0200

    Added proper headers

--- src/gui/softkeys/main.cpp
+++ src/gui/softkeys/main.cpp
@@ -1,3 +1,13 @@
+/****************************************************************************
+**
+** Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Qt Software Information (qt-info@nokia.com)
+**
+** This file is part of the $MODULE$ of the Qt Toolkit.
+**
+** $TROLLTECH_DUAL_LICENSE$
+**
+****************************************************************************/
 
 #include <QApplication>
 #include <QMainWindow>
