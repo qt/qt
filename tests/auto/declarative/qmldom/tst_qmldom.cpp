74c8e4c15fce7dce9d279b6a8086aaf679500fab
Erik Verbruggen <erik.verbruggen@nokia.com>
2009-05-04 15:42:32 +0200

    Extended the test for QmlDomValueValueSource.

--- tests/auto/declarative/qmldom/tst_qmldom.cpp
+++ tests/auto/declarative/qmldom/tst_qmldom.cpp
@@ -100,8 +100,22 @@ void tst_qmldom::testValueSource()
     QmlDomProperty heightProperty = rootItem.properties().at(0);
     QVERIFY(heightProperty.propertyName() == "height");
     QVERIFY(heightProperty.value().isValueSource());
+
     const QmlDomValueValueSource valueSource = heightProperty.value().toValueSource();
-    QVERIFY(valueSource.object().isValid());
+    QmlDomObject valueSourceObject = valueSource.object();
+    QVERIFY(valueSourceObject.isValid());
+
+    QVERIFY(valueSourceObject.objectType() == "Follow");
+    
+    const QmlDomValue springValue = valueSourceObject.property("spring").value();
+    QVERIFY(!springValue.isInvalid());
+    QVERIFY(springValue.isLiteral());
+    QVERIFY(springValue.toLiteral().literal() == "1.4");
+
+    const QmlDomValue sourceValue = valueSourceObject.property("source").value();
+    QVERIFY(!sourceValue.isInvalid());
+    QVERIFY(sourceValue.isBinding());
+    QVERIFY(sourceValue.toBinding().binding() == "Math.min(Math.max(-130, value*2.2 - 130), 133)");
 }
 
 QTEST_MAIN(tst_qmldom)
