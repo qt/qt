qt/qt

commit a794ded85f74516239a08cf848e6b4f8b6dcac6a

Fix assertion failure in QCompleter::setCompletionPrefix().
Calling setCompletionPrefix() on a QCompleter from a slot connected
to the editingFinished() signal of the corresponding QLineEdit
could in some cases alter the internal state of the completer in
such a way that an assertion would fail. The fix prevents the
asserting code from being called in this particular state.

Reviewed-by: janarve
Task-number: 246056

src/gui/util/qcompleter.cpp
@@ -824,9 +824,9 @@ void QCompleterPrivate::_q_complete(QModelIndex index, bool highlighted)
     Q_Q(QCompleter);
     QString completion;
 
-    if (!index.isValid())
+    if (!index.isValid() || (index.row() >= proxy->engine->matchCount())) {
         completion = prefix;
-    else {
+    } else {
         QModelIndex si = proxy->mapToSource(index);
         si = si.sibling(si.row(), column); // for clicked()
         completion = q->pathFromIndex(si);

tests/auto/qcompleter/tst_qcompleter.cpp
@@ -140,6 +140,7 @@ private slots:
     // task-specific tests below me
     void task178797_activatedOnReturn();
     void task189564_omitNonSelectableItems();
+    void task246056_setCompletionPrefix();
 
 private:
     void filter();
@@ -1091,6 +1092,38 @@ void tst_QCompleter::task189564_omitNonSelectableItems()
     QModelIndexList matches2 =
         completionModel->match(completionModel->index(0, 0), Qt::DisplayRole, omitString);
     QVERIFY(matches2.isEmpty());
+}
+
+class task246056_ComboBox : public QComboBox
+{
+    Q_OBJECT
+public:
+    task246056_ComboBox()
+    {
+        setEditable(true);
+        setInsertPolicy(NoInsert);
+        Q_ASSERT(completer());
+        completer()->setCompletionMode(QCompleter::PopupCompletion);
+        completer()->setCompletionRole(Qt::DisplayRole);
+        connect(lineEdit(), SIGNAL(editingFinished()), SLOT(setCompletionPrefix()));
+    }
+private slots:
+    void setCompletionPrefix() { completer()->setCompletionPrefix(lineEdit()->text()); }
+};
+
+void tst_QCompleter::task246056_setCompletionPrefix()
+{
+    task246056_ComboBox *comboBox = new task246056_ComboBox;
+    comboBox->addItem("");
+    comboBox->addItem("a1");
+    comboBox->addItem("a2");
+    comboBox->show();
+    comboBox->setFocus();
+    QTest::qWait(100);
+    QTest::keyPress(comboBox, 'a');
+    QTest::keyPress(comboBox->completer()->popup(), Qt::Key_Down);
+    QTest::keyPress(comboBox->completer()->popup(), Qt::Key_Down);
+    QTest::keyPress(comboBox->completer()->popup(), Qt::Key_Enter); // don't crash!
 }
 
 QTEST_MAIN(tst_QCompleter)

commit 813835f67a1053561eba67bf958eb39c0f4eedfe

Fix QCompleter with UnfilteredPopupCompletion
Fixes regression introduced in a794ded85f74516239a08cf848e6b4f8b6dcac6a.

When using UnfilteredPopupCompletion the matchCount is always zero and
completion was being skipped. By adding the check for showAll we still
avoid the assert but retain correct behavior.

Task-number: 253125
Reviewed-by: jasplin

src/gui/util/qcompleter.cpp
@@ -824,7 +824,7 @@ void QCompleterPrivate::_q_complete(QModelIndex index, bool highlighted)
     Q_Q(QCompleter);
     QString completion;
 
-    if (!index.isValid() || (index.row() >= proxy->engine->matchCount())) {
+    if (!index.isValid() || (!proxy->showAll && (index.row() >= proxy->engine->matchCount()))) {
         completion = prefix;
     } else {
         QModelIndex si = proxy->mapToSource(index);

tests/auto/qcompleter/tst_qcompleter.cpp
@@ -143,6 +143,9 @@ private slots:
     void task246056_setCompletionPrefix();
     void task250064_lostFocus();
 
+    void task253125_lineEditCompletion_data();
+    void task253125_lineEditCompletion();
+
 private:
     void filter();
     void testRowCount();
@@ -1182,6 +1185,49 @@ void tst_QCompleter::task250064_lostFocus()
     QVERIFY(origPolicy != Qt::NoFocus);
     widget->setCompletionModel();
     QCOMPARE(textEdit->focusPolicy(), origPolicy);
+}
+
+void tst_QCompleter::task253125_lineEditCompletion_data()
+{
+    QTest::addColumn<QStringList>("list");
+    QTest::addColumn<int>("completionMode");
+
+    QStringList list = QStringList()
+        << "alpha" << "beta"    << "gamma"   << "delta" << "epsilon" << "zeta"
+        << "eta"   << "theta"   << "iota"    << "kappa" << "lambda"  << "mu"
+        << "nu"    << "xi"      << "omicron" << "pi"    << "rho"     << "sigma"
+        << "tau"   << "upsilon" << "phi"     << "chi"   << "psi"     << "omega";
+
+    QTest::newRow("Inline") << list << (int)QCompleter::InlineCompletion;
+    QTest::newRow("Filtered") << list << (int)QCompleter::PopupCompletion;
+    QTest::newRow("Unfiltered") << list << (int)QCompleter::UnfilteredPopupCompletion;
+}
+
+void tst_QCompleter::task253125_lineEditCompletion()
+{
+    QFETCH(QStringList, list);
+    QFETCH(int, completionMode);
+
+    QStringListModel *model = new QStringListModel;
+    model->setStringList(list);
+
+    QCompleter *completer = new QCompleter(list);
+    completer->setModel(model);
+    completer->setCompletionMode((QCompleter::CompletionMode)completionMode);
+
+    QLineEdit edit;
+    edit.setCompleter(completer);
+    edit.show();
+    edit.setFocus();
+
+    QTest::qWait(100);
+
+    QTest::keyClick(&edit, 'i');
+    QCOMPARE(edit.completer()->currentCompletion(), QString("iota"));
+    QTest::keyClick(edit.completer()->popup(), Qt::Key_Down);
+    QTest::keyClick(edit.completer()->popup(), Qt::Key_Enter);
+
+    QCOMPARE(edit.text(), QString("iota"));
 }
 
 QTEST_MAIN(tst_QCompleter)