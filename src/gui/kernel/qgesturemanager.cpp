ce851964f45266cafd51f2c1ac4e9a7d89ce0ef0
Denis Dzyubenko <denis.dzyubenko@nokia.com>
2009-05-12 11:39:16 +0200

    Replaced evil foreach with a for loop.

--- src/gui/kernel/qgesturemanager.cpp
+++ src/gui/kernel/qgesturemanager.cpp
@@ -248,11 +248,15 @@ bool QGestureManager::filterEvent(QWidget *receiver, QEvent *event)
         activeGestures -= newMaybeGestures;
         activeGestures -= cancelledGestures;
         activeGestures += startedGestures;
-        foreach(QGestureRecognizer *r, startedGestures+finishedGestures+notGestures) {
-            QMap<QGestureRecognizer*, int>::iterator it = maybeGestures.find(r);
-            if (it != maybeGestures.end()) {
+        for(QMap<QGestureRecognizer*, int>::iterator it = maybeGestures.begin();
+            it != maybeGestures.end();) {
+            QGestureRecognizer *r = it.key();
+            if (startedGestures.contains(r) || finishedGestures.contains(r) ||
+                notGestures.contains(r)) {
                 killTimer(it.value());
-                maybeGestures.erase(it);
+                it = maybeGestures.erase(it);
+            } else {
+                ++it;
             }
         }
         foreach(QGestureRecognizer *r, newMaybeGestures) {
