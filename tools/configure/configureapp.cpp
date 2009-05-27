46f74a8c226ab67a6a9942db041f2294ff1c6f7c
Joerg Bornemann <joerg.bornemann@trolltech.com>
2009-05-27 10:19:44 +0200

    support for -ltcg configure switch for Windows CE builds
    Additionally we turn -ltcg on by default on Windows CE.
    
    See commit 56191830 for details.
    
    Reviewed-by: mauricek
    BT: yes
    (cherry picked from commit b2a1e5938e1abf45d70cbfa7ec1ab0c21c01911d)

--- tools/configure/configureapp.cpp
+++ tools/configure/configureapp.cpp
@@ -1326,6 +1326,7 @@ void Configure::applySpecSpecifics()
         dictionary[ "WEBKIT" ]              = "no";
         dictionary[ "PHONON" ]              = "yes";
         dictionary[ "DIRECTSHOW" ]          = "no";
+        dictionary[ "LTCG" ]                = "yes";
         // We only apply MMX/IWMMXT for mkspecs we know they work
         if (dictionary[ "XQMAKESPEC" ].startsWith("wincewm")) {
             dictionary[ "MMX" ]    = "yes";
