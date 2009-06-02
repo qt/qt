6e7c48df05189e4066c9e455a4b47cfd5d47a7e7
Janne Anttila <janne.anttila@digia.com>
2009-06-02 10:12:07 +0300

    Fixed qiodevice compilation error when QIODEVICE_DEBUG is defined.

--- src/corelib/io/qiodevice.cpp
+++ src/corelib/io/qiodevice.cpp
@@ -362,7 +362,7 @@ QIODevice::QIODevice()
 {
 #if defined QIODEVICE_DEBUG
     QFile *file = qobject_cast<QFile *>(this);
-    printf("%p QIODevice::QIODevice(\"%s\") %s\n", this, className(),
+    printf("%p QIODevice::QIODevice(\"%s\") %s\n", this, metaObject()->className(),
            qPrintable(file ? file->fileName() : QString()));
 #endif
 }
@@ -375,7 +375,7 @@ QIODevice::QIODevice(QObject *parent)
     : QObject(*new QIODevicePrivate, parent)
 {
 #if defined QIODEVICE_DEBUG
-    printf("%p QIODevice::QIODevice(%p \"%s\")\n", this, parent, className());
+    printf("%p QIODevice::QIODevice(%p \"%s\")\n", this, parent, metaObject()->className());
 #endif
 }
 
