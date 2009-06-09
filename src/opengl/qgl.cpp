qt/qt

commit 1bbe23c5814dfda4cc6154c49e5bbf7c6ead7eba

Make OpenGL/ES 1.1 work again for Qt/Embedded
Reviewed-by: trustme

src/opengl/qgl.cpp
@@ -65,10 +65,17 @@
 #include "qimage.h"
 #include "qgl_p.h"
 
+#if !defined(QT_OPENGL_ES_1) && !defined(QT_OPENGL_ES_1_CL)
 #include "gl2paintengineex/qpaintengineex_opengl2_p.h"
+#endif
 
 #ifndef QT_OPENGL_ES_2
 #include <private/qpaintengine_opengl_p.h>
+#endif
+
+#ifdef Q_WS_QWS
+#include <private/qglpaintdevice_qws_p.h>
+#include <private/qglwindowsurface_qws_p.h>
 #endif
 
 #include <qglpixelbuffer.h>
@@ -4428,7 +4435,11 @@ void QGLDrawable::swapBuffers()
 void QGLDrawable::makeCurrent()
 {
     previous_fbo = 0;
+#if !defined(QT_OPENGL_ES_1) && !defined(QT_OPENGL_ES_1_CL)
     if (!pixmapData && !fbo) {
+#else
+#endif
+    if (!fbo) {
         QGLContext *ctx = context();
         previous_fbo = ctx->d_ptr->current_fbo;
         ctx->d_ptr->current_fbo = 0;
@@ -4561,8 +4572,10 @@ QColor QGLDrawable::backgroundColor() const
 {
     if (widget)
         return widget->palette().brush(widget->backgroundRole()).color();
+#if !defined(QT_OPENGL_ES_1) && !defined(QT_OPENGL_ES_1_CL)
     else if (pixmapData)
         return pixmapData->fillColor();
+#endif
     return QApplication::palette().brush(QPalette::Background).color();
 }
 
@@ -4590,8 +4603,10 @@ bool QGLDrawable::autoFillBackground() const
 {
     if (widget)
         return widget->autoFillBackground();
+#if !defined(QT_OPENGL_ES_1) && !defined(QT_OPENGL_ES_1_CL)
     else if (pixmapData)
         return pixmapData->needsFill();
+#endif
     else
         return false;
 }

src/opengl/qgl_p.h
@@ -428,8 +428,7 @@ private:
 extern Q_OPENGL_EXPORT QGLShareRegister* qgl_share_reg();
 
 #ifdef Q_WS_QWS
-class QOpenGLPaintEngine;
-extern QOpenGLPaintEngine* qt_qgl_paint_engine();
+extern QPaintEngine* qt_qgl_paint_engine();
 
 extern EGLDisplay qt_qgl_egl_display();
 #endif

src/opengl/qglframebufferobject.cpp
@@ -43,7 +43,9 @@
 
 #include <qdebug.h>
 #include <private/qgl_p.h>
+#if !defined(QT_OPENGL_ES_1) && !defined(QT_OPENGL_ES_1_CL)
 #include <private/qpaintengineex_opengl2_p.h>
+#endif
 
 #ifndef QT_OPENGL_ES_2
 #include <private/qpaintengine_opengl_p.h>

src/opengl/qglpixelbuffer.cpp
@@ -76,12 +76,13 @@
     \sa {opengl/pbuffers}{Pbuffers Example}
 */
 
-#include <private/qpaintengineex_opengl2_p.h>
-
 #include <qglpixelbuffer.h>
 #include <private/qglpixelbuffer_p.h>
 #include <qimage.h>
 
+#if !defined(QT_OPENGL_ES_1) && !defined(QT_OPENGL_ES_1_CL)
+#include <private/qpaintengineex_opengl2_p.h>
+#endif
 #ifndef QT_OPENGL_ES_2
 #include <private/qpaintengine_opengl_p.h>
 #endif

commit 6d4fe549e7a258913b19f52e29bcfb84cc8a97ad

Fix build breakage from 1bbe23c5 - endif in the wrong place
Reviewed-by: trustme

src/opengl/qgl.cpp
@@ -4438,8 +4438,8 @@ void QGLDrawable::makeCurrent()
 #if !defined(QT_OPENGL_ES_1) && !defined(QT_OPENGL_ES_1_CL)
     if (!pixmapData && !fbo) {
 #else
-#endif
     if (!fbo) {
+#endif
         QGLContext *ctx = context();
         previous_fbo = ctx->d_ptr->current_fbo;
         ctx->d_ptr->current_fbo = 0;