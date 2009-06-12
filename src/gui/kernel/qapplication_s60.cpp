2a7d22330226c0a1ef6bb5c35f07241a05429913
Janne Koskinen <janne.p.koskinen@digia.com>
2009-06-12 14:09:37 +0200

    bug fix for doubleclick event for symbian

--- src/gui/kernel/qapplication_s60.cpp
+++ src/gui/kernel/qapplication_s60.cpp
@@ -392,7 +392,7 @@ void QSymbianControl::HandlePointerEventL(const TPointerEvent& pEvent)
     alienWidget = S60->mousePressTarget;
 
     if (alienWidget != S60->lastPointerEventTarget)
-        if (type == QEvent::MouseButtonPress || QEvent::MouseButtonDblClick || type == QEvent::MouseMove)
+        if (type == QEvent::MouseButtonPress || type == QEvent::MouseButtonDblClick || type == QEvent::MouseMove)
         {
             //moved to another widget, create enter and leave events
             if (S60->lastPointerEventTarget)
