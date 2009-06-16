qt/qt

commit 7bf4512659113f8cc78e72f1c84158ce4f70a526

Fixed problems with the event handling in QCompleter on Mac.
On Mac, a new completer popup must initially have its show()
method called rather than its hide() method. Otherwise the event
handling done by the completer results in a bad state.
On other platforms it doesn't matter.

Reviewed-by: Richard Moe Gustavsen
Task-number: 255374

src/gui/util/qcompleter.cpp
@@ -1077,7 +1077,7 @@ void QCompleter::setPopup(QAbstractItemView *popup)
         delete d->popup;
     if (popup->model() != d->proxy)
         popup->setModel(d->proxy);
-    popup->hide();
+    popup->show();
     popup->setParent(0, Qt::Popup);
 
     Qt::FocusPolicy origPolicy = Qt::NoFocus;

commit 6e022c5c21a44a25b1579f03498ecaeba6da9024

Prevented QCompleter popup from appearing briefly on Windows.
It turns out that the fix 7bf4512659113f8cc78e72f1c84158ce4f70a526
caused the QCompleter popup from appearing for a split second on
Windows. This is because the popup is shown as a toplevel window
on this platform. On other platforms than Mac and Windows it doesn't
seem to matter, but be on the safe side, the fix is modified to
apply for Mac only.

Reviewed-by: Daniel Molkentin
Task-number: 255374

src/gui/util/qcompleter.cpp
@@ -1077,7 +1077,11 @@ void QCompleter::setPopup(QAbstractItemView *popup)
         delete d->popup;
     if (popup->model() != d->proxy)
         popup->setModel(d->proxy);
-    popup->show();
+#ifdef Q_OS_MAC
+     popup->show();
+#else
+     popup->hide();
+#endif
     popup->setParent(0, Qt::Popup);
 
     Qt::FocusPolicy origPolicy = Qt::NoFocus;