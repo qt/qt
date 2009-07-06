423d40c56f090f3619c8df5667dcfcb0c0a0dc32
Martin Smith <msmith@trolltech.com>
2009-07-06 11:24:21 +0200

    doc: Added not about Mac OS X.
    On Mac OS X, QMenuBar::clear() does not remove menu items
    that have been merged into the system menu bar.
    
    Task-number: 255222

--- src/gui/widgets/qmenubar.cpp
+++ src/gui/widgets/qmenubar.cpp
@@ -956,6 +956,13 @@ void QMenuBar::setActiveAction(QAction *act)
 /*!
     Removes all the actions from the menu bar.
 
+    \note On Mac OS X, menu items that have been merged to the system
+    menu bar are not removed by this function. One way to handle this
+    would be to remove the extra actions yourself. You can set the
+    \l{QAction::MenuRole}{menu role} on the different menus, so that
+    you know ahead of time which menu items get merged and which do
+    not. Then decide what to recreate or remove yourself.
+
     \sa removeAction()
 */
 void QMenuBar::clear()
