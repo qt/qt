qt/qt

commit 60e965fd35037f4a27816d2aeccafdff0d6ae9d6

Refactored gesture api
Rewritten the api almost from scratch, making it simplier and more
flexible at the same time.

The current implementation will not have complex gseturemanager class
inside Qt, but the QGesture base class, which represents both a
gesture recognizer and a gesture itself with a set of properties. A
set of common gestures that can use used in third-party applications
(and in Qt itself internally) is supposed to be found in
qstandardgestures.h, and a base class for user-defined gestures is in
qgesture.h

Gesture implementation for Pan on Windows7 has also been added as a
reference implementation for platform gestures.

src/gui/kernel/qwidget.cpp
@@ -82,6 +82,8 @@
 #include "private/qstyle_p.h"
 #include "private/qinputcontext_p.h"
 #include "qfileinfo.h"
+#include "qstandardgestures.h"
+#include "qstandardgestures_p.h"
 
 #if defined (Q_WS_WIN)
 # include <private/qwininputcontext_p.h>
@@ -107,9 +109,9 @@
 #include "private/qgraphicsproxywidget_p.h"
 #include "QtGui/qabstractscrollarea.h"
 #include "private/qabstractscrollarea_p.h"
+#include "private/qevent_p.h"
 
 #include "private/qgraphicssystem_p.h"
-#include "private/qgesturemanager_p.h"
 
 // widget/widget data creation count
 //#define QWIDGET_EXTRA_DEBUG
@@ -127,8 +129,6 @@ Q_GUI_EXPORT void qt_x11_set_global_double_buffer(bool enable)
     qt_enable_backingstore = enable;
 }
 #endif
-
-QString qt_getStandardGestureTypeName(Qt::GestureType);
 
 static inline bool qRectIntersects(const QRect &r1, const QRect &r2)
 {
@@ -7484,8 +7484,6 @@ bool QWidget::event(QEvent *event)
 #ifndef QT_NO_WHEELEVENT
         case QEvent::Wheel:
 #endif
-        case QEvent::Gesture:
-            return false;
         default:
             break;
         }
@@ -7880,9 +7878,6 @@ bool QWidget::event(QEvent *event)
         d->needWindowChange = false;
         break;
 #endif
-    case QEvent::Gesture:
-        event->ignore();
-        break;
     case QEvent::TouchBegin:
     case QEvent::TouchUpdate:
     case QEvent::TouchEnd:
@@ -7920,6 +7915,59 @@ bool QWidget::event(QEvent *event)
         (void) QApplication::sendEvent(this, &mouseEvent);
         break;
     }
+#ifdef Q_WS_WIN
+    case QEvent::WinGesture: {
+        QWinGestureEvent *ev = static_cast<QWinGestureEvent*>(event);
+        QApplicationPrivate *qAppPriv = qApp->d_func();
+        QApplicationPrivate::WidgetStandardGesturesMap::iterator it;
+        it = qAppPriv->widgetGestures.find(this);
+        if (it != qAppPriv->widgetGestures.end()) {
+            Qt::GestureState state = Qt::GestureUpdated;
+            if (qAppPriv->lastGestureId == 0)
+                state = Qt::GestureStarted;
+            QWinGestureEvent::Type type = ev->gestureType;
+            if (ev->gestureType == QWinGestureEvent::GestureEnd) {
+                type = (QWinGestureEvent::Type)qAppPriv->lastGestureId;
+                state = Qt::GestureFinished;
+            }
+
+            QGesture *gesture = 0;
+            switch (type) {
+            case QWinGestureEvent::Pan: {
+                QPanGesture *pan = it.value().pan;
+                gesture = pan;
+                if (state == Qt::GestureStarted) {
+                    gesture->setStartPos(ev->position);
+                    gesture->setLastPos(ev->position);
+                } else {
+                    gesture->setLastPos(gesture->pos());
+                }
+                gesture->setPos(ev->position);
+                break;
+            }
+            case QWinGestureEvent::Pinch:
+                break;
+            default:
+                break;
+            }
+            if (gesture) {
+                gesture->setState(state);
+                if (state == Qt::GestureStarted)
+                    emit gesture->started();
+                emit gesture->triggered();
+                if (state == Qt::GestureFinished)
+                    emit gesture->finished();
+                event->accept();
+            }
+            if (ev->gestureType == QWinGestureEvent::GestureEnd) {
+                qAppPriv->lastGestureId = 0;
+            } else {
+                qAppPriv->lastGestureId = type;
+            }
+        }
+        break;
+    }
+#endif
 #ifndef QT_NO_PROPERTIES
     case QEvent::DynamicPropertyChange: {
         const QByteArray &propName = static_cast<QDynamicPropertyChangeEvent *>(event)->propertyName();
@@ -11014,105 +11062,6 @@ QWindowSurface *QWidget::windowSurface() const
 #endif // Q_BACKINGSTORE_SUBSURFACES
 
     return bs ? bs->windowSurface : 0;
-}
-
-/*!
-    \since 4.6
-
-    Subscribes the widget to the specified \a gesture type.
-
-    Returns the id of the gesture.
-
-    \sa releaseGesture(), setGestureEnabled()
-*/
-int QWidget::grabGesture(const QString &gesture)
-{
-    Q_D(QWidget);
-    int id = d->grabGesture(QGestureManager::instance()->makeGestureId(gesture));
-    if (d->extra && d->extra->proxyWidget)
-        d->extra->proxyWidget->QGraphicsItem::d_ptr->grabGesture(id);
-    return id;
-}
-
-int QWidgetPrivate::grabGesture(int gestureId)
-{
-    gestures << gestureId;
-    ++qApp->d_func()->grabbedGestures[QGestureManager::instance()->gestureNameFromId(gestureId)];
-    return gestureId;
-}
-
-bool QWidgetPrivate::releaseGesture(int gestureId)
-{
-    QApplicationPrivate *qAppPriv = qApp->d_func();
-    if (gestures.contains(gestureId)) {
-        QString name = QGestureManager::instance()->gestureNameFromId(gestureId);
-        Q_ASSERT(qAppPriv->grabbedGestures[name] > 0);
-        --qAppPriv->grabbedGestures[name];
-        gestures.remove(gestureId);
-        return true;
-    }
-    return false;
-}
-
-bool QWidgetPrivate::hasGesture(const QString &name) const
-{
-    QGestureManager *gm = QGestureManager::instance();
-    QSet<int>::const_iterator it = gestures.begin(),
-                               e = gestures.end();
-    for (; it != e; ++it) {
-        if (gm->gestureNameFromId(*it) == name)
-            return true;
-    }
-    return false;
-}
-
-/*!
-    \since 4.6
-
-    Subscribes the widget to the specified \a gesture type.
-
-    Returns the id of the gesture.
-
-    \sa releaseGesture(), setGestureEnabled()
-*/
-int QWidget::grabGesture(Qt::GestureType gesture)
-{
-    return grabGesture(qt_getStandardGestureTypeName(gesture));
-}
-
-/*!
-    \since 4.6
-
-    Unsubscribes the widget from a gesture, which is specified by the
-    \a gestureId.
-
-    \sa grabGesture(), setGestureEnabled()
-*/
-void QWidget::releaseGesture(int gestureId)
-{
-    Q_D(QWidget);
-    if (d->releaseGesture(gestureId)) {
-        if (d->extra && d->extra->proxyWidget)
-            d->extra->proxyWidget->QGraphicsItem::d_ptr->releaseGesture(gestureId);
-        QGestureManager::instance()->releaseGestureId(gestureId);
-    }
-}
-
-/*!
-    \since 4.6
-
-    If \a enable is true, the gesture with the given \a gestureId is
-    enabled; otherwise the gesture is disabled.
-
-    The id of the gesture is returned by the grabGesture().
-
-    \sa grabGesture(), releaseGesture()
-*/
-void QWidget::setGestureEnabled(int gestureId, bool enable)
-{
-    Q_UNUSED(gestureId);
-    Q_UNUSED(enable);
-    //###
 }
 
 void QWidgetPrivate::getLayoutItemMargins(int *left, int *top, int *right, int *bottom) const

src/gui/widgets/qabstractscrollarea.cpp
@@ -51,9 +51,12 @@
 #include "qdebug.h"
 #include "qboxlayout.h"
 #include "qpainter.h"
+#include "qstandardgestures.h"
 
 #include "qabstractscrollarea_p.h"
 #include <qwidget.h>
+
+#include <private/qapplication_p.h>
 
 #ifdef Q_WS_MAC
 #include <private/qt_mac_p.h>
@@ -157,6 +160,9 @@ QAbstractScrollAreaPrivate::QAbstractScrollAreaPrivate()
     :hbar(0), vbar(0), vbarpolicy(Qt::ScrollBarAsNeeded), hbarpolicy(Qt::ScrollBarAsNeeded),
      viewport(0), cornerWidget(0), left(0), top(0), right(0), bottom(0),
      xoffset(0), yoffset(0), viewportFilter(0)
+#ifdef Q_WS_WIN
+     , singleFingerPanEnabled(false)
+#endif
 {
 }
 
@@ -288,6 +294,31 @@ void QAbstractScrollAreaPrivate::init()
     q->setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
     q->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     layoutChildren();
+}
+
+void QAbstractScrollAreaPrivate::setupGestures()
+{
+#ifdef Q_OS_WIN
+    if (!viewport)
+        return;
+    QApplicationPrivate *qAppPriv = getQApplicationPrivateInternal();
+    bool needh = (hbarpolicy == Qt::ScrollBarAlwaysOn
+                  || (hbarpolicy == Qt::ScrollBarAsNeeded && hbar->minimum() < hbar->maximum()));
+
+    bool needv = (vbarpolicy == Qt::ScrollBarAlwaysOn
+                  || (vbarpolicy == Qt::ScrollBarAsNeeded && vbar->minimum() < vbar->maximum()));
+    if (qAppPriv->SetGestureConfig && (needh || needv)) {
+        GESTURECONFIG gc[1];
+        gc[0].dwID = GID_PAN;
+        gc[0].dwWant = GC_PAN;
+        gc[0].dwBlock = 0;
+        if (needv && singleFingerPanEnabled)
+            gc[0].dwWant |= GC_PAN_WITH_SINGLE_FINGER_VERTICALLY;
+        if (needh && singleFingerPanEnabled)
+            gc[0].dwWant |= GC_PAN_WITH_SINGLE_FINGER_HORIZONTALLY;
+        qAppPriv->SetGestureConfig(viewport->winId(), 0, 1, gc, sizeof(gc));
+    }
+#endif // Q_OS_WIN
 }
 
 void QAbstractScrollAreaPrivate::layoutChildren()
@@ -909,8 +940,6 @@ bool QAbstractScrollArea::event(QEvent *e)
     case QEvent::DragMove:
     case QEvent::DragLeave:
 #endif
-    case QEvent::Gesture:
-        return false;
     case QEvent::StyleChange:
     case QEvent::LayoutDirectionChange:
     case QEvent::ApplicationLayoutDirectionChange:
@@ -1239,6 +1268,7 @@ void QAbstractScrollAreaPrivate::_q_vslide(int y)
 void QAbstractScrollAreaPrivate::_q_showOrHideScrollBars()
 {
     layoutChildren();
+    setupGestures();
 }
 
 QPoint QAbstractScrollAreaPrivate::contentsOffset() const

commit 5f48e2a16b0b64536a205320fbf7a22f7461d80d

Fixes a crash when scrolling a scrollarea with a mouse wheel.
This fixes a crashed introduced in
60e965fd35037f4a27816d2aeccafdff0d6ae9d6 - those lines were removed by
accident.

Reviewed-by: João Abecasis
Author: João Abecasis

src/gui/kernel/qwidget.cpp
@@ -7484,6 +7484,7 @@ bool QWidget::event(QEvent *event)
 #ifndef QT_NO_WHEELEVENT
         case QEvent::Wheel:
 #endif
+            return false;
         default:
             break;
         }

src/gui/widgets/qabstractscrollarea.cpp
@@ -937,6 +937,7 @@ bool QAbstractScrollArea::event(QEvent *e)
     case QEvent::DragMove:
     case QEvent::DragLeave:
 #endif
+        return false;
     case QEvent::StyleChange:
     case QEvent::LayoutDirectionChange:
     case QEvent::ApplicationLayoutDirectionChange: