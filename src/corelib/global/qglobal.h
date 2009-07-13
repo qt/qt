9b6eacab99673d7d9848b341c4cf36a7c35f07c0
Frans Englich <frans.englich@nokia.com>
2009-07-13 13:02:20 +0200

    QFlags::testFlag(): handle the zero case appropriately.
    Brought up by Andy. See perforce change 314809,
    17b07e3ab6192b31f77fd2f126705b9ab53b3937. Related to task 221708.
    
    Reviewed-By: Andy Shaw
    (cherry picked from commit cc24c46c117248ecb98200416e7f25375e6bb476)

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -2017,7 +2017,7 @@ public:
 
     inline bool operator!() const { return !i; }
 
-    inline bool testFlag(Enum f) const { return (i & f) == f; }
+    inline bool testFlag(Enum f) const { return (i & f) == f && (f != 0 || i == f ); }
 };
 
 #define Q_DECLARE_FLAGS(Flags, Enum)\
