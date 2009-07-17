qt/qt

commit 86ea4dbb5a748491656d9621ecd58238bc3e3d82

Don't assume that raster can do porter duff in dfb
PorterDuff should only be enabled if the raster engine says it is. E.g.
if we're painting on a format with alpha.

Reviewed-by: TrustMe

src/plugins/gfxdrivers/directfb/qdirectfbpaintengine.cpp
@@ -232,11 +232,7 @@ bool QDirectFBPaintEngine::begin(QPaintDevice *device)
                device->devType());
     }
 
-    const bool status = QRasterPaintEngine::begin(device);
-    // XXX: QRasterPaintEngine::begin() resets the capabilities
-    gccaps |= PorterDuff;
-    d->prepare(d->dfbDevice);
-    return status;
+    return QRasterPaintEngine::begin(device);
 }
 
 bool QDirectFBPaintEngine::end()

commit 7d4a3a102c562d057c7fa5de900fe4a6c662dda4

Prepare device in DFBPaintEngine::begin
86ea4dbb5a748491656d9621ecd58238bc3e3d82 accidentally took out this
line.

Reviewed-by: TrustMe

src/plugins/gfxdrivers/directfb/qdirectfbpaintengine.cpp
@@ -232,6 +232,8 @@ bool QDirectFBPaintEngine::begin(QPaintDevice *device)
                device->devType());
     }
 
+    d->prepare(d->dfbDevice);
+
     return QRasterPaintEngine::begin(device);
 }
 