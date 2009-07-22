220b1cbcd253c8133ad185cd2be55db584071e67
Rohan McGovern <rohan.mcgovern@nokia.com>
2009-07-22 15:33:39 +1000

    Add a (failing) test for QProcess bug 258462.

--- tests/auto/qprocess/tst_qprocess.cpp
+++ tests/auto/qprocess/tst_qprocess.cpp
@@ -141,6 +141,7 @@ private slots:
     void startFinishStartFinish();
     void invalidProgramString_data();
     void invalidProgramString();
+    void processEventsInAReadyReadSlot();
 
     // keep these at the end, since they use lots of processes and sometimes
     // caused obscure failures to occur in tests that followed them (esp. on the Mac)
@@ -154,6 +155,7 @@ protected slots:
     void restartProcess();
     void waitForReadyReadInAReadyReadSlotSlot();
     void waitForBytesWrittenInABytesWrittenSlotSlot();
+    void processEventsInAReadyReadSlotSlot();
     
 private:
     QProcess *process;
@@ -2022,6 +2024,35 @@ void tst_QProcess::invalidProgramString()
     QCOMPARE(spy.count(), 1);
 
     QVERIFY(!QProcess::startDetached(programString));
+}
+
+//-----------------------------------------------------------------------------
+void tst_QProcess::processEventsInAReadyReadSlot()
+{
+#ifdef Q_OS_WINCE
+    QSKIP("Reading and writing to a process is not supported on Qt/CE", SkipAll);
+#endif
+
+    QProcess process;
+    QVERIFY(QObject::connect(&process, SIGNAL(readyReadStandardOutput()), this, SLOT(processEventsInAReadyReadSlotSlot())));
+
+    for (int i = 0; i < 10; ++i) {
+        QCOMPARE(process.state(), QProcess::NotRunning);
+
+#ifdef Q_OS_MAC
+        process.start("testProcessOutput/testProcessOutput.app");
+#else
+        process.start("testProcessOutput/testProcessOutput");
+#endif
+
+        QVERIFY(process.waitForFinished(10000));
+    }
+}
+
+//-----------------------------------------------------------------------------
+void tst_QProcess::processEventsInAReadyReadSlotSlot()
+{
+    qApp->processEvents();
 }
 
 QTEST_MAIN(tst_QProcess)
