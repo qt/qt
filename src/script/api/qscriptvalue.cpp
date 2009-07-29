c85a085a7c08941b616b855679e3a1f8f5e51ca2
Olivier Goffart <ogoffart@trolltech.com>
2009-07-29 12:06:58 +0200

    Fix license headers

--- src/script/api/qscriptvalue.cpp
+++ src/script/api/qscriptvalue.cpp
@@ -1,11 +1,41 @@
 /****************************************************************************
 **
-** Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies).
-** Contact: Qt Software Information (qt-info@nokia.com)
+** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Nokia Corporation (qt-info@nokia.com)
 **
-** This file is part of the $MODULE$ of the Qt Toolkit.
+** This file is part of the QtScript module of the Qt Toolkit.
 **
-** $TROLLTECH_DUAL_LICENSE$
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the either Technology Preview License Agreement or the
+** Beta Release License Agreement.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain
+** additional rights. These rights are described in the Nokia Qt LGPL
+** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
+** package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+** If you are unsure which license is appropriate for your use, please
+** contact the sales department at http://www.qtsoftware.com/contact.
+** $QT_END_LICENSE$
 **
 ****************************************************************************/
 
@@ -445,7 +475,7 @@ QScriptValue::QScriptValue(const QScriptValue &other)
 }
 
 /*!
-  \obsolete 
+  \obsolete
 
   Constructs a new QScriptValue with the special \a value and
   registers it with the script \a engine.
@@ -1194,7 +1224,7 @@ bool QScriptValue::equals(const QScriptValue &other) const
     \row    \o String     \o true if both values are exactly the same sequence of characters, false otherwise
     \row    \o Object     \o true if both values refer to the same object, false otherwise
     \endtable
-  
+
   \sa equals()
 */
 bool QScriptValue::strictlyEquals(const QScriptValue &other) const
@@ -1805,7 +1835,7 @@ void QScriptValue::setProperty(quint32 arrayIndex, const QScriptValue &value,
     }
 }
 
-/*! 
+/*!
   \since 4.4
 
   Returns the value of this QScriptValue's property with the given \a name,
