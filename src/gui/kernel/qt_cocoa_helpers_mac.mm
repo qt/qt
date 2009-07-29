a45fe18569be1fc91e26f6e58d2f16bc8c6958de
Morten Sørvig <msorvig@trolltech.com>
2009-07-29 07:42:30 +0200

    Compile.
    Remobe another instance of for ... in use.

--- src/gui/kernel/qt_cocoa_helpers_mac.mm
+++ src/gui/kernel/qt_cocoa_helpers_mac.mm
@@ -1229,7 +1229,9 @@ void qt_mac_menu_collapseSeparators(void */*NSMenu **/ theMenu, bool collapse)
         if (previousItem && previousIsSeparator)
             [previousItem setHidden:YES];
     } else {
-        for (NSMenuItem *item in [menu itemArray]) {
+        NSArray *itemArray = [menu itemArray];
+        for (unsigned int i = 0; i < [itemArray count]; ++i) {
+            NSMenuItem *item = reinterpret_cast<NSMenuItem *>([itemArray objectAtIndex:i]);
             if (QAction *action = reinterpret_cast<QAction *>([item tag]))
                 [item setHidden:!action->isVisible()];
         }
