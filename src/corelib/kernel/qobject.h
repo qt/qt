2d05bf654cfa27a7843e50aef181b0e27bfb1c12
Aaron Kennedy <aaron.kennedy@nokia.com>
2009-07-30 10:52:40 +1000

    Allow retrieval of interface IId from class type.
    This method is necessary for QML to support Qt interfaces, but probably
    shouldn't be used otherwise.

--- src/corelib/kernel/qobject.h
+++ src/corelib/kernel/qobject.h
@@ -379,6 +379,9 @@ inline QList<T> qFindChildren(const QObject *o, const QRegExp &re)
 #endif // Q_MOC_RUN
 
 
+template <class T> inline const char * qobject_interface_iid() 
+{ return 0; }
+
 template <class T> inline T qobject_cast_helper(QObject *object, T)
 { return static_cast<T>(((T)0)->staticMetaObject.cast(object)); }
 
@@ -395,6 +398,8 @@ inline T qobject_cast(const QObject *object)
 
 #ifndef Q_MOC_RUN
 #  define Q_DECLARE_INTERFACE(IFace, IId) \
+    template <> inline const char *qobject_interface_iid<IFace *>() \
+    { return IId; } \
     template <> inline IFace *qobject_cast_helper<IFace *>(QObject *object, IFace *) \
     { return (IFace *)(object ? object->qt_metacast(IId) : 0); } \
     template <> inline IFace *qobject_cast_helper<IFace *>(const QObject *object, IFace *) \
@@ -458,8 +463,13 @@ inline T qobject_cast(const QObject *object)
 }
 
 
+template <class T> inline const char * qobject_interface_iid() 
+{ return 0; }
+
 #ifndef Q_MOC_RUN
 #  define Q_DECLARE_INTERFACE(IFace, IId) \
+    template <> inline const char *qobject_interface_iid<IFace *>() \
+    { return IId; } \
     template <> inline IFace *qobject_cast<IFace *>(QObject *object) \
     { return reinterpret_cast<IFace *>((object ? object->qt_metacast(IId) : 0)); } \
     template <> inline IFace *qobject_cast<IFace *>(const QObject *object) \
