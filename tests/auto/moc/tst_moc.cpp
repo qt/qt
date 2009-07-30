qt/qt

commit e43eae35b242bf90c801e719d61fff4a20549ead

Fix Warning saying that signal cannot be made virtual
The test for virtual signal did not work.
But we cannot make an error right now or it might break existing code
(exemple in task 210879)

Reviewed-by: Kent Hansen

src/tools/moc/moc.cpp
@@ -337,11 +337,10 @@ bool Moc::testFunctionAttribute(Token tok, FunctionDef *def)
 bool Moc::parseFunction(FunctionDef *def, bool inMacro)
 {
     def->isVirtual = false;
-    while (test(INLINE) || test(STATIC) || test(VIRTUAL)
-           || testFunctionAttribute(def)) {
-        if (lookup() == VIRTUAL)
-            def->isVirtual = true;
-    }
+    //skip modifiers and attributes
+    while (test(INLINE) || test(STATIC) ||
+        (test(VIRTUAL) && (def->isVirtual = true)) //mark as virtual
+        || testFunctionAttribute(def)) {}
     bool templateFunction = (lookup() == TEMPLATE);
     def->type = parseType();
     if (def->type.name.isEmpty()) {
@@ -429,11 +428,10 @@ bool Moc::parseFunction(FunctionDef *def, bool inMacro)
 bool Moc::parseMaybeFunction(const ClassDef *cdef, FunctionDef *def)
 {
     def->isVirtual = false;
-    while (test(EXPLICIT) || test(INLINE) || test(STATIC) || test(VIRTUAL)
-           || testFunctionAttribute(def)) {
-        if (lookup() == VIRTUAL)
-            def->isVirtual = true;
-    }
+    //skip modifiers and attributes
+    while (test(INLINE) || test(STATIC) ||
+        (test(VIRTUAL) && (def->isVirtual = true)) //mark as virtual
+        || testFunctionAttribute(def)) {}
     bool tilde = test(TILDE);
     def->type = parseType();
     if (def->type.name.isEmpty())
@@ -862,7 +860,7 @@ void Moc::parseSignals(ClassDef *def)
         funcDef.access = FunctionDef::Protected;
         parseFunction(&funcDef);
         if (funcDef.isVirtual)
-            error("Signals cannot be declared virtual");
+            warning("Signals cannot be declared virtual");
         if (funcDef.inlineCode)
             error("Not a signal declaration");
         def->signalList += funcDef;

tests/auto/moc/tst_moc.cpp
@@ -488,6 +488,7 @@ private slots:
     void warnOnPropertyWithoutREAD();
     void constructors();
     void typenameWithUnsigned();
+    void warnOnVirtualSignal();
 
 signals:
     void sigWithUnsignedArg(unsigned foo);
@@ -1178,6 +1179,27 @@ void tst_Moc::typenameWithUnsigned()
     QVERIFY(mobj->indexOfSlot("j(unsigned1,uint)") != -1);
     QVERIFY(mobj->indexOfSlot("k(unsignedQImage)") != -1);
     QVERIFY(mobj->indexOfSlot("l(unsignedQImage)") != -1);
+}
+
+
+void tst_Moc::warnOnVirtualSignal()
+{
+#ifdef MOC_CROSS_COMPILED
+    QSKIP("Not tested when cross-compiled", SkipAll);
+#endif
+#if defined(Q_OS_LINUX) && defined(Q_CC_GNU) && !defined(QT_NO_PROCESS)
+    QProcess proc;
+    proc.start("moc", QStringList(srcify("pure-virtual-signals.h")));
+    QVERIFY(proc.waitForFinished());
+    QCOMPARE(proc.exitCode(), 0);
+    QByteArray mocOut = proc.readAllStandardOutput();
+    QVERIFY(!mocOut.isEmpty());
+    QString mocWarning = QString::fromLocal8Bit(proc.readAllStandardError());
+    QCOMPARE(mocWarning, QString(SRCDIR) + QString("/pure-virtual-signals.h:48: Warning: Signals cannot be declared virtual\n") +
+                         QString(SRCDIR) + QString("/pure-virtual-signals.h:50: Warning: Signals cannot be declared virtual\n"));
+#else
+    QSKIP("Only tested on linux/gcc", SkipAll);
+#endif
 }
 
 QTEST_MAIN(tst_Moc)

commit 9523b27be4fe00870d19c4ff4caa2d78dcf520e6

parse Q_INVOKABLE explicit constructors
regression introduced in e43eae35 because the code of maybeParseFunction
and parseFunction is not exactly the same.

Reviewed-by: Kent Hansen

src/tools/moc/moc.cpp
@@ -429,7 +429,7 @@ bool Moc::parseMaybeFunction(const ClassDef *cdef, FunctionDef *def)
 {
     def->isVirtual = false;
     //skip modifiers and attributes
-    while (test(INLINE) || test(STATIC) ||
+    while (test(EXPLICIT) || test(INLINE) || test(STATIC) ||
         (test(VIRTUAL) && (def->isVirtual = true)) //mark as virtual
         || testFunctionAttribute(def)) {}
     bool tilde = test(TILDE);

tests/auto/moc/tst_moc.cpp
@@ -1157,6 +1157,13 @@ void tst_Moc::constructors()
     QObject *o3 = mo->newInstance(Q_ARG(QString, str));
     QVERIFY(o3 != 0);
     QCOMPARE(qobject_cast<CtorTestClass*>(o3)->m_str, str);
+
+    {
+        //explicit constructor
+        QObject *o = QObject::staticMetaObject.newInstance();
+        QVERIFY(o);
+        delete o;
+    }
 }
 
 #include "task234909.h"