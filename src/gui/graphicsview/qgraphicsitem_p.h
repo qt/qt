0ba7569ab7911bf65b6321ad65ac979d9d034c2b
Andreas Aardal Hanssen <andreas.aardal.hanssen@nokia.com>
2009-07-30 06:27:34 +0200

    Docs: Improve QGraphicsTransform and subclasses
    Fix a few typos and add more descriptive documentation to the
    class itself and its subclasses.
    
    Reviewed-by: Volker Hilsheimer
    Reviewed-by: Martin Smith

--- src/gui/graphicsview/qgraphicsitem_p.h
+++ src/gui/graphicsview/qgraphicsitem_p.h
@@ -468,7 +468,8 @@ public:
     QGraphicsItem *q_ptr;
 };
 
-struct QGraphicsItemPrivate::TransformData {
+struct QGraphicsItemPrivate::TransformData
+{
     QTransform transform;
     qreal scale;
     qreal rotation;
@@ -481,7 +482,7 @@ struct QGraphicsItemPrivate::TransformData {
         scale(1.0), rotation(0.0),
         xOrigin(0.0), yOrigin(0.0),
         onlyTransform(true)
-    {}
+    { }
 
     QTransform computedFullTransform(QTransform *postmultiplyTransform = 0) const
     {
