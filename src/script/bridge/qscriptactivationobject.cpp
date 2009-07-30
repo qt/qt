3963331128f8af4dc1008a7efbbd1ccf292df567
Olivier Goffart <ogoffart@trolltech.com>
2009-07-30 15:08:13 +0200

    Add missing file

---
+++ src/script/bridge/qscriptactivationobject.cpp
@@ -0,0 +1,88 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtScript module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the either Technology Preview License Agreement or the
+** Beta Release License Agreement.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain
+** additional rights. These rights are described in the Nokia Qt LGPL
+** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
+** package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+** If you are unsure which license is appropriate for your use, please
+** contact the sales department at http://www.qtsoftware.com/contact.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qscriptactivationobject_p.h"
+
+#ifndef QT_NO_SCRIPT
+
+#include "JSVariableObject.h"
+
+namespace JSC
+{
+    ASSERT_CLASS_FITS_IN_CELL(QScript::QScriptActivationObject);
+}
+
+QT_BEGIN_NAMESPACE
+
+/*!
+\class QScriptActivationObject
+\internal
+
+  Represent a scope for native function call.
+*/
+
+namespace QScript
+{
+QScriptActivationObject::QScriptActivationObject(JSC::ExecState *callFrame)
+    : JSC::JSVariableObject(callFrame->globalData().activationStructure, new QScriptActivationObjectData(callFrame->registers()))
+{
+}
+
+QScriptActivationObject::~QScriptActivationObject()
+{
+    delete d;
+}
+
+void QScriptActivationObject::putWithAttributes(JSC::ExecState *exec, const JSC::Identifier &propertyName, JSC::JSValue value, unsigned attributes)
+{
+    if (symbolTablePutWithAttributes(propertyName, value, attributes))
+        return;
+    
+    JSC::PutPropertySlot slot;
+    JSObject::putWithAttributes(exec, propertyName, value, attributes, true, slot);
+}
+
+} // namespace QScript
+
+QT_END_NAMESPACE
+
+#endif // QT_NO_SCRIPT
+
