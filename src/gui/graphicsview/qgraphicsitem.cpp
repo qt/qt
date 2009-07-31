2cadc571339d4abee6d7cfb0da08e3119d934524
Volker Hilsheimer <volker.hilsheimer@nokia.com>
2009-07-31 15:52:55 +0200

    Doc: Use new APIs in example and correct documentation regarding replacement.

--- src/gui/graphicsview/qgraphicsitem.cpp
+++ src/gui/graphicsview/qgraphicsitem.cpp
@@ -3535,7 +3535,13 @@ void QGraphicsItem::resetTransform()
 /*!
     \obsolete
 
-    Use setRotation() instead
+    Use
+
+    \code
+    setRotation(rotation() + angle);
+    \endcode
+
+    instead.
 
     Rotates the current item transformation \a angle degrees clockwise around
     its origin. To translate around an arbitrary point (x, y), you need to
@@ -3555,7 +3561,13 @@ void QGraphicsItem::rotate(qreal angle)
 /*!
     \obsolete
 
-    Use setScale() instead
+    Use
+
+    \code
+    setTransform(QTransform::fromScale(sx, sy), true);
+    \encode
+
+    instead.
 
     Scales the current item transformation by (\a sx, \a sy) around its
     origin. To scale from an arbitrary point (x, y), you need to combine
@@ -3575,7 +3587,13 @@ void QGraphicsItem::scale(qreal sx, qreal sy)
 /*!
     \obsolete
 
-    Use setTransform() instead.
+    Use 
+
+    \code
+    setTransform(QTransform().shear(sh, sv), true);
+    \endcode
+
+    instead.
 
     Shears the current item transformation by (\a sh, \a sv).
 
@@ -3589,7 +3607,12 @@ void QGraphicsItem::shear(qreal sh, qreal sv)
 /*!
     \obsolete
 
-    Use setPos() or setTransformOriginPoint() instead.
+    Use setPos() or setTransformOriginPoint() instead. For identical
+    behavior, use
+    
+    \code
+    setTransform(QTransform::fromTranslate(dx, dy), true);
+    \endcode
 
     Translates the current item transformation by (\a dx, \a dy).
 
