a02efd252df18c1869663d774743781e228fd73c
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2009-08-06 12:58:53 +0200

    Tests: Adapted uic baseline to 33604fb02fa463f36fa78e515bb42a34a746f0f2

--- tests/auto/uic/baseline/tablewidgeteditor.ui.h
+++ tests/auto/uic/baseline/tablewidgeteditor.ui.h
@@ -41,12 +41,12 @@
 */
 
 /********************************************************************************
-** Form generated from reading ui file 'tablewidgeteditor.ui'
+** Form generated from reading UI file 'tablewidgeteditor.ui'
 **
 ** Created: Mon Jun 16 17:48:45 2008
 **      by: Qt User Interface Compiler version 4.5.0
 **
-** WARNING! All changes made in this file will be lost when recompiling ui file!
+** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
 
 #ifndef TABLEWIDGETEDITOR_H
