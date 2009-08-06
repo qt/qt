qt/qt

commit 6a2621b6832dbdd349f77cf1f3242b4a6ba3c740

Added an ability to remove size constraints from a widget.
QWidget::setFixedSize() constrains the size of the widget, however there
was no way to remove those constraints, so setting the constraints to
QWIDGETSIZE_MAX will make the widget resizable again.

Reviewed-by: Leonardo Sobral Cunha

src/gui/kernel/qwidget.cpp
@@ -3491,11 +3491,16 @@ bool QWidgetPrivate::setMinimumSize_helper(int &minw, int &minh)
         minh = qMax(minh, 0);
     }
     createExtra();
-    if (extra->minw == minw && extra->minh == minh)
+    int mw = minw, mh = minh;
+    if (mw == QWIDGETSIZE_MAX)
+        mw = 0;
+    if (mh == QWIDGETSIZE_MAX)
+        mh = 0;
+    if (extra->minw == mw && extra->minh == mh)
         return false;
-    extra->minw = minw;
-    extra->minh = minh;
-    extra->explicitMinSize = (minw ? Qt::Horizontal : 0) | (minh ? Qt::Vertical : 0);
+    extra->minw = mw;
+    extra->minh = mh;
+    extra->explicitMinSize = (mw ? Qt::Horizontal : 0) | (mh ? Qt::Vertical : 0);
     return true;
 }
 
@@ -3555,7 +3560,8 @@ bool QWidgetPrivate::setMaximumSize_helper(int &maxw, int &maxh)
         return false;
     extra->maxw = maxw;
     extra->maxh = maxh;
-    extra->explicitMaxSize = (maxw != QWIDGETSIZE_MAX ? Qt::Horizontal : 0) | (maxh != QWIDGETSIZE_MAX ? Qt::Vertical : 0);
+    extra->explicitMaxSize = (maxw != QWIDGETSIZE_MAX ? Qt::Horizontal : 0) |
+                             (maxh != QWIDGETSIZE_MAX ? Qt::Vertical : 0);
     return true;
 }
 
@@ -3634,6 +3640,8 @@ void QWidget::setBaseSize(int basew, int baseh)
 
     This will override the default size constraints set by QLayout.
 
+    To remove constraints, set the size to QWIDGETSIZE_MAX.
+
     Alternatively, if you want the widget to have a
     fixed size based on its contents, you can call
     QLayout::setSizeConstraint(QLayout::SetFixedSize);
@@ -3675,7 +3683,8 @@ void QWidget::setFixedSize(int w, int h)
     else
         d->updateGeometry_helper(true);
 
-    resize(w, h);
+    if (w != QWIDGETSIZE_MAX || h != QWIDGETSIZE_MAX)
+        resize(w, h);
 }
 
 void QWidget::setMinimumWidth(int w)

commit 69597149e606a3d8a22a2ea7c2964b799751d898

Fixes a regression in qwidget when setting a large minimum size.
Setting a minimum size to a value that is larger then the biggest
allowed widget size, we should costrain the widget to that max allowed
size as we did before. Change 6a2621b6832dbdd349f77cf1f3242b4a6ba3c740
broke it.

Reviewed-by: Bradley T. Hughes

src/gui/kernel/qwidget.cpp
@@ -3480,27 +3480,27 @@ bool QWidgetPrivate::setMinimumSize_helper(int &minw, int &minh)
         }
     }
 #endif
-    if (minw > QWIDGETSIZE_MAX || minh > QWIDGETSIZE_MAX) {
-        qWarning("QWidget::setMinimumSize: (%s/%s) "
-                "The largest allowed size is (%d,%d)",
-                 q->objectName().toLocal8Bit().data(), q->metaObject()->className(), QWIDGETSIZE_MAX,
-                QWIDGETSIZE_MAX);
-        minw = qMin<int>(minw, QWIDGETSIZE_MAX);
-        minh = qMin<int>(minh, QWIDGETSIZE_MAX);
-    }
-    if (minw < 0 || minh < 0) {
-        qWarning("QWidget::setMinimumSize: (%s/%s) Negative sizes (%d,%d) "
-                "are not possible",
-                q->objectName().toLocal8Bit().data(), q->metaObject()->className(), minw, minh);
-        minw = qMax(minw, 0);
-        minh = qMax(minh, 0);
-    }
-    createExtra();
     int mw = minw, mh = minh;
     if (mw == QWIDGETSIZE_MAX)
         mw = 0;
     if (mh == QWIDGETSIZE_MAX)
         mh = 0;
+    if (minw > QWIDGETSIZE_MAX || minh > QWIDGETSIZE_MAX) {
+        qWarning("QWidget::setMinimumSize: (%s/%s) "
+                "The largest allowed size is (%d,%d)",
+                 q->objectName().toLocal8Bit().data(), q->metaObject()->className(), QWIDGETSIZE_MAX,
+                QWIDGETSIZE_MAX);
+        minw = mw = qMin<int>(minw, QWIDGETSIZE_MAX);
+        minh = mh = qMin<int>(minh, QWIDGETSIZE_MAX);
+    }
+    if (minw < 0 || minh < 0) {
+        qWarning("QWidget::setMinimumSize: (%s/%s) Negative sizes (%d,%d) "
+                "are not possible",
+                q->objectName().toLocal8Bit().data(), q->metaObject()->className(), minw, minh);
+        minw = mw = qMax(minw, 0);
+        minh = mh = qMax(minh, 0);
+    }
+    createExtra();
     if (extra->minw == mw && extra->minh == mh)
         return false;
     extra->minw = mw;