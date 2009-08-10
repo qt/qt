eeb206d41d386d4d3dee1ff2813d132d15f670d0
Trond Kjernåsen <trond@trolltech.com>
2009-08-10 16:16:16 +0200

    Compile and work on Linux.
    Reviewed-by: Kim

--- tests/arthur/lance/main.cpp
+++ tests/arthur/lance/main.cpp
@@ -271,7 +271,7 @@ int main(int argc, char **argv)
                 QString format = QString(argv[++i]).toLower();
 
                 imageFormat = QImage::Format_Invalid;
-                static const unsigned int formatCount =
+                static const int formatCount =
                     sizeof(imageFormats) / sizeof(imageFormats[0]);
                 for (int ff = 0; ff < formatCount; ++ff) {
                     if (QLatin1String(imageFormats[ff].name) == format) {
