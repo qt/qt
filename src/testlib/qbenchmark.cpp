qt/qt

commit 7aa2d76dfc4a107f38c5cb0aac00a0b31d0fbbb6

Add QBENCMARK_ONCE to QTestLib.
The code block associated with QBENCHMARK_ONCE macro will only be executed once,
whether the backend returns a valid result or not. The "-iterations" command line
argument is also ignored.

This is useful for benchmarking code that has side effects.

Revby: jasplin

src/testlib/qbenchmark.cpp
@@ -116,7 +116,7 @@ int QBenchmarkGlobalData::adjustMedianIterationCount()
 QBenchmarkTestMethodData *QBenchmarkTestMethodData::current;
 
 QBenchmarkTestMethodData::QBenchmarkTestMethodData()
-:resultAccepted(false), iterationCount(-1)
+:resultAccepted(false), runOnce(false), iterationCount(-1)
 {
    
 }
@@ -157,6 +157,11 @@ void QBenchmarkTestMethodData::setResult(qint64 value)
     if (QBenchmarkGlobalData::current->iterationCount != -1)
         accepted = true;
 
+    if (QBenchmarkTestMethodData::current->runOnce) {
+        iterationCount = 1;
+        accepted = true;
+    }
+    
     // Test the result directly without calling the measurer if the minimum time 
     // has been specifed on the command line with -minimumvalue.
     else if (QBenchmarkGlobalData::current->walltimeMinimum != -1)
@@ -179,10 +184,12 @@ void QBenchmarkTestMethodData::setResult(qint64 value)
     drive the benchmarking loop. It is repsonsible for starting and stopping
     the timing measurements as well as calling the result reporting functions.
 */
-QTest::QBenchmarkIterationController::QBenchmarkIterationController()
+QTest::QBenchmarkIterationController::QBenchmarkIterationController(RunMode runMode)
 {
     QTest::beginBenchmarkMeasurement();
     i = 0;
+    if (runMode == RunOnce)
+        QBenchmarkTestMethodData::current->runOnce = true;    
 }
 /*! \internal
 */
@@ -195,6 +202,8 @@ QTest::QBenchmarkIterationController::~QBenchmarkIterationController()
 */
 bool QTest::QBenchmarkIterationController::isDone()
 {
+    if (QBenchmarkTestMethodData::current->runOnce)
+        return i > 0;
     return i >= QTest::iterationCount();
 }
 

src/testlib/qbenchmark.h
@@ -64,7 +64,8 @@ namespace QTest
 class Q_TESTLIB_EXPORT QBenchmarkIterationController
 {
 public:
-    QBenchmarkIterationController();
+    enum RunMode { RepeatUntilValidMeasurement, RunOnce };
+    QBenchmarkIterationController(RunMode runMode);
     ~QBenchmarkIterationController();
     bool isDone();
     void next();
@@ -74,7 +75,12 @@ public:
 }
 
 #define QBENCHMARK \
-    for (QTest::QBenchmarkIterationController __iteration_controller; __iteration_controller.isDone() == false; __iteration_controller.next())
+    for (QTest::QBenchmarkIterationController __iteration_controller(QTest::QBenchmarkIterationController::RepeatUntilValidMeasurement); \
+            __iteration_controller.isDone() == false; __iteration_controller.next())
+
+#define QBENCHMARK_ONCE \
+    for (QTest::QBenchmarkIterationController __iteration_controller(QTest::QBenchmarkIterationController::RunOnce); \
+            __iteration_controller.isDone() == false; __iteration_controller.next())
 
 QT_END_NAMESPACE
 

src/testlib/qbenchmark_p.h
@@ -171,6 +171,7 @@ public:
 
     QBenchmarkResult result;
     bool resultAccepted;
+    bool runOnce;
     int iterationCount;
 };
 

src/testlib/qtestcase.cpp
@@ -351,6 +351,25 @@ QT_BEGIN_NAMESPACE
         {Chapter 5: Writing a Benchmark}{Writing a Benchmark}
 */
 
+/*!
+    \macro QBENCHMARK_ONCE
+
+    \relates QTest
+
+    This macro is used to measure the performance of code within a test.
+    The code to be benchmarked is contained within a code block following
+    this macro.
+
+    Unlike QBENCHMARK, the contents of the contained code block is only run
+    once. The elapsed time will be reported as "0" if it's to short to 
+    be measured by the selected backend. (Use)
+ 
+    \sa {QTestLib Manual#Creating a Benchmark}{Creating a Benchmark},
+    {Chapter 5: Writing a Benchmark}{Writing a Benchmark}
+*/
+
+
+
 /*! \enum QTest::SkipMode
 
     This enum describes the modes for skipping tests during execution

tests/auto/selftests/benchlibwalltime/tst_benchlibwalltime.cpp
@@ -50,6 +50,7 @@ class tst_BenchlibWalltime: public QObject
 private slots:
     void waitForOneThousand();
     void waitForFourThousand();
+    void qbenchmark_once();
 };
 
 void tst_BenchlibWalltime::waitForOneThousand()
@@ -65,6 +66,16 @@ void tst_BenchlibWalltime::waitForFourThousand()
         QTest::qWait(4000);
     }
 }
+
+void tst_BenchlibWalltime::qbenchmark_once()
+{
+    int iterations = 0;
+    QBENCHMARK_ONCE {
+        ++iterations;
+    }
+    QCOMPARE(iterations, 1);
+}
+
 
 QTEST_MAIN(tst_BenchlibWalltime)
 

tests/auto/selftests/expected_benchlibwalltime.txt
@@ -7,6 +7,9 @@ PASS   : tst_BenchlibWalltime::waitForOneThousand()
 RESULT : tst_BenchlibWalltime::waitForFourThousand():
      4,000 msec per iteration (total: 4000, iterations: 1)
 PASS   : tst_BenchlibWalltime::waitForFourThousand()
+RESULT : tst_BenchlibWalltime::qbenchmark_once():
+     0 msec per iteration (total: 0, iterations: 1)
+PASS   : tst_BenchlibWalltime::qbenchmark_once()
 PASS   : tst_BenchlibWalltime::cleanupTestCase()
-Totals: 4 passed, 0 failed, 0 skipped
+Totals: 5 passed, 0 failed, 0 skipped
 ********* Finished testing of tst_BenchlibWalltime *********

commit 73b61029b00e12f313f9097d665539792d53e885

Fix BC breakage caused by commit 7aa2d76d.
Code compiled with 4.5 expects to find the QBenchmarkIterationController()
constructor, so ad an overload instead of replacing it.

src/testlib/qbenchmark.cpp
@@ -197,6 +197,13 @@ QTest::QBenchmarkIterationController::QBenchmarkIterationController(RunMode runM
     if (runMode == RunOnce)
         QBenchmarkTestMethodData::current->runOnce = true;    
 }
+
+QTest::QBenchmarkIterationController::QBenchmarkIterationController()
+{
+    QTest::beginBenchmarkMeasurement();
+    i = 0;
+}
+
 /*! \internal
 */
 QTest::QBenchmarkIterationController::~QBenchmarkIterationController()

src/testlib/qbenchmark.h
@@ -65,6 +65,7 @@ class Q_TESTLIB_EXPORT QBenchmarkIterationController
 {
 public:
     enum RunMode { RepeatUntilValidMeasurement, RunOnce };
+    QBenchmarkIterationController();
     QBenchmarkIterationController(RunMode runMode);
     ~QBenchmarkIterationController();
     bool isDone();
@@ -75,7 +76,7 @@ public:
 }
 
 #define QBENCHMARK \
-    for (QTest::QBenchmarkIterationController __iteration_controller(QTest::QBenchmarkIterationController::RepeatUntilValidMeasurement); \
+    for (QTest::QBenchmarkIterationController __iteration_controller(); \
             __iteration_controller.isDone() == false; __iteration_controller.next())
 
 #define QBENCHMARK_ONCE \