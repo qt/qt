qt/qt

commit a6782030bc6077b3b1ffe380dfd303cfb7662795

Fix warnings for mingw
did a small refactor and used QStyleHelper::uniqueName in plastique and
windows styles

src/gui/styles/qwindowsvistastyle.cpp
@@ -735,7 +735,7 @@ void QWindowsVistaStyle::drawPrimitive(PrimitiveElement element, const QStyleOpt
             if (const QListView *listview = qobject_cast<const QListView *>(widget)) {
                 if (listview->viewMode() == QListView::IconMode)
                     newStyle = true;
-            } else if (const QTreeView* treeview = qobject_cast<const QTreeView *>(widget)) {
+            } else if (qobject_cast<const QTreeView *>(widget)) {
                 newStyle = true;
             }
             if (newStyle && view && (vopt = qstyleoption_cast<const QStyleOptionViewItemV4 *>(option))) {
@@ -1093,7 +1093,7 @@ void QWindowsVistaStyle::drawControl(ControlElement element, const QStyleOption 
 
             XPThemeData theme(widget, painter, QLatin1String("PROGRESS"), vertical ? PP_FILLVERT : PP_FILL);
             theme.rect = option->rect;
-            bool reverse = bar->direction == Qt::LeftToRight && inverted || bar->direction == Qt::RightToLeft && !inverted;
+            bool reverse = bar->direction == (Qt::LeftToRight && inverted) || (bar->direction == Qt::RightToLeft && !inverted);
             QTime current = QTime::currentTime();
 
             if (isIndeterminate) {
@@ -1271,7 +1271,7 @@ void QWindowsVistaStyle::drawControl(ControlElement element, const QStyleOption 
                 int yoff = y-2 + h / 2;
                 QPoint p1 = QPoint(x + checkcol, yoff);
                 QPoint p2 = QPoint(x + w + 6 , yoff);
-                int stateId = stateId = MBI_HOT;
+                stateId = MBI_HOT;
                 QRect subRect(p1.x(), p1.y(), p2.x() - p1.x(), 6);
                 subRect  = QStyle::visualRect(option->direction, option->rect, subRect );
                 XPThemeData theme2(widget, painter, QLatin1String("MENU"), MENU_POPUPSEPARATOR, stateId, subRect);
@@ -1283,7 +1283,7 @@ void QWindowsVistaStyle::drawControl(ControlElement element, const QStyleOption 
                                           menuitem->rect.y(), checkcol - 6, menuitem->rect.height()));
 
             if (act) {
-                int stateId = stateId = MBI_HOT;
+                stateId = MBI_HOT;
                 XPThemeData theme2(widget, painter, QLatin1String("MENU"), MENU_POPUPITEM, stateId, option->rect);
                 d->drawBackground(theme2);
             }
@@ -1403,7 +1403,7 @@ void QWindowsVistaStyle::drawControl(ControlElement element, const QStyleOption 
         break;
     case CE_MenuBarEmptyArea:
         {
-            int stateId = MBI_NORMAL;
+            stateId = MBI_NORMAL;
             if (!(state & State_Enabled))
                 stateId = MBI_DISABLED;
             XPThemeData theme(widget, painter, QLatin1String("MENU"), MENU_BARBACKGROUND, stateId, option->rect);
@@ -1500,7 +1500,7 @@ void QWindowsVistaStyle::drawControl(ControlElement element, const QStyleOption 
             if (const QListView *listview = qobject_cast<const QListView *>(widget)) {
                 if (listview->viewMode() == QListView::IconMode)
                     newStyle = true;
-            } else if (const QTreeView* treeview = qobject_cast<const QTreeView *>(widget)) {
+            } else if (qobject_cast<const QTreeView *>(widget)) {
                 newStyle = true;
             }
             if (newStyle && view && (vopt = qstyleoption_cast<const QStyleOptionViewItemV4 *>(option))) {
@@ -2014,7 +2014,7 @@ QRect QWindowsVistaStyle::subElementRect(SubElement element, const QStyleOption 
             MARGINS borderSize;
             HTHEME theme = pOpenThemeData(widget ? QWindowsVistaStylePrivate::winId(widget) : 0, L"Button");
             if (theme) {
-                int stateId;
+                int stateId = PBS_NORMAL;
                 if (!(option->state & State_Enabled))
                     stateId = PBS_DISABLED;
                 else if (option->state & State_Sunken)
@@ -2023,8 +2023,6 @@ QRect QWindowsVistaStyle::subElementRect(SubElement element, const QStyleOption 
                     stateId = PBS_HOT;
                 else if (btn->features & QStyleOptionButton::DefaultButton)
                     stateId = PBS_DEFAULTED;
-                else
-                    stateId = PBS_NORMAL;
 
                 int border = proxy()->pixelMetric(PM_DefaultFrameWidth, btn, widget);
                 rect = option->rect.adjusted(border, border, -border, -border);
@@ -2097,7 +2095,7 @@ QRect QWindowsVistaStyle::subElementRect(SubElement element, const QStyleOption 
         rect = QCommonStyle::subElementRect(SE_ProgressBarGroove, option, widget);
         break;
     case SE_ItemViewItemDecoration:
-        if (const QStyleOptionViewItemV4 *vopt = qstyleoption_cast<const QStyleOptionViewItemV4 *>(option))
+        if (qstyleoption_cast<const QStyleOptionViewItemV4 *>(option))
             rect.adjust(-2, 0, 2, 0);
         break;
     case SE_ItemViewItemFocusRect:
@@ -2302,6 +2300,8 @@ QRect QWindowsVistaStyle::subControlRect(ComplexControl control, const QStyleOpt
                     rect.translate(0, 3);
                     rect = visualRect(option->direction, option->rect, rect);
                 }
+                break;
+            default:
                 break;
             }
         }

src/gui/dialogs/qwizard.cpp
@@ -343,8 +343,8 @@ void QWizardHeader::setup(const QWizardLayoutInfo &info, const QString &title,
 {
     bool modern = ((info.wizStyle == QWizard::ModernStyle)
 #if !defined(QT_NO_STYLE_WINDOWSVISTA)
-        || ((info.wizStyle == QWizard::AeroStyle)
-            && (QVistaHelper::vistaState() == QVistaHelper::Classic) || vistaDisabled())
+        || ((info.wizStyle == QWizard::AeroStyle
+            && QVistaHelper::vistaState() == QVistaHelper::Classic) || vistaDisabled())
 #endif
     );
 

src/gui/dialogs/qwizard_win.cpp
@@ -616,7 +616,7 @@ bool QVistaHelper::drawTitleText(QPainter *painter, const QString &text, const Q
         // Set up a memory DC and bitmap that we'll draw into
         HDC dcMem;
         HBITMAP bmp;
-        BITMAPINFO dib = {0};
+        BITMAPINFO dib = {{0}};
         dcMem = CreateCompatibleDC(hdc);
 
         dib.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
@@ -662,7 +662,7 @@ bool QVistaHelper::drawBlackRect(const QRect &rect, HDC hdc)
         // Set up a memory DC and bitmap that we'll draw into
         HDC dcMem;
         HBITMAP bmp;
-        BITMAPINFO dib = {0};
+        BITMAPINFO dib = {{0}};
         dcMem = CreateCompatibleDC(hdc);
 
         dib.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);

src/gui/kernel/qapplication_win.cpp
@@ -275,6 +275,8 @@ extern HRGN qt_tryCreateRegion(QRegion::RegionType type, int left, int top, int 
 #define GET_XBUTTON_WPARAM(wParam)      (HIWORD(wParam))
 #define XBUTTON1      0x0001
 #define XBUTTON2      0x0002
+#endif
+#ifndef MK_XBUTTON1
 #define MK_XBUTTON1         0x0020
 #define MK_XBUTTON2         0x0040
 #endif
@@ -807,9 +809,10 @@ void qt_init(QApplicationPrivate *priv, int)
         ptrUpdateLayeredWindowIndirect = qt_updateLayeredWindowIndirect;
 
     // Notify Vista and Windows 7 that we support highter DPI settings
-    if (ptrSetProcessDPIAware = (PtrSetProcessDPIAware)
-        QLibrary::resolve(QLatin1String("user32"), "SetProcessDPIAware"))
-    ptrSetProcessDPIAware();
+    ptrSetProcessDPIAware = (PtrSetProcessDPIAware)
+        QLibrary::resolve(QLatin1String("user32"), "SetProcessDPIAware");
+    if (ptrSetProcessDPIAware)
+        ptrSetProcessDPIAware();
 #endif
 
     priv->lastGestureId = 0;
@@ -1351,13 +1354,13 @@ static int inputcharset = CP_ACP;
 
 static bool qt_is_translatable_mouse_event(UINT message)
 {
-    return (message >= WM_MOUSEFIRST && message <= WM_MOUSELAST ||
-                message >= WM_XBUTTONDOWN && message <= WM_XBUTTONDBLCLK)
+    return (((message >= WM_MOUSEFIRST && message <= WM_MOUSELAST) ||
+                (message >= WM_XBUTTONDOWN && message <= WM_XBUTTONDBLCLK))
             && message != WM_MOUSEWHEEL
-            && message != WM_MOUSEHWHEEL
+            && message != WM_MOUSEHWHEEL)
 
 #ifndef Q_WS_WINCE
-            || message >= WM_NCMOUSEMOVE && message <= WM_NCMBUTTONDBLCLK
+            || (message >= WM_NCMOUSEMOVE && message <= WM_NCMBUTTONDBLCLK)
 #endif
             ;
 }
@@ -2601,7 +2604,7 @@ bool qt_try_modal(QWidget *widget, MSG *msg, int& ret)
 
     bool block_event = false;
 #ifndef Q_WS_WINCE
-    if (type != WM_NCHITTEST)
+    if (type != WM_NCHITTEST) {
 #endif
         if ((type >= WM_MOUSEFIRST && type <= WM_MOUSELAST) ||
              type == WM_MOUSEWHEEL || type == WM_MOUSEHWHEEL ||
@@ -2631,17 +2634,18 @@ bool qt_try_modal(QWidget *widget, MSG *msg, int& ret)
             block_event = true;
         }
 #ifndef Q_WS_WINCE
-    else if (type == WM_MOUSEACTIVATE || type == WM_NCLBUTTONDOWN){
-        if (!top->isActiveWindow()) {
-            top->activateWindow();
-        } else {
-            QApplication::beep();
-        }
-        block_event = true;
-        ret = MA_NOACTIVATEANDEAT;
-    } else if (type == WM_SYSCOMMAND) {
-        if (!(msg->wParam == SC_RESTORE && widget->isMinimized()))
+        else if (type == WM_MOUSEACTIVATE || type == WM_NCLBUTTONDOWN){
+            if (!top->isActiveWindow()) {
+                top->activateWindow();
+            } else {
+                QApplication::beep();
+            }
             block_event = true;
+            ret = MA_NOACTIVATEANDEAT;
+        } else if (type == WM_SYSCOMMAND) {
+            if (!(msg->wParam == SC_RESTORE && widget->isMinimized()))
+                block_event = true;
+        }
     }
 #endif
 
@@ -2843,7 +2847,7 @@ void qt_win_eatMouseMove()
     // remove all those messages (usually 1) and post the last one with a
     // reset button state
 
-    MSG msg = {0, 0, 0, 0, 0, 0, 0};
+    MSG msg = {0, 0, 0, 0, 0, {0, 0} };
     while (PeekMessage(&msg, 0, WM_MOUSEMOVE, WM_MOUSEMOVE, PM_REMOVE))
         ;
     if (msg.message == WM_MOUSEMOVE)

src/gui/kernel/qkeymapper_win.cpp
@@ -851,8 +851,8 @@ bool QKeyMapperPrivate::translateKeyEvent(QWidget *widget, const MSG &msg, bool 
                     }
                 } else if (msgType == WM_KEYUP) {
                     if (dirStatus == VK_LSHIFT
-                        && (msg.wParam == VK_SHIFT && GetKeyState(VK_LCONTROL)
-                        || msg.wParam == VK_CONTROL && GetKeyState(VK_LSHIFT))) {
+                        && ((msg.wParam == VK_SHIFT && GetKeyState(VK_LCONTROL))
+                        || (msg.wParam == VK_CONTROL && GetKeyState(VK_LSHIFT)))) {
                             k0 = q->sendKeyEvent(widget, grab, QEvent::KeyPress, Qt::Key_Direction_L, 0,
                                                  QString(), false, 0,
                                                  scancode, msg.wParam, nModifiers);
@@ -861,8 +861,8 @@ bool QKeyMapperPrivate::translateKeyEvent(QWidget *widget, const MSG &msg, bool 
                                                  scancode, msg.wParam, nModifiers);
                             dirStatus = 0;
                         } else if (dirStatus == VK_RSHIFT
-                                   && (msg.wParam == VK_SHIFT && GetKeyState(VK_RCONTROL)
-                                   || msg.wParam == VK_CONTROL && GetKeyState(VK_RSHIFT))) {
+                                   && ( (msg.wParam == VK_SHIFT && GetKeyState(VK_RCONTROL))
+                                   || (msg.wParam == VK_CONTROL && GetKeyState(VK_RSHIFT)))) {
                                 k0 = q->sendKeyEvent(widget, grab, QEvent::KeyPress, Qt::Key_Direction_R,
                                                      0, QString(), false, 0,
                                                      scancode, msg.wParam, nModifiers);

src/gui/kernel/qmime_win.cpp
@@ -170,7 +170,7 @@ static QByteArray getData(int cf, IDataObject *pDataObj)
         if (pDataObj->GetData(&formatetc, &s) == S_OK) {
             char szBuffer[4096];
             ULONG actualRead = 0;
-            LARGE_INTEGER pos = {0, 0};
+            LARGE_INTEGER pos = {{0, 0}};
             //Move to front (can fail depending on the data model implemented)
             HRESULT hr = s.pstm->Seek(pos, STREAM_SEEK_SET, NULL);
             while(SUCCEEDED(hr)){

src/gui/painting/qdrawutil.cpp
@@ -1008,8 +1008,7 @@ void qDrawItem(QPainter *p, Qt::GUIStyle gs,
                 ;
 #ifndef QT_NO_IMAGE_HEURISTIC_MASK
             } else {                                // color pixmap, no mask
-                QString k;
-                k.sprintf("$qt-drawitem-%llx", pm.cacheKey());
+                QString k = QString::fromLatin1("$qt-drawitem-%1").arg(pm.cacheKey());
                 if (!QPixmapCache::find(k, pm)) {
                     pm = pm.createHeuristicMask();
                     pm.setMask((QBitmap&)pm);

src/gui/painting/qdrawutil.h
@@ -133,7 +133,7 @@ Q_GUI_EXPORT QT3_SUPPORT void qDrawArrow(QPainter *p, Qt::ArrowType type, Qt::GU
                           const QPalette &pal, bool enabled);
 #endif
 
-struct Q_GUI_EXPORT QMargins
+struct QMargins
 {
     inline QMargins(int margin = 0)
         : top(margin),
@@ -151,7 +151,7 @@ struct Q_GUI_EXPORT QMargins
     int right;
 };
 
-struct Q_GUI_EXPORT QTileRules
+struct QTileRules
 {
     inline QTileRules(Qt::TileRule horizontalRule, Qt::TileRule verticalRule = Qt::Stretch)
             : horizontal(horizontalRule), vertical(verticalRule) {}
@@ -168,8 +168,7 @@ Q_GUI_EXPORT void qDrawBorderPixmap(QPainter *painter,
                                     const QRect &sourceRect, 
                                     const QMargins &sourceMargins, 
                                     const QTileRules &rules = QTileRules());
-
-Q_GUI_EXPORT inline void qDrawBorderPixmap(QPainter *painter, 
+inline void qDrawBorderPixmap(QPainter *painter, 
                                            const QRect &target, 
                                            const QMargins &margins, 
                                            const QPixmap &pixmap)

commit dcdbb81f28bc8fc4bb7fedf78421e473413da1a7

Fix reversed progress bars on Vista
This issue seems to be caused by change a6782030 and was
caused by incorrectly applying paranthesis to the expression.

Task-number: 259515
Reviewed-by: joao

src/gui/styles/qwindowsvistastyle.cpp
@@ -1094,7 +1094,7 @@ void QWindowsVistaStyle::drawControl(ControlElement element, const QStyleOption 
 
             XPThemeData theme(widget, painter, QLatin1String("PROGRESS"), vertical ? PP_FILLVERT : PP_FILL);
             theme.rect = option->rect;
-            bool reverse = bar->direction == (Qt::LeftToRight && inverted) || (bar->direction == Qt::RightToLeft && !inverted);
+            bool reverse = (bar->direction == Qt::LeftToRight && inverted) || (bar->direction == Qt::RightToLeft && !inverted);
             QTime current = QTime::currentTime();
 
             if (isIndeterminate) {