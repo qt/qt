39accaec06629da15c073859660fd94c78f1b261
Matthew Cattell <matthew.cattell@nokia.com>
2009-08-11 16:32:32 +0200

    Revert "Frame property for QDateTimeEdit can be set independently of"
    This reverts commit 6844dea0cb583a86bc72e7f008720ab76deef040.
    
    added to wrong branch. should be in 4.6 but this was added to 4.5

--- src/gui/widgets/qdatetimeedit.cpp
+++ src/gui/widgets/qdatetimeedit.cpp
@@ -2278,7 +2278,6 @@ void QDateTimeEdit::paintEvent(QPaintEvent *event)
 
     optCombo.init(this);
     optCombo.editable = true;
-	optCombo.frame = opt.frame;
     optCombo.subControls = opt.subControls;
     optCombo.activeSubControls = opt.activeSubControls;
     optCombo.state = opt.state;
