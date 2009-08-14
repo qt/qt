c57c204a63168e875e513d299bb152c0a1ab0094
Warwick Allison <warwick.allison@nokia.com>
2009-08-14 16:56:46 +1000

    Fix MLS test "sm"+ellipsis (...) is same length as "small" in some fonts

--- tests/auto/qfontmetrics/tst_qfontmetrics.cpp
+++ tests/auto/qfontmetrics/tst_qfontmetrics.cpp
@@ -218,8 +218,9 @@ void tst_QFontMetrics::elidedMultiLength()
     QCOMPARE(fm.elidedText(text1,Qt::ElideRight, width_short), text1_short);
     QCOMPARE(fm.elidedText(text1,Qt::ElideRight, width_short - 1), text1_small);
 
+    // Not even wide enough for "small" - should use ellipsis
     QChar ellipsisChar(0x2026);
-    QString text1_el = QString::fromLatin1("sm") + ellipsisChar;
+    QString text1_el = QString::fromLatin1("s") + ellipsisChar;
     int width_small = fm.width(text1_el);
     QCOMPARE(fm.elidedText(text1,Qt::ElideRight, width_small + 1), text1_el);
 
