774543a3336841df4a13d3e283af83cf4b53b966
Denis Dzyubenko <denis.dzyubenko@nokia.com>
2009-08-19 11:35:24 +0200

    Improved the documentation of saving and restoring window geometry.
    Mentioned in the doc that the preferred way to save/restore a geometry
    of a QMainWindow is to use both saveGeometry() and saveState().
    
    Reviewed-by: Kavindra Devi Palaraja

---
+++ doc/src/snippets/code/src_gui_widgets_qmainwindow.cpp
@@ -0,0 +1,19 @@
+//! [0]
+void MyMainWindow::closeEvent(QCloseEvent *event)
+{
+    QSettings settings("MyCompany", "MyApp");
+    settings.setValue("geometry", saveGeometry());
+    settings.setValue("windowState", saveState());
+    QMainWindow::closeEvent(event);
+}
+//! [0]
+
+
+//! [1]
+void MainWindow::readSettings()
+{
+    QSettings settings("MyCompany", "MyApp");
+    restoreGeometry(settings.value("myWidget/geometry").toByteArray());
+    restoreState(settings.value("myWidget/windowState").toByteArray());
+}
+//! [1]
