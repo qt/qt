qt/qt

commit 9d639659f2334bb5f3ea9b16b23bcb0127752f64

Fix broken drop shadow pixmap filter on X11.
Xrender is not rocket science when it comes to composition modes, so we
have to use the raster engine for the fill. toImage/fromImage is a no-op
on QWS and Windows, and on other platforms it is required, so we don't
have to add any ifdefs or clever checks.

Reviewed-by: Gunnar

src/gui/image/qpixmapfilter.cpp
@@ -1047,7 +1047,7 @@ void QPixmapDropShadowFilter::draw(QPainter *p,
 {
     Q_D(const QPixmapDropShadowFilter);
 
-    QPixmap tmp = src.isNull() ? px : px.copy(src.toRect());
+    QImage tmp = src.isNull() ? px : px.copy(src.toRect()).toImage();
     QPainter tmpPainter(&tmp);
 
     // blacken the image...
@@ -1055,8 +1055,10 @@ void QPixmapDropShadowFilter::draw(QPainter *p,
     tmpPainter.fillRect(0, 0, tmp.width(), tmp.height(), d->color);
     tmpPainter.end();
 
+    const QPixmap pixTmp = QPixmap::fromImage(tmp);
+
     // draw the blurred drop shadow...
-    d->convolution->draw(p, pos + d->offset, tmp);
+    d->convolution->draw(p, pos + d->offset, pixTmp);
 
     // Draw the actual pixmap...
     p->drawPixmap(pos, px, src);

commit a022a65ca9f4c073c9490eb3d0831fa9d954c3e5

Doh, we have to convert the pixmap to an image.
Broke after 9d639659f2334bb5f3ea9b16b23bcb0127752f64

Reviewed-by: TrustMe

src/gui/image/qpixmapfilter.cpp
@@ -1033,7 +1033,7 @@ void QPixmapDropShadowFilter::draw(QPainter *p,
 {
     Q_D(const QPixmapDropShadowFilter);
 
-    QImage tmp = src.isNull() ? px : px.copy(src.toRect()).toImage();
+    QImage tmp = src.isNull() ? px.toImage() : px.copy(src.toRect()).toImage();
     QPainter tmpPainter(&tmp);
 
     // blacken the image...