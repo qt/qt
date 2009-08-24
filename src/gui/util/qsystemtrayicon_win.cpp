e1309dcfe2b61beb7c3ab23a0250382043dc9168
Joerg Bornemann <joerg.bornemann@nokia.com>
2009-08-24 17:20:47 +0200

    qsystemtrayicon_win.cpp fix determination of NOTIFYICONDATA struct size
    Now, that we define _WIN32_WINNT as 0x0500, we have no access to guidItem
    in NOTIFYICONDATA. Thus, we cannot determine NOTIFYICONDATAW_V2_SIZE.
    But in this case, NOTIFYICONDATAW_V2_SIZE is the same as
    sizeof(NOTIFYICONDATA).
    On Windows CE exists only one version of this struct anyway.
    
    Reviewed-By: mauricek

--- src/gui/util/qsystemtrayicon_win.cpp
+++ src/gui/util/qsystemtrayicon_win.cpp
@@ -132,14 +132,10 @@ QSystemTrayIconSys::QSystemTrayIconSys(QSystemTrayIcon *object)
     : hIcon(0), q(object), ignoreNextMouseRelease(false)
 
 {
-#ifdef Q_OS_WINCE
-    notifyIconSize = FIELD_OFFSET(NOTIFYICONDATA, szTip[64]); // NOTIFYICONDATAW_V1_SIZE;
-    maxTipLength = 64;
-#elif WINVER <= 0x0500
     notifyIconSize = sizeof(NOTIFYICONDATA);
+#ifdef Q_OS_WINCE
     maxTipLength = 64;
 #else
-    notifyIconSize = FIELD_OFFSET(NOTIFYICONDATA, guidItem); // NOTIFYICONDATAW_V2_SIZE;
     maxTipLength = 128;
 #endif
 
