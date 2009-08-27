qt/qt

commit 2ec56d158dc140f68efb45e2e0613f0e4026ddf6

Fixed count(), itemAt() and removeAt() in QGraphicsLinearLayout.
Due to this, the behaviour of count(), itemAt() and removeAt() was
different between QGraphicsLinearLayout and QGraphicsGridLayout.
QGraphicsGridLayout does it right.

This adds some behaviour changes:
1. QGraphicsLinearLayout::count() is no longer affected by inserted
   stretches (through insertStretch)
  This means that code like this will break:

    QGraphicsLinearLayout *linearLayout = new QGraphicsLinearLayout;
    linearLayout->addItem(new QGraphicsWidget);
    linearLayout->addStretch();
    int count = linearLayout->count();
    linearLayout->removeAt(count - 1);
    // before this patch it would do nothing (and it wouldn't
    // actually remove the stretch), with the new patch it would
    // remove the QGraphicsWidget.

2. count(), itemAt() and removeAt() now prints a warning for an invalid
   index.
  The documentation actually says that "The reimplementation can assume
  that index is valid (i.e., it respects the value of count()", but I
  decided that its too risky to not assume that it is valid, since it
  would break the following common pattern:

    while(layout->itemAt(0)) {
      layout->removeAt(0);
    }

  Cleaned up autotests (and a small codeblock) that assumed
  itemAt/removeAt with an invalid index to work, since we should try to
  follow our own advice. :-)
  This change is also an alignment with what QGraphicsGridLayout does
  (it checks the index argument and prints a warning too.)

src/gui/graphicsview/qgraphicslinearlayout.cpp
@@ -323,7 +323,11 @@ void QGraphicsLinearLayout::removeItem(QGraphicsLayoutItem *item)
 void QGraphicsLinearLayout::removeAt(int index)
 {
     Q_D(QGraphicsLinearLayout);
-    if (QGridLayoutItem *gridItem = d->engine.itemAt(d->gridRow(index), d->gridColumn(index))) {
+    if (index < 0 || index >= d->engine.itemCount()) {
+        qWarning("QGraphicsLinearLayout::removeAt: invalid index %d", index);
+        return;
+    }
+    if (QGridLayoutItem *gridItem = d->engine.itemAt(index)) {
         if (QGraphicsLayoutItem *layoutItem = gridItem->layoutItem())
             layoutItem->setParentLayoutItem(0);
         d->removeGridItem(gridItem);
@@ -463,7 +467,7 @@ QSizePolicy::ControlTypes QGraphicsLinearLayout::controlTypes(LayoutSide side) c
 int QGraphicsLinearLayout::count() const
 {
     Q_D(const QGraphicsLinearLayout);
-    return d->engine.rowCount(d->orientation);
+    return d->engine.itemCount();
 }
 
 /*!
@@ -472,8 +476,12 @@ int QGraphicsLinearLayout::count() const
 QGraphicsLayoutItem *QGraphicsLinearLayout::itemAt(int index) const
 {
     Q_D(const QGraphicsLinearLayout);
+    if (index < 0 || index >= d->engine.itemCount()) {
+        qWarning("QGraphicsLinearLayout::itemAt: invalid index %d", index);
+        return 0;
+    }
     QGraphicsLayoutItem *item = 0;
-    if (QGridLayoutItem *gridItem = d->engine.itemAt(d->gridRow(index), d->gridColumn(index)))
+    if (QGridLayoutItem *gridItem = d->engine.itemAt(index))
         item = gridItem->layoutItem();
     return item;
 }

tests/auto/qgraphicslinearlayout/tst_qgraphicslinearlayout.cpp
@@ -210,17 +210,16 @@ void tst_QGraphicsLinearLayout::qgraphicslinearlayout()
     layout.setOrientation(Qt::Vertical);
     layout.orientation();
     QTest::ignoreMessage(QtWarningMsg, "QGraphicsLinearLayout::insertItem: cannot insert null item");
-    //QCOMPARE(layout.count(), 0);
+    QCOMPARE(layout.count(), 0);
     layout.addItem(0);
-    //QCOMPARE(layout.count(), 0);
+    QCOMPARE(layout.count(), 0);
     layout.addStretch(0);
-    //QCOMPARE(layout.count(), 1);
+    QCOMPARE(layout.count(), 0);
     QTest::ignoreMessage(QtWarningMsg, "QGraphicsLinearLayout::insertItem: cannot insert null item");
     layout.insertItem(0, 0);
     layout.insertStretch(0, 0);
     layout.removeItem(0);
-    layout.removeAt(0);
-    //QCOMPARE(layout.count(), 1);
+    QCOMPARE(layout.count(), 0);
     layout.setSpacing(0);
     layout.spacing();
     QTest::ignoreMessage(QtWarningMsg, "QGraphicsLinearLayout::setStretchFactor: cannot assign a stretch factor to a null item");
@@ -231,8 +230,7 @@ void tst_QGraphicsLinearLayout::qgraphicslinearlayout()
     QCOMPARE(layout.alignment(0), 0);
     layout.setGeometry(QRectF());
     layout.geometry();
-    //QCOMPARE(layout.count(), 1);
-    layout.itemAt(0);
+    QCOMPARE(layout.count(), 0);
     layout.invalidate();
     layout.sizeHint(Qt::MinimumSize, QSizeF());
 }
@@ -537,7 +535,10 @@ void tst_QGraphicsLinearLayout::insertItem()
     QSizeF oldSizeHint = layout.sizeHint(Qt::PreferredSize, QSizeF());
     layout.insertItem(insertItemAt, item);
     QCOMPARE(layout.count(), itemCount + layoutCount + 1);
-    QCOMPARE(layout.itemAt(insertItemAt), insertItemAt == -1 ? (QGraphicsLayoutItem*)0 : item);
+    
+    if (insertItemAt >= 0 && (itemCount + layoutCount >= 0)) {
+        QCOMPARE(layout.itemAt(itemCount + layoutCount), item);
+    }
 
     layout.activate();
     QSizeF newSizeHint = layout.sizeHint(Qt::PreferredSize, QSizeF());
@@ -599,8 +600,7 @@ void tst_QGraphicsLinearLayout::insertStretch()
     }
     widget->setLayout(layout);
     layout->insertStretch(insertItemAt, stretch);
-    QCOMPARE(layout->itemAt(insertItemAt), (QGraphicsLayoutItem*)0);
-    QCOMPARE(layout->count(), itemCount + layoutCount + 1);
+    QCOMPARE(layout->count(), itemCount + layoutCount);
 
     layout->activate();
     view.show();
@@ -669,7 +669,6 @@ void tst_QGraphicsLinearLayout::invalidate()
 void tst_QGraphicsLinearLayout::itemAt_data()
 {
     QTest::addColumn<int>("index");
-    QTest::newRow("-1") << -1;
     QTest::newRow("0") << 0;
     QTest::newRow("1") << 1;
     QTest::newRow("2") << 2;
@@ -681,7 +680,10 @@ void tst_QGraphicsLinearLayout::itemAt()
     // see also the insertItem() etc tests
     QFETCH(int, index);
     SubQGraphicsLinearLayout layout;
-    QCOMPARE(layout.itemAt(index), (QGraphicsLayoutItem*)0);
+    for (int i = 0; i < 3; ++i)
+        layout.addItem(new QGraphicsWidget);
+
+    QVERIFY(layout.itemAt(index) != 0);
 }
 
 void tst_QGraphicsLinearLayout::orientation_data()
@@ -779,19 +781,18 @@ void tst_QGraphicsLinearLayout::removeAt()
         layout.addItem(new SubQGraphicsLinearLayout);
     QSizeF oldSizeHint = layout.sizeHint(Qt::PreferredSize, QSizeF());
 
-    QGraphicsLayoutItem *w = layout.itemAt(removeItemAt);
-    QGraphicsLayoutItem *wParent = 0;
+    QGraphicsLayoutItem *w = 0;
+    if (removeItemAt >= 0 && removeItemAt < layout.count())
+        w = layout.itemAt(removeItemAt);
     if (w) {
-        wParent = w->parentLayoutItem();
+        QGraphicsLayoutItem *wParent = w->parentLayoutItem();
         QCOMPARE(wParent, static_cast<QGraphicsLayoutItem *>(&layout));
-    }
-    layout.removeAt(removeItemAt);
-    if (w) {
+        layout.removeAt(removeItemAt);
         wParent = w->parentLayoutItem();
         QCOMPARE(wParent, static_cast<QGraphicsLayoutItem *>(0));
+        delete w;
     }
-    delete w;
-    QCOMPARE(layout.count(), itemCount + layoutCount - ((removeItemAt == -1) ? 0 : 1));
+    QCOMPARE(layout.count(), itemCount + layoutCount - (w ? 1 : 0));
 
     layout.activate();
     QSizeF newSizeHint = layout.sizeHint(Qt::PreferredSize, QSizeF());
@@ -829,11 +830,15 @@ void tst_QGraphicsLinearLayout::removeItem()
     for (int i = 0; i < layoutCount; ++i)
         layout.addItem(new SubQGraphicsLinearLayout);
 
-    QGraphicsLayoutItem *w = layout.itemAt(removeItemAt);
+    QGraphicsLayoutItem *w = 0;
+    if (removeItemAt >= 0 && removeItemAt < layout.count())
+        w = layout.itemAt(removeItemAt);
     QSizeF oldSizeHint = layout.sizeHint(Qt::PreferredSize, QSizeF());
-    layout.removeItem(w);
-    delete w;
-    QCOMPARE(layout.count(), itemCount + layoutCount - ((removeItemAt == -1) ? 0 : 1));
+    if (w) {
+        layout.removeItem(w);
+        delete w;
+    }
+    QCOMPARE(layout.count(), itemCount + layoutCount - (w ? 1 : 0));
 
     layout.activate();
     QSizeF newSizeHint = layout.sizeHint(Qt::PreferredSize, QSizeF());

src/gui/graphicsview/qgraphicslayout_p.cpp
@@ -120,8 +120,8 @@ static bool removeLayoutItemFromLayout(QGraphicsLayout *lay, QGraphicsLayoutItem
     if (!lay)
         return false;
 
-    QGraphicsLayoutItem *child;
-    for (int i = 0; (child = lay->itemAt(i)); ++i) {
+    for (int i = lay->count() - 1; i >= 0; --i) {
+        QGraphicsLayoutItem *child = lay->itemAt(i);
         if (child && child->isLayout()) {
             if (removeLayoutItemFromLayout(static_cast<QGraphicsLayout*>(child), layoutItem))
                 return true;

tests/auto/qgraphicsgridlayout/tst_qgraphicsgridlayout.cpp
@@ -153,8 +153,11 @@ void tst_QGraphicsGridLayout::qgraphicsgridlayout()
     layout.columnStretchFactor(0);
     layout.count();
     layout.horizontalSpacing();
+    QTest::ignoreMessage(QtWarningMsg, "QGraphicsGridLayout::itemAt: invalid row, column 0, 0");
     layout.itemAt(0, 0);
+    QTest::ignoreMessage(QtWarningMsg, "QGraphicsGridLayout::itemAt: invalid index 0");
     layout.itemAt(0);
+    QTest::ignoreMessage(QtWarningMsg, "QGraphicsGridLayout::removeAt: invalid index 0");
     layout.removeAt(0);
     layout.rowAlignment(0);
     layout.rowCount();

commit 574fcf93bd5420a7e668466259de5c89b485fef2

Make sure itemAt() reflects the visual order.
This means it should respect the order that was defined with
insertItem() and addItem().
Note that this is not strictly necessary (as now explicitly written in
the docs for QGraphicsLayout::itemAt()), but commit
2ec56d158dc140f68efb45e2e0613f0e4026ddf6 broke the order and for people
that relied on this that commit caused a regression.

In addition, after commit 2ec56d158dc140f68efb45e2e0613f0e4026ddf6 it
was not longer possible to query the "item at visual index". Thus,
instead of adding another function (like QGGL::itemAt(int,int)) we make
sure that itemAt() also returns the "item at visual index".

src/gui/graphicsview/qgraphicslayout.cpp
@@ -390,8 +390,10 @@ void QGraphicsLayout::widgetEvent(QEvent *e)
     QGraphicsLayout to return a pointer to the item at index \a i. The
     reimplementation can assume that \a i is valid (i.e., it respects the
     value of count()).
+    Together with count(), it is provided as a means of iterating over all items in a layout.
 
-    The subclass is free to decide how to store the items.
+    The subclass is free to decide how to store the items, and the visual arrangement
+    does not have to be reflected through this function.
 
     \sa count(), removeAt()
 */

src/gui/graphicsview/qgraphicslinearlayout.cpp
@@ -277,7 +277,7 @@ void QGraphicsLinearLayout::insertItem(int index, QGraphicsLayoutItem *item)
     Q_ASSERT(item);
     d->fixIndex(&index);
     d->engine.insertRow(index, d->orientation);
-    new QGridLayoutItem(&d->engine, item, d->gridRow(index), d->gridColumn(index));
+    new QGridLayoutItem(&d->engine, item, d->gridRow(index), d->gridColumn(index), 1, 1, 0, index);
     invalidate();
 }
 
@@ -471,6 +471,7 @@ int QGraphicsLinearLayout::count() const
 
 /*!
     \reimp
+    When iterating from 0 and up, it will return the items in the visual arranged order.
 */
 QGraphicsLayoutItem *QGraphicsLinearLayout::itemAt(int index) const
 {

src/gui/graphicsview/qgridlayoutengine.cpp
@@ -461,7 +461,7 @@ void QGridLayoutRowData::dump(int indent) const
 
 QGridLayoutItem::QGridLayoutItem(QGridLayoutEngine *engine, QGraphicsLayoutItem *layoutItem,
                                  int row, int column, int rowSpan, int columnSpan,
-                                 Qt::Alignment alignment)
+                                 Qt::Alignment alignment, int itemAtIndex)
     : q_engine(engine), q_layoutItem(layoutItem), q_alignment(alignment)
 {
     q_firstRows[Hor] = column;
@@ -471,7 +471,7 @@ QGridLayoutItem::QGridLayoutItem(QGridLayoutEngine *engine, QGraphicsLayoutItem 
     q_stretches[Hor] = -1;
     q_stretches[Ver] = -1;
 
-    q_engine->addItem(this);
+    q_engine->insertItem(this, itemAtIndex);
 }
 
 int QGridLayoutItem::firstRow(Qt::Orientation orientation) const
@@ -937,11 +937,20 @@ Qt::Alignment QGridLayoutEngine::effectiveAlignment(const QGridLayoutItem *layou
     return align;
 }
 
-void QGridLayoutEngine::addItem(QGridLayoutItem *item)
+/*!
+    \internal
+    The \a index is only used by QGraphicsLinearLayout to ensure that itemAt() reflects the order
+    of visual arrangement. Strictly speaking it does not have to, but most people expect it to.
+    (And if it didn't we would have to add itemArrangedAt(int index) or something..)
+ */
+void QGridLayoutEngine::insertItem(QGridLayoutItem *item, int index)
 {
     maybeExpandGrid(item->lastRow(), item->lastColumn());
 
-    q_items.append(item);
+    if (index == -1)
+        q_items.append(item);
+    else
+        q_items.insert(index, item);
 
     for (int i = item->firstRow(); i <= item->lastRow(); ++i) {
         for (int j = item->firstColumn(); j <= item->lastColumn(); ++j) {
@@ -950,6 +959,11 @@ void QGridLayoutEngine::addItem(QGridLayoutItem *item)
             setItemAt(i, j, item);
         }
     }
+}
+
+void QGridLayoutEngine::addItem(QGridLayoutItem *item)
+{
+    insertItem(item, -1);
 }
 
 void QGridLayoutEngine::removeItem(QGridLayoutItem *item)

src/gui/graphicsview/qgridlayoutengine_p.h
@@ -267,7 +267,8 @@ class QGridLayoutItem
 {
 public:
     QGridLayoutItem(QGridLayoutEngine *engine, QGraphicsLayoutItem *layoutItem, int row, int column,
-                    int rowSpan = 1, int columnSpan = 1, Qt::Alignment alignment = 0);
+                    int rowSpan = 1, int columnSpan = 1, Qt::Alignment alignment = 0,
+                    int itemAtIndex = -1);
 
     inline int firstRow() const { return q_firstRows[Ver]; }
     inline int firstColumn() const { return q_firstRows[Hor]; }
@@ -378,6 +379,7 @@ public:
     Qt::Alignment effectiveAlignment(const QGridLayoutItem *layoutItem) const;
 
 
+    void insertItem(QGridLayoutItem *item, int index);
     void addItem(QGridLayoutItem *item);
     void removeItem(QGridLayoutItem *item);
     QGridLayoutItem *findLayoutItem(QGraphicsLayoutItem *layoutItem) const;

tests/auto/qgraphicslinearlayout/tst_qgraphicslinearlayout.cpp
@@ -76,6 +76,7 @@ private slots:
     void invalidate();
     void itemAt_data();
     void itemAt();
+    void itemAt_visualOrder();
     void orientation_data();
     void orientation();
     void removeAt_data();
@@ -537,7 +538,7 @@ void tst_QGraphicsLinearLayout::insertItem()
     QCOMPARE(layout.count(), itemCount + layoutCount + 1);
 
     if (insertItemAt >= 0 && (itemCount + layoutCount >= 0)) {
-        QCOMPARE(layout.itemAt(itemCount + layoutCount), item);
+        QCOMPARE(layout.itemAt(insertItemAt), item);
     }
 
     layout.activate();
@@ -684,6 +685,28 @@ void tst_QGraphicsLinearLayout::itemAt()
         layout.addItem(new QGraphicsWidget);
 
     QVERIFY(layout.itemAt(index) != 0);
+}
+
+void tst_QGraphicsLinearLayout::itemAt_visualOrder()
+{
+    QGraphicsLinearLayout *l = new QGraphicsLinearLayout;
+
+    QGraphicsWidget *w1 = new QGraphicsWidget;
+    l->addItem(w1);
+
+    QGraphicsWidget *w3 = new QGraphicsWidget;
+    l->addItem(w3);
+
+    QGraphicsWidget *w0 = new QGraphicsWidget;
+    l->insertItem(0, w0);
+
+    QGraphicsWidget *w2 = new QGraphicsWidget;
+    l->insertItem(2, w2);
+
+    QCOMPARE(l->itemAt(0), w0);
+    QCOMPARE(l->itemAt(1), w1);
+    QCOMPARE(l->itemAt(2), w2);
+    QCOMPARE(l->itemAt(3), w3);
 }
 
 void tst_QGraphicsLinearLayout::orientation_data()