qt/qt

commit 23c2aea7ce6379927609109e18c6252fbee68fbc

Fix gcc compiler warning about unsigned/signed integer comparison
gcc 4.4.3 with option "-Wall" complained about
  'comparison between signed and unsigned integer expressons'
in every code path using testFlag().

Reviewed-by: Frans Englich

src/corelib/global/qglobal.h
@@ -2170,7 +2170,7 @@ public:
 
     inline bool operator!() const { return !i; }
 
-    inline bool testFlag(Enum f) const { return (i & f) == f && (f != 0 || i == f ); }
+    inline bool testFlag(Enum f) const { return (i & f) == f && (f != 0 || i == (int)f ); }
 };
 
 #define Q_DECLARE_FLAGS(Flags, Enum)\

commit 43a45357ba22e6992b1543b8ece864343aa22e57

Fix tst_Moc::oldStyleCasts
The warning in qglobal.h made the test to fail
(because the purpose of the test is to test that moc doesn't produce
code that generates warnings with lots of warnings falgs on)

(Regression in rev 23c2aea7ce637992)

Reviewed-by: Gabriel de Dietrich

src/corelib/global/qglobal.h
@@ -2164,7 +2164,7 @@ public:
 
     inline bool operator!() const { return !i; }
 
-    inline bool testFlag(Enum f) const { return (i & f) == f && (f != 0 || i == (int)f ); }
+    inline bool testFlag(Enum f) const { return (i & f) == f && (f != 0 || i == int(f) ); }
 };
 
 #define Q_DECLARE_FLAGS(Flags, Enum)\