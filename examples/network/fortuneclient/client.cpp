5e3775ae4c5263a25e63868e8a3f16244e4dde02
Peter Hartmann <peter.hartmann@trolltech.com>
2009-09-03 10:33:34 +0200

    FortuneServer/Client example: fix displayed IP
    fortune server: listen to the first non-local IPv4 address found,
    fortune client: display that address as default in the line edit.
    
    Reviewed-by: Prasanth Ullattil
    Reviewed-by: Aleksandar Sasha Babic

--- examples/network/fortuneclient/client.cpp
+++ examples/network/fortuneclient/client.cpp
@@ -56,7 +56,20 @@ Client::Client(QWidget *parent)
     hostLabel = new QLabel(tr("&Server name:"));
     portLabel = new QLabel(tr("S&erver port:"));
 
-    hostLineEdit = new QLineEdit("Localhost");
+    // find out which IP to connect to
+    QString ipAddress;
+    QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
+    // use the first non-localhost IPv4 address
+    for (int i = 0; i < ipAddressesList.size(); ++i) {
+        if (ipAddressesList.at(i) != QHostAddress::LocalHost &&
+            ipAddressesList.at(i).toIPv4Address())
+            ipAddress = ipAddressesList.at(i).toString();
+    }
+    // if we did not find one, use IPv4 localhost
+    if (ipAddress.isEmpty())
+        ipAddress = QHostAddress(QHostAddress::LocalHost).toString();
+
+    hostLineEdit = new QLineEdit(ipAddress);
     portLineEdit = new QLineEdit;
     portLineEdit->setValidator(new QIntValidator(1, 65535, this));
 
