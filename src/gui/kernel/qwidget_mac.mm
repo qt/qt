aad2316995d46cb8a1bfc2763c0617b0c7c92307
Morten Sorvig <msorvig@trolltech.com>
2009-09-11 07:27:16 +0200

    Run on 10.3 Panther.
     HIViewSetNeedsDisplayInRect was added in 10.4.

--- src/gui/kernel/qwidget_mac.mm
+++ src/gui/kernel/qwidget_mac.mm
@@ -3781,7 +3781,10 @@ static void qt_mac_update_widget_posisiton(QWidget *q, QRect oldRect, QRect newR
         (oldRect.isValid() == false || newRect.isValid() == false)  ||
 
         // the position update is a part of a drag-and-drop operation
-        QDragManager::self()->object
+        QDragManager::self()->object || 
+        
+        // we are on Panther (no HIViewSetNeedsDisplayInRect) 
+        QSysInfo::MacintoshVersion < QSysInfo::MV_10_4 
     ){
         HIViewSetFrame(view, &bounds);
         return;
