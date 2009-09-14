qt/qt

commit 21cf7b4c4317421da4b7ba63473b639e33386ffc

QMenubar could popup a menu when undocking a toolbar
This is due to the fact that it receives a mousemoveevent.
But as it doesn't receive a mousepress event, we can detect whether it
should popup or not.

Task-number: 260873
Reviewed-by: prasanth

src/gui/widgets/qmenubar.cpp
@@ -1094,6 +1094,8 @@ void QMenuBar::mousePressEvent(QMouseEvent *e)
     if(e->button() != Qt::LeftButton)
         return;
 
+    d->mouseDown = true;
+
     QAction *action = d->actionAt(e->pos());
     if (!action || !d->isVisible(action)) {
         d->setCurrentAction(0);
@@ -1103,8 +1105,6 @@ void QMenuBar::mousePressEvent(QMouseEvent *e)
 #endif
         return;
     }
-
-    d->mouseDown = true;
 
     if(d->currentAction == action && d->popupState) {
         if(QMenu *menu = d->activeMenu) {
@@ -1252,10 +1252,11 @@ void QMenuBar::keyPressEvent(QKeyEvent *e)
 void QMenuBar::mouseMoveEvent(QMouseEvent *e)
 {
     Q_D(QMenuBar);
-    d->mouseDown = e->buttons() & Qt::LeftButton;
+    bool popupState = d->popupState || e->buttons() & Qt::LeftButton;
+    if (!d->mouseDown || !popupState)
+        return;
     QAction *action = d->actionAt(e->pos());
-    bool popupState = d->popupState || d->mouseDown;
-    if ((action && d->isVisible(action)) || !popupState)
+    if (action && d->isVisible(action))
         d->setCurrentAction(action, popupState);
 }
 

commit 41dbc4406791f82b7e3c1385acc594e22b5b0799

Fix QMenuBar hover effect.
Regression introduced by commit 21cf7b4c431742

Task-number: 261199
Reviewed-by: Prasanth

src/gui/widgets/qmenubar.cpp
@@ -1252,9 +1252,7 @@ void QMenuBar::keyPressEvent(QKeyEvent *e)
 void QMenuBar::mouseMoveEvent(QMouseEvent *e)
 {
     Q_D(QMenuBar);
-    bool popupState = d->popupState || e->buttons() & Qt::LeftButton;
-    if (!d->mouseDown || !popupState)
-        return;
+    bool popupState = d->popupState || d->mouseDown;
     QAction *action = d->actionAt(e->pos());
     if (action && d->isVisible(action))
         d->setCurrentAction(action, popupState);