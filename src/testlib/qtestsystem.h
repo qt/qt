bbcef4e4b596324aeda80e0aa7d313c586454740
Jan-Arve Sæther <jan-arve.saether@nokia.com>
2009-09-23 14:39:22 +0200

    Don't warn of an unused 'window' argument.

--- src/testlib/qtestsystem.h
+++ src/testlib/qtestsystem.h
@@ -76,8 +76,10 @@ namespace QTest
 #if defined(Q_WS_X11)
         qt_x11_wait_for_window_manager(window);
 #elif defined(Q_WS_QWS)
+        Q_UNUSED(window);
         qWait(100);
 #else
+        Q_UNUSED(window);
         qWait(50);
 #endif
         return true;
