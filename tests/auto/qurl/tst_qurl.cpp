af9cb619c2357e07def6b5ee76e4d5950e17d45b
Thiago Macieira <thiago.macieira@nokia.com>
2009-10-06 11:13:51 +0200

    Autotest: add missing copyright attribution for tests copied from kurltest (cherry picked from commit 65a101502bb04ea95110ce6e12a3848c790eb7a1)

--- tests/auto/qurl/tst_qurl.cpp
+++ tests/auto/qurl/tst_qurl.cpp
@@ -671,6 +671,26 @@ void tst_QUrl::setUrl()
     }
 
 /*
+   The tests below are copied from kdelibs/kdecore/tests/kurltest.cpp (an old version of)
+
+    Copyright (c) 1999-2005 Waldo Bastian <bastian@kde.org>
+    Copyright (c) 2000-2005 David Faure <faure@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+ */
+/*
     ### File / directory specifics
 
     KURL u2( QCString("/home/dfaure/") );
