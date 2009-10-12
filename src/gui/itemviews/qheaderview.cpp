dd24ba2bc9c6c25fe2e7ba3f308253ae7dfc4cb7
Olivier Goffart <ogoffart@trolltech.com>
2009-10-12 09:44:51 +0200

    Revert merge commit 3945fd75a93d790434b33c2d23a
    The resolution of conflicts introduced regressions. And the commit
    was already in 4.6

--- src/gui/itemviews/qheaderview.cpp
+++ src/gui/itemviews/qheaderview.cpp
@@ -524,8 +524,8 @@ QSize QHeaderView::sizeHint() const
     Q_D(const QHeaderView);
     if (d->cachedSizeHint.isValid())
         return d->cachedSizeHint;
+    d->cachedSizeHint = QSize(0, 0); //reinitialize the cached size hint
     const int sectionCount = count();
-    d->executePostedLayout();
 
     // get size hint for the first n sections
     int i = 0;
