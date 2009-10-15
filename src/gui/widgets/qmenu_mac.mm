qt/qt

commit faec535829a0e454a6784b0c5c37cb63e7da8f73

Application crashes when a menu is inserted twice on a menubar (Cocoa).
Cocoa does not allow NSMenu to have multiple supermenu's. If a menu is
added again as submenu, Qt will now disable the menu item or the menu
will not be added at all if it is added again to the menubar.

Task-number: 258822
Reviewed-by: MortenS

src/gui/widgets/qmenu_mac.mm
@@ -1412,7 +1412,15 @@ QMenuPrivate::QMacMenuPrivate::syncAction(QMacMenuAction *action)
         GetMenuItemProperty(action->menu, 0, kMenuCreatorQt, kMenuPropertyQWidget, sizeof(caused), 0, &caused);
         SetMenuItemProperty(data.submenuHandle, 0, kMenuCreatorQt, kMenuPropertyCausedQWidget, sizeof(caused), &caused);
 #else
-        [item setSubmenu:static_cast<NSMenu *>(action->action->menu()->macMenu())];
+        NSMenu *subMenu  = static_cast<NSMenu *>(action->action->menu()->macMenu());
+        if ([subMenu supermenu] != nil) {
+            // The menu is already a sub-menu of another one. Cocoa will throw an exception,
+            // in such cases. For the time being, a new QMenu with same set of actions is the
+            // only workaround.
+            action->action->setEnabled(false);
+        } else {
+            [item setSubmenu:subMenu];
+        }
 #endif
     } else { //respect some other items
 #ifndef QT_MAC_USE_COCOA
@@ -1678,7 +1686,10 @@ QMenuBarPrivate::QMacMenuBarPrivate::syncAction(QMacMenuAction *action)
             GetMenuItemProperty(action->menu, 0, kMenuCreatorQt, kMenuPropertyQWidget, sizeof(caused), 0, &caused);
             SetMenuItemProperty(submenu, 0, kMenuCreatorQt, kMenuPropertyCausedQWidget, sizeof(caused), &caused);
 #else
-            [item setSubmenu:submenu];
+            if ([submenu supermenu] != nil)
+                return;
+            else
+                [item setSubmenu:submenu];
 #endif
         }
 #ifndef QT_MAC_USE_COCOA

src/gui/widgets/qmenu.cpp
@@ -1302,6 +1302,11 @@ void QMenu::initStyleOption(QStyleOptionMenuItem *option, const QAction *action)
     do not support the signals: aboutToHide (), aboutToShow () and hovered ().
     It is not possible to display an icon in a native menu on Windows Mobile.
 
+    \section1 QMenu on Mac OS X with Qt build against Cocoa
+
+    QMenu can be inserted only once in a menu/menubar. Subsequent insertions will
+    have no effect or will result in a disabled menu item.
+
     See the \l{mainwindows/menus}{Menus} example for an example of how
     to use QMenuBar and QMenu in your application.
 

commit 2eb373a68367a6511e12d60034e920345431bcc8

Some sub menus are disabled in Cocoa
The commit faec535829a0e454a6784b0c5c37cb63e7da8f73 introduced this bug.
Since we can add a submenu to the same supermenu, we should consider it
before disabling the submenu who already have a supermenu.

Reviewed-by: MortenS

src/gui/widgets/qmenu_mac.mm
@@ -1413,7 +1413,7 @@ QMenuPrivate::QMacMenuPrivate::syncAction(QMacMenuAction *action)
         SetMenuItemProperty(data.submenuHandle, 0, kMenuCreatorQt, kMenuPropertyCausedQWidget, sizeof(caused), &caused);
 #else
         NSMenu *subMenu  = static_cast<NSMenu *>(action->action->menu()->macMenu());
-        if ([subMenu supermenu] != nil) {
+        if ([subMenu supermenu] && [subMenu supermenu] != [item menu]) {
             // The menu is already a sub-menu of another one. Cocoa will throw an exception,
             // in such cases. For the time being, a new QMenu with same set of actions is the
             // only workaround.
@@ -1686,7 +1686,7 @@ QMenuBarPrivate::QMacMenuBarPrivate::syncAction(QMacMenuAction *action)
             GetMenuItemProperty(action->menu, 0, kMenuCreatorQt, kMenuPropertyQWidget, sizeof(caused), 0, &caused);
             SetMenuItemProperty(submenu, 0, kMenuCreatorQt, kMenuPropertyCausedQWidget, sizeof(caused), &caused);
 #else
-            if ([submenu supermenu] != nil)
+            if ([submenu supermenu] && [submenu supermenu] != [item menu])
                 return;
             else
                 [item setSubmenu:submenu];