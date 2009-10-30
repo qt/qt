qt/qt

commit 68c0e6a8ba1e92bf0152adcaa86eebb83dcfd1d8

Move QMacStyle icon handling down to the common style.
This is more follow the cue of what is done on X11, mainly, if you are
creating things like messageboxes or file views, you want them to follow
the desktop (yes, you do). If you disable desktop settings aware, you
get the old look. This also meant shifting around some functions into
qt_cocoa_helpers_mac to make them more readily available instead of
living in differnt files. People who use standard pixmap get the old
values, but I think that's fine. If you haven't moved onto standardIcon
(introduced in 4.1), you don't get the latest bling.

Review-by: Jens Bache-Wiig

src/gui/styles/qcommonstyle.cpp
@@ -84,6 +84,8 @@
 
 #ifdef Q_WS_X11
 #   include <private/qt_x11_p.h>
+#elif defined(Q_WS_MAC)
+#   include <private/qt_cocoa_helpers_mac_p.h>
 #endif
 
 QT_BEGIN_NAMESPACE
@@ -4876,7 +4878,14 @@ int QCommonStyle::pixelMetric(PixelMetric m, const QStyleOption *opt, const QWid
         ret = int(QStyleHelper::dpiScaled(13.));
         break;
     case PM_MessageBoxIconSize:
-        ret = int(QStyleHelper::dpiScaled(32.));
+#ifdef Q_WS_MAC
+        if (QApplication::desktopSettingsAware()) {
+            ret = 64; // No DPI scaling, it's handled elsewhere.
+        } else
+#endif
+        {
+            ret = int(QStyleHelper::dpiScaled(32.));
+        }
         break;
     case PM_TextCursorWidth:
         ret = 1;
@@ -5774,9 +5783,9 @@ QIcon QCommonStyle::standardIconImplementation(StandardPixmap standardIcon, cons
                                                const QWidget *widget) const
 {
     QIcon icon;
-#ifdef Q_WS_X11
-    Q_D(const QCommonStyle);
     if (QApplication::desktopSettingsAware()) {
+#ifdef Q_WS_X11
+        Q_D(const QCommonStyle);
         d->lookupIconTheme();
         QPixmap pixmap;
         switch (standardIcon) {
@@ -5794,6 +5803,7 @@ QIcon QCommonStyle::standardIconImplementation(StandardPixmap standardIcon, cons
             }
         case SP_MessageBoxWarning:
             {
+                icon = d->createIcon(QLatin1String("dialog-warning.png"));
                 icon = d->createIcon(QLatin1String("dialog-warning.png"));
                 if (icon.isNull())
                     icon = d->createIcon(QLatin1String("messagebox_warning.png"));
@@ -6020,8 +6030,101 @@ QIcon QCommonStyle::standardIconImplementation(StandardPixmap standardIcon, cons
         }
         if (!icon.isNull())
             return icon;
+#elif defined(Q_WS_MAC)
+    OSType iconType = 0;
+    switch (standardIcon) {
+    case QStyle::SP_MessageBoxQuestion:
+    case QStyle::SP_MessageBoxInformation:
+    case QStyle::SP_MessageBoxWarning:
+    case QStyle::SP_MessageBoxCritical:
+        iconType = kGenericApplicationIcon;
+        break;
+    case SP_DesktopIcon:
+        iconType = kDesktopIcon;
+        break;
+    case SP_TrashIcon:
+        iconType = kTrashIcon;
+        break;
+    case SP_ComputerIcon:
+        iconType = kComputerIcon;
+        break;
+    case SP_DriveFDIcon:
+        iconType = kGenericFloppyIcon;
+        break;
+    case SP_DriveHDIcon:
+        iconType = kGenericHardDiskIcon;
+        break;
+    case SP_DriveCDIcon:
+    case SP_DriveDVDIcon:
+        iconType = kGenericCDROMIcon;
+        break;
+    case SP_DriveNetIcon:
+        iconType = kGenericNetworkIcon;
+        break;
+    case SP_DirOpenIcon:
+        iconType = kOpenFolderIcon;
+        break;
+    case SP_DirClosedIcon:
+    case SP_DirLinkIcon:
+        iconType = kGenericFolderIcon;
+        break;
+    case SP_FileLinkIcon:
+    case SP_FileIcon:
+        iconType = kGenericDocumentIcon;
+        break;
+    case SP_DirIcon: {
+        // A rather special case
+        QIcon closeIcon = QStyle::standardIcon(SP_DirClosedIcon, option, widget);
+        QIcon openIcon = QStyle::standardIcon(SP_DirOpenIcon, option, widget);
+        closeIcon.addPixmap(openIcon.pixmap(16, 16), QIcon::Normal, QIcon::On);
+        closeIcon.addPixmap(openIcon.pixmap(32, 32), QIcon::Normal, QIcon::On);
+        closeIcon.addPixmap(openIcon.pixmap(64, 64), QIcon::Normal, QIcon::On);
+        closeIcon.addPixmap(openIcon.pixmap(128, 128), QIcon::Normal, QIcon::On);
+        return closeIcon;
     }
-#endif//Q_WS_X11
+    case SP_TitleBarNormalButton:
+    case SP_TitleBarCloseButton: {
+        QIcon titleBarIcon;
+        if (standardIcon == SP_TitleBarCloseButton) {
+            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/closedock-16.png"));
+            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/closedock-down-16.png"), QSize(16, 16), QIcon::Normal, QIcon::On);
+        } else {
+            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/dockdock-16.png"));
+            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/dockdock-down-16.png"), QSize(16, 16), QIcon::Normal, QIcon::On);
+        }
+        return titleBarIcon;
+    }
+    default:
+        break;
+    }
+    if (iconType != 0) {
+        QIcon retIcon;
+        IconRef icon;
+        IconRef overlayIcon = 0;
+        if (iconType != kGenericApplicationIcon) {
+            GetIconRef(kOnSystemDisk, kSystemIconsCreator, iconType, &icon);
+        } else {
+            FSRef fsRef;
+            ProcessSerialNumber psn = { 0, kCurrentProcess };
+            GetProcessBundleLocation(&psn, &fsRef);
+            GetIconRefFromFileInfo(&fsRef, 0, 0, 0, 0, kIconServicesNormalUsageFlag, &icon, 0);
+            if (standardIcon == SP_MessageBoxCritical) {
+                overlayIcon = icon;
+                GetIconRef(kOnSystemDisk, kSystemIconsCreator, kAlertCautionIcon, &icon);
+            }
+        }
+        if (icon) {
+            qt_mac_constructQIconFromIconRef(icon, overlayIcon, &retIcon, standardIcon);
+            ReleaseIconRef(icon);
+        }
+        if (overlayIcon)
+            ReleaseIconRef(overlayIcon);
+        return retIcon;
+    }
+
+#endif //Q_WS_X11 || Q_WS_MAC
+    }
+
 
     switch (standardIcon) {
 #ifndef QT_NO_IMAGEFORMAT_PNG

src/gui/image/qpixmap_mac.cpp
@@ -1169,25 +1169,6 @@ IconRef qt_mac_create_iconref(const QPixmap &px)
 }
 #endif
 
-QPixmap qt_mac_convert_iconref(const IconRef icon, int width, int height)
-{
-    QPixmap ret(width, height);
-    ret.fill(QColor(0, 0, 0, 0));
-
-    CGRect rect = CGRectMake(0, 0, width, height);
-
-    CGContextRef ctx = qt_mac_cg_context(&ret);
-    CGAffineTransform old_xform = CGContextGetCTM(ctx);
-    CGContextConcatCTM(ctx, CGAffineTransformInvert(old_xform));
-    CGContextConcatCTM(ctx, CGAffineTransformIdentity);
-
-    ::RGBColor b;
-    b.blue = b.green = b.red = 255*255;
-    PlotIconRefInContext(ctx, &rect, kAlignNone, kTransformNone, &b, kPlotIconRefNormalFlags, icon);
-    CGContextRelease(ctx);
-    return ret;
-}
-
 /*! \internal */
 QPaintEngine* QMacPixmapData::paintEngine() const
 {

src/gui/itemviews/qfileiconprovider.cpp
@@ -53,7 +53,7 @@
 #include <private/qpixmapdata_p.h>
 #include <qpixmapcache.h>
 #elif defined(Q_WS_MAC)
-#include <private/qt_mac_p.h>
+#include <private/qt_cocoa_helpers_mac_p.h>
 #endif
 #include <private/qfunctions_p.h>
 
@@ -326,10 +326,11 @@ QIcon QFileIconProviderPrivate::getMacIcon(const QFileInfo &fi) const
         return retIcon;
     IconRef iconRef;
     SInt16 iconLabel;
-    status = GetIconRefFromFileInfo(&macRef, macName.length, macName.unicode, kIconServicesCatalogInfoMask, &info, kIconServicesNormalUsageFlag, &iconRef, &iconLabel);
+    status = GetIconRefFromFileInfo(&macRef, macName.length, macName.unicode,
+                                    kIconServicesCatalogInfoMask, &info, kIconServicesNormalUsageFlag,
+                                    &iconRef, &iconLabel);
     if (status != noErr)
         return retIcon;
-    extern void qt_mac_constructQIconFromIconRef(const IconRef, const IconRef, QIcon*, QStyle::StandardPixmap = QStyle::SP_CustomBase); // qmacstyle_mac.cpp
     qt_mac_constructQIconFromIconRef(iconRef, 0, &retIcon);
     ReleaseIconRef(iconRef);
     return retIcon;

src/gui/kernel/qt_cocoa_helpers_mac.mm
@@ -77,6 +77,7 @@
 #include <qwidget.h>
 #include <qdesktopwidget.h>
 #include <qevent.h>
+#include <qpixmapcache.h>
 #include <private/qevent_p.h>
 #include <private/qt_cocoa_helpers_mac_p.h>
 #include <private/qt_mac_p.h>
@@ -1144,6 +1145,51 @@ QString qt_mac_get_pasteboardString()
         return qt_mac_NSStringToQString(text);
     } else {
         return QString();
+    }
+}
+
+QPixmap qt_mac_convert_iconref(const IconRef icon, int width, int height)
+{
+    QPixmap ret(width, height);
+    ret.fill(QColor(0, 0, 0, 0));
+
+    CGRect rect = CGRectMake(0, 0, width, height);
+
+    CGContextRef ctx = qt_mac_cg_context(&ret);
+    CGAffineTransform old_xform = CGContextGetCTM(ctx);
+    CGContextConcatCTM(ctx, CGAffineTransformInvert(old_xform));
+    CGContextConcatCTM(ctx, CGAffineTransformIdentity);
+
+    ::RGBColor b;
+    b.blue = b.green = b.red = 255*255;
+    PlotIconRefInContext(ctx, &rect, kAlignNone, kTransformNone, &b, kPlotIconRefNormalFlags, icon);
+    CGContextRelease(ctx);
+    return ret;
+}
+
+void qt_mac_constructQIconFromIconRef(const IconRef icon, const IconRef overlayIcon, QIcon *retIcon, QStyle::StandardPixmap standardIcon)
+{
+    int size = 16;
+    while (size <= 128) {
+
+        const QString cacheKey = QLatin1String("qt_mac_constructQIconFromIconRef") + QString::number(standardIcon) + QString::number(size);
+        QPixmap mainIcon;
+        if (standardIcon >= QStyle::SP_CustomBase) {
+            mainIcon = qt_mac_convert_iconref(icon, size, size);
+        } else if (QPixmapCache::find(cacheKey, mainIcon) == false) {
+            mainIcon = qt_mac_convert_iconref(icon, size, size);
+            QPixmapCache::insert(cacheKey, mainIcon);
+        }
+
+        if (overlayIcon) {
+            int littleSize = size / 2;
+            QPixmap overlayPix = qt_mac_convert_iconref(overlayIcon, littleSize, littleSize);
+            QPainter painter(&mainIcon);
+            painter.drawPixmap(size - littleSize, size - littleSize, overlayPix);
+        }
+
+        retIcon->addPixmap(mainIcon);
+        size += size;  // 16 -> 32 -> 64 -> 128
     }
 }
 

src/gui/kernel/qt_cocoa_helpers_mac_p.h
@@ -143,6 +143,9 @@ struct ::TabletProximityRec;
 void qt_dispatchTabletProximityEvent(const ::TabletProximityRec &proxRec);
 Qt::KeyboardModifiers qt_cocoaModifiers2QtModifiers(ulong modifierFlags);
 Qt::KeyboardModifiers qt_cocoaDragOperation2QtModifiers(uint dragOperations);
+QPixmap qt_mac_convert_iconref(const IconRef icon, int width, int height);
+void qt_mac_constructQIconFromIconRef(const IconRef icon, const IconRef overlayIcon, QIcon *retIcon,
+                                      QStyle::StandardPixmap standardIcon = QStyle::SP_CustomBase);
 inline int flipYCoordinate(int y)
 {
     return QApplication::desktop()->screenGeometry(0).height() - y;    

src/gui/styles/qmacstyle_mac.mm
@@ -558,7 +558,6 @@ QT_END_INCLUDE_NAMESPACE
   External functions
  *****************************************************************************/
 extern CGContextRef qt_mac_cg_context(const QPaintDevice *); //qpaintdevice_mac.cpp
-extern QPixmap qt_mac_convert_iconref(const IconRef, int, int); //qpixmap_mac.cpp
 extern QRegion qt_mac_convert_mac_region(HIShapeRef); //qregion_mac.cpp
 void qt_mac_dispose_rgn(RgnHandle r); //qregion_mac.cpp
 extern QPaintDevice *qt_mac_safe_pdev; //qapplication_mac.cpp
@@ -2302,9 +2301,6 @@ int QMacStyle::pixelMetric(PixelMetric metric, const QStyleOption *opt, const QW
         break;
     case PM_ToolBarItemSpacing:
         ret = 4;
-        break;
-    case PM_MessageBoxIconSize:
-        ret = 64;
         break;
     case PM_SplitterWidth:
         ret = qMax(7, QApplication::globalStrut().width());
@@ -5858,76 +5854,12 @@ bool QMacStyle::event(QEvent *e)
     return false;
 }
 
-void qt_mac_constructQIconFromIconRef(const IconRef icon, const IconRef overlayIcon, QIcon *retIcon, QStyle::StandardPixmap standardIcon = QStyle::SP_CustomBase)
-{
-    int size = 16;
-    while (size <= 128) {
-
-        const QString cacheKey = QLatin1String("qt_mac_constructQIconFromIconRef") + QString::number(standardIcon) + QString::number(size);
-        QPixmap mainIcon;
-        if (standardIcon >= QStyle::SP_CustomBase) {
-            mainIcon = qt_mac_convert_iconref(icon, size, size);
-        } else if (QPixmapCache::find(cacheKey, mainIcon) == false) {
-            mainIcon = qt_mac_convert_iconref(icon, size, size);
-            QPixmapCache::insert(cacheKey, mainIcon);
-        }
-
-        if (overlayIcon) {
-            int littleSize = size / 2;
-            QPixmap overlayPix = qt_mac_convert_iconref(overlayIcon, littleSize, littleSize);
-            QPainter painter(&mainIcon);
-            painter.drawPixmap(size - littleSize, size - littleSize, overlayPix);
-        }
-
-        retIcon->addPixmap(mainIcon);
-        size += size;  // 16 -> 32 -> 64 -> 128
-    }
-}
-
 QIcon QMacStyle::standardIconImplementation(StandardPixmap standardIcon, const QStyleOption *opt,
                                             const QWidget *widget) const
 {
-    OSType iconType = 0;
     switch (standardIcon) {
-    case QStyle::SP_MessageBoxQuestion:
-    case QStyle::SP_MessageBoxInformation:
-    case QStyle::SP_MessageBoxWarning:
-    case QStyle::SP_MessageBoxCritical:
-        iconType = kGenericApplicationIcon;
-        break;
-    case SP_DesktopIcon:
-        iconType = kDesktopIcon;
-        break;
-    case SP_TrashIcon:
-        iconType = kTrashIcon;
-        break;
-    case SP_ComputerIcon:
-        iconType = kComputerIcon;
-        break;
-    case SP_DriveFDIcon:
-        iconType = kGenericFloppyIcon;
-        break;
-    case SP_DriveHDIcon:
-        iconType = kGenericHardDiskIcon;
-        break;
-    case SP_DriveCDIcon:
-    case SP_DriveDVDIcon:
-        iconType = kGenericCDROMIcon;
-        break;
-    case SP_DriveNetIcon:
-        iconType = kGenericNetworkIcon;
-        break;
-    case SP_DirOpenIcon:
-        iconType = kOpenFolderIcon;
-        break;
-    case SP_DirClosedIcon:
-    case SP_DirLinkIcon:
-        iconType = kGenericFolderIcon;
-        break;
-    case SP_FileLinkIcon:
-    case SP_FileIcon:
-        iconType = kGenericDocumentIcon;
-        break;
+    default:
+        return QWindowsStyle::standardIconImplementation(standardIcon, opt, widget);
     case SP_ToolBarHorizontalExtensionButton:
     case SP_ToolBarVerticalExtensionButton: {
         QPixmap pixmap(qt_mac_toolbar_ext);
@@ -5941,58 +5873,8 @@ QIcon QMacStyle::standardIconImplementation(StandardPixmap standardIcon, const Q
             return pix2;
         }
         return pixmap;
-        }
-        break;
-    case SP_DirIcon: {
-        // A rather special case
-        QIcon closeIcon = QStyle::standardIcon(SP_DirClosedIcon, opt, widget);
-        QIcon openIcon = QStyle::standardIcon(SP_DirOpenIcon, opt, widget);
-        closeIcon.addPixmap(openIcon.pixmap(16, 16), QIcon::Normal, QIcon::On);
-        closeIcon.addPixmap(openIcon.pixmap(32, 32), QIcon::Normal, QIcon::On);
-        closeIcon.addPixmap(openIcon.pixmap(64, 64), QIcon::Normal, QIcon::On);
-        closeIcon.addPixmap(openIcon.pixmap(128, 128), QIcon::Normal, QIcon::On);
-        return closeIcon;
     }
-    case SP_TitleBarNormalButton:
-    case SP_TitleBarCloseButton: {
-        QIcon titleBarIcon;
-        if (standardIcon == SP_TitleBarCloseButton) {
-            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/closedock-16.png"));
-            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/closedock-down-16.png"), QSize(16, 16), QIcon::Normal, QIcon::On);
-        } else {
-            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/dockdock-16.png"));
-            titleBarIcon.addFile(QLatin1String(":/trolltech/styles/macstyle/images/dockdock-down-16.png"), QSize(16, 16), QIcon::Normal, QIcon::On);
-        }
-        return titleBarIcon;
     }
-    default:
-        break;
-    }
-    if (iconType != 0) {
-        QIcon retIcon;
-        IconRef icon;
-        IconRef overlayIcon = 0;
-        if (iconType != kGenericApplicationIcon) {
-            GetIconRef(kOnSystemDisk, kSystemIconsCreator, iconType, &icon);
-        } else {
-            FSRef fsRef;
-            ProcessSerialNumber psn = { 0, kCurrentProcess };
-            GetProcessBundleLocation(&psn, &fsRef);
-            GetIconRefFromFileInfo(&fsRef, 0, 0, 0, 0, kIconServicesNormalUsageFlag, &icon, 0);
-            if (standardIcon == SP_MessageBoxCritical) {
-                overlayIcon = icon;
-                GetIconRef(kOnSystemDisk, kSystemIconsCreator, kAlertCautionIcon, &icon);
-            }
-        }
-        if (icon) {
-            qt_mac_constructQIconFromIconRef(icon, overlayIcon, &retIcon, standardIcon);
-            ReleaseIconRef(icon);
-        }
-        if (overlayIcon)
-            ReleaseIconRef(overlayIcon);
-        return retIcon;
-    }
-    return QWindowsStyle::standardIconImplementation(standardIcon, opt, widget);
 }
 
 int QMacStyle::layoutSpacingImplementation(QSizePolicy::ControlType control1,

commit 157e218d90ba832b2846889e9f7c94c95ed2cfbc

Fix issue with wrong QMessagebox icons on Mac.
Task: QTBUG-4988

68c0e6a8ba1e92bf0152adcaa86eebb83dcfd1d8 introduced
a regression preventing the mac-spesific standardIcon
code form being called.

Call the Mac code if desktopSettingsAware is set.
(QIcon::themeName() is only set on X11)

src/gui/styles/qcommonstyle.cpp
@@ -5657,10 +5657,11 @@ QIcon QCommonStyle::standardIconImplementation(StandardPixmap standardIcon, cons
         default:
             break;
         }
-
+    } // if (QApplication::desktopSettingsAware() && !QIcon::themeName().isEmpty())
         if (!icon.isNull())
             return icon;
 #if defined(Q_WS_MAC)
+    if (QApplication::desktopSettingsAware()) {
     OSType iconType = 0;
     switch (standardIcon) {
     case QStyle::SP_MessageBoxQuestion:
@@ -5751,8 +5752,8 @@ QIcon QCommonStyle::standardIconImplementation(StandardPixmap standardIcon, cons
             ReleaseIconRef(overlayIcon);
         return retIcon;
     }
+    } // if (QApplication::desktopSettingsAware())
 #endif // Q_WS_MAC
-    }
 
     switch (standardIcon) {
 #ifndef QT_NO_IMAGEFORMAT_PNG