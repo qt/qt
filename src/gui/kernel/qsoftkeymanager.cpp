qt/qt

commit dd48c27fcdded149c8db7d08107632c2476178ca

Softkeys should not put exit by default on RSK for dialogs and popups.
The idea is that softkeys and pop-ups need to take care of setting all
softkeys by themselves.

Task-number: QTBUG-4916
Reviewed-by: Jason Barron

src/gui/kernel/qsoftkeymanager.cpp
@@ -237,7 +237,8 @@ void QSoftKeyManagerPrivate::updateSoftKeys_sys(const QList<QAction*> &softkeys)
         }
     }
 
-    if (needsExitButton)
+    Qt::WindowType sourceWindowType = QSoftKeyManagerPrivate::softKeySource->window()->windowType();
+    if (needsExitButton && sourceWindowType != Qt::Dialog && sourceWindowType != Qt::Popup)
         QT_TRAP_THROWING(nativeContainer->SetCommandL(2, EAknSoftkeyExit, qt_QString2TPtrC(QSoftKeyManager::tr("Exit"))));
 
     nativeContainer->DrawDeferred(); // 3.1 needs an extra invitation

commit 6a89291da7e1322ad81c4939cc9d69db01957a4c

Fix for defect introduced by dd48c27f
This change causes some applications to crash due to a null
pointer dereference in QSoftKeyManagerPrivate::updateSoftKeys_sys.

f3854db6 fixes the crash, but introduces incorrect behaviour: the
softkey labels are not updated.  To see this:

1. Launch qmediaplayer
2. Open a video clip
3. Video clip starts playing but softkeys still have labels "Open"
and "Cancel"

Reviewed-by: Janne Anttila

src/gui/kernel/qsoftkeymanager.cpp
@@ -237,7 +237,10 @@ void QSoftKeyManagerPrivate::updateSoftKeys_sys(const QList<QAction*> &softkeys)
         }
     }
 
-    Qt::WindowType sourceWindowType = QSoftKeyManagerPrivate::softKeySource->window()->windowType();
+    const Qt::WindowType sourceWindowType = QSoftKeyManagerPrivate::softKeySource
+        ?   QSoftKeyManagerPrivate::softKeySource->window()->windowType()
+        :   Qt::Widget;
+
     if (needsExitButton && sourceWindowType != Qt::Dialog && sourceWindowType != Qt::Popup)
         QT_TRAP_THROWING(nativeContainer->SetCommandL(2, EAknSoftkeyExit, qt_QString2TPtrC(QSoftKeyManager::tr("Exit"))));
 