af6ad82b04135a485b3efc88d654c8209db70673
axis <qt-info@nokia.com>
2009-11-13 16:31:22 +0100

    Some compile fixes.

--- src/gui/painting/qwindowsurface_s60.cpp
+++ src/gui/painting/qwindowsurface_s60.cpp
@@ -44,7 +44,7 @@
 #include <QtGui/qpaintdevice.h>
 #include <private/qwidget_p.h>
 #include "qwindowsurface_s60_p.h"
-#include "qpixmap_s60_p.h"
+#include <private/qpixmap_s60_p.h>
 #include <private/qt_s60_p.h>
 #include "private/qdrawhelper_p.h"
 
