7ca2dacff951827daa7bc4d7da1832b282c19a8f
Olivier Goffart <ogoffart@trolltech.com>
2009-11-16 16:29:34 +0100

    QMenu: do not crash if action is destroyed in the triggered signal.
    Task-number: QTBUG-4480
    Reviewed-by: Thierry
    (cherry picked from commit dc2cb80ceb35edd958685189e9075ac1061870f4)

--- src/gui/widgets/qmenu.cpp
+++ src/gui/widgets/qmenu.cpp
@@ -1107,6 +1107,7 @@ void QMenuPrivate::_q_actionTriggered()
 {
     Q_Q(QMenu);
     if (QAction *action = qobject_cast<QAction *>(q->sender())) {
+        QWeakPointer<QAction> actionGuard = action;
 #ifdef QT3_SUPPORT
         //we store it here because the action might be deleted/changed by connected slots
         const int id = q->findIdForAction(action);
@@ -1116,7 +1117,7 @@ void QMenuPrivate::_q_actionTriggered()
         emit q->activated(id);
 #endif
 
-        if (!activationRecursionGuard) {
+        if (!activationRecursionGuard && actionGuard) {
             //in case the action has not been activated by the mouse
             //we check the parent hierarchy
             QList< QPointer<QWidget> > list;
