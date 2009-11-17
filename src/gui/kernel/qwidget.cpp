798ff84b2bd26da5ba2d01bf44e4a4d40dfcacac
Martin Smith <msmith@trolltech.com>
2009-11-17 15:08:29 +0100

    doc: Clarified function of setLayoutDirection()
    Task-number: QTBUG-5432

--- src/gui/kernel/qwidget.cpp
+++ src/gui/kernel/qwidget.cpp
@@ -4677,8 +4677,10 @@ void QWidgetPrivate::resolveLayoutDirection()
     By default, this property is set to Qt::LeftToRight.
 
     When the layout direction is set on a widget, it will propagate to
-    the widget's children. Children added after the call to \c
-    setLayoutDirection() will not inherit the parent's layout
+    the widget's children, but not to a child that is a window and not
+    to a child for which setLayoutDirection() has been explicitly
+    called. Also, child widgets added \e after setLayoutDirection()
+    has been called for the parent do not inherit the parent's layout
     direction.
 
     \sa QApplication::layoutDirection
