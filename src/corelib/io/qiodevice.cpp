qt/qt

commit 98a05681851db9d88b1364af52be543715fbe306

Fix QIODevice::getChar optimization
In Text mode there would be a huge penalty on each '\r' found even if
the internal buffer was not exhausted, because we would repeatedly
remove the '\r' from the buffer and put it back it. Before following
through to the unoptimized code, anyway.

Instead, we now loop over the internal buffer until we find a suitable
character.

Reduced code duplication by having QIODevice::getChar directly call
QIODevice::read and letting compilers do their job.

Reviewed-by: Markus Goetz

src/corelib/io/qiodevice.cpp
@@ -757,22 +757,20 @@ qint64 QIODevice::read(char *data, qint64 maxSize)
 
     // Short circuit for getChar()
     if (maxSize == 1) {
-        int chint = d->buffer.getChar();
-        if (chint != -1) {
+        int chint;
+        while ((chint = d->buffer.getChar()) != -1) {
+            if (!sequential)
+                ++d->pos;
+
             char c = char(uchar(chint));
-            if (c == '\r' && (d->openMode & Text)) {
-                d->buffer.ungetChar(c);
-            } else {
-                if (data)
-                    *data = c;
-                if (!sequential)
-                    ++d->pos;
+            if (c == '\r' && (d->openMode & Text))
+                continue;
+            *data = c;
 #if defined QIODEVICE_DEBUG
-                printf("%p \tread 0x%hhx (%c) returning 1 (shortcut)\n", this,
-                       int(c), isprint(c) ? c : '?');
+            printf("%p \tread 0x%hhx (%c) returning 1 (shortcut)\n", this,
+                   int(c), isprint(c) ? c : '?');
 #endif
-                return qint64(1);
-            }
+            return qint64(1);
         }
     }
 
@@ -1379,40 +1377,8 @@ bool QIODevicePrivate::putCharHelper(char c)
 */
 bool QIODevice::getChar(char *c)
 {
-    Q_D(QIODevice);
-    const OpenMode openMode = d->openMode;
-    if (!(openMode & ReadOnly)) {
-        if (openMode == NotOpen)
-            qWarning("QIODevice::getChar: Closed device");
-        else
-            qWarning("QIODevice::getChar: WriteOnly device");
-        return false;
-    }
-
-    // Shortcut for QIODevice::read(c, 1)
-    QRingBuffer *buffer = &d->buffer;
-    const int chint = buffer->getChar();
-    if (chint != -1) {
-        char ch = char(uchar(chint));
-        if ((openMode & Text) && ch == '\r') {
-            buffer->ungetChar(ch);
-        } else {
-            if (c)
-                *c = ch;
-            if (!d->isSequential())
-                ++d->pos;
-            return true;
-        }
-    }
-
-    // Fall back to read().
     char ch;
-    if (read(&ch, 1) == 1) {
-        if (c)
-            *c = ch;
-        return true;
-    }
-    return false;
+    return (1 == read(c ? c : &ch, 1));
 }
 
 /*!

commit 0ff77fe68850ad6448f644c3ee11b3a7914bb8b7

Fix regression introduced in 98a05681851db9d88b1364af52be543715fbe306
Some network tests were set up to ignore a qWarning that was removed.
Fixed those tests.

Made warning from QIODevice::getChar have the right function name.

Reviewed-by: Thiago Macieira

src/corelib/io/qiodevice.cpp
@@ -1404,6 +1404,9 @@ bool QIODevicePrivate::putCharHelper(char c)
 */
 bool QIODevice::getChar(char *c)
 {
+    Q_D(QIODevice);
+    CHECK_READABLE(getChar, false);
+
     char ch;
     return (1 == read(c ? c : &ch, 1));
 }

tests/auto/qsslsocket/tst_qsslsocket.cpp
@@ -347,7 +347,6 @@ void tst_QSslSocket::constructing()
     QCOMPARE(socket.sslConfiguration(), QSslConfiguration::defaultConfiguration());
     QCOMPARE(socket.errorString(), QString("Unknown error"));
     char c = '\0';
-    QTest::ignoreMessage(QtWarningMsg, "QIODevice::getChar: Closed device");
     QVERIFY(!socket.getChar(&c));
     QCOMPARE(c, '\0');
     QVERIFY(!socket.isOpen());

tests/auto/qtcpsocket/tst_qtcpsocket.cpp
@@ -371,7 +371,6 @@ void tst_QTcpSocket::constructing()
     QCOMPARE(socket->socketType(), QTcpSocket::TcpSocket);
 
     char c;
-    QTest::ignoreMessage(QtWarningMsg, "QIODevice::getChar: Closed device");
     QCOMPARE(socket->getChar(&c), false);
     QCOMPARE((int) socket->bytesAvailable(), 0);
     QCOMPARE(socket->canReadLine(), false);
@@ -830,7 +829,6 @@ void tst_QTcpSocket::openCloseOpenClose()
         QVERIFY(socket->socketType() == QTcpSocket::TcpSocket);
 
         char c;
-        QTest::ignoreMessage(QtWarningMsg, "QIODevice::getChar: Closed device");
         QCOMPARE(socket->getChar(&c), false);
         QCOMPARE((int) socket->bytesAvailable(), 0);
         QCOMPARE(socket->canReadLine(), false);
@@ -1959,7 +1957,6 @@ void tst_QTcpSocket::zeroAndMinusOneReturns()
     QCOMPARE(socket->write("BLUBBER"), qint64(-1));
     QCOMPARE(socket->read(c, 16), qint64(-1));
     QCOMPARE(socket->readLine(c, 16), qint64(-1));
-    QTest::ignoreMessage(QtWarningMsg, "QIODevice::getChar: Closed device");
     QVERIFY(!socket->getChar(c));
     QVERIFY(!socket->putChar('a'));
 