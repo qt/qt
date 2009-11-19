qt/qt

commit d0b0b525deb34ad84b580bd76470f4f6d2da5b04

Replace mmfphonondebug.lib with #ifndef QT_NO_DEBUG.
The debug{} directive in src/plugins/phonon/mmf/plugin/plugin.pro does not have
any effect - this can be seen by looking at the generated MMP file, which has a
STATICLIBRARY directive which is applied in both UDEB and UREL builds. This is
the general problem that .pro files cannot tell distinction between the
different targets that one makespec covers.

Also remove objectdumpstub; objectdump was originally prepared for QtGui
inclusion, but since that never happened, no other platforms than Symbian needs
to be covered.

Task-number: QTBUG-5466
Reviewed-by: Gareth Stockwell
(cherry picked from commit cc2d8068fea504483b2602128e2169b482feb308)

src/3rdparty/phonon/mmf/objectdump.cpp
@@ -16,6 +16,8 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+#ifndef QT_NO_DEBUG
+
 #include <QByteArray>
 #include <QDebug>
 #include <QHash>
@@ -523,5 +525,5 @@ void dumpAncestors(const QObject& leaf, QVisitor& visitor)
 
 QT_END_NAMESPACE
 
-
+#endif
 

src/3rdparty/phonon/mmf/objectdump_symbian.cpp
@@ -16,6 +16,8 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+#ifndef QT_NO_DEBUG
+
 #include <QTextStream>
 #include <QWidget>
 #include <coecntrl.h>
@@ -165,4 +167,5 @@ void addDefaultAnnotators_sys(QVisitor& visitor)
 
 QT_END_NAMESPACE
 
+#endif
 

src/3rdparty/phonon/mmf/objecttree.cpp
@@ -16,6 +16,8 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+#ifndef QT_NO_DEBUG
+
 #include <QTextStream>
 #include <QWidget>
 #include "objecttree.h"
@@ -98,5 +100,5 @@ AncestorConstIterator::AncestorConstIterator(const QObject& leaf)
 
 QT_END_NAMESPACE
 
-
+#endif
 

src/plugins/phonon/mmf/mmf.pro
@@ -1,4 +1,101 @@
-TEMPLATE    = subdirs
-SUBDIRS     = mmfphonondebug plugin
+# MMF Phonon backend
 
-plugin.depends = mmfphonondebug
+QT += phonon
+TARGET = phonon_mmf
+PHONON_MMF_DIR = $$QT_SOURCE_TREE/src/3rdparty/phonon/mmf
+
+# Uncomment the following line in order to use the CDrmPlayerUtility client
+# API for audio playback, rather than CMdaAudioPlayerUtility.
+#CONFIG += phonon_mmf_audio_drm
+
+phonon_mmf_audio_drm {
+    LIBS += -lDrmAudioPlayUtility
+    DEFINES += QT_PHONON_MMF_AUDIO_DRM
+} else {
+    LIBS += -lmediaclientaudio
+}
+
+# This is necessary because both epoc32/include and Phonon contain videoplayer.h.
+# By making /epoc32/include the first SYSTEMINCLUDE, we ensure that
+# '#include <videoplayer.h>' picks up the Symbian header, as intended.
+PREPEND_INCLUDEPATH = /epoc32/include
+
+INCLUDEPATH += $$MW_LAYER_SYSTEMINCLUDE
+
+HEADERS +=                                           \
+           $$PHONON_MMF_DIR/abstractaudioeffect.h    \
+           $$PHONON_MMF_DIR/abstractmediaplayer.h    \
+           $$PHONON_MMF_DIR/abstractplayer.h         \
+           $$PHONON_MMF_DIR/ancestormovemonitor.h    \
+           $$PHONON_MMF_DIR/audioequalizer.h         \
+           $$PHONON_MMF_DIR/audiooutput.h            \
+           $$PHONON_MMF_DIR/audioplayer.h            \
+           $$PHONON_MMF_DIR/backend.h                \
+           $$PHONON_MMF_DIR/bassboost.h              \
+           $$PHONON_MMF_DIR/defs.h                   \
+           $$PHONON_MMF_DIR/dummyplayer.h            \
+           $$PHONON_MMF_DIR/effectfactory.h          \
+           $$PHONON_MMF_DIR/mediaobject.h            \
+           $$PHONON_MMF_DIR/mmf_medianode.h          \
+           $$PHONON_MMF_DIR/mmf_videoplayer.h        \
+           $$PHONON_MMF_DIR/objectdump.h             \
+           $$PHONON_MMF_DIR/objectdump_global.h      \
+           $$PHONON_MMF_DIR/objecttree.h             \
+           $$PHONON_MMF_DIR/utils.h                  \
+           $$PHONON_MMF_DIR/videooutput.h            \
+           $$PHONON_MMF_DIR/videooutputobserver.h    \
+           $$PHONON_MMF_DIR/videowidget.h            \
+           $$PHONON_MMF_DIR/volumeobserver.h
+
+SOURCES +=                                           \
+           $$PHONON_MMF_DIR/abstractaudioeffect.cpp  \
+           $$PHONON_MMF_DIR/abstractmediaplayer.cpp  \
+           $$PHONON_MMF_DIR/abstractplayer.cpp       \
+           $$PHONON_MMF_DIR/ancestormovemonitor.cpp  \
+           $$PHONON_MMF_DIR/audioequalizer.cpp       \
+           $$PHONON_MMF_DIR/audiooutput.cpp          \
+           $$PHONON_MMF_DIR/audioplayer.cpp          \
+           $$PHONON_MMF_DIR/backend.cpp              \
+           $$PHONON_MMF_DIR/bassboost.cpp            \
+           $$PHONON_MMF_DIR/dummyplayer.cpp          \
+           $$PHONON_MMF_DIR/effectfactory.cpp        \
+           $$PHONON_MMF_DIR/mediaobject.cpp          \
+           $$PHONON_MMF_DIR/mmf_medianode.cpp        \
+           $$PHONON_MMF_DIR/mmf_videoplayer.cpp      \
+           $$PHONON_MMF_DIR/objectdump.cpp           \
+           $$PHONON_MMF_DIR/objecttree.cpp           \
+           $$PHONON_MMF_DIR/utils.cpp                \
+           $$PHONON_MMF_DIR/videooutput.cpp          \
+           $$PHONON_MMF_DIR/videowidget.cpp
+
+HEADERS += $$PHONON_MMF_DIR/objectdump_symbian.h
+SOURCES += $$PHONON_MMF_DIR/objectdump_symbian.cpp
+
+LIBS += -lcone
+LIBS += -lws32
+
+# This is only needed for debug builds, but is always linked against.
+LIBS += -lhal
+
+TARGET.CAPABILITY = all -tcb
+
+LIBS += -lmediaclientvideo        # For CVideoPlayerUtility
+LIBS += -lcone                    # For CCoeEnv
+LIBS += -lws32                    # For RWindow
+LIBS += -lefsrv                   # For file server
+LIBS += -lapgrfx -lapmime         # For recognizer
+LIBS += -lmmfcontrollerframework  # For CMMFMetaDataEntry
+
+# These are for effects.
+LIBS += -lAudioEqualizerEffect -lBassBoostEffect -lDistanceAttenuationEffect -lDopplerBase -lEffectBase -lEnvironmentalReverbEffect -lListenerDopplerEffect -lListenerLocationEffect -lListenerOrientationEffect -lLocationBase -lLoudnessEffect -lOrientationBase -lSourceDopplerEffect -lSourceLocationEffect -lSourceOrientationEffect -lStereoWideningEffect
+
+# This is needed for having the .qtplugin file properly created on Symbian.
+QTDIR_build:DESTDIR = $$QT_BUILD_TREE/plugins/phonon_backend
+
+target.path = $$[QT_INSTALL_PLUGINS]/phonon_backend
+INSTALLS += target
+
+include(../../qpluginbase.pri)
+
+TARGET.UID3=0x2001E629
+

src/3rdparty/phonon/mmf/mmfphonondebug/objectdump_stub.cpp
@@ -1,40 +1,0 @@
-/*  This file is part of the KDE project.
-
-Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
-
-This library is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 2.1 or 3 of the License.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this library.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-#include "objectdump.h"
-
-QT_BEGIN_NAMESPACE
-
-namespace ObjectDump
-{
-
-void addDefaultAnnotators_sys(QDumper& /*dumper*/)
-{
-
-}
-
-void addDefaultAnnotators_sys(QVisitor& /*visitor*/)
-{
-
-}
-
-} // namespace ObjectDump
-
-QT_END_NAMESPACE
-
-

commit f3118df6af745759a73d8a9f40d1fa1a90a4385a

Fix for compile error in Phonon MMF backend
This was introduced by d0b0b525de.

The use of QT_NO_DEBUG macros causes release-armv5 builds to fail
when the -release option is passed to configure.

This change causes the debugging code (ObjectDump and related
classes) to be included in release builds, although these functions
are not executed.

This is a temporary fix; task QTBUG-6012 has been created for
re-removing this code from release builds.

Reviewed-by: Frans Englich

src/3rdparty/phonon/mmf/objectdump.cpp
@@ -16,8 +16,6 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-#ifndef QT_NO_DEBUG
-
 #include <QByteArray>
 #include <QDebug>
 #include <QHash>
@@ -524,6 +522,4 @@ void dumpAncestors(const QObject& leaf, QVisitor& visitor)
 } // namespace ObjectDump
 
 QT_END_NAMESPACE
-
-#endif
 

src/3rdparty/phonon/mmf/objectdump.h
@@ -19,8 +19,6 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef OBJECTDUMP_H
 #define OBJECTDUMP_H
 
-#include "objectdump_global.h"
-
 #include <QObject>
 #include <QList>
 #include <QByteArray>
@@ -34,7 +32,7 @@ namespace ObjectDump
 /**
  * Abstract base for annotator classes invoked by QVisitor.
  */
-class OBJECTDUMP_EXPORT QAnnotator : public QObject
+class QAnnotator : public QObject
 {
     Q_OBJECT
 public:
@@ -45,7 +43,7 @@ public:
 /**
  * Annotator which replicates QObject::dumpObjectTree functionality.
  */
-class OBJECTDUMP_EXPORT QAnnotatorBasic : public QAnnotator
+class QAnnotatorBasic : public QAnnotator
 {
     Q_OBJECT
 public:
@@ -55,7 +53,7 @@ public:
 /**
  * Annotator which returns widget information.
  */
-class OBJECTDUMP_EXPORT QAnnotatorWidget : public QAnnotator
+class QAnnotatorWidget : public QAnnotator
 {
     Q_OBJECT
 public:
@@ -68,7 +66,7 @@ class QDumperPrivate;
 /**
  * Class used to dump information about individual QObjects.
  */
-class OBJECTDUMP_EXPORT QDumper : public QObject
+class QDumper : public QObject
 {
     Q_OBJECT
     Q_DECLARE_PRIVATE(QDumper)
@@ -103,7 +101,7 @@ class QVisitorPrivate;
 /**
  * Visitor class which dumps information about nodes in the object tree.
  */
-class OBJECTDUMP_EXPORT QVisitor : public QObject
+class QVisitor : public QObject
 {
     Q_OBJECT
     Q_DECLARE_PRIVATE(QVisitor)
@@ -152,12 +150,12 @@ private:
 // Utility functions
 //-----------------------------------------------------------------------------
 
-void OBJECTDUMP_EXPORT addDefaultAnnotators(QDumper& dumper);
-void OBJECTDUMP_EXPORT addDefaultAnnotators(QVisitor& visitor);
+void addDefaultAnnotators(QDumper& dumper);
+void addDefaultAnnotators(QVisitor& visitor);
 
-void OBJECTDUMP_EXPORT dumpTreeFromRoot(const QObject& root, QVisitor& visitor);
-void OBJECTDUMP_EXPORT dumpTreeFromLeaf(const QObject& leaf, QVisitor& visitor);
-void OBJECTDUMP_EXPORT dumpAncestors(const QObject& leaf, QVisitor& visitor);
+void dumpTreeFromRoot(const QObject& root, QVisitor& visitor);
+void dumpTreeFromLeaf(const QObject& leaf, QVisitor& visitor);
+void dumpAncestors(const QObject& leaf, QVisitor& visitor);
 
 } // namespace ObjectDump
 

src/3rdparty/phonon/mmf/objectdump_global.h
@@ -1,30 +1,0 @@
-/*  This file is part of the KDE project.
-
-Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
-
-This library is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 2.1 or 3 of the License.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this library.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-#ifndef OBJECTDUMP_GLOBAL_H
-#define OBJECTDUMP_GLOBAL_H
-
-#include <QtCore/QtGlobal>
-
-#if defined(OBJECTDUMP_LIBRARY)
-#  define OBJECTDUMP_EXPORT
-#else
-#  define OBJECTDUMP_EXPORT
-#endif
-
-#endif

src/3rdparty/phonon/mmf/objectdump_symbian.cpp
@@ -16,8 +16,6 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-#ifndef QT_NO_DEBUG
-
 #include <QTextStream>
 #include <QWidget>
 #include <coecntrl.h>
@@ -159,6 +157,4 @@ void addDefaultAnnotators_sys(QVisitor& visitor)
 } // namespace ObjectDump
 
 QT_END_NAMESPACE
-
-#endif
 

src/3rdparty/phonon/mmf/objecttree.cpp
@@ -16,8 +16,6 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-#ifndef QT_NO_DEBUG
-
 #include <QTextStream>
 #include <QWidget>
 #include "objecttree.h"
@@ -99,6 +97,4 @@ AncestorConstIterator::AncestorConstIterator(const QObject& leaf)
 } // namespace ObjectTree
 
 QT_END_NAMESPACE
-
-#endif
 

src/3rdparty/phonon/mmf/objecttree.h
@@ -19,8 +19,6 @@ along with this library.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef OBJECTTREE_H
 #define OBJECTTREE_H
 
-#include "objectdump_global.h"
-
 #include <QObject>
 #include <QStack>
 
@@ -32,7 +30,7 @@ namespace ObjectTree
 /**
  * Depth-first iterator for QObject tree
  */
-class OBJECTDUMP_EXPORT DepthFirstConstIterator
+class DepthFirstConstIterator
 {
 public:
     DepthFirstConstIterator();
@@ -60,7 +58,7 @@ private:
 /**
  * Ancestor iterator for QObject tree
  */
-class OBJECTDUMP_EXPORT AncestorConstIterator
+class AncestorConstIterator
 {
 public:
     AncestorConstIterator();

src/plugins/phonon/mmf/mmf.pro
@@ -39,7 +39,7 @@ HEADERS +=                                           \
            $$PHONON_MMF_DIR/mmf_medianode.h          \
            $$PHONON_MMF_DIR/mmf_videoplayer.h        \
            $$PHONON_MMF_DIR/objectdump.h             \
-           $$PHONON_MMF_DIR/objectdump_global.h      \
+           $$PHONON_MMF_DIR/objectdump_symbian.h     \
            $$PHONON_MMF_DIR/objecttree.h             \
            $$PHONON_MMF_DIR/utils.h                  \
            $$PHONON_MMF_DIR/videooutput.h            \
@@ -61,13 +61,11 @@ SOURCES +=                                           \
            $$PHONON_MMF_DIR/mmf_medianode.cpp        \
            $$PHONON_MMF_DIR/mmf_videoplayer.cpp      \
            $$PHONON_MMF_DIR/objectdump.cpp           \
+           $$PHONON_MMF_DIR/objectdump_symbian.cpp   \
            $$PHONON_MMF_DIR/objecttree.cpp           \
            $$PHONON_MMF_DIR/utils.cpp                \
            $$PHONON_MMF_DIR/videooutput.cpp          \
            $$PHONON_MMF_DIR/videowidget.cpp
-
-HEADERS += $$PHONON_MMF_DIR/objectdump_symbian.h
-SOURCES += $$PHONON_MMF_DIR/objectdump_symbian.cpp
 
 LIBS += -lcone
 LIBS += -lws32