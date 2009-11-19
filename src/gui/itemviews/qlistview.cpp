qt/qt

commit ecc202c0410125c944f156b5d0de67f093c02723

Fix long text regression in listview that does not fits into the viewport.
In commit f44fe888ee82b9e04, we adjusted the item size to the size of the
viewport.  It should really be adjusted to the size of the content.

Task-number: QTBUG-5854
Reviewed-by: Gabi

src/gui/itemviews/qlistview.cpp
@@ -972,9 +972,9 @@ void QListView::paintEvent(QPaintEvent *e)
         option.rect = visualRect(*it);
 
         if (flow() == TopToBottom)
-            option.rect.setWidth(qMin(viewport()->size().width() - 2 * d->spacing(), option.rect.width()));
+            option.rect.setWidth(qMin(d->contentsSize().width() - 2 * d->spacing(), option.rect.width()));
         else
-            option.rect.setHeight(qMin(viewport()->size().height() - 2 * d->spacing(), option.rect.height()));
+            option.rect.setHeight(qMin(d->contentsSize().height() - 2 * d->spacing(), option.rect.height()));
 
         option.state = state;
         if (selections && selections->isSelected(*it))

commit 73153fa5288e22d27f26d0e1045e82698c9a6343

Fixes regression in QListView: Selection would not expend to the full viewport
Commit ecc202c introduced the regression while fixing another
regression (QTBUG-5854)

The selection would not expend to the whole viewport.

Reviewed-by: Robert Griebl

src/gui/itemviews/qlistview.cpp
@@ -966,15 +966,19 @@ void QListView::paintEvent(QPaintEvent *e)
     bool alternateBase = false;
     int previousRow = -2; // trigger the alternateBase adjustment on first pass
 
+    int maxSize = (flow() == TopToBottom)
+        ? qMax(viewport()->size().width(), d->contentsSize().width()) - 2 * d->spacing()
+        : qMax(viewport()->size().height(), d->contentsSize().height()) - 2 * d->spacing();
+
     QVector<QModelIndex>::const_iterator end = toBeRendered.constEnd();
     for (QVector<QModelIndex>::const_iterator it = toBeRendered.constBegin(); it != end; ++it) {
         Q_ASSERT((*it).isValid());
         option.rect = visualRect(*it);
 
         if (flow() == TopToBottom)
-            option.rect.setWidth(qMin(d->contentsSize().width() - 2 * d->spacing(), option.rect.width()));
+            option.rect.setWidth(qMin(maxSize, option.rect.width()));
         else
-            option.rect.setHeight(qMin(d->contentsSize().height() - 2 * d->spacing(), option.rect.height()));
+            option.rect.setHeight(qMin(maxSize, option.rect.height()));
 
         option.state = state;
         if (selections && selections->isSelected(*it))