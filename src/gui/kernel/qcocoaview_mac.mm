qt/qt

commit 0d231c32cc7670d356d486b13648cb5bd471ffef

Fix a crash in cocoa when a QMessageBox is destroyed from dropEvent()
The gobal variable which stores the current mouse event can be updated
before dragImage() call(blocking) is finished. So make a local copy of
the information required by the QDragManager::drag().

Task-number: QTBUG-4814
Reviewed-by: MortenS

src/gui/kernel/qcocoaview_mac.mm
@@ -1277,29 +1277,29 @@ Qt::DropAction QDragManager::drag(QDrag *o)
     // convert the image to NSImage.
     NSImage *image = (NSImage *)qt_mac_create_nsimage(pix);
     [image retain];
-    DnDParams *dndParams = [QT_MANGLE_NAMESPACE(QCocoaView) currentMouseEvent];
+    DnDParams dndParams = *[QT_MANGLE_NAMESPACE(QCocoaView) currentMouseEvent];
     // save supported actions 
-    [dndParams->view setSupportedActions: qt_mac_mapDropActions(dragPrivate()->possible_actions)];
-    NSPoint imageLoc = {dndParams->localPoint.x - hotspot.x(),
-                        dndParams->localPoint.y + pix.height() - hotspot.y()};
+    [dndParams.view setSupportedActions: qt_mac_mapDropActions(dragPrivate()->possible_actions)];
+    NSPoint imageLoc = {dndParams.localPoint.x - hotspot.x(),
+                        dndParams.localPoint.y + pix.height() - hotspot.y()};
     NSSize mouseOffset = {0.0, 0.0};
     NSPasteboard *pboard = [NSPasteboard pasteboardWithName:NSDragPboard];
-    NSPoint windowPoint = [dndParams->theEvent locationInWindow];
+    NSPoint windowPoint = [dndParams.theEvent locationInWindow];
     dragPrivate()->executed_action = Qt::ActionMask;
     // do the drag
-    [dndParams->view retain];
-    [dndParams->view dragImage:image
+    [dndParams.view retain];
+    [dndParams.view dragImage:image
                             at:imageLoc
                         offset:mouseOffset
-                         event:dndParams->theEvent
+                         event:dndParams.theEvent
                     pasteboard:pboard
-                        source:dndParams->view
+                        source:dndParams.view
                      slideBack:YES];
-    [dndParams->view release];
+    [dndParams.view release];
     [image release];
     dragPrivate()->executed_action = Qt::IgnoreAction;
     object = 0;
-    Qt::DropAction performedAction(qt_mac_mapNSDragOperation(dndParams->performedAction));
+    Qt::DropAction performedAction(qt_mac_mapNSDragOperation(dndParams.performedAction));
     // do post drag processing, if required.
     if(performedAction != Qt::IgnoreAction) {
         // check if the receiver points us to a file location. 

commit a222302ca45e33306dacae0aa16a572c13abc483

Incorrect DropAction returned by QDrag::exec() on Cocoa.
The action performed by the latest drag and drop operation is stored in
the global DnDParams structure. The QDrag::exec() return value has to be
fetcted from this global variable instead of the temporary copy on stack.
Commit 0d231c32cc7670d356d486b13648cb5bd471ffef broke this.

Task-number: QTBUG-6001
Reviewed-by: Denis

src/gui/kernel/qcocoaview_mac.mm
@@ -1451,7 +1451,7 @@ Qt::DropAction QDragManager::drag(QDrag *o)
     [image release];
     dragPrivate()->executed_action = Qt::IgnoreAction;
     object = 0;
-    Qt::DropAction performedAction(qt_mac_mapNSDragOperation(dndParams.performedAction));
+    Qt::DropAction performedAction(qt_mac_mapNSDragOperation(qMacDnDParams()->performedAction));
     // do post drag processing, if required.
     if(performedAction != Qt::IgnoreAction) {
         // check if the receiver points us to a file location.