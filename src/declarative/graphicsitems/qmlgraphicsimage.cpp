1f8085ce84dad88c27c8fffed84250baf20ee89c
Martin Jones <martin.jones@nokia.com>
2009-11-24 17:24:43 +1000

    Docs.

--- src/declarative/graphicsitems/qmlgraphicsimage.cpp
+++ src/declarative/graphicsitems/qmlgraphicsimage.cpp
@@ -141,6 +141,15 @@ QmlGraphicsImage::~QmlGraphicsImage()
 {
 }
 
+/*!
+    \qmlproperty QPixmap Image::pixmap
+
+    This property holds the QPixmap image to display.
+
+    This is useful for displaying images provided by a C++ implementation,
+    for example, a model may provide a data role of type QPixmap.
+*/
+
 QPixmap QmlGraphicsImage::pixmap() const
 {
     Q_D(const QmlGraphicsImage);
