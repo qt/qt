qt/qt

commit 6ef5b4485b9fe5a9d25d2d9b5e67f110fb728a8b

Fix positioning of diacritics in .otf fonts on Windows
Since .otf fonts are not considered truetype fonts, they take the
code paths intended for non-outline fonts. When calculating the bounding
box of a glyph, this would mean we'd find the largest possible bounding
rect of any glyph in the font, while the other metrics, such as the GPOS
tables used to position diacritics in relation to base glyphs, are
positioning the actual outline of the glyph. The result was that certain
diacritics that depended on the opentype positioning would not be shown on
Windows at all, as they would be positioned based on the wrong left bearing
and height when drawn into the glyph cache.

The fix is to find the tight bounding rect of the outline whenever
possible and fall back to the old code when this fails. I've also added
the left bearing of the glyph to the bounding box in the fallback case,
as we did not respect this before and would misplace glyphs that has
a bearing.

Task-number: QTBUG-5860
Reviewed-by: Trond

src/gui/text/qfontengine_win.cpp
@@ -485,61 +485,78 @@ glyph_metrics_t QFontEngineWin::boundingBox(const QGlyphLayout &glyphs)
     return glyph_metrics_t(0, -tm.tmAscent, w, tm.tmHeight, w, 0);
 }
 
+bool QFontEngineWin::getOutlineMetrics(glyph_t glyph, const QTransform &t, glyph_metrics_t *metrics) const
+{
+    Q_ASSERT(metrics != 0);
+
+    HDC hdc = shared_dc();
+
+    GLYPHMETRICS gm;
+    DWORD res = 0;
+    MAT2 mat;
+    mat.eM11.value = mat.eM22.value = 1;
+    mat.eM11.fract = mat.eM22.fract = 0;
+    mat.eM21.value = mat.eM12.value = 0;
+    mat.eM21.fract = mat.eM12.fract = 0;
+
+    if (t.type() > QTransform::TxTranslate) {
+        // We need to set the transform using the HDC's world
+        // matrix rather than using the MAT2 above, because the
+        // results provided when transforming via MAT2 does not
+        // match the glyphs that are drawn using a WorldTransform
+        XFORM xform;
+        xform.eM11 = t.m11();
+        xform.eM12 = t.m12();
+        xform.eM21 = t.m21();
+        xform.eM22 = t.m22();
+        xform.eDx = 0;
+        xform.eDy = 0;
+        SetGraphicsMode(hdc, GM_ADVANCED);
+        SetWorldTransform(hdc, &xform);
+    }
+
+    uint format = GGO_METRICS;
+    if (ttf)
+        format |= GGO_GLYPH_INDEX;
+    res = GetGlyphOutline(hdc, glyph, format, &gm, 0, 0, &mat);
+
+    if (t.type() > QTransform::TxTranslate) {
+        XFORM xform;
+        xform.eM11 = xform.eM22 = 1;
+        xform.eM12 = xform.eM21 = xform.eDx = xform.eDy = 0;
+        SetWorldTransform(hdc, &xform);
+        SetGraphicsMode(hdc, GM_COMPATIBLE);
+    }
+
+    if (res != GDI_ERROR) {
+        *metrics = glyph_metrics_t(gm.gmptGlyphOrigin.x, -gm.gmptGlyphOrigin.y,
+                                  (int)gm.gmBlackBoxX, (int)gm.gmBlackBoxY, gm.gmCellIncX, gm.gmCellIncY);
+        return true;
+    } else {
+        return false;
+    }
+}
 
 glyph_metrics_t QFontEngineWin::boundingBox(glyph_t glyph, const QTransform &t)
 {
 #ifndef Q_WS_WINCE
-    GLYPHMETRICS gm;
-
     HDC hdc = shared_dc();
     SelectObject(hdc, hfont);
-    if (!ttf) {
+
+    glyph_metrics_t glyphMetrics;
+    bool success = getOutlineMetrics(glyph, t, &glyphMetrics);
+
+    if (!ttf && !success) {
+        // Bitmap fonts
         wchar_t ch = glyph;
         ABCFLOAT abc;
         GetCharABCWidthsFloat(hdc, ch, ch, &abc);
         int width = qRound(abc.abcfB);
 
-        return glyph_metrics_t(0, -tm.tmAscent, width, tm.tmHeight, width, 0).transformed(t);
-    } else {
-        DWORD res = 0;
-        MAT2 mat;
-        mat.eM11.value = mat.eM22.value = 1;
-        mat.eM11.fract = mat.eM22.fract = 0;
-        mat.eM21.value = mat.eM12.value = 0;
-        mat.eM21.fract = mat.eM12.fract = 0;
-
-        if (t.type() > QTransform::TxTranslate) {
-            // We need to set the transform using the HDC's world
-            // matrix rather than using the MAT2 above, because the
-            // results provided when transforming via MAT2 does not
-            // match the glyphs that are drawn using a WorldTransform
-            XFORM xform;
-            xform.eM11 = t.m11();
-            xform.eM12 = t.m12();
-            xform.eM21 = t.m21();
-            xform.eM22 = t.m22();
-            xform.eDx = 0;
-            xform.eDy = 0;
-            SetGraphicsMode(hdc, GM_ADVANCED);
-            SetWorldTransform(hdc, &xform);
-        }
-
-        res = GetGlyphOutline(hdc, glyph, GGO_METRICS | GGO_GLYPH_INDEX, &gm, 0, 0, &mat);
-
-        if (t.type() > QTransform::TxTranslate) {
-            XFORM xform;
-            xform.eM11 = xform.eM22 = 1;
-            xform.eM12 = xform.eM21 = xform.eDx = xform.eDy = 0;
-            SetWorldTransform(hdc, &xform);
-            SetGraphicsMode(hdc, GM_COMPATIBLE);
-        }
-
-        if (res != GDI_ERROR) {
-            return glyph_metrics_t(gm.gmptGlyphOrigin.x, -gm.gmptGlyphOrigin.y,
-                                  (int)gm.gmBlackBoxX, (int)gm.gmBlackBoxY, gm.gmCellIncX, gm.gmCellIncY);
-        }
+        return glyph_metrics_t(QFixed::fromReal(abc.abcfA), -tm.tmAscent, width, tm.tmHeight, width, 0).transformed(t);
     }
-    return glyph_metrics_t();
+
+    return glyphMetrics;
 #else
     HDC hdc = shared_dc();
     HGDIOBJ oldFont = SelectObject(hdc, hfont);
@@ -1135,7 +1152,7 @@ QNativeImage *QFontEngineWin::drawGDIGlyph(HFONT font, glyph_t glyph, int margin
     {
         ExtTextOut(hdc, -gx + margin, -gy + margin, options, 0, (LPCWSTR) &glyph, 1, 0);
     }
-
+    
     SelectObject(hdc, old_font);
     return ni;
 }

src/gui/text/qfontengine_win_p.h
@@ -109,6 +109,8 @@ public:
     int getGlyphIndexes(const QChar *ch, int numChars, QGlyphLayout *glyphs, bool mirrored) const;
     void getCMap();
 
+    bool getOutlineMetrics(glyph_t glyph, const QTransform &t, glyph_metrics_t *metrics) const;
+
     QString     _name;
     HFONT       hfont;
     LOGFONT     logfont;

commit f50cfa3a5a6b8408276e540a52f4232568c871fa

Fix compilation on Windows CE
Commit 6ef5b4485b9fe5a9d25d2d9b5e67f110fb728a8b broke compilation on
Windows CE by moving code out from a #ifndef Q_WS_WINCE block.

Reviewed-by: Rohan McGovern

src/gui/text/qfontengine_win.cpp
@@ -485,6 +485,7 @@ glyph_metrics_t QFontEngineWin::boundingBox(const QGlyphLayout &glyphs)
     return glyph_metrics_t(0, -tm.tmAscent, w, tm.tmHeight, w, 0);
 }
 
+#ifndef Q_WS_WINCE
 bool QFontEngineWin::getOutlineMetrics(glyph_t glyph, const QTransform &t, glyph_metrics_t *metrics) const
 {
     Q_ASSERT(metrics != 0);
@@ -536,6 +537,7 @@ bool QFontEngineWin::getOutlineMetrics(glyph_t glyph, const QTransform &t, glyph
         return false;
     }
 }
+#endif
 
 glyph_metrics_t QFontEngineWin::boundingBox(glyph_t glyph, const QTransform &t)
 {

src/gui/text/qfontengine_win_p.h
@@ -109,7 +109,9 @@ public:
     int getGlyphIndexes(const QChar *ch, int numChars, QGlyphLayout *glyphs, bool mirrored) const;
     void getCMap();
 
+#ifndef Q_WS_WINCE
     bool getOutlineMetrics(glyph_t glyph, const QTransform &t, glyph_metrics_t *metrics) const;
+#endif
 
     QString     _name;
     HFONT       hfont;