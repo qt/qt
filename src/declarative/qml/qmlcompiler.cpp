qt/qt

commit 6fe6e803f08750534a6c14b4a2847a6ec1b29350

Use the component name as the base class name for dynamic meta objects when appropriate.
This allows for much improved debug presentation. e.g. what used to be shown as "QmlGraphicsRectangle_QML_3" can now be show as "MyComponent".

src/declarative/qml/qmlcompiler.cpp
@@ -2182,6 +2182,14 @@ bool QmlCompiler::buildDynamicMeta(QmlParser::Object *obj, DynamicMetaMode mode)
     newClassName.append("_QML_");
     int idx = classIndexCounter()->fetchAndAddRelaxed(1);
     newClassName.append(QByteArray::number(idx));
+    if (compileState.root == obj) {
+        QString path = output->url.path();
+        int lastSlash = path.lastIndexOf(QLatin1Char('/'));
+        if (lastSlash > -1) {
+            QString nameBase = path.mid(lastSlash + 1, path.length()-lastSlash-5);
+            newClassName = nameBase.toUtf8() + "_QMLTYPE_" + QByteArray::number(idx);
+        }
+    }
 
     QMetaObjectBuilder builder;
     builder.setClassName(newClassName);

src/declarative/qml/qmlenginedebug.cpp
@@ -285,6 +285,9 @@ QmlEngineDebugServer::objectData(QObject *object)
         rv.objectType = lastSlash < 0 ? typeName : typeName.mid(lastSlash+1);
     } else {
         rv.objectType = QString::fromUtf8(object->metaObject()->className());
+        int marker = rv.objectType.indexOf(QLatin1String("_QMLTYPE_"));
+        if (marker != -1)
+            rv.objectType = rv.objectType.left(marker);
     }
 
     return rv;

commit e757e999bbc5a65e6cdc2f1c53a77f7918e88928

Fix regression introduced in 6fe6e803f08750534a6c14b4a2847a6ec1b29350.
Only use the component name if it is actually a type.

src/declarative/qml/qmlcompiler.cpp
@@ -2195,7 +2195,8 @@ bool QmlCompiler::buildDynamicMeta(QmlParser::Object *obj, DynamicMetaMode mode)
         int lastSlash = path.lastIndexOf(QLatin1Char('/'));
         if (lastSlash > -1) {
             QString nameBase = path.mid(lastSlash + 1, path.length()-lastSlash-5);
-            newClassName = nameBase.toUtf8() + "_QMLTYPE_" + QByteArray::number(idx);
+            if (!nameBase.isEmpty() && nameBase.at(0).isUpper())
+                newClassName = nameBase.toUtf8() + "_QMLTYPE_" + QByteArray::number(idx);
         }
     }
 