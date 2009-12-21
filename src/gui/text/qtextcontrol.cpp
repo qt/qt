71ee64f5a44dc310272b14fe6cd913b2e75672ed
Sami Merila <sami.merila@nokia.com>
2009-12-21 13:28:47 +0200

    Edit focus should not be lost unless by explicit action
    Currently non-touch devices lose edit focus if window switch
    happens. I.e. in cases when screensaver activates, an incoming call
    occurs, a global notification is shown...
    
    With this fix, edit focus remain active, until user removes the
    focus explicitly.
    
    Task-number: 4792
    Reviewed-by: axis

--- src/gui/text/qtextcontrol.cpp
+++ src/gui/text/qtextcontrol.cpp
@@ -1938,7 +1938,11 @@ void QTextControlPrivate::focusEvent(QFocusEvent *e)
     emit q->updateRequest(q->selectionRect());
     if (e->gotFocus()) {
 #ifdef QT_KEYPAD_NAVIGATION
-        if (!QApplication::keypadNavigationEnabled() || (hasEditFocus && e->reason() == Qt::PopupFocusReason)) {
+        if (!QApplication::keypadNavigationEnabled() || (hasEditFocus && (e->reason() == Qt::PopupFocusReason
+#ifdef Q_OS_SYMBIAN
+            || e->reason() == Qt::ActiveWindowFocusReason
+#endif
+            ))) {
 #endif
         cursorOn = (interactionFlags & Qt::TextSelectableByKeyboard);
         if (interactionFlags & Qt::TextEditable) {
