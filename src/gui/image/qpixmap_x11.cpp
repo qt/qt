0304ded29a510e098524395d693783f19660610c
Anders Bakken <anders.bakken@nokia.com>
2009-12-30 16:16:59 -0800

    Set serial number in QX11PixmapData::transformed
    QX11PixmapData::transformed initializes a new QX11PixmapData object but
    doesn't set its serial number.
    
    Reviewed-by: Donald Carr <donald.carr@nokia.com>
    (cherry picked from commit eb94abb952114e826e02ba4562d9048e77f46644)

--- src/gui/image/qpixmap_x11.cpp
+++ src/gui/image/qpixmap_x11.cpp
@@ -1932,6 +1932,8 @@ QPixmap QX11PixmapData::transformed(const QTransform &transform,
         x11Data->hd = (Qt::HANDLE)XCreatePixmap(X11->display,
                                                 RootWindow(X11->display, xinfo.screen()),
                                                 w, h, d);
+        x11Data->setSerialNumber(++qt_pixmap_serial);
+
 #ifndef QT_NO_XRENDER
         if (X11->use_xrender) {
             XRenderPictFormat *format = x11Data->d == 32
