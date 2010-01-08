9b0502aa6abf6bb9c07f205bccdf2d9c65027bde
Olivier Goffart <ogoffart@trolltech.com>
2010-01-08 10:18:52 +0100

    Fixes: Checked QPushbutton ignores stylesheet backround:
    This is because the light() color of the palette is used and it was
    not modified.
    
    Task-number: QTBUG-7216
    Reviewed-by: jbache

--- src/gui/styles/qstylesheetstyle.cpp
+++ src/gui/styles/qstylesheetstyle.cpp
@@ -1350,6 +1350,12 @@ void QRenderRule::configurePalette(QPalette *p, QPalette::ColorRole fr, QPalette
         if (br != QPalette::NoRole)
             p->setBrush(br, bg->brush);
         p->setBrush(QPalette::Window, bg->brush);
+        if (bg->brush.style() == Qt::SolidPattern) {
+            p->setBrush(QPalette::Light, bg->brush.color().lighter(115));
+            p->setBrush(QPalette::Midlight, bg->brush.color().lighter(107));
+            p->setBrush(QPalette::Dark, bg->brush.color().darker(150));
+            p->setBrush(QPalette::Shadow, bg->brush.color().darker(300));
+        }
     }
 
     if (!hasPalette())
