qt/qt

commit e49aee9f7f82a004f41dc83ac1139e1897fa90ee

QNAM HTTP: change the caching semantics to match documentation
PreferCache should not use the network even if must-revalidate is
set. But if it is not fresh enough, then it should use the network.

Reviewed-by: Peter Hartmann

src/network/access/qnetworkaccesshttpbackend.cpp
@@ -385,6 +385,15 @@ void QNetworkAccessHttpBackend::validateCache(QHttpNetworkRequest &httpRequest, 
     QNetworkHeadersPrivate::RawHeadersList::ConstIterator it;
     cacheHeaders.setAllRawHeaders(metaData.rawHeaders());
 
+    if (CacheLoadControlAttribute == QNetworkRequest::PreferNetwork) {
+        it = cacheHeaders.findRawHeader("Cache-Control");
+        if (it != cacheHeaders.rawHeaders.constEnd()) {
+            QHash<QByteArray, QByteArray> cacheControl = parseHttpOptionHeader(it->second);
+            if (cacheControl.contains("must-revalidate"))
+                return;
+        }
+    }
+
     it = cacheHeaders.findRawHeader("etag");
     if (it != cacheHeaders.rawHeaders.constEnd())
         httpRequest.setHeaderField("If-None-Match", it->second);
@@ -393,12 +402,6 @@ void QNetworkAccessHttpBackend::validateCache(QHttpNetworkRequest &httpRequest, 
     if (lastModified.isValid())
         httpRequest.setHeaderField("If-Modified-Since", QNetworkHeadersPrivate::toHttpDate(lastModified));
 
-    it = cacheHeaders.findRawHeader("Cache-Control");
-    if (it != cacheHeaders.rawHeaders.constEnd()) {
-        QHash<QByteArray, QByteArray> cacheControl = parseHttpOptionHeader(it->second);
-        if (cacheControl.contains("must-revalidate"))
-            return;
-    }
 
     /*
      * age_value
@@ -462,7 +465,7 @@ void QNetworkAccessHttpBackend::validateCache(QHttpNetworkRequest &httpRequest, 
     int freshness_lifetime = dateHeader.secsTo(expirationDate);
     bool response_is_fresh = (freshness_lifetime > current_age);
 
-    if (!response_is_fresh && CacheLoadControlAttribute == QNetworkRequest::PreferNetwork)
+    if (!response_is_fresh)
         return;
 
     loadedFromCache = true;

commit 9a820c4219c67aa401f0ac99d7bcc966c249d637

Fix regression introduced in e49aee9f: set If-Modified-Since.
I moved the code above the other tests because it's more complex. But,
of course, that's wrong because it returns without setting the
necessary headers.

Reviewed-by: Peter Hartmann

src/network/access/qnetworkaccesshttpbackend.cpp
@@ -385,6 +385,14 @@ void QNetworkAccessHttpBackend::validateCache(QHttpNetworkRequest &httpRequest, 
     QNetworkHeadersPrivate::RawHeadersList::ConstIterator it;
     cacheHeaders.setAllRawHeaders(metaData.rawHeaders());
 
+    it = cacheHeaders.findRawHeader("etag");
+    if (it != cacheHeaders.rawHeaders.constEnd())
+        httpRequest.setHeaderField("If-None-Match", it->second);
+
+    QDateTime lastModified = metaData.lastModified();
+    if (lastModified.isValid())
+        httpRequest.setHeaderField("If-Modified-Since", QNetworkHeadersPrivate::toHttpDate(lastModified));
+
     if (CacheLoadControlAttribute == QNetworkRequest::PreferNetwork) {
         it = cacheHeaders.findRawHeader("Cache-Control");
         if (it != cacheHeaders.rawHeaders.constEnd()) {
@@ -393,14 +401,6 @@ void QNetworkAccessHttpBackend::validateCache(QHttpNetworkRequest &httpRequest, 
                 return;
         }
     }
-
-    it = cacheHeaders.findRawHeader("etag");
-    if (it != cacheHeaders.rawHeaders.constEnd())
-        httpRequest.setHeaderField("If-None-Match", it->second);
-
-    QDateTime lastModified = metaData.lastModified();
-    if (lastModified.isValid())
-        httpRequest.setHeaderField("If-Modified-Since", QNetworkHeadersPrivate::toHttpDate(lastModified));
 
     QDateTime currentDateTime = QDateTime::currentDateTime();
     QDateTime expirationDate = metaData.expirationDate();