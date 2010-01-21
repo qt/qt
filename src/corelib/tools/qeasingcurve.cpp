848ae6da25e567e9f64bc98dcbf868912448d136
Jan-Arve Sæther <jan-arve.saether@nokia.com>
2010-01-21 10:32:14 +0100

    Doc fixes: Remove some lies from QEasingCurve.
    Task-number: QTBUG-7418

--- src/corelib/tools/qeasingcurve.cpp
+++ src/corelib/tools/qeasingcurve.cpp
@@ -125,7 +125,7 @@
     \value OutCubic     \inlineimage qeasingcurve-outcubic.png
                         \br
                         Easing curve for a cubic (t^3) function:
-                        decelerating from zero velocity.
+                        decelerating to zero velocity.
     \value InOutCubic   \inlineimage qeasingcurve-inoutcubic.png
                         \br
                         Easing curve for a cubic (t^3) function:
@@ -141,7 +141,7 @@
     \value OutQuart     \inlineimage qeasingcurve-outquart.png
                         \br
                         Easing curve for a cubic (t^4) function:
-                        decelerating from zero velocity.
+                        decelerating to zero velocity.
     \value InOutQuart   \inlineimage qeasingcurve-inoutquart.png
                         \br
                         Easing curve for a cubic (t^4) function:
@@ -157,7 +157,7 @@
     \value OutQuint     \inlineimage qeasingcurve-outquint.png
                         \br
                         Easing curve for a cubic (t^5) function:
-                        decelerating from zero velocity.
+                        decelerating to zero velocity.
     \value InOutQuint   \inlineimage qeasingcurve-inoutquint.png
                         \br
                         Easing curve for a cubic (t^5) function:
