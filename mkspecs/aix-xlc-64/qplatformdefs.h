2547e8d27fc7d651d21ba3805918ee3681f03f05
João Abecasis <joao@trolltech.com>
2010-01-22 11:31:31 +0100

    Merged qplatformdefs.h for AIX
    I don't think the differences warranted having to maintain 3 slightly
    differing copies. "Old" locations changed to a forwarding header to the
    merged one.
    
    Differences and how they were resolved in the merged file:
    
    - comment on a workaround for a potential issue when using g++
        => comment kept
    
    - #ifdef's for xlc-64 had been simplified, because it is only supported
    on AIX 4.3 and above
        => versioned #ifdefs should still work on 64-bit platforms, so that
        was preferred; comment left for future reference
    
    - use of different condition in #if clause: (_XOPEN_SOURCE-0 >= 500)
        => verbosity preferred, since it is probably a workaround for a
        preprocessor or #define bug.
    
    Reviewed-by: Thiago Macieira

--- mkspecs/aix-xlc-64/qplatformdefs.h
+++ mkspecs/aix-xlc-64/qplatformdefs.h
@@ -42,51 +42,6 @@
 #ifndef QPLATFORMDEFS_H
 #define QPLATFORMDEFS_H
 
-// Get Qt defines/settings
-
-#include "qglobal.h"
-
-// Set any POSIX/XOPEN defines at the top of this file to turn on specific APIs
-
-#include <unistd.h>
-
-
-// We are hot - unistd.h should have turned on the specific APIs we requested
-
-
-#include <pthread.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <grp.h>
-#include <pwd.h>
-#include <signal.h>
-#include <dlfcn.h>
-#include <strings.h> // AIX X11 headers define FD_ZERO using bzero()
-
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <sys/ipc.h>
-#include <sys/time.h>
-#include <sys/select.h>
-#include <sys/shm.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <netinet/in.h>
-#ifndef QT_NO_IPV6IFNAME
-#include <net/if.h>
-#endif
-
-#define QT_USE_XOPEN_LFS_EXTENSIONS
-#include "../common/posix/qplatformdefs.h"
-
-// QT_SOCKLEN_T
-// Only AIX 4.3 and better support 64-bit
-
-#if defined(_XOPEN_SOURCE) && (_XOPEN_SOURCE >= 500)
-// Only AIX 4.3 and better support 64-bit
-#define QT_SNPRINTF		::snprintf
-#define QT_VSNPRINTF		::vsnprintf
-#endif
+#include "../common/aix/qplatformdefs.h"
 
 #endif // QPLATFORMDEFS_H
--- mkspecs/aix-xlc/qplatformdefs.h
+++ mkspecs/aix-xlc/qplatformdefs.h
@@ -42,63 +42,6 @@
 #ifndef QPLATFORMDEFS_H
 #define QPLATFORMDEFS_H
 
-// Get Qt defines/settings
-
-#include "qglobal.h"
-
-// Set any POSIX/XOPEN defines at the top of this file to turn on specific APIs
-
-#include <unistd.h>
-
-
-// We are hot - unistd.h should have turned on the specific APIs we requested
-
-
-#include <pthread.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <grp.h>
-#include <pwd.h>
-#include <signal.h>
-#include <dlfcn.h>
-#include <strings.h> // AIX X11 headers define FD_ZERO using bzero()
-
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <sys/ipc.h>
-#include <sys/time.h>
-#include <sys/select.h>
-#include <sys/shm.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <netinet/in.h>
-#ifndef QT_NO_IPV6IFNAME
-#include <net/if.h>
-#endif
-
-#define QT_USE_XOPEN_LFS_EXTENSIONS
-#include "../common/posix/qplatformdefs.h"
-
-#undef QT_SOCKLEN_T
-
-#ifdef _AIX43
-// AIX 4.3 and better
-#define QT_SOCKLEN_T            socklen_t
-#elif _AIX42
-// AIX 4.2
-#define QT_SOCKLEN_T            size_t
-#else
-// AIX 4.1
-#define QT_SOCKLEN_T            size_t
-// override
-#define QT_SOCKOPTLEN_T         int
-#endif
-
-#if defined(_XOPEN_SOURCE) && (_XOPEN_SOURCE-0 >= 500)
-// AIX 4.3 and better
-#define QT_SNPRINTF		::snprintf
-#define QT_VSNPRINTF		::vsnprintf
-#endif
+#include "../common/aix/qplatformdefs.h"
 
 #endif // QPLATFORMDEFS_H
