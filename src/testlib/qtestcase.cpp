qt/qt

commit c7739f7dd55a5a88e0efb00895f63d512cb16fe5

Don't force a crash handler in QtTest.
In some cases, it's best to just let the crash happen straight away.

Reviewed-by: Harald Fernengel

src/testlib/qtestcase.cpp
@@ -846,6 +846,9 @@ namespace QTest
     static int mouseDelay = -1;
     static int eventDelay = -1;
     static int keyVerbose = -1;
+#if defined(Q_OS_UNIX) && !defined(Q_OS_SYMBIAN)
+    static bool noCrashHandler = false;
+#endif
 
 void filter_unprintable(char *str)
 {
@@ -976,6 +979,9 @@ static void qParseArgs(int argc, char *argv[])
          " -keyevent-verbose : Turn on verbose messages for keyboard simulation\n"
          " -maxwarnings n    : Sets the maximum amount of messages to output.\n"
          "                     0 means unlimited, default: 2000\n"
+#if defined(Q_OS_UNIX) && !defined(Q_OS_SYMBIAN)
+         " -nocrashhandler   : Disables the crash handler\n"
+#endif
          "\n"
          " Benchmark related options:\n"
 #ifdef QTESTLIB_USE_VALGRIND
@@ -1056,6 +1062,10 @@ static void qParseArgs(int argc, char *argv[])
             } else {
                 QTestLog::setMaxWarnings(qToInt(argv[++i]));
             }
+#if defined(Q_OS_UNIX) && !defined(Q_OS_SYMBIAN)
+        } else if (strcmp(argv[i], "-nocrashhandler") == 0) {
+            QTest::noCrashHandler = true;
+#endif
         } else if (strcmp(argv[i], "-keyevent-verbose") == 0) {
             QTest::keyVerbose = 1;
 #ifdef QTESTLIB_USE_VALGRIND
@@ -1661,7 +1671,8 @@ int QTest::qExec(QObject *testObject, int argc, char **argv)
 #endif
     {
 #if defined(Q_OS_UNIX) && !defined(Q_OS_SYMBIAN)
-        FatalSignalHandler handler;
+        if (!noCrashHandler)
+            FatalSignalHandler handler;
 #endif
         qInvokeTestMethods(testObject);
     }

commit 97f7c9afb295140a7d979203f41d91082ee73cd9

Fix crash handler regression introduced by c7739f7dd.
The crash handler is removed when the FatalSignalHandler object goes
out of scope, which it did right inside that if.

Reviewed-By: Harald Fernengel

src/testlib/qtestcase.cpp
@@ -1671,8 +1671,9 @@ int QTest::qExec(QObject *testObject, int argc, char **argv)
 #endif
     {
 #if defined(Q_OS_UNIX) && !defined(Q_OS_SYMBIAN)
+        QScopedPointer<FatalSignalHandler> handler;
         if (!noCrashHandler)
-            FatalSignalHandler handler;
+            handler.reset(new FatalSignalHandler);
 #endif
         qInvokeTestMethods(testObject);
     }