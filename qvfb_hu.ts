40361096213895db05750b32dc05331ef56c7783
Laszlo Papp <djszapi at archlinux us>
2010-02-03 11:10:43 +0100

    Add Qvfb hungarian translation
    Signed-off-by: Laszlo Papp <djszapi@archlinux.us>
    
    Reviewed-by: Benjamin Poulain <benjamin.poulain@nokia.com>

---
+++ qvfb_hu.ts
@@ -0,0 +1,344 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hu">
+<context>
+    <name>AnimationSaveWidget</name>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="+865"/>
+        <location line="+204"/>
+        <source>Record</source>
+        <translation>Rekord</translation>
+    </message>
+    <message>
+        <location line="-202"/>
+        <source>Reset</source>
+        <translation>Visszaállítás</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Save</source>
+        <translation>Mentés</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Save in MPEG format (requires netpbm package installed)</source>
+        <translation>MPEG formátumba mentés (netpbm csomag telepítése szükséges)</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <location line="+206"/>
+        <source>Click record to begin recording.</source>
+        <translation>Kattintson az írásra az írás megkezdéséhez.</translation>
+    </message>
+    <message>
+        <location line="-115"/>
+        <location line="+147"/>
+        <source>Finished saving.</source>
+        <translation>Befejezett mentés.</translation>
+    </message>
+    <message>
+        <location line="-63"/>
+        <source>Paused. Click record to resume, or save if done.</source>
+        <translation>Szüneteltetve. Kattintson az írásra a folytatáshoz, vagy mentse el, ha kész.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Pause</source>
+        <translation>Szünet</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Recording...</source>
+        <translation>Felvétel...</translation>
+    </message>
+    <message>
+        <location line="+40"/>
+        <source>Saving... </source>
+        <translation>Mentés...</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <location line="+4"/>
+        <source>Save animation...</source>
+        <translation>Animáció mentése...</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Save canceled.</source>
+        <translation>Megnés visszavonva.</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Save failed!</source>
+        <translation>Mentési hiba!</translation>
+    </message>
+</context>
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../tools/qvfb/config.ui" line="+54"/>
+        <source>Configure</source>
+        <translation>Konfigurálás</translation>
+    </message>
+    <message>
+        <location line="+29"/>
+        <source>Size</source>
+        <translation>Méret</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>176x220 &quot;SmartPhone&quot;</source>
+        <translation>176x220 &quot;ÜgyesTelefon&quot;</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>240x320 &quot;PDA&quot;</source>
+        <translation>240x320 &quot;PDA&quot;</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>320x240 &quot;TV&quot; / &quot;QVGA&quot;</source>
+        <translation>320x240 &quot;TV&quot; / &quot;QVGA&quot;</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>640x480 &quot;VGA&quot;</source>
+        <translation>640x480 &quot;VGA&quot;</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>800x600</source>
+        <translation>800x600</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>1024x768</source>
+        <translation>1024x768</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>Custom</source>
+        <translation>Szokásos</translation>
+    </message>
+    <message>
+        <location line="+44"/>
+        <source>Depth</source>
+        <translation>Mélység</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>1 bit monochrome</source>
+        <translation>1 bites egyszínű</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>2 bit grayscale</source>
+        <translation>2 bites szürke skálázás</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>4 bit grayscale</source>
+        <translation>4 bites szürke slálázás</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>8 bit</source>
+        <translation>8 bites</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>12 (16) bit</source>
+        <translation>12 (16) bites</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>15 bit</source>
+        <translation>15 bites</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>16 bit</source>
+        <translation>16 bites</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>18 bit</source>
+        <translation>18 bites</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>24 bit</source>
+        <translation>24 bites</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>32 bit</source>
+        <translation>32 bites</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>32 bit ARGB</source>
+        <translation>32 bites ARGB</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Swap red and blue channels</source>
+        <translation>Piros és kék csatornák felcserélése</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>BGR format</source>
+        <translation>BGR formátum</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Skin</source>
+        <translation>Szkin</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>None</source>
+        <translation>Semmi</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Emulate touch screen (no mouse move)</source>
+        <translatorcomment>указателя?</translatorcomment>
+        <translation>Érintő képernyő emulálása (nincs egér mozgatás)</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Emulate LCD screen (Only with fixed zoom of 3.0 times magnification)</source>
+        <translation>LCD kijelző emulálása (Kizárólag egy fix 3-szoros nagyítással)</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>&lt;p&gt;Note that any applications using the virtual framebuffer will be terminated if you change the Size or Depth &lt;i&gt;above&lt;/i&gt;. You may freely modify the Gamma &lt;i&gt;below&lt;/i&gt;.</source>
+        <translation>&lt;p&gt;Vegye figyelembe azt, hogy bármely alkalmazás, amely virtuális framebuffer-t használ, be lesz fejezve, ha megváltoztatja a méretet vagy a mélységet &lt;i&gt;felül&lt;/i&gt;. Szabadon módosíthatja a Gamma-t &lt;i&gt;alul&lt;/i&gt;.</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Gamma</source>
+        <translation>Gamma</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Blue</source>
+        <translation>Kék</translation>
+    </message>
+    <message>
+        <location line="+489"/>
+        <location line="+496"/>
+        <location line="+14"/>
+        <location line="+496"/>
+        <source>1.0</source>
+        <translation>1.0</translation>
+    </message>
+    <message>
+        <location line="-999"/>
+        <source>Green</source>
+        <translation>Zöld</translation>
+    </message>
+    <message>
+        <location line="+496"/>
+        <source>All</source>
+        <translation>Mind</translation>
+    </message>
+    <message>
+        <location line="+496"/>
+        <source>Red</source>
+        <translation>Piros</translation>
+    </message>
+    <message>
+        <location line="+496"/>
+        <source>Set all to 1.0</source>
+        <translation>Az összes 1.0-ra állítása</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Mégse</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSkin</name>
+    <message>
+        <location filename="../tools/shared/deviceskin/deviceskin.cpp" line="+79"/>
+        <source>The image file &apos;%1&apos; could not be loaded.</source>
+        <translation>Nem sikerült betölteni a(z) &apos;%1&apos; kép fájlt.</translation>
+    </message>
+    <message>
+        <location line="+64"/>
+        <source>The skin directory &apos;%1&apos; does not contain a configuration file.</source>
+        <translation>A(z) &apos;%1&apos; skin könyvtár nem tartalmaz egyetlen konfigurációs fájlt sem.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>The skin configuration file &apos;%1&apos; could not be opened.</source>
+        <translation>Nem lehetett  megnyitni a(z) &apos;%1&apos; skin konfigurációs fájlt.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>The skin configuration file &apos;%1&apos; could not be read: %2</source>
+        <translation>Nem lehetett olvasni a(z) &apos;%1&apos; skin konfigurációs fájlt: %2</translation>
+    </message>
+    <message>
+        <location line="+70"/>
+        <source>Syntax error: %1</source>
+        <translation>Szintakszis hiba: %1</translation>
+    </message>
+    <message>
+        <location line="+21"/>
+        <source>The skin &quot;up&quot; image file &apos;%1&apos; does not exist.</source>
+        <translation>A(z) &apos;%1 &apos;fel&apos; skinezett kép fájl nem létezik.</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>The skin &quot;down&quot; image file &apos;%1&apos; does not exist.</source>
+        <translation>A(z) &apos;%1 &apos;le&apos; skinezett kép fájl nem létezik.</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>The skin &quot;closed&quot; image file &apos;%1&apos; does not exist.</source>
+        <translation>A(z) &apos;%1&apos; skin &quot;bezárt&quot; kép fájl nem létezik.</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>The skin cursor image file &apos;%1&apos; does not exist.</source>
+        <translation>A(z) &apos;%1&apos; skin kurzor kép fájl nem létezik.</translation>
+    </message>
+    <message>
+        <location line="+25"/>
+        <source>Syntax error in area definition: %1</source>
+        <translation>Szintaszis hiba a terület definícióban: %1</translation>
+    </message>
+    <message>
+        <location line="+38"/>
+        <source>Mismatch in number of areas, expected %1, got %2.</source>
+        <translation>Nem megfelelő terület szám, %1 várt, %2 kapott.</translation>
+    </message>
+</context>
+<context>
+    <name>QVFb</name>
+    <message>
+        <location filename="../tools/qvfb/qvfb.cpp" line="-501"/>
+        <source>Browse...</source>
+        <translation>Böngészés...</translation>
+    </message>
+    <message>
+        <location line="+140"/>
+        <source>Load Custom Skin...</source>
+        <translation>Szokásos skin betöltése...</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>All QVFB Skins (*.skin)</source>
+        <translation>Minden QVFB skin (*.skin)</translation>
+    </message>
+</context>
+</TS>
