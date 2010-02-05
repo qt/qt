qt/qt

commit 34f1758428282a327c12b0d8040061c1f67ecc7f

Crash when closing any top-level widget on Symbian.
The window surface must be deleted while the window is valid because on
some graphics systems a notifcation is sent to the window when the
surface is released.

This fix is also an optmization as we no longer process any backing
store requests while deleting the top-level. Previously it would
handle requests from the window itself and all its children.

Task-number: QT-2513
Reviewed-by: jbarron

src/gui/kernel/qwidget.cpp
@@ -1439,7 +1439,16 @@ QWidget::~QWidget()
     }
 #endif
 
-    if (QWidgetBackingStore *bs = d->maybeBackingStore()) {
+    if (d->extra && d->extra->topextra && d->extra->topextra->backingStore) {
+        // Okay, we are about to destroy the top-level window that owns
+        // the backing store. Make sure we delete the backing store right away
+        // before the window handle is invalid. This is important because
+        // the backing store will delete its window surface, which may or may
+        // not have a reference to this widget that will be used later to
+        // notify the window it no longer has a surface.
+        delete d->extra->topextra->backingStore;
+        d->extra->topextra->backingStore = 0;
+    } else if (QWidgetBackingStore *bs = d->maybeBackingStore()) {
         bs->removeDirtyWidget(this);
         if (testAttribute(Qt::WA_StaticContents))
             bs->removeStaticWidget(this);

commit f9c314aa306bfd4a237594775a8aeb14c858e66d

Fix tst_QAccessiblity failure.
This started to fail after 34f1758428282a327c12b0d8040061c1f67ecc7f.
Or actually, the test crashes on my machine. Reason is that the the test
first creates a top-level line edit (which then gets its own backing
store). The line edit is then reparented into another top-level. When
the line edit is destroyed extra->topextra->backingStore is true and we
delete the backing store it first got when created as a top-level.
However, the line edit was reparented so this backing store is not the
"active" one. We should still delete topextra->backingstore, but we must
also remove any pointer references to the line edit in the "active" backing
store.

Reviewed-by: jbarron

src/gui/kernel/qwidget.cpp
@@ -1448,7 +1448,8 @@ QWidget::~QWidget()
         // notify the window it no longer has a surface.
         delete d->extra->topextra->backingStore;
         d->extra->topextra->backingStore = 0;
-    } else if (QWidgetBackingStore *bs = d->maybeBackingStore()) {
+    }
+    if (QWidgetBackingStore *bs = d->maybeBackingStore()) {
         bs->removeDirtyWidget(this);
         if (testAttribute(Qt::WA_StaticContents))
             bs->removeStaticWidget(this);