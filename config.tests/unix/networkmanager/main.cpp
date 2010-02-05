c470f8d1523ec974a7f0e1cb61ef13f0e953c500
Aaron McCarthy <aaron.mccarthy@nokia.com>
2010-02-05 13:59:49 +1000

    Fix Network Manager and CoreWlan config tests.

--- config.tests/unix/networkmanager/main.cpp
+++ config.tests/unix/networkmanager/main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
@@ -40,8 +40,9 @@
 ****************************************************************************/
 
 #if defined(QT_NO_DBUS)
-sjkp //error is no QtDBus
+#error Qt is not configured with DBus support.
 #endif
+
 #include <NetworkManager/NetworkManager.h>
 
 int main(int argc, char** argv)
--- config.tests/unix/networkmanager/networkmanager.pro
+++ config.tests/unix/networkmanager/networkmanager.pro
@@ -1,7 +1,3 @@
 SOURCES = main.cpp
 CONFIG -= qt dylib
 mac:CONFIG -= app_bundle
-
-!contains(QT_CONFIG,dbus): {
-    DEFINES += QT_NO_DBUS
-}
