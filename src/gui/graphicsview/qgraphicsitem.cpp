71f9752031658caebf42c06e1e04f7d03f6860de
Michael Brasser <michael.brasser@nokia.com>
2010-02-05 16:03:07 +1000

    Remove hacky fix (breaks autotest).
    Proper fix should be in 4.6 soon.

--- src/gui/graphicsview/qgraphicsitem.cpp
+++ src/gui/graphicsview/qgraphicsitem.cpp
@@ -1356,7 +1356,6 @@ QGraphicsItem::~QGraphicsItem()
     d_ptr->removeExtraItemCache();
 
     clearFocus();
-    d_ptr->subFocusItem = 0;    //### QT-2649
 
     // Update focus scope item ptr.
     QGraphicsItem *p = d_ptr->parent;
