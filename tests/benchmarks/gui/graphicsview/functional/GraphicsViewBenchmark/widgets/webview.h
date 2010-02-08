2f389a95f5b9e4c7130aa333586d803b639bf259
Bjørn Erik Nilsen <bjorn.nilsen@nokia.com>
2010-02-08 17:08:01 +0100

    Add functional Graphics View benchmarks.
    Widgets and use cases are externally developed and imported from:
    git://gitorious.org/+qt-performance-test-developers/qt/qt-performance-test-developers-clone.git
    (master branch, tests/benchmarks/uimodels/GraphicsViewBenchmark)
    
    I couldn't simply import everything because the benchmarks were heavily
    dependent on an internal measuring tool involving QtScripts and whatnot,
    not suitable for inclusion in the Qt repository. Everything is now converted
    into proper QTestLib compatible benchmark functions.

---
+++ tests/benchmarks/gui/graphicsview/functional/GraphicsViewBenchmark/widgets/webview.h
@@ -0,0 +1,40 @@
+#ifndef WEBVIEW_H
+#define WEBVIEW_H
+
+#include "scrollbar.h"
+#include "abstractscrollarea.h"
+
+class WebViewPrivate;
+
+class WebView : public AbstractScrollArea
+{
+    Q_OBJECT
+
+public:
+
+    WebView(QGraphicsWidget *parent = 0);
+    ~WebView();
+
+public:
+
+    void setUrl(const QUrl& url);
+
+private:
+
+    void scrollContentsBy(qreal dx, qreal dy);
+    void resizeEvent(QGraphicsSceneResizeEvent *event);
+    QSizeF sizeHint(Qt::SizeHint which, const QSizeF & constraint) const;
+
+private:
+
+    Q_PRIVATE_SLOT(d, void _q_loadStarted())
+    Q_PRIVATE_SLOT(d, void _q_loadProgress(int))
+    Q_PRIVATE_SLOT(d, void _q_loadFinished(bool))
+    Q_PRIVATE_SLOT(d, void _q_viewportChanged(QGraphicsWidget*))
+    Q_PRIVATE_SLOT(d, void _q_motionEnded())
+
+    WebViewPrivate *d;
+    friend class WebViewPrivate;
+};
+
+#endif // WEBVIEW_H
---
+++ tests/benchmarks/gui/graphicsview/functional/GraphicsViewBenchmark/widgets/webview_p.h
@@ -0,0 +1,58 @@
+#ifndef WEBVIEW_P_H
+#define WEBVIEW_P_H
+
+#include "webview.h"
+#include <QtWebKit/qgraphicswebview.h>
+#include <QtWebKit/qwebpage.h>
+#include <QtWebKit/qwebframe.h>
+#include <QGraphicsEffect>
+#include <QPainter>
+#include <QPixmapCache>
+#include <QTimer>
+#include <QDebug>
+
+class WebViewCache;
+
+class WebViewPrivate {
+public:
+
+    WebViewPrivate(WebView *w);
+    void adjustSize();
+    void _q_loadStarted();
+    void _q_loadProgress(int);
+    void _q_loadFinished(bool);
+    void _q_viewportChanged(QGraphicsWidget*);
+    void _q_motionEnded();
+
+    WebView *q;
+    QGraphicsWebView *web;
+    WebViewCache *cache;
+    QTimer motionTimer;
+};
+
+class WebViewCache : public QGraphicsEffect 
+{
+    Q_OBJECT
+
+public:
+
+    WebViewCache(QGraphicsWebView *webView);
+    virtual ~WebViewCache();
+
+public:
+
+    void refresh();
+
+    void draw(QPainter * painter, QGraphicsEffectSource * source);
+
+private:
+
+    QVector<QRectF> m_tileRects;
+    QVector<QPixmapCache::Key> m_tilePixmaps;
+    QSizeF m_itemSize;
+    QGraphicsWebView *m_webView;
+
+    friend class WebViewPrivate;
+};
+
+#endif // WEBVIEW_P_H
