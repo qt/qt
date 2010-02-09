004b09ffd423cd3100e346825dd7d730fc86d366
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@nokia.com>
2010-02-09 15:10:00 +0100

    Make the GL glyph cache a POT texture
    This is a significant speed-up on some architectures and drivers.

--- src/opengl/gl2paintengineex/qtextureglyphcache_gl.cpp
+++ src/opengl/gl2paintengineex/qtextureglyphcache_gl.cpp
@@ -70,8 +70,12 @@ QGLTextureGlyphCache::~QGLTextureGlyphCache()
     }
 }
 
-void QGLTextureGlyphCache::createTextureData(int width, int height)
+void QGLTextureGlyphCache::createTextureData(int width, int unalteredHeight)
 {
+    extern int qt_next_power_of_two(int v);
+    // Set height to lowest POT number which is higher than or equal to height
+    int height = qt_next_power_of_two(unalteredHeight);
+
     glGenTextures(1, &m_texture);
     glBindTexture(GL_TEXTURE_2D, m_texture);
 
