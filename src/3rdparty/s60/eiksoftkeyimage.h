9b65c1bf5944e4640f1e5598200819006eb804df
Janne Anttila <janne.anttila@digia.com>
2010-02-10 14:08:17 +0200

    License update as requested by legal for file relicensed from S60.
    Rev-By: TrustMe
    (cherry picked from commit 709a3a4b96f4cc483e63958494adbc5efdf6c57d)

--- src/3rdparty/s60/eiksoftkeyimage.h
+++ src/3rdparty/s60/eiksoftkeyimage.h
@@ -39,23 +39,6 @@
 **
 ****************************************************************************/
 
-/*
-* Copyright (c) 2002 Nokia Corporation and/or its subsidiary(-ies).
-* All rights reserved.
-* This component and the accompanying materials are made available
-* under the terms of "Eclipse Public License v1.0"
-* which accompanies this distribution, and is available
-* at the URL "http://www.eclipse.org/legal/epl-v10.html".
-*
-* Initial Contributors:
-* Nokia Corporation - initial contribution.
-*
-* Contributors:
-*
-* Description:  Changes cba button's label to image.
-*
-*/
-
 #ifndef EIKSOFTKEYIMAGE_H
 #define EIKSOFTKEYIMAGE_H
 
