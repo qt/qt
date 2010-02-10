64b0f5be361958fec2bb0eba48914773a7d525ad
Martin Jones <martin.jones@nokia.com>
2010-02-10 13:06:53 +1000

    Revert "Remove unimplemented methods from header"
    This reverts commit e712ecee850e04084643243a096befbf47e4e8b9.

--- src/declarative/qml/qmlmetatype.h
+++ src/declarative/qml/qmlmetatype.h
@@ -86,6 +86,7 @@ public:
     static int listType(int);
     static bool clear(const QVariant &);
     static bool append(const QVariant &, const QVariant &);
+    static QVariant fromObject(QObject *, int type);
     static const QMetaObject *rawMetaObjectForType(int);
     static const QMetaObject *metaObjectForType(int);
     static int attachedPropertiesFuncId(const QMetaObject *);
@@ -141,6 +142,7 @@ public:
     const QMetaObject *attachedPropertiesType() const;
 
     int parserStatusCast() const;
+    QVariant fromObject(QObject *) const;
     const char *interfaceIId() const;
     int propertyValueSourceCast() const;
     int propertyValueInterceptorCast() const;
