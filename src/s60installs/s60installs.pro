qt/qt

commit cfdf813056c5e6063e97bd01266c249ec2f9ecf1

Fixed position of plugins on Symbian.

src/s60installs/s60installs.pro
@@ -32,7 +32,7 @@ symbian: {
         }
     } else {
         defineReplace(pluginLocations) {
-            return ($$QT_BUILD_TREE/src/plugins/s60/$$1)
+            return ($$QT_BUILD_TREE/plugins/s60/$$1)
         }
     }
 

src/plugins/s60/s60pluginbase.pri
@@ -4,6 +4,8 @@ include(../qpluginbase.pri)
 
 CONFIG  -= plugin
 
+QTDIR_build:DESTDIR	 = $$QT_BUILD_TREE/plugins/s60
+
 MMP_RULES += NOEXPORTLIBRARY
 
 defBlock = \

commit 6f83ced46abe14725fc6e431d2688abaffb61015

Fixed plugin locations in the pkg file.
This broke after commit cfdf813056c5e6063e97bd01266c249ec2f9ecf1.

src/s60installs/s60installs.pro
@@ -27,24 +27,20 @@ symbian: {
         $$QMAKE_LIBDIR_QT/QtSql.dll
 
     symbian-abld|symbian-sbsv2 {
-        defineReplace(pluginLocations) {
-            return ($${EPOCROOT}epoc32/release/$(PLATFORM)/$(TARGET))
-        }
+        pluginLocations = $${EPOCROOT}epoc32/release/$(PLATFORM)/$(TARGET)
     } else {
-        defineReplace(pluginLocations) {
-            return ($$QT_BUILD_TREE/plugins/s60/$$1)
-        }
+        pluginLocations = $$QT_BUILD_TREE/plugins/s60
     }
 
     qts60plugindeployment = \
         "IF package(0x1028315F)" \
-        "   \"$$pluginLocations(5_0)/qts60plugin_5_0.dll\" - \"c:\\sys\\bin\\qts60plugin_5_0.dll\"" \
+        "   \"$$pluginLocations/qts60plugin_5_0.dll\" - \"c:\\sys\\bin\\qts60plugin_5_0.dll\"" \
         "ELSEIF package(0x102752AE)" \
-        "   \"$$pluginLocations(3_2)/qts60plugin_3_2.dll\" - \"c:\\sys\\bin\\qts60plugin_3_2.dll\"" \
+        "   \"$$pluginLocations/qts60plugin_3_2.dll\" - \"c:\\sys\\bin\\qts60plugin_3_2.dll\"" \
         "ELSEIF package(0x102032BE)" \
-        "   \"$$pluginLocations(3_1)/qts60plugin_3_1.dll\" - \"c:\\sys\\bin\\qts60plugin_3_1.dll\"" \
+        "   \"$$pluginLocations/qts60plugin_3_1.dll\" - \"c:\\sys\\bin\\qts60plugin_3_1.dll\"" \
         "ELSE" \
-        "   \"$$pluginLocations(5_0)/qts60plugin_5_0.dll\" - \"c:\\sys\\bin\\qts60plugin_5_0.dll\"" \
+        "   \"$$pluginLocations/qts60plugin_5_0.dll\" - \"c:\\sys\\bin\\qts60plugin_5_0.dll\"" \
         "ENDIF"
     qtlibraries.pkg_postrules += qts60plugindeployment
 