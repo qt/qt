095496e02610c66d34cf3ee0fc329abed2be47e3
Thiago Macieira <thiago.macieira@nokia.com>
2010-02-16 17:28:40 +0100

    Change all ptrdiff_t to qptrdiff.
    Reviewed-By: Bradley T. Hughes

--- src/corelib/concurrent/qfuture.h
+++ src/corelib/concurrent/qfuture.h
@@ -111,7 +111,7 @@ public:
     {
     public:
         typedef std::bidirectional_iterator_tag iterator_category;
-        typedef ptrdiff_t difference_type;
+        typedef qptrdiff difference_type;
         typedef T value_type;
         typedef const T *pointer;
         typedef const T &reference;
--- src/corelib/tools/qhash.h
+++ src/corelib/tools/qhash.h
@@ -329,7 +329,7 @@ public:
 
     public:
         typedef std::bidirectional_iterator_tag iterator_category;
-        typedef ptrdiff_t difference_type;
+        typedef qptrdiff difference_type;
         typedef T value_type;
         typedef T *pointer;
         typedef T &reference;
@@ -394,7 +394,7 @@ public:
 
     public:
         typedef std::bidirectional_iterator_tag iterator_category;
-        typedef ptrdiff_t difference_type;
+        typedef qptrdiff difference_type;
         typedef T value_type;
         typedef const T *pointer;
         typedef const T &reference;
@@ -478,7 +478,7 @@ public:
     // STL compatibility
     typedef T mapped_type;
     typedef Key key_type;
-    typedef ptrdiff_t difference_type;
+    typedef qptrdiff difference_type;
     typedef int size_type;
 
     inline bool empty() const { return isEmpty(); }
