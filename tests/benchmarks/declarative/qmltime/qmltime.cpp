62fc581eb84504d05bca7b70ac3dc912982133d1
Aleksandar Sasha Babic <aleksandar.babic@nokia.com>
2010-02-23 14:12:14 +0100

    Fixing deployment and default values for Symbian
    Reviewed-by: TrustMe

--- tests/benchmarks/declarative/qmltime/qmltime.cpp
+++ tests/benchmarks/declarative/qmltime/qmltime.cpp
@@ -183,7 +183,11 @@ int main(int argc, char ** argv)
     }
 
     if (filename.isEmpty())
+#ifdef Q_OS_SYMBIAN
+        filename = QLatin1String("./tests/item_creation/data.qml");
+#else
         usage(argv[0]);
+#endif
 
     QmlEngine engine;
     QmlComponent component(&engine, filename);
@@ -204,12 +208,19 @@ int main(int argc, char ** argv)
         return -1;
     }
 
+#ifdef Q_OS_SYMBIAN
+    willParent = true;
+#endif
     timer->setWillParent(willParent);
 
     if (!timer->component()) {
         qWarning() << "The timer has no component";
         return -1;
     }
+
+#ifdef Q_OS_SYMBIAN
+    iterations = 1024;
+#endif
 
     timer->run(iterations);
 
