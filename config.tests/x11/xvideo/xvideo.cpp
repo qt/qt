2724520fd551512138731058467c9acea0aa3951
Tom Cooksey <thomas.cooksey@nokia.com>
2010-02-23 10:12:34 +0100

    Add a configure test for XVideo support
    Reviewed-By: TrustMe

---
+++ config.tests/x11/xvideo/xvideo.cpp
@@ -0,0 +1,10 @@
+#include <X11/extensions/Xv.h>
+#include <X11/extensions/Xvlib.h>
+
+int main(int argc, char** argv)
+{
+    unsigned int count = 0;
+    XvAdaptorInfo *adaptors = 0;
+    XvQueryAdaptors(0, 0, &count, &adaptors);
+    return 0;
+}
