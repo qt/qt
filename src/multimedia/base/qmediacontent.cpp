f56a36d771bb71fe6d0f7025ae427a76a1d22324
Nicholas Young <nicholas.young@nokia.com>
2010-02-23 13:32:23 +1000

    Added QNetworkRequest constructor to QMediaContent.
    Now consistent with QMediaResource.
    
    Reviewed-by: Dmytro Poplavskiy

--- src/multimedia/base/qmediacontent.cpp
+++ src/multimedia/base/qmediacontent.cpp
@@ -109,6 +109,19 @@ QMediaContent::QMediaContent(const QUrl &url):
 }
 
 /*!
+    Constructs a media content with \a request providing a reference to the content.
+
+    This constructor can be used to reference media content via network protocols such as HTTP.
+    This may include additional information required to obtain the resource, such as Cookies or HTTP headers.
+*/
+
+QMediaContent::QMediaContent(const QNetworkRequest &request):
+    d(new QMediaContentPrivate)
+{
+    d->resources << QMediaResource(request);
+}
+
+/*!
     Constructs a media content with \a resource providing a reference to the content.
 */
 
