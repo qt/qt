6ffb4e1e8d56d1fab58fcaa369afde33fd983c1f
Justin McPherson <justin.mcpherson@nokia.com>
2010-02-23 11:07:22 +1000

    Add private header warning.
    Reviewed-by: Kurt Korbatits

--- src/multimedia/qml/qmlgraphicsvideo_p.h
+++ src/multimedia/qml/qmlgraphicsvideo_p.h
@@ -42,6 +42,17 @@
 #ifndef QMLGRAPHICSVIDEO_H
 #define QMLGRAPHICSVIDEO_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtMultimedia/private/qmlmediabase_p.h>
 
 #include <QtMultimedia/qgraphicsvideoitem.h>
