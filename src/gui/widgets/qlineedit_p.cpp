qt/qt

commit 0568fb9f428a84a344baaa5c53395db4b99f082c

Text cursor is not shown properly in line edits on Mac OS X.
The mac stlye disables the blinking cursor when there is a selection in
the line edit. The cursor is not shown back when this selection is
removed (either by mouse or key press events).
This patch watches the selection changed signal from the line control
and updates the cursor status accordingly.

Task-number: QTBUG-6378
Reviewed-by: Olivier Goffart

src/gui/widgets/qlineedit_p.cpp
@@ -126,6 +126,21 @@ void QLineEditPrivate::_q_editFocusChange(bool e)
 }
 #endif
 
+void QLineEditPrivate::_q_selectionChanged()
+{
+    Q_Q(QLineEdit);
+    if (control->preeditAreaText().isEmpty()) {
+        QStyleOptionFrameV2 opt;
+        q->initStyleOption(&opt);
+        bool showCursor = control->hasSelectedText() ?
+                          q->style()->styleHint(QStyle::SH_BlinkCursorWhenTextSelected, &opt, q):
+                          true;
+        setCursorVisible(showCursor);
+    }
+
+    emit q->selectionChanged();
+}
+
 void QLineEditPrivate::init(const QString& txt)
 {
     Q_Q(QLineEdit);
@@ -138,7 +153,7 @@ void QLineEditPrivate::init(const QString& txt)
     QObject::connect(control, SIGNAL(cursorPositionChanged(int,int)),
             q, SLOT(_q_cursorPositionChanged(int,int)));
     QObject::connect(control, SIGNAL(selectionChanged()),
-            q, SIGNAL(selectionChanged()));
+            q, SLOT(_q_selectionChanged()));
     QObject::connect(control, SIGNAL(accepted()),
             q, SIGNAL(returnPressed()));
     QObject::connect(control, SIGNAL(editingFinished()),

src/gui/widgets/qlineedit.h
@@ -288,6 +288,7 @@ private:
 #ifdef QT_KEYPAD_NAVIGATION
     Q_PRIVATE_SLOT(d_func(), void _q_editFocusChange(bool))
 #endif
+    Q_PRIVATE_SLOT(d_func(), void _q_selectionChanged())
 };
 
 #endif // QT_NO_LINEEDIT

src/gui/widgets/qlineedit_p.h
@@ -128,7 +128,7 @@ public:
 #ifdef QT_KEYPAD_NAVIGATION
     void _q_editFocusChange(bool);
 #endif
-
+    void _q_selectionChanged();
 #ifndef QT_NO_COMPLETER
     void _q_completionHighlighted(QString);
 #endif

commit 5cc417be7eb5e43180f92ea2cc46cbf4c4d7395a

QLineEdit shows leftovers of edit cursor after clear()
The commit 0568fb9f428a84a344baaa5c53395db4b99f082c introduced this
regression. Make sure that we have text in the lineedit before showing
the cursor.

Task-number: QTBUG-7826
Reviewed-by: Olivier Goffart

src/gui/widgets/qlineedit_p.cpp
@@ -129,7 +129,7 @@ void QLineEditPrivate::_q_editFocusChange(bool e)
 void QLineEditPrivate::_q_selectionChanged()
 {
     Q_Q(QLineEdit);
-    if (control->preeditAreaText().isEmpty()) {
+    if (!control->text().isEmpty() && control->preeditAreaText().isEmpty()) {
         QStyleOptionFrameV2 opt;
         q->initStyleOption(&opt);
         bool showCursor = control->hasSelectedText() ?