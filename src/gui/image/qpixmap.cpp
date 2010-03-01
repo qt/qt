qt/qt

commit 8721d060a67a01ac891cab9d3d17aacf7373bcf0

Fixed QBitmap::load to load into bitmap format again.
Task: http://bugreports.qt.nokia.com/browse/QTBUG-7468
Reviewed-by: Trond

src/gui/image/qpixmap.cpp
@@ -833,14 +833,21 @@ bool QPixmap::load(const QString &fileName, const char *format, Qt::ImageConvers
     if (QPixmapCache::find(key, *this))
         return true;
 
-    QPixmapData *tmp = QPixmapData::create(0, 0, QPixmapData::PixmapType);
-    if (tmp->fromFile(fileName, format, flags)) {
-        data = tmp;
-        QPixmapCache::insert(key, *this);
-        return true;
+    bool ok;
+
+    if (data) {
+        ok = data->fromFile(fileName, format, flags);
+    } else {
+        QScopedPointer<QPixmapData> tmp(QPixmapData::create(0, 0, QPixmapData::PixmapType));
+        ok = tmp->fromFile(fileName, format, flags);
+        if (ok)
+            data = tmp.take();
     }
-    delete tmp;
-    return false;
+
+    if (ok)
+        QPixmapCache::insert(key, *this);
+
+    return ok;
 }
 
 /*!

tests/auto/qpixmap/tst_qpixmap.cpp
@@ -171,6 +171,8 @@ private slots:
 
     void preserveDepth();
     void splash_crash();
+
+    void loadAsBitmapOrPixmap();
 };
 
 static bool lenientCompare(const QPixmap &actual, const QPixmap &expected)
@@ -1509,6 +1511,42 @@ void tst_QPixmap::preserveDepth()
 
     QCOMPARE(depth, source.depth());
 }
+
+void tst_QPixmap::loadAsBitmapOrPixmap()
+{
+    QImage tmp(10, 10, QImage::Format_RGB32);
+    tmp.save("tmp.png");
+
+    bool ok;
+
+    // Check that we can load the pixmap as a pixmap and that it then turns into a pixmap
+    QPixmap pixmap("tmp.png");
+    QVERIFY(!pixmap.isNull());
+    QVERIFY(pixmap.depth() > 1);
+    QVERIFY(!pixmap.isQBitmap());
+
+    pixmap = QPixmap();
+    ok = pixmap.load("tmp.png");
+    QVERIFY(ok);
+    QVERIFY(!pixmap.isNull());
+    QVERIFY(pixmap.depth() > 1);
+    QVERIFY(!pixmap.isQBitmap());
+
+    // The do the same check for bitmaps..
+    QBitmap bitmap("tmp.png");
+    QVERIFY(!bitmap.isNull());
+    QVERIFY(bitmap.depth() == 1);
+    QVERIFY(bitmap.isQBitmap());
+
+    bitmap = QBitmap();
+    ok = bitmap.load("tmp.png");
+    QVERIFY(ok);
+    QVERIFY(!bitmap.isNull());
+    QVERIFY(bitmap.depth() == 1);
+    QVERIFY(bitmap.isQBitmap());
+}
+
+
 
 QTEST_MAIN(tst_QPixmap)
 #include "tst_qpixmap.moc"

commit 1ab5feb6260589f254ed209816cb67dbe9d3e4a5

Fixed QPixmap::load() to not modify referenced copies (again!)
Change 8721d060a67a01ac891cab9d3d17aacf7373bcf0 broke the previous
fix.

Task-number: QTBUG-8606
Reviewed-by: Gunnar

src/gui/image/qpixmap.cpp
@@ -831,21 +831,14 @@ bool QPixmap::load(const QString &fileName, const char *format, Qt::ImageConvers
     if (QPixmapCache::find(key, *this))
         return true;
 
-    bool ok;
-
-    if (data) {
-        ok = data->fromFile(fileName, format, flags);
-    } else {
-        QScopedPointer<QPixmapData> tmp(QPixmapData::create(0, 0, QPixmapData::PixmapType));
-        ok = tmp->fromFile(fileName, format, flags);
-        if (ok)
-            data = tmp.take();
+    QScopedPointer<QPixmapData> tmp(QPixmapData::create(0, 0, data ? data->type : QPixmapData::PixmapType));
+    if (tmp->fromFile(fileName, format, flags)) {
+        data = tmp.take();
+        QPixmapCache::insert(key, *this);
+        return true;
     }
 
-    if (ok)
-        QPixmapCache::insert(key, *this);
-
-    return ok;
+    return false;
 }
 
 /*!