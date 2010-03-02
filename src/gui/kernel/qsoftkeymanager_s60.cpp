a76d12862122504bae2e75cb78bdb21d180aaca0
Janne Anttila <janne.anttila@digia.com>
2010-03-02 16:03:26 +0200

    Re-applied a fix which get lost during softkey refactoring.
    See original commit (b461da1040c32d6690870290d6716a3b5cb7e9e9)
    for more information.
    
    Task-number: QTBUG-6220
    Reviewed-By: TrustMe

--- src/gui/kernel/qsoftkeymanager_s60.cpp
+++ src/gui/kernel/qsoftkeymanager_s60.cpp
@@ -284,7 +284,12 @@ bool QSoftKeyManagerPrivateS60::setSoftkey(CEikButtonGroupContainer &cba,
         TPtrC nativeText = qt_QString2TPtrC(text);
         int command = S60_COMMAND_START + position;
         setNativeSoftkey(cba, position, command, nativeText);
-        cba.DimCommand(command, !action->isEnabled());
+        // QMainWindow "Options" action is set to invisible in order it does not appear in context menu
+        // and all invisible actions are by default disabled.
+        // However we never want to dim options softkey, even it is set to invisible
+        QVariant property = action->property(MENU_ACTION_PROPERTY);
+        const bool dimmed = (property.isValid() && property.toBool()) ? false : !action->isEnabled();
+        cba.DimCommand(command, dimmed);
         realSoftKeyActions.insert(command, action);
         return true;
     }
