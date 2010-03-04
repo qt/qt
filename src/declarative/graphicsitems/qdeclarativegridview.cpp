08cf6561c4ad2288e385e63d941b60b268cc484e
Martin Jones <martin.jones@nokia.com>
2010-03-04 13:05:17 +1000

    Fix spelling.

--- src/declarative/graphicsitems/qdeclarativegridview.cpp
+++ src/declarative/graphicsitems/qdeclarativegridview.cpp
@@ -698,7 +698,7 @@ void QDeclarativeGridViewPrivate::updateCurrent(int modelIndex)
     In this case ListModel is a handy way for us to test our UI.  In practice
     the model would be implemented in C++, or perhaps via a SQL data source.
 
-    Note that views do not enabel \e clip automatically.  If the view
+    Note that views do not enable \e clip automatically.  If the view
     is not clipped by another item or the screen, it will be necessary
     to set \e {clip: true} in order to have the out of view items clipped
     nicely.
