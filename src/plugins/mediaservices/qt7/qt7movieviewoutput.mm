43d2aeab612022228eb9357bfa7e8bb88809c64f
Dmytro Poplavskiy <dmytro.poplavskiy@nokia.com>
2010-03-05 10:52:55 +1000

    QuickTime movie view output: emit the natural size changes
    Reviewed-by: Justin McPherson

--- src/plugins/mediaservices/qt7/qt7movieviewoutput.mm
+++ src/plugins/mediaservices/qt7/qt7movieviewoutput.mm
@@ -201,7 +201,10 @@ void QT7MovieViewOutput::setMovie(void *movie)
 
 void QT7MovieViewOutput::updateNaturalSize(const QSize &newSize)
 {
-    m_nativeSize = newSize;
+    if (m_nativeSize != newSize) {
+        m_nativeSize = newSize;
+        emit nativeSizeChanged();
+    }
 }
 
 WId QT7MovieViewOutput::winId() const
