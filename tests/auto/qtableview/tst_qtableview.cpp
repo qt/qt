qt/qt

commit 718905c097a7f3bbf9805a2561cd855a0b2d8f59

Speed up selectAll in QTreeView with spans
When we had spans, in order to have the selection rect, we took the slow
path in all cases, even if the header section did not move.

Now, take the fast path if the sections did not move.

This requires few adjustement to the code: we cannot call visualRect
anymore since the index may be within a span.

Task-number: 244651
Reviewed-by: Marius Bugge Monsen

src/gui/itemviews/qtableview.cpp
@@ -1344,7 +1344,7 @@ QRegion QTableView::visualRegionForSelection(const QItemSelection &selection) co
     bool verticalMoved = verticalHeader()->sectionsMoved();
     bool horizontalMoved = horizontalHeader()->sectionsMoved();
 
-    if ((verticalMoved && horizontalMoved) || d->hasSpans()) {
+    if ((verticalMoved && horizontalMoved) || (d->hasSpans() && (verticalMoved || horizontalMoved))) {
         for (int i = 0; i < selection.count(); ++i) {
             QItemSelectionRange range = selection.at(i);
             if (range.parent() != d->root || !range.isValid())
@@ -1387,9 +1387,19 @@ QRegion QTableView::visualRegionForSelection(const QItemSelection &selection) co
             if (range.parent() != d->root || !range.isValid())
                 continue;
             d->trimHiddenSelections(&range);
-            QRect tl = visualRect(range.topLeft());
-            QRect br = visualRect(range.bottomRight());
-            selectionRegion += QRegion(tl|br);
+
+            const int rtop = rowViewportPosition(range.top());
+            const int rbottom = rowViewportPosition(range.bottom()) + rowHeight(range.bottom());
+            const int rleft = columnViewportPosition(range.left());
+            const int rright = columnViewportPosition(range.right()) + columnWidth(range.right());
+            selectionRegion += QRect(QPoint(rleft, rtop), QPoint(rright, rbottom));
+            if (d->hasSpans()) {
+                foreach (QSpanCollection::Span *s,
+                         d->spans.spansInRect(range.left(), range.top(), range.width(), range.height())) {
+                    if (range.contains(s->top(), s->left(), range.parent()))
+                        selectionRegion += d->visualSpanRect(*s);
+                }
+            }
         }
     }
 

commit 0f89d7ef7b5e961cfc6ee7960ad6bf07eff71691

Wrong dirty region after row selection in right-to-left mode in QTableView
When computing the region from the selection range, we didn't take
care of the actual position of the cells, which is reverted when in
RtoL mode. Also gets fixed a 2-pixel error introduced in commit
718905c097a7f3bbf9805a2561cd855a0b2d8f59, and that was responsible for
(potentialy) painting more cells than needed.

Auto-test included.

Reviewed-by: Olivier
Task-number: QTBUG-7774

src/gui/itemviews/qtableview.cpp
@@ -1908,6 +1908,7 @@ QRegion QTableView::visualRegionForSelection(const QItemSelection &selection) co
                                                  width, rowHeight(r)));
         }
     } else { // nothing moved
+        const int gridAdjust = showGrid() ? 1 : 0;
         for (int i = 0; i < selection.count(); ++i) {
             QItemSelectionRange range = selection.at(i);
             if (range.parent() != d->root || !range.isValid())
@@ -1916,9 +1917,16 @@ QRegion QTableView::visualRegionForSelection(const QItemSelection &selection) co
 
             const int rtop = rowViewportPosition(range.top());
             const int rbottom = rowViewportPosition(range.bottom()) + rowHeight(range.bottom());
-            const int rleft = columnViewportPosition(range.left());
-            const int rright = columnViewportPosition(range.right()) + columnWidth(range.right());
-            selectionRegion += QRect(QPoint(rleft, rtop), QPoint(rright, rbottom));
+            int rleft;
+            int rright;
+            if (isLeftToRight()) {
+                rleft = columnViewportPosition(range.left());
+                rright = columnViewportPosition(range.right()) + columnWidth(range.right());
+            } else {
+                rleft = columnViewportPosition(range.right());
+                rright = columnViewportPosition(range.left()) + columnWidth(range.left());
+            }
+            selectionRegion += QRect(QPoint(rleft, rtop), QPoint(rright - 1 - gridAdjust, rbottom - 1 - gridAdjust));
             if (d->hasSpans()) {
                 foreach (QSpanCollection::Span *s,
                          d->spans.spansInRect(range.left(), range.top(), range.width(), range.height())) {

tests/auto/qtableview/tst_qtableview.cpp
@@ -200,6 +200,7 @@ private slots:
     void taskQTBUG_4516_clickOnRichTextLabel();
     void taskQTBUG_5237_wheelEventOnHeader();
     void taskQTBUG_8585_crashForNoGoodReason();
+    void taskQTBUG_7774_RtoLVisualRegionForSelection();
 
     void mouseWheel_data();
     void mouseWheel();
@@ -3993,6 +3994,31 @@ void tst_QTableView::taskQTBUG_8585_crashForNoGoodReason()
     }
 }
 
+
+class TableView7774 : public QTableView
+{
+public:
+    QRegion visualRegionForSelection(const QItemSelection &selection) const
+    {
+        return QTableView::visualRegionForSelection(selection);
+    }
+};
+
+void tst_QTableView::taskQTBUG_7774_RtoLVisualRegionForSelection()
+{
+    TableView7774 view;
+    QStandardItemModel model(5,5);
+    view.setModel(&model);
+    view.setLayoutDirection(Qt::RightToLeft);
+    view.show();
+    QTest::qWaitForWindowShown(&view);
+
+    QItemSelectionRange range(model.index(2, 0), model.index(2, model.columnCount() - 1));
+    QItemSelection selection;
+    selection << range;
+    QRegion region = view.visualRegionForSelection(selection);
+    QCOMPARE(region.rects().at(0), view.visualRect(range.topLeft()) | view.visualRect(range.bottomRight()));
+}
 
 QTEST_MAIN(tst_QTableView)
 #include "tst_qtableview.moc"