qt/qt

commit 83e73a69d5a1c6b94c5c83f91141f35d65d27f36

Fixed invokeMethod in alsa implementation.
Reviewed-by:Dmytro Poplavskiy

src/multimedia/audio/qaudiooutput_alsa_p.cpp
@@ -648,7 +648,7 @@ void QAudioOutputPrivate::userFeed()
 
 void QAudioOutputPrivate::feedback()
 {
-    QMetaObject::invokeMethod(this, SLOT(updateAvailable()), Qt::QueuedConnection);
+    QMetaObject::invokeMethod(this, "updateAvailable", Qt::QueuedConnection);
 }
 
 void QAudioOutputPrivate::updateAvailable()

commit 346b0f6247f1f8ca967f6686a849aa7464cb6b87

Fixed deadlock introduced by change 83e73a69d5a1c6b94c5c83f91141f35d65d27f36. alsa callback was deadlocking with QEventDispatcherGlib.
Reviewed-by:Dmytro Poplavskiy

src/multimedia/audio/qaudiooutput_alsa_p.cpp
@@ -648,8 +648,9 @@ void QAudioOutputPrivate::userFeed()
 
 void QAudioOutputPrivate::feedback()
 {
-    QMetaObject::invokeMethod(this, "updateAvailable", Qt::QueuedConnection);
+    updateAvailable();
 }
+
 
 void QAudioOutputPrivate::updateAvailable()
 {