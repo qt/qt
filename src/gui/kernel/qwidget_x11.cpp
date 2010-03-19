0e0637799f6c54924f711d7cbc84857dc1813a19
Denis Dzyubenko <denis.dzyubenko@nokia.com>
2010-03-19 17:22:27 +0100

    Do not create native window handle just because a parent has one.
    On X11 when creating a widget that has a parent, there is no reason to create a
    native window handle right away since we don't know yet if the window is going
    to be ever shown.
    
    Task-number: QTBUG-9215
    Reviewed-by: Bradley T. Hughes
    (cherry picked from commit 1d506981bcc3d2b8aad67989fd7946a0ad826856)

--- src/gui/kernel/qwidget_x11.cpp
+++ src/gui/kernel/qwidget_x11.cpp
@@ -1166,7 +1166,7 @@ void QWidgetPrivate::setParent_sys(QWidget *parent, Qt::WindowFlags f)
     adjustFlags(data.window_flags, q);
     // keep compatibility with previous versions, we need to preserve the created state
     // (but we recreate the winId for the widget being reparented, again for compatibility)
-    if (wasCreated || (!q->isWindow() && parent->testAttribute(Qt::WA_WState_Created)))
+    if (wasCreated)
         createWinId();
     if (q->isWindow() || (!parent || parent->isVisible()) || explicitlyHidden)
         q->setAttribute(Qt::WA_WState_Hidden);
