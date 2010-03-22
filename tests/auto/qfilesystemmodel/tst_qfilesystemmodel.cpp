c1958338bb94f045c846147bdb7224377a2122a6
Martin Jones <martin.jones@nokia.com>
2010-03-22 14:09:20 +1000

    Set role names for QFileSystemModel and QDirModel
    Task-number: QTBUG-7479
    Reviewed-by: Alexis Menard

--- tests/auto/qfilesystemmodel/tst_qfilesystemmodel.cpp
+++ tests/auto/qfilesystemmodel/tst_qfilesystemmodel.cpp
@@ -137,6 +137,10 @@ private slots:
     void drives_data();
     void drives();
     void dirsBeforeFiles();
+
+    void roleNames_data();
+    void roleNames();
+
 protected:
     bool createFiles(const QString &test_path, const QStringList &initial_files, int existingFileCount = 0, const QStringList &intial_dirs = QStringList(), const QString &baseDir = QDir::temp().absolutePath());
 
@@ -1014,6 +1018,31 @@ void tst_QFileSystemModel::dirsBeforeFiles()
                 model->fileInfo(model->index(i, 0, root)).fileName());
 #endif
     }
+}
+
+void tst_QFileSystemModel::roleNames_data()
+{
+    QTest::addColumn<int>("role");
+    QTest::addColumn<QByteArray>("roleName");
+    QTest::newRow("decoration") << int(Qt::DecorationRole) << QByteArray("decoration");
+    QTest::newRow("display") << int(Qt::DisplayRole) << QByteArray("display");
+    QTest::newRow("fileIcon") << int(QFileSystemModel::FileIconRole) << QByteArray("fileIcon");
+    QTest::newRow("filePath") << int(QFileSystemModel::FilePathRole) << QByteArray("filePath");
+    QTest::newRow("fileName") << int(QFileSystemModel::FileNameRole) << QByteArray("fileName");
+    QTest::newRow("filePermissions") << int(QFileSystemModel::FilePermissions) << QByteArray("filePermissions");
+}
+
+void tst_QFileSystemModel::roleNames()
+{
+    QFileSystemModel model;
+    QHash<int, QByteArray> roles = model.roleNames();
+
+    QFETCH(int, role);
+    QVERIFY(roles.contains(role));
+
+    QFETCH(QByteArray, roleName);
+    QList<QByteArray> values = roles.values(role);
+    QVERIFY(values.contains(roleName));
 }
 
 QTEST_MAIN(tst_QFileSystemModel)
