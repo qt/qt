77b913e5f8fa93c62a245685d55d76eec2e9021a
Kenji Sugita <sugita@sra.co.jp>
2010-03-22 15:38:10 +0100

    Fix behavior change QStringList::join() for null
    Before 4.6.2 both str.isNull() and str.isEmpty() return true for the
    following code.
    
        QStringList list;
        QString str = list.join(QString());
    
    This commit revert the behavior change introduced by
    7461ed5227e3002c4a6f74d458aa0255b7c1217d. Joining null
    QString was giving a empty string instead of a null
    string.
    
    Reviewed-by: Benjamin Poulain <benjamin.poulain@nokia.com>

--- tests/auto/qstringlist/tst_qstringlist.cpp
+++ tests/auto/qstringlist/tst_qstringlist.cpp
@@ -77,6 +77,7 @@ private slots:
     void streamingOperator();
     void join() const;
     void join_data() const;
+    void join_emptiness() const;
 };
 
 extern const char email[];
@@ -309,6 +310,15 @@ void tst_QStringList::join_data() const
                         << QLatin1String("c"))
                 << QString(QLatin1String(" "))
                 << QString("a b c");
+}
+
+void tst_QStringList::join_emptiness() const
+{
+    QStringList list;
+    QString string = list.join(QString());
+
+    QCOMPARE(string.isEmpty(), true);
+    QCOMPARE(string.isNull(), true);
 }
 
 QTEST_APPLESS_MAIN(tst_QStringList)
