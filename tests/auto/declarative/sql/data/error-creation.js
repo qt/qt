873a09f26963bd49cdae3cdf5e0d1229c945862f
Warwick Allison <warwick.allison@nokia.com>
2010-03-29 16:27:48 +1000

    test error code too

--- tests/auto/declarative/sql/data/error-creation.js
+++ tests/auto/declarative/sql/data/error-creation.js
@@ -3,10 +3,12 @@ function test() {
     try {
         var db = openDatabaseSync("QmlTestDB-creation", "2.0", "Test database from Qt autotests", 1000000);
     } catch (err) {
-        if (err.message == "SQL: database version mismatch")
-            r = "passed";
-        else
+        if (err.code != SQLException.VERSION_ERR)
+            r = "WRONG ERROR CODE="+err.code;
+        else if (err.message != "SQL: database version mismatch")
             r = "WRONG ERROR="+err.message;
+        else
+            r = "passed";
     }
     return r;
 }
