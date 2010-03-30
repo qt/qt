b9b305f6c7bc960101bfd883ca6e3df889483481
Aaron Kennedy <aaron.kennedy@nokia.com>
2010-03-30 16:37:28 +1000

    Correctly construct boolean JSValue's
    Task-number: QTBUG-9205

--- tests/auto/declarative/qdeclarativeecmascript/tst_qdeclarativeecmascript.cpp
+++ tests/auto/declarative/qdeclarativeecmascript/tst_qdeclarativeecmascript.cpp
@@ -128,6 +128,7 @@ private slots:
     void scriptDisconnect();
     void ownership();
     void qlistqobjectMethods();
+    void strictlyEquals();
 
     void bug1();
     void dynamicCreationCrash();
@@ -2001,6 +2002,26 @@ void tst_qdeclarativeecmascript::qlistqobjectMethods()
 
     QCOMPARE(object->property("test").toInt(), 2);
     QCOMPARE(object->property("test2").toBool(), true);
+
+    delete object;
+}
+
+// QTBUG-9205
+void tst_qdeclarativeecmascript::strictlyEquals()
+{
+    QDeclarativeComponent component(&engine, TEST_FILE("strictlyEquals.qml"));
+
+    QObject *object = component.create();
+    QVERIFY(object != 0);
+
+    QCOMPARE(object->property("test1").toBool(), true);
+    QCOMPARE(object->property("test2").toBool(), true);
+    QCOMPARE(object->property("test3").toBool(), true);
+    QCOMPARE(object->property("test4").toBool(), true);
+    QCOMPARE(object->property("test5").toBool(), true);
+    QCOMPARE(object->property("test6").toBool(), true);
+    QCOMPARE(object->property("test7").toBool(), true);
+    QCOMPARE(object->property("test8").toBool(), true);
 
     delete object;
 }
