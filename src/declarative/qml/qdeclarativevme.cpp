b8b4613800abfe1bc8861011c7152d6d242414e1
Aaron Kennedy <aaron.kennedy@nokia.com>
2010-04-01 15:59:59 +1000

    Optimization: Minor object allocation speedup

--- src/declarative/qml/qdeclarativevme.cpp
+++ src/declarative/qml/qdeclarativevme.cpp
@@ -255,7 +255,13 @@ QObject *QDeclarativeVME::run(QDeclarativeVMEStack<QObject *> &stack,
                 ddata->columnNumber = instr.createSimple.column;
 
                 QObjectPrivate::get(o)->declarativeData = ddata;                                                      
-                ctxt->addObject(o);                                                                                   
+
+                ddata->nextContextObject = ctxt->contextObjects;                                                      
+                if (ddata->nextContextObject) 
+                    ddata->nextContextObject->prevContextObject = &ddata->nextContextObject;                          
+                ddata->prevContextObject = &ctxt->contextObjects;                                                     
+                ctxt->contextObjects = ddata;                                
+                
                 QObject *parent = stack.top();                                                                    
                 QDeclarative_setParent_noEvent(o, parent);                                                        
 
