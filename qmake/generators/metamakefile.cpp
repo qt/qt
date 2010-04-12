qt/qt

commit f5b19c173109c53bf3d8167573f7276cf39262d2

decouple host platform mode from target platform mode
derive the host mode from the generator - this doesn't work *too* well
if the mode is different from the real host platform, so it's only for
testing.

get the target platform mode from the qmakespec, falling back to the
host platform mode.

Reviewed-by: mariusSO

qmake/generators/metamakefile.cpp
@@ -486,6 +486,40 @@ MetaMakefileGenerator::createMakefileGenerator(QMakeProject *proj, bool noIO)
     return mkfile;
 }
 
+bool
+MetaMakefileGenerator::modesForGenerator(const QString &gen,
+        Option::HOST_MODE *host_mode, Option::TARG_MODE *target_mode)
+{
+    if (gen == "UNIX") {
+#ifdef Q_OS_MAC
+        *host_mode = Option::HOST_MACX_MODE;
+        *target_mode = Option::TARG_MACX_MODE;
+#else
+        *host_mode = Option::HOST_UNIX_MODE;
+        *target_mode = Option::TARG_UNIX_MODE;
+#endif
+    } else if (gen == "MSVC.NET" || gen == "MINGW" || gen == "BMAKE") {
+        *host_mode = Option::HOST_WIN_MODE;
+        *target_mode = Option::TARG_WIN_MODE;
+    } else if (gen == "PROJECTBUILDER" || gen == "XCODE") {
+        *host_mode = Option::HOST_MACX_MODE;
+        *target_mode = Option::TARG_MACX_MODE;
+    } else if (gen == "SYMBIAN_ABLD" || gen == "SYMBIAN_SBSV2") {
+#if defined(Q_OS_MAC)
+        *host_mode = Option::HOST_MACX_MODE;
+#elif defined(Q_OS_UNIX)
+        *host_mode = Option::HOST_UNIX_MODE;
+#else
+        *host_mode = Option::HOST_WIN_MODE;
+#endif
+        *target_mode = Option::TARG_WIN_MODE; // anything, just not unknown
+    } else {
+        fprintf(stderr, "Unknown generator specified: %s\n", gen.toLatin1().constData());
+        return false;
+    }
+    return true;
+}
+
 MetaMakefileGenerator *
 MetaMakefileGenerator::createMetaGenerator(QMakeProject *proj, const QString &name, bool op, bool *success)
 {

qmake/project.cpp
@@ -43,6 +43,7 @@
 #include "property.h"
 #include "option.h"
 #include "cachekeys.h"
+#include "generators/metamakefile.h"
 
 #include <qdatetime.h>
 #include <qfile.h>
@@ -592,6 +593,7 @@ QStringList qmake_feature_paths(QMakeProperty *prop=0)
             concat << base_concat + QDir::separator() + "macx";
             concat << base_concat + QDir::separator() + "unix";
             break;
+        default: // Can't happen, just make the compiler shut up
         case Option::TARG_UNIX_MODE:
             {
                 if (isForSymbian())
@@ -1445,6 +1447,7 @@ QMakeProject::read(uchar cmd)
                 fprintf(stderr, "Failure to read QMAKESPEC conf file %s.\n", spec.toLatin1().constData());
                 return false;
             }
+            validateModes();
 
             init_symbian(base_vars);
 
@@ -1570,6 +1573,44 @@ QMakeProject::read(uchar cmd)
     return true;
 }
 
+void QMakeProject::validateModes()
+{
+    if (Option::host_mode == Option::HOST_UNKNOWN_MODE
+        || Option::target_mode == Option::TARG_UNKNOWN_MODE) {
+        Option::HOST_MODE host_mode;
+        Option::TARG_MODE target_mode;
+        const QStringList &gen = base_vars.value("MAKEFILE_GENERATOR");
+        if (gen.isEmpty()) {
+            fprintf(stderr, "%s:%d: Using OS scope before setting MAKEFILE_GENERATOR\n",
+                            parser.file.toLatin1().constData(), parser.line_no);
+        } else if (MetaMakefileGenerator::modesForGenerator(gen.first(),
+                                                            &host_mode, &target_mode)) {
+            if (Option::host_mode == Option::HOST_UNKNOWN_MODE) {
+                Option::host_mode = host_mode;
+                Option::applyHostMode();
+            }
+
+            if (Option::target_mode == Option::TARG_UNKNOWN_MODE) {
+                const QStringList &tgt = base_vars.value("TARGET_PLATFORM");
+                if (!tgt.isEmpty()) {
+                    const QString &os = tgt.first();
+                    if (os == "unix")
+                        Option::target_mode = Option::TARG_UNIX_MODE;
+                    else if (os == "macx")
+                        Option::target_mode = Option::TARG_MACX_MODE;
+                    else if (os == "win32")
+                        Option::target_mode = Option::TARG_WIN_MODE;
+                    else
+                        fprintf(stderr, "Unknown target platform specified: %s\n",
+                                os.toLatin1().constData());
+                } else {
+                    Option::target_mode = target_mode;
+                }
+            }
+        }
+    }
+}
+
 bool
 QMakeProject::isActiveConfig(const QString &x, bool regex, QMap<QString, QStringList> *place)
 {
@@ -1588,14 +1629,17 @@ QMakeProject::isActiveConfig(const QString &x, bool regex, QMap<QString, QString
     // using a mkspec starting with 'symbian*' will resolve both the 'symbian'
     // and the 'unix' (because of Open C) scopes to true.
     if (x == "unix") {
+        validateModes();
         return Option::target_mode == Option::TARG_UNIX_MODE
                || Option::target_mode == Option::TARG_MACX_MODE
                || isForSymbian();
     } else if (x == "macx" || x == "mac") {
+        validateModes();
         return Option::target_mode == Option::TARG_MACX_MODE && !isForSymbian();
     } else if (x == "symbian") {
         return isForSymbian();
     } else if (x == "win32") {
+        validateModes();
         return Option::target_mode == Option::TARG_WIN_MODE && !isForSymbian();
     }
 
@@ -1694,6 +1738,7 @@ QMakeProject::doProjectInclude(QString file, uchar flags, QMap<QString, QStringL
         if(file.indexOf(Option::dir_sep) == -1 || !QFile::exists(file)) {
             static QStringList *feature_roots = 0;
             if(!feature_roots) {
+                validateModes();
                 init_symbian(base_vars);
                 feature_roots = new QStringList(qmake_feature_paths(prop));
                 qmakeAddCacheClear(qmakeDeleteCacheClear_QStringList, (void**)&feature_roots);

qmake/generators/makefile.cpp
@@ -2152,12 +2152,14 @@ QString MakefileGenerator::buildArgs(const QString &outdir)
         ret += " -nodependheuristics";
     if(!Option::mkfile::qmakespec_commandline.isEmpty())
         ret += " -spec " + specdir(outdir);
-    if(Option::target_mode == Option::TARG_MACX_MODE)
-        ret += " -macx";
-    else if(Option::target_mode == Option::TARG_UNIX_MODE)
-        ret += " -unix";
-    else if(Option::target_mode == Option::TARG_WIN_MODE)
-        ret += " -win32";
+    if (Option::target_mode_overridden) {
+        if (Option::target_mode == Option::TARG_MACX_MODE)
+            ret += " -macx";
+        else if (Option::target_mode == Option::TARG_UNIX_MODE)
+            ret += " -unix";
+        else if (Option::target_mode == Option::TARG_WIN_MODE)
+            ret += " -win32";
+    }
 
     //configs
     for(QStringList::Iterator it = Option::user_configs.begin();

qmake/generators/makefile.h
@@ -247,7 +247,7 @@ public:
     virtual bool supportsMergedBuilds() { return false; }
     virtual bool mergeBuildProject(MakefileGenerator * /*other*/) { return false; }
     virtual bool openOutput(QFile &, const QString &build) const;
-    virtual bool isWindowsShell() const { return Option::target_mode == Option::TARG_WIN_MODE; }
+    virtual bool isWindowsShell() const { return Option::host_mode == Option::HOST_WIN_MODE; }
 };
 
 inline void MakefileGenerator::setNoIO(bool o)

qmake/generators/metamakefile.h
@@ -42,6 +42,8 @@
 #ifndef METAMAKEFILE_H
 #define METAMAKEFILE_H
 
+#include <option.h>
+
 #include <qlist.h>
 #include <qstring.h>
 
@@ -64,6 +66,9 @@ public:
 
     static MetaMakefileGenerator *createMetaGenerator(QMakeProject *proj, const QString &name, bool op=true, bool *success = 0);
     static MakefileGenerator *createMakefileGenerator(QMakeProject *proj, bool noIO = false);
+
+    static bool modesForGenerator(const QString &generator,
+                                  Option::HOST_MODE *host_mode, Option::TARG_MODE *target_mode);
 
     inline QMakeProject *projectFile() const { return project; }
 

qmake/option.cpp
@@ -96,13 +96,9 @@ QStringList Option::after_user_configs;
 QString Option::user_template;
 QString Option::user_template_prefix;
 QStringList Option::shellPath;
-#if defined(Q_OS_WIN32)
-Option::TARG_MODE Option::target_mode = Option::TARG_WIN_MODE;
-#elif defined(Q_OS_MAC)
-Option::TARG_MODE Option::target_mode = Option::TARG_MACX_MODE;
-#else
-Option::TARG_MODE Option::target_mode = Option::TARG_UNIX_MODE;
-#endif
+Option::HOST_MODE Option::host_mode = Option::HOST_UNKNOWN_MODE;
+Option::TARG_MODE Option::target_mode = Option::TARG_UNKNOWN_MODE;
+bool Option::target_mode_overridden = false;
 
 //QMAKE_*_PROPERTY stuff
 QStringList Option::prop::properties;
@@ -251,11 +247,17 @@ Option::parseCommandLine(int argc, char **argv, int skip)
             } else if(opt == "tp" || opt == "template_prefix") {
                 Option::user_template_prefix = argv[++x];
             } else if(opt == "macx") {
+                Option::host_mode = HOST_MACX_MODE;
                 Option::target_mode = TARG_MACX_MODE;
+                Option::target_mode_overridden = true;
             } else if(opt == "unix") {
+                Option::host_mode = HOST_UNIX_MODE;
                 Option::target_mode = TARG_UNIX_MODE;
+                Option::target_mode_overridden = true;
             } else if(opt == "win32") {
+                Option::host_mode = HOST_WIN_MODE;
                 Option::target_mode = TARG_WIN_MODE;
+                Option::target_mode_overridden = true;
             } else if(opt == "d") {
                 Option::debug_level++;
             } else if(opt == "version" || opt == "v" || opt == "-version") {
@@ -405,6 +407,7 @@ Option::init(int argc, char **argv)
 #ifdef Q_OS_WIN
     Option::dirlist_sep = ";";
     Option::shellPath = detectShellPath();
+    Option::res_ext = ".res";
 #else
     Option::dirlist_sep = ":";
 #endif
@@ -525,7 +528,8 @@ Option::init(int argc, char **argv)
     }
 
     //defaults for globals
-    applyHostMode();
+    if (Option::host_mode != Option::HOST_UNKNOWN_MODE)
+        applyHostMode();
     return QMAKE_CMDLINE_SUCCESS;
 }
 

qmake/option.h
@@ -152,8 +152,11 @@ struct Option
     enum QMAKE_RECURSIVE { QMAKE_RECURSIVE_DEFAULT, QMAKE_RECURSIVE_YES, QMAKE_RECURSIVE_NO };
     static QMAKE_RECURSIVE recursive;
     static QStringList before_user_vars, after_user_vars, user_configs, after_user_configs;
-    enum TARG_MODE { TARG_UNIX_MODE, TARG_WIN_MODE, TARG_MACX_MODE };
+    enum HOST_MODE { HOST_UNKNOWN_MODE, HOST_UNIX_MODE, HOST_WIN_MODE, HOST_MACX_MODE };
+    static HOST_MODE host_mode;
+    enum TARG_MODE { TARG_UNKNOWN_MODE, TARG_UNIX_MODE, TARG_WIN_MODE, TARG_MACX_MODE };
     static TARG_MODE target_mode;
+    static bool target_mode_overridden;
     static QString user_template, user_template_prefix;
     static QStringList shellPath;
 

qmake/project.h
@@ -106,6 +106,7 @@ class QMakeProject
     QStringList doVariableReplaceExpand(const QString &str, QMap<QString, QStringList> &place, bool *ok=0);
     void init(QMakeProperty *, const QMap<QString, QStringList> *);
     QStringList &values(const QString &v, QMap<QString, QStringList> &place);
+    void validateModes();
 
 public:
     QMakeProject() { init(0, 0); }

commit f1c74441b17dde565a91ab596cc1b9a920c2ab41

fix cetest build properly
Commit f5b19c173109c53bf3d8167573f7276cf39262d2 broke the build for
cetest. Reverting my initial naive attempt to fix this
bd5d323373dbaf9d827126b77895da253128c1e5.

We're introducing a new define for building qmake without generators.
QT_QMAKE_PARSER_ONLY is used for cetest and the qmake COM wrapper of
the Visual Studio Add-in.

Reviewed-by: ossi

qmake/generators/metamakefile.cpp
@@ -58,6 +58,8 @@ MetaMakefileGenerator::~MetaMakefileGenerator()
         delete project;
 }
 
+#ifndef QT_QMAKE_PARSER_ONLY
+
 class BuildsMetaMakefileGenerator : public MetaMakefileGenerator
 {
 private:
@@ -489,6 +491,25 @@ MetaMakefileGenerator::createMakefileGenerator(QMakeProject *proj, bool noIO)
     return mkfile;
 }
 
+MetaMakefileGenerator *
+MetaMakefileGenerator::createMetaGenerator(QMakeProject *proj, const QString &name, bool op, bool *success)
+{
+    MetaMakefileGenerator *ret = 0;
+    if ((Option::qmake_mode == Option::QMAKE_GENERATE_MAKEFILE ||
+         Option::qmake_mode == Option::QMAKE_GENERATE_PRL)) {
+        if (proj->first("TEMPLATE").endsWith("subdirs"))
+            ret = new SubdirsMetaMakefileGenerator(proj, name, op);
+    }
+    if (!ret)
+        ret = new BuildsMetaMakefileGenerator(proj, name, op);
+    bool res = ret->init();
+    if (success)
+        *success = res;
+    return ret;
+}
+
+#endif // QT_QMAKE_PARSER_ONLY
+
 bool
 MetaMakefileGenerator::modesForGenerator(const QString &gen,
         Option::HOST_MODE *host_mode, Option::TARG_MODE *target_mode)
@@ -521,23 +542,6 @@ MetaMakefileGenerator::modesForGenerator(const QString &gen,
         return false;
     }
     return true;
-}
-
-MetaMakefileGenerator *
-MetaMakefileGenerator::createMetaGenerator(QMakeProject *proj, const QString &name, bool op, bool *success)
-{
-    MetaMakefileGenerator *ret = 0;
-    if ((Option::qmake_mode == Option::QMAKE_GENERATE_MAKEFILE ||
-         Option::qmake_mode == Option::QMAKE_GENERATE_PRL)) {
-        if (proj->first("TEMPLATE").endsWith("subdirs"))
-            ret = new SubdirsMetaMakefileGenerator(proj, name, op);
-    }
-    if (!ret)
-        ret = new BuildsMetaMakefileGenerator(proj, name, op);
-    bool res = ret->init();
-    if (success)
-        *success = res;
-    return ret;
 }
 
 QT_END_NAMESPACE

qmake/project.cpp
@@ -1507,7 +1507,6 @@ QMakeProject::read(uchar cmd)
 
 void QMakeProject::validateModes()
 {
-#if !defined(QT_BUILD_QMAKE_NO_GENERATORS)
     if (Option::host_mode == Option::HOST_UNKNOWN_MODE
         || Option::target_mode == Option::TARG_UNKNOWN_MODE) {
         Option::HOST_MODE host_mode;
@@ -1544,7 +1543,6 @@ void QMakeProject::validateModes()
             }
         }
     }
-#endif // !defined(QT_BUILD_QMAKE_NO_GENERATORS)
 }
 
 bool

tools/qtestlib/wince/cetest/cetest.pro
@@ -13,8 +13,7 @@ DEFINES        += QT_BUILD_QMAKE QT_BOOTSTRAPPED QT_NO_CODECS QT_LITE_UNICODE QT
                   QT_NO_STL QT_NO_COMPRESS QT_NO_DATASTREAM  \
                   QT_NO_TEXTCODEC QT_NO_UNICODETABLES QT_NO_THREAD \
                   QT_NO_SYSTEMLOCALE QT_NO_GEOM_VARIANT \
-                  QT_NODLL QT_NO_QOBJECT \
-                  QT_BUILD_QMAKE_NO_GENERATORS
+                  QT_NODLL QT_NO_QOBJECT
 
 INCLUDEPATH = \
               $$QT_SOURCE_TREE/tools/qtestlib/ce/cetest \

tools/qtestlib/wince/cetest/qmake_include.pri
@@ -5,7 +5,9 @@ SOURCES += \
            $$QT_SOURCE_TREE/qmake/option.cpp \
            $$QT_SOURCE_TREE/qmake/project.cpp \
            $$QT_SOURCE_TREE/qmake/property.cpp \
+           $$QT_SOURCE_TREE/qmake/generators/metamakefile.cpp \
            $$QT_SOURCE_TREE/qmake/generators/symbian/initprojectdeploy_symbian.cpp \
            $$QT_SOURCE_TREE/tools/shared/symbian/epocroot.cpp \
            $$QT_SOURCE_TREE/tools/shared/windows/registry.cpp
 
+DEFINES += QT_QMAKE_PARSER_ONLY