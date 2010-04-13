a165d1cfbb15ced6079a0d752fbdb37a478ff46f
Thierry Bastian <thierry.bastian@nokia.com>
2010-04-13 14:18:03 +0200

    Removed double setting of _WIN32_WINNT
    Derived from the comments in merge request #2175

--- src/corelib/thread/qthread_win.cpp
+++ src/corelib/thread/qthread_win.cpp
@@ -39,12 +39,6 @@
 **
 ****************************************************************************/
 
-//#define WINVER 0x0500
-#if _WIN32_WINNT < 0x0400
-#define _WIN32_WINNT 0x0400
-#endif
-
-
 #include "qthread.h"
 #include "qthread_p.h"
 #include "qthreadstorage.h"
--- src/gui/styles/qwindowsxpstyle_p.h
+++ src/gui/styles/qwindowsxpstyle_p.h
@@ -59,15 +59,10 @@
 #include <qt_windows.h>
 
 // Note, these tests are duplicated in qwizard_win.cpp.
-#ifdef Q_CC_GNU
-#  include <w32api.h>
-#  if (__W32API_MAJOR_VERSION >= 3 || (__W32API_MAJOR_VERSION == 2 && __W32API_MINOR_VERSION >= 5))
-#    ifdef _WIN32_WINNT
-#      undef _WIN32_WINNT
-#    endif
-#    define _WIN32_WINNT 0x0501
-#    include <commctrl.h>
-#  endif
+// minimum version for Windows XP is 0x501
+#if _WIN32_WINNT < 0x0501
+#  undef _WIN32_WINNT
+#  define _WIN32_WINNT 0x0501
 #endif
 
 #include <uxtheme.h>
