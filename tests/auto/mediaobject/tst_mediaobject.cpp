a484dd7cf655818aa849cf9dd002f76e137b3319
Gareth Stockwell <ext-gareth.stockwell@nokia.com>
2010-04-13 16:22:33 +0100

    tst_mediaobject: Removed compiler warnings
    Removed declaration of variables which are subsequently unused.
    
    Reviewed-by: Frans Englich

--- tests/auto/mediaobject/tst_mediaobject.cpp
+++ tests/auto/mediaobject/tst_mediaobject.cpp
@@ -194,12 +194,6 @@ static qint32 castQVariantToInt32(const QVariant &variant)
 }
 
 static const char *const red    = "\033[0;31m";
-static const char *const green  = "\033[0;32m";
-static const char *const yellow = "\033[0;33m";
-static const char *const blue   = "\033[0;34m";
-static const char *const purple = "\033[0;35m";
-static const char *const cyan   = "\033[0;36m";
-static const char *const white  = "\033[0;37m";
 static const char *const normal = "\033[0m";
 
 void tst_MediaObject::stateChanged(Phonon::State newstate, Phonon::State oldstate)
