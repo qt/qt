6b0e7b394452eaae091ddbce677002a9c3fbaa22
Martin Smith <msmith@trolltech.com>
2010-04-14 10:46:45 +0200

    qdoc: Checked for empty title. (cherry picked from commit 4607210dfef3b51933f03deab1037c98cbb8869c)

--- tools/qdoc3/htmlgenerator.cpp
+++ tools/qdoc3/htmlgenerator.cpp
@@ -1889,7 +1889,8 @@ void HtmlGenerator::generateTitle(const QString& title,
                                   const Node *relative,
                                   CodeMarker *marker)
 {
-    out() << "<h1 class=\"title\">" << protectEnc(title);
+    if (!title.isEmpty())
+        out() << "<h1 class=\"title\">" << protectEnc(title);
     if (!subTitle.isEmpty()) {
         out() << "<br />";
         if (subTitleSize == SmallSubTitle)
@@ -1899,7 +1900,8 @@ void HtmlGenerator::generateTitle(const QString& title,
         generateText(subTitle, relative, marker);
         out() << "</span>\n";
     }
-    out() << "</h1>\n";
+    if (!title.isEmpty())
+        out() << "</h1>\n";
 }
 
 void HtmlGenerator::generateFooter(const Node *node)
