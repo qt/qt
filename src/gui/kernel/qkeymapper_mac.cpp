qt/qt

commit 8da880e77db04cc4509e0f48e0b5b1d6265da223

QTextEdit character are lost after special characters like ^ ´ ` This happens only on keyboard layouts like French. The is mainly due to the key event processing done by the Input manager. In carbon, the key down event has to be replayed after the input manager finishes his processing. In Cocoa, while unmarking we have to accept the current text.
Task-number: 123740
Reviewed-by: nrc

src/gui/kernel/qkeymapper_mac.cpp
@@ -48,6 +48,7 @@
 #include <qinputcontext.h>
 #include <private/qkeymapper_p.h>
 #include <private/qapplication_p.h>
+#include <private/qmacinputcontext_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -480,7 +481,8 @@ static bool translateKeyEventInternal(EventHandlerCallRef er, EventRef keyEvent,
 #ifdef QT_MAC_USE_COCOA
             if (outHandled) {
                 qt_mac_eat_unicode_key = false;
-                CallNextEventHandler(er, keyEvent);
+                if (er)
+                    CallNextEventHandler(er, keyEvent);
                 *outHandled = qt_mac_eat_unicode_key;
             }
 #endif
@@ -692,8 +694,14 @@ bool QKeyMapperPrivate::translateKeyEvent(QWidget *widget, EventHandlerCallRef e
         return true;
     }
 
-    if (qApp->inputContext() && qApp->inputContext()->isComposing())
+    if (qApp->inputContext() && qApp->inputContext()->isComposing()) {
+        if (ekind == kEventRawKeyDown) {
+            QMacInputContext *context = qobject_cast<QMacInputContext*>(qApp->inputContext());
+            if (context)
+                context->setLastKeydownEvent(event);
+        }
         return false;
+    }
     //get modifiers
     Qt::KeyboardModifiers modifiers;
     int qtKey;
@@ -721,7 +729,8 @@ bool QKeyMapperPrivate::translateKeyEvent(QWidget *widget, EventHandlerCallRef e
             //is it of use to text services? If so we won't bother
             //with a QKeyEvent.
             qt_mac_eat_unicode_key = false;
-            CallNextEventHandler(er, event);	
+            if (er)
+                CallNextEventHandler(er, event);
             extern bool qt_mac_menubar_is_open();   
             if (qt_mac_eat_unicode_key || qt_mac_menubar_is_open()) {
                 return true;

src/gui/inputmethod/qmacinputcontext_mac.cpp
@@ -45,6 +45,7 @@
 #include "qtextformat.h"
 #include <qdebug.h>
 #include <private/qapplication_p.h>
+#include <private/qkeymapper_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -63,7 +64,8 @@ static QTextFormat qt_mac_compose_format()
 }
 
 QMacInputContext::QMacInputContext(QObject *parent)
-    : QInputContext(parent), composing(false), recursionGuard(false), textDocument(0)
+    : QInputContext(parent), composing(false), recursionGuard(false), textDocument(0),
+      keydownEvent(0)
 {
 //    createTextDocument();
 }
@@ -181,6 +183,16 @@ QMacInputContext::cleanup()
         input_proc_handlerUPP = 0;
     }
 #endif
+}
+
+void QMacInputContext::setLastKeydownEvent(EventRef event)
+{
+    EventRef tmpEvent = keydownEvent;
+    keydownEvent = event;
+    if (keydownEvent)
+        RetainEvent(keydownEvent);
+    if (tmpEvent)
+        ReleaseEvent(tmpEvent);
 }
 
 OSStatus
@@ -335,6 +347,12 @@ QMacInputContext::globalEventProcessor(EventHandlerCallRef, EventRef event, void
             GetEventParameter(key_ev, kEventParamKeyMacCharCodes, typeChar, 0, sizeof(chr), 0, &chr);
             if(!chr || chr >= 128 || (text.length() > 0 && (text.length() > 1 || text.at(0) != QLatin1Char(chr))))
                 handled_event = !widget->testAttribute(Qt::WA_InputMethodEnabled);
+            QMacInputContext *context = qobject_cast<QMacInputContext*>(qApp->inputContext());
+            if (context && context->lastKeydownEvent()) {
+                qt_keymapper_private()->translateKeyEvent(widget, 0, context->lastKeydownEvent(),
+                                                          0, false);
+                context->setLastKeydownEvent(0);
+            }
         }
         break; }
     default:

src/gui/inputmethod/qmacinputcontext_p.h
@@ -78,6 +78,10 @@ public:
     static OSStatus globalEventProcessor(EventHandlerCallRef, EventRef, void *);
     static void initialize();
     static void cleanup();
+
+    EventRef lastKeydownEvent() { return keydownEvent; }
+    void setLastKeydownEvent(EventRef);
+
 protected:
     void mouseHandler(int pos, QMouseEvent *);
 private:
@@ -85,6 +89,7 @@ private:
     bool recursionGuard;
     TSMDocumentID textDocument;
     QString currentText;
+    EventRef keydownEvent;
 };
 
 QT_END_NAMESPACE

src/gui/kernel/qcocoaview_mac.mm
@@ -195,6 +195,7 @@ extern "C" {
     if (self) {
         [self finishInitWithQWidget:widget widgetPrivate:widgetprivate];
     }
+    composingText = new QString();
     composing = false;
     sendKeyEvents = true;
     [self setHidden:YES];
@@ -364,6 +365,7 @@ extern "C" {
 
 - (void)dealloc
 {
+    delete composingText;
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     [super dealloc];
 }
@@ -917,7 +919,7 @@ extern "C" {
 
 - (void) insertText:(id)aString
 {
-    if (composing) {
+    if ([aString length]) {
         // Send the commit string to the widget.
         QString commitText;
         if ([aString isKindOfClass:[NSAttributedString class]]) {
@@ -931,6 +933,7 @@ extern "C" {
         e.setCommitString(commitText);
         qt_sendSpontaneousEvent(qwidget, &e);
     }
+    composingText->clear();
 }
 
 - (void) setMarkedText:(id)aString selectedRange:(NSRange)selRange
@@ -984,12 +987,21 @@ extern "C" {
         attrs<<QInputMethodEvent::Attribute(QInputMethodEvent::TextFormat,
                                             0, composingLength, format);
     }
+    *composingText = qtText;
     QInputMethodEvent e(qtText, attrs);
     qt_sendSpontaneousEvent(qwidget, &e);
+    if (!composingLength)
+        composing = false;
 }
 
 - (void) unmarkText
 {
+    if (composing) {
+        QInputMethodEvent e;
+        e.setCommitString(*composingText);
+        qt_sendSpontaneousEvent(qwidget, &e);
+    }
+    composingText->clear();
     composing = false;
 }
 

src/gui/kernel/qcocoaview_mac_p.h
@@ -83,6 +83,7 @@ Q_GUI_EXPORT
     bool composing;
     int composingLength;
     bool sendKeyEvents;
+    QString *composingText;
 }
 - (id)initWithQWidget:(QWidget *)widget widgetPrivate:(QWidgetPrivate *)widgetprivate;
 - (void) finishInitWithQWidget:(QWidget *)widget widgetPrivate:(QWidgetPrivate *)widgetprivate;

commit e30e3bb524536ef0f6b55272901b24bbbd4dfbd6

Diacritic input broken, duplicate the base glypha (on Mac OS X carbon)
This was a regression introduced by 8da880e77db04c. The saved key
event needs to be cleared if we process a valid raw key event.

Task-number: QTBUG-7099
Reviewed-by: Richard Moe Gustavsen

src/gui/kernel/qkeymapper_mac.cpp
@@ -756,14 +756,23 @@ bool QKeyMapperPrivate::translateKeyEvent(QWidget *widget, EventHandlerCallRef e
         return true;
     }
 
-    if (qApp->inputContext() && qApp->inputContext()->isComposing()) {
+    QInputContext *currentContext = qApp->inputContext();
+    if (currentContext && currentContext->isComposing()) {
         if (ekind == kEventRawKeyDown) {
-            QMacInputContext *context = qobject_cast<QMacInputContext*>(qApp->inputContext());
+            QMacInputContext *context = qobject_cast<QMacInputContext*>(currentContext);
             if (context)
                 context->setLastKeydownEvent(event);
         }
         return false;
     }
+    // Once we process the key down , we dont need to send the saved event again from
+    // kEventTextInputUnicodeForKeyEvent, so clear it.
+    if (currentContext && ekind == kEventRawKeyDown) {
+        QMacInputContext *context = qobject_cast<QMacInputContext*>(currentContext);
+        if (context)
+            context->setLastKeydownEvent(0);
+    }
+
     //get modifiers
     Qt::KeyboardModifiers modifiers;
     int qtKey;