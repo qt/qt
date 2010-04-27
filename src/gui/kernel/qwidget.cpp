4580c2e9c2bbcb911f9e1cecd4551054598d256d
Carlos Manuel Duclos Vergara <carlos.duclos@nokia.com>
2010-04-27 13:26:42 +0200

    QTabWidget documentMode not working correctly on Mac
    This feature was implemented in Carbon and was never ported to Cocoa.
    The major problem is the fact that Cocoa draws a line under the titlebar,
    regardless of what we say. The only way to avoid drawing that line is
    by adding a native toolbar and ask it not to draw its baseline. If there
    is not toolbar, as it happens in this case, there is no way to prevent
    that line from being drawn. So instead we paint over that line and hope
    for the best.
    
    Task-number: QTBUG-8159
    Reviewed-by: Richard Moe Gustavsen

--- src/gui/kernel/qwidget.cpp
+++ src/gui/kernel/qwidget.cpp
@@ -220,6 +220,11 @@ QWidgetPrivate::QWidgetPrivate(int version)
 
     isWidget = true;
     memset(high_attributes, 0, sizeof(high_attributes));
+#if QT_MAC_USE_COCOA
+    drawRectOriginalAdded = false;
+    originalDrawMethod = true;
+    changeMethods = false;
+#endif // QT_MAC_USE_COCOA
 #ifdef QWIDGET_EXTRA_DEBUG
     static int count = 0;
     qDebug() << "widgets" << ++count;
@@ -12291,6 +12296,28 @@ void QWidgetPrivate::_q_delayedDestroy(WId winId)
     delete winId;
 }
 #endif
+
+#if QT_MAC_USE_COCOA
+void QWidgetPrivate::syncUnifiedMode() {
+    // The whole purpose of this method is to keep the unifiedToolbar in sync.
+    // That means making sure we either exchange the drawing methods or we let
+    // the toolbar know that it does not require to draw the baseline.
+    Q_Q(QWidget);
+    // This function makes sense only if this is a top level
+    if(!q->isWindow())
+        return;
+    OSWindowRef window = qt_mac_window_for(q);
+    if(changeMethods) {
+        // Ok, we are in documentMode.
+        if(originalDrawMethod)
+            qt_mac_replaceDrawRect(window, this);
+    } else {
+        if(!originalDrawMethod)
+            qt_mac_replaceDrawRectOriginal(window, this);
+    }
+}
+
+#endif // QT_MAC_USE_COCOA
 
 QT_END_NAMESPACE
 
