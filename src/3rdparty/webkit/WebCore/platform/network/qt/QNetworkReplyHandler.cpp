3eec5af3f07fdab9084f40d6955411a667b433c4
Simon Hausmann <simon.hausmann@nokia.com>
2010-05-11 12:26:30 +0200

    Updated WebKit to b4aa5e1ddc41edab895132aba3cc66d9d7129444
    Integrated changes:
    
    || <https://webkit.org/b/38747> || mingw-w64 and JIT support ||
    || <https://webkit.org/b/38119> || [Qt] QWebPage viewMode property ||
    || <https://webkit.org/b/37097> || [Qt] Fix infinite redirection loop in QNetworkReplyHandler ||
    || <https://webkit.org/b/38668> || [Qt] Enable YARR_JIT for X86 Mac for QtWebKit ||
    || <https://webkit.org/b/38834> || [Qt] Adapt DNS pre-fetching to Qt DNS cache code ||
    || <https://webkit.org/b/38574> || [Qt] Crash in QGraphicsWebViewPrivate::~QGraphicsWebViewPrivate when animation were used ||
    || <https://webkit.org/b/31407> || [PATCH] [Qt] Compilation with Plugins disabled is broken ||
    || <https://webkit.org/b/38514> || Crash in handleTouchEvent: using dangling node ptrs in hashmap ||
    || <https://webkit.org/b/38646> || Potential crash in EventHandler::handleTouchEvent ||
    || <https://webkit.org/b/38488> || Spatial Navigation: create a getter for the "fudgeFactor" ||
    || <https://webkit.org/b/38523> || [Qt] QtWebKit has render bugs on Google Maps markers ||
    || <https://webkit.org/b/37276> || LayoutTests/fast/canvas/pointInPath.html passed, actually it failed ||
    || <https://webkit.org/b/37023> || [Qt] Expose HTMLTokenizer yielding parameters ||

--- src/3rdparty/webkit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
+++ src/3rdparty/webkit/WebCore/platform/network/qt/QNetworkReplyHandler.cpp
@@ -49,6 +49,7 @@
 #define SIGNAL_CONN Qt::QueuedConnection
 #endif
 
+static const int gMaxRecursionLimit = 10;
 
 namespace WebCore {
 
@@ -139,6 +140,7 @@ QNetworkReplyHandler::QNetworkReplyHandler(ResourceHandle* handle, LoadMode load
     , m_shouldFinish(false)
     , m_shouldSendResponse(false)
     , m_shouldForwardData(false)
+    , m_redirectionTries(gMaxRecursionLimit)
 {
     const ResourceRequest &r = m_resourceHandle->request();
 
@@ -336,9 +338,18 @@ void QNetworkReplyHandler::sendResponseIfNeeded()
 
     QUrl redirection = m_reply->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
     if (redirection.isValid()) {
+        QUrl newUrl = m_reply->url().resolved(redirection);
+
+        m_redirectionTries--;
+        if (m_redirectionTries == 0) { // 10 or more redirections to the same url is considered infinite recursion
+            ResourceError error(newUrl.host(), 400 /*bad request*/,
+                                newUrl.toString(),
+                                QCoreApplication::translate("QWebPage", "Redirection limit reached"));
+            client->didFail(m_resourceHandle, error);
+            return;
+        }
         m_redirected = true;
 
-        QUrl newUrl = m_reply->url().resolved(redirection);
         ResourceRequest newRequest = m_resourceHandle->request();
         newRequest.setURL(newUrl);
 
