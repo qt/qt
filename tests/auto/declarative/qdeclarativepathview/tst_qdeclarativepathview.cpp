ae8e4afcadc9ff084e1d1859c29fbc8b629e3392
Martin Jones <martin.jones@nokia.com>
2010-05-13 15:07:21 +1000

    Add an example spinner. Also add missing increment/decrementCurrentIndex() slots to PathView, and tweak the number of points cached along a Path.

--- tests/auto/declarative/qdeclarativepathview/tst_qdeclarativepathview.cpp
+++ tests/auto/declarative/qdeclarativepathview/tst_qdeclarativepathview.cpp
@@ -438,7 +438,8 @@ void tst_QDeclarativePathView::pathMoved()
 
     for(int i=0; i<model.count(); i++){
         QDeclarativeRectangle *curItem = findItem<QDeclarativeRectangle>(pathview, "wrapper", i);
-        QCOMPARE(curItem->pos() + offset, path->pointAt(0.25 + i*0.25));
+        QPointF itemPos(path->pointAt(0.25 + i*0.25));
+        QCOMPARE(curItem->pos() + offset, QPointF(qRound(itemPos.x()), qRound(itemPos.y())));
     }
 
     pathview->setOffset(0.0);
@@ -479,12 +480,35 @@ void tst_QDeclarativePathView::setCurrentIndex()
     QCOMPARE(canvas->rootObject()->property("currentB").toInt(), 0);
 
     pathview->setCurrentIndex(2);
-    QTest::qWait(1000);
 
     firstItem = findItem<QDeclarativeRectangle>(pathview, "wrapper", 2);
-    QCOMPARE(firstItem->pos() + offset, start);
+    QTRY_COMPARE(firstItem->pos() + offset, start);
     QCOMPARE(canvas->rootObject()->property("currentA").toInt(), 2);
     QCOMPARE(canvas->rootObject()->property("currentB").toInt(), 2);
+
+    pathview->decrementCurrentIndex();
+    QTRY_COMPARE(pathview->currentIndex(), 1);
+    firstItem = findItem<QDeclarativeRectangle>(pathview, "wrapper", 1);
+    QVERIFY(firstItem);
+    QTRY_COMPARE(firstItem->pos() + offset, start);
+
+    pathview->decrementCurrentIndex();
+    QTRY_COMPARE(pathview->currentIndex(), 0);
+    firstItem = findItem<QDeclarativeRectangle>(pathview, "wrapper", 0);
+    QVERIFY(firstItem);
+    QTRY_COMPARE(firstItem->pos() + offset, start);
+
+    pathview->decrementCurrentIndex();
+    QTRY_COMPARE(pathview->currentIndex(), 3);
+    firstItem = findItem<QDeclarativeRectangle>(pathview, "wrapper", 3);
+    QVERIFY(firstItem);
+    QTRY_COMPARE(firstItem->pos() + offset, start);
+
+    pathview->incrementCurrentIndex();
+    QTRY_COMPARE(pathview->currentIndex(), 0);
+    firstItem = findItem<QDeclarativeRectangle>(pathview, "wrapper", 0);
+    QVERIFY(firstItem);
+    QTRY_COMPARE(firstItem->pos() + offset, start);
 
     delete canvas;
 }
