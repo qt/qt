f752b927c6f7115646da7e86209e60e7c17e7d95
Iain <qt-info@nokia.com>
2010-05-14 14:32:04 +0200

    Shadow building on Symbian fixes: part 1 - files to right place
    First in a set of fixes for shadow building on Symbian - make sure we
    generate all the files into the shadow directory, *not* the source
    directory
    
    Task-number: QTBUG-10432
    Reviewed-by: Miikka Heikkinen

--- qmake/generators/symbian/symmake.cpp
+++ qmake/generators/symbian/symmake.cpp
@@ -214,7 +214,7 @@ bool SymbianMakefileGenerator::writeMakefile(QTextStream &t)
     // Generate empty wrapper makefile here, because wrapper makefile must exist before writeMkFile,
     // but all required data is not yet available.
     bool isPrimaryMakefile = true;
-    QString wrapperFileName("Makefile");
+    QString wrapperFileName = Option::output_dir + QLatin1Char('/') + QLatin1String("Makefile");
     QString outputFileName = fileInfo(Option::output.fileName()).fileName();
     if (outputFileName != BLD_INF_FILENAME) {
         wrapperFileName.append(".").append(outputFileName.startsWith(BLD_INF_FILENAME)
@@ -246,10 +246,8 @@ bool SymbianMakefileGenerator::writeMakefile(QTextStream &t)
     shortProFilename.replace(0, shortProFilename.lastIndexOf("/") + 1, QString(""));
     shortProFilename.replace(Option::pro_ext, QString(""));
 
-    QString mmpFilename = shortProFilename;
-    mmpFilename.append("_");
-    mmpFilename.append(uid3);
-    mmpFilename.append(Option::mmp_ext);
+    QString mmpFilename = Option::output_dir + QLatin1Char('/') + shortProFilename + QLatin1Char('_')
+                          + uid3 + Option::mmp_ext;
     writeMmpFile(mmpFilename, symbianLangCodes);
 
     if (targetType == TypeExe) {
@@ -270,7 +268,7 @@ void SymbianMakefileGenerator::writeCustomDefFile()
 {
     if (targetType == TypePlugin && !project->isActiveConfig("stdbinary")) {
         // Create custom def file for plugin
-        QFile ft(QLatin1String(PLUGIN_COMMON_DEF_FILE_ACTUAL));
+        QFile ft(Option::output_dir + QLatin1Char('/') + QLatin1String(PLUGIN_COMMON_DEF_FILE_ACTUAL));
 
         if (ft.open(QIODevice::WriteOnly)) {
             generatedFiles << ft.fileName();
