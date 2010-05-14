d53b7477039ee9394a798b09ddc8c20323959c01
Denis Dzyubenko <denis.dzyubenko@nokia.com>
2010-05-14 17:03:32 +0200

    Added Korean and Nynorsk locales support on Symbian.
    Task-number: QT-3368
    Task-number: QT-3370
    Reviewed-by: Thiago Macieira

--- src/corelib/tools/qlocale_symbian.cpp
+++ src/corelib/tools/qlocale_symbian.cpp
@@ -98,6 +98,7 @@ static const symbianToISO symbian_to_iso_list[] = {
     { ELangSwedish,             "sv_SE" },
     { ELangDanish,              "da_DK" },
     { ELangNorwegian,           "no_NO" },
+    { ELangNorwegianNynorsk,    "nn_NO" },
     { ELangFinnish,             "fi_FI" },
     { ELangAmerican,            "en_US" },
     { ELangPortuguese,          "pt_PT" },
@@ -138,6 +139,7 @@ static const symbianToISO symbian_to_iso_list[] = {
     { ELangUkrainian,           "uk_UA" },
     { ELangUrdu,                "ur_PK" }, // India/Pakistan
     { ELangVietnamese,          "vi_VN" },
+    { ELangKorean,              "ko_KO" },
 #ifdef __E32LANG_H__
 // 5.0
     { ELangBasque,              "eu_ES" },
