qt/qt

commit 645b9ee9dd6e0576542cc61872ecedb408ca8a89

Add Qt.include() method for scoped inclusion of JavaScript files

src/declarative/qml/qdeclarativeinclude.cpp
@@ -1,0 +1,255 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtDeclarative module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qdeclarativeinclude_p.h"
+
+#include <QtScript/qscriptengine>
+#include <QtNetwork/qnetworkrequest.h>
+#include <QtNetwork/qnetworkreply.h>
+#include <QtCore/qfile.h>
+
+#include <private/qdeclarativeengine_p.h>
+#include <private/qdeclarativeglobalscriptclass_p.h>
+
+QT_BEGIN_NAMESPACE
+
+QDeclarativeInclude::QDeclarativeInclude(const QUrl &url, 
+                                                       QDeclarativeEngine *engine, 
+                                                       QScriptContext *ctxt)
+: QObject(engine), m_engine(engine), m_network(0), m_reply(0), m_url(url), m_redirectCount(0)
+{
+    QDeclarativeEnginePrivate *ep = QDeclarativeEnginePrivate::get(engine);
+    m_context = ep->contextClass->contextFromValue(QScriptDeclarativeClass::scopeChainValue(ctxt, -3));
+
+    m_scope[0] = QScriptDeclarativeClass::scopeChainValue(ctxt, -4);
+    m_scope[1] = QScriptDeclarativeClass::scopeChainValue(ctxt, -5);
+
+    m_scriptEngine = QDeclarativeEnginePrivate::getScriptEngine(engine);
+    m_network = QDeclarativeScriptEngine::get(m_scriptEngine)->networkAccessManager();
+
+    m_result = resultValue(m_scriptEngine);
+
+    QNetworkRequest request;
+    request.setUrl(url);
+
+    m_reply = m_network->get(request);
+    QObject::connect(m_reply, SIGNAL(finished()), this, SLOT(finished()));
+}
+
+QDeclarativeInclude::~QDeclarativeInclude()
+{
+    if (m_reply)
+        delete m_reply;
+}
+
+QScriptValue QDeclarativeInclude::resultValue(QScriptEngine *engine, Status status)
+{
+    QScriptValue result = engine->newObject();
+    result.setProperty(QLatin1String("OK"), QScriptValue(engine, Ok));
+    result.setProperty(QLatin1String("LOADING"), QScriptValue(engine, Loading));
+    result.setProperty(QLatin1String("NETWORK_ERROR"), QScriptValue(engine, NetworkError));
+    result.setProperty(QLatin1String("EXCEPTION"), QScriptValue(engine, Exception));
+
+    result.setProperty(QLatin1String("status"), QScriptValue(engine, status));
+    return result;
+}
+
+QScriptValue QDeclarativeInclude::result() const
+{
+    return m_result;
+}
+
+void QDeclarativeInclude::setCallback(const QScriptValue &c)
+{
+    m_callback = c;
+}
+
+QScriptValue QDeclarativeInclude::callback() const
+{
+    return m_callback;
+}
+
+#define INCLUDE_MAXIMUM_REDIRECT_RECURSION 15
+void QDeclarativeInclude::finished()
+{
+    m_redirectCount++;
+
+    if (m_redirectCount < INCLUDE_MAXIMUM_REDIRECT_RECURSION) {
+        QVariant redirect = m_reply->attribute(QNetworkRequest::RedirectionTargetAttribute);
+        if (redirect.isValid()) {
+            m_url = m_url.resolved(redirect.toUrl());
+            delete m_reply;
+            
+            QNetworkRequest request;
+            request.setUrl(m_url);
+
+            m_reply = m_network->get(request);
+            QObject::connect(m_reply, SIGNAL(finished()), this, SLOT(finished()));
+            return;
+        }
+    }
+
+    if (m_reply->error() == QNetworkReply::NoError) {
+        QDeclarativeEnginePrivate *ep = QDeclarativeEnginePrivate::get(m_engine);
+
+        QByteArray data = m_reply->readAll();
+
+        QString code = QString::fromUtf8(data);
+
+        QString urlString = m_url.toString();
+        QScriptContext *scriptContext = QScriptDeclarativeClass::pushCleanContext(m_scriptEngine);
+        scriptContext->pushScope(ep->contextClass->newUrlContext(m_context, 0, urlString));
+        scriptContext->pushScope(m_scope[0]);
+
+        scriptContext->pushScope(m_scope[1]);
+        scriptContext->setActivationObject(m_scope[1]);
+
+        m_scriptEngine->evaluate(code, urlString, 1);
+
+        m_scriptEngine->popContext();
+
+        if (m_scriptEngine->hasUncaughtException()) {
+            m_result.setProperty(QLatin1String("status"), QScriptValue(m_scriptEngine, Exception));
+            m_result.setProperty(QLatin1String("exception"), m_scriptEngine->uncaughtException());
+        } else {
+            m_result.setProperty(QLatin1String("status"), QScriptValue(m_scriptEngine, Ok));
+        }
+    } else {
+        m_result.setProperty(QLatin1String("status"), QScriptValue(m_scriptEngine, NetworkError));
+    }
+
+    callback(m_scriptEngine, m_callback, m_result);
+
+    delete this;
+}
+
+void QDeclarativeInclude::callback(QScriptEngine *engine, QScriptValue &callback, QScriptValue &status)
+{
+    if (callback.isValid()) {
+        QScriptValue args = engine->newArray(1);
+        args.setProperty(0, status);
+        callback.call(QScriptValue(), args);
+    }
+}
+
+static QString toLocalFileOrQrc(const QUrl& url)
+{
+    if (url.scheme() == QLatin1String("qrc")) {
+        if (url.authority().isEmpty())
+            return QLatin1Char(':') + url.path();
+        return QString();
+    }
+    return url.toLocalFile();
+}
+
+QScriptValue QDeclarativeInclude::include(QScriptContext *ctxt, QScriptEngine *engine)
+{
+    if (ctxt->argumentCount() == 0)
+        return engine->undefinedValue();
+
+    QDeclarativeEnginePrivate *ep = QDeclarativeEnginePrivate::get(engine);
+
+    QUrl contextUrl = ep->contextClass->urlFromValue(QScriptDeclarativeClass::scopeChainValue(ctxt, -3));
+    if (contextUrl.isEmpty()) 
+        return ctxt->throwError(QLatin1String("Qt.include(): Can only be called from JavaScript files"));
+
+    QString urlString = ctxt->argument(0).toString();
+    QUrl url(ctxt->argument(0).toString());
+    if (url.isRelative()) {
+        url = QUrl(contextUrl).resolved(url);
+        urlString = url.toString();
+    }
+
+    QString localFile = toLocalFileOrQrc(url);
+
+    QScriptValue func = ctxt->argument(1);
+    if (!func.isFunction())
+        func = QScriptValue();
+
+    QScriptValue result;
+    if (localFile.isEmpty()) {
+        QDeclarativeInclude *i = 
+            new QDeclarativeInclude(url, QDeclarativeEnginePrivate::getEngine(engine), ctxt);
+
+        if (func.isValid())
+            i->setCallback(func);
+
+        result = i->result();
+    } else {
+
+        QFile f(localFile);
+        if (f.open(QIODevice::ReadOnly)) {
+            QByteArray data = f.readAll();
+            QString code = QString::fromUtf8(data);
+
+            QDeclarativeContextData *context = 
+                ep->contextClass->contextFromValue(QScriptDeclarativeClass::scopeChainValue(ctxt, -3));
+
+            QScriptContext *scriptContext = QScriptDeclarativeClass::pushCleanContext(engine);
+            scriptContext->pushScope(ep->contextClass->newUrlContext(context, 0, urlString));
+            scriptContext->pushScope(ep->globalClass->globalObject());
+            QScriptValue scope = QScriptDeclarativeClass::scopeChainValue(ctxt, -5);
+            scriptContext->pushScope(scope);
+            scriptContext->setActivationObject(scope);
+
+            engine->evaluate(code, urlString, 1);
+
+            engine->popContext();
+
+            if (engine->hasUncaughtException()) {
+                result = resultValue(engine, Exception);
+                result.setProperty(QLatin1String("exception"), engine->uncaughtException());
+            } else {
+                result = resultValue(engine, Ok);
+            }
+            callback(engine, func, result);
+        } else {
+            result = resultValue(engine, NetworkError);
+            callback(engine, func, result);
+        }
+    }
+
+    return result;
+}
+
+
+
+QT_END_NAMESPACE

src/declarative/qml/qdeclarativeinclude_p.h
@@ -1,0 +1,112 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtDeclarative module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QDECLARATIVEINCLUDE_P_H
+#define QDECLARATIVEINCLUDE_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtCore/qobject.h>
+#include <QtCore/qurl.h>
+#include <QtScript/qscriptvalue.h>
+
+#include <private/qdeclarativecontext_p.h>
+
+QT_BEGIN_NAMESPACE
+
+class QDeclarativeEngine;
+class QScriptContext;
+class QScriptEngine;
+class QNetworkAccessManager;
+class QNetworkReply;
+class QDeclarativeInclude : public QObject
+{
+    Q_OBJECT
+public:
+    enum Status {
+        Ok = 0,
+        Loading = 1,
+        NetworkError = 2,
+        Exception = 3
+    };
+
+    QDeclarativeInclude(const QUrl &, QDeclarativeEngine *, QScriptContext *ctxt);
+    ~QDeclarativeInclude();
+
+    void setCallback(const QScriptValue &);
+    QScriptValue callback() const;
+
+    QScriptValue result() const;
+
+    static QScriptValue resultValue(QScriptEngine *, Status status = Loading);
+    static void callback(QScriptEngine *, QScriptValue &callback, QScriptValue &status);
+    static QScriptValue include(QScriptContext *ctxt, QScriptEngine *engine);
+
+public slots:
+    void finished();
+
+private:
+    QDeclarativeEngine *m_engine;
+    QScriptEngine *m_scriptEngine;
+    QNetworkAccessManager *m_network;
+    QNetworkReply *m_reply;
+
+    QUrl m_url;
+    int m_redirectCount;
+    QScriptValue m_callback;
+    QScriptValue m_result;
+    QDeclarativeGuardedContextData m_context;
+    QScriptValue m_scope[2];
+};
+
+QT_END_NAMESPACE
+
+#endif // QDECLARATIVEINCLUDE_P_H
+

tests/auto/declarative/qdeclarativeecmascript/data/exception.js
@@ -1,0 +1,1 @@
+throw("Whoops!");

tests/auto/declarative/qdeclarativeecmascript/data/include.js
@@ -1,0 +1,8 @@
+var test1 = true
+var test2 = false
+var test3 = false
+
+function go() {
+    Qt.include("js/include2.js");
+}
+

tests/auto/declarative/qdeclarativeecmascript/data/include.qml
@@ -1,0 +1,23 @@
+import Qt 4.7
+import "include.js" as IncludeTest
+
+QtObject {
+    property int test0: 0
+    property bool test1: false
+    property bool test2: false
+    property bool test2_1: false
+    property bool test3: false
+    property bool test3_1: false
+
+    property int testValue: 99
+
+    Component.onCompleted: { 
+        IncludeTest.go(); 
+        test0 = IncludeTest.value
+        test1 = IncludeTest.test1
+        test2 = IncludeTest.test2
+        test2_1 = IncludeTest.test2_1
+        test3 = IncludeTest.test3
+        test3_1 = IncludeTest.test3_1
+    }
+}

tests/auto/declarative/qdeclarativeecmascript/data/include_callback.js
@@ -1,0 +1,11 @@
+function go() {
+    var a = Qt.include("missing.js", function(o) { test2 = o.status == o.NETWORK_ERROR });
+    test1 = a.status == a.NETWORK_ERROR
+
+    var b = Qt.include("blank.js", function(o) { test4 = o.status == o.OK });
+    test3 = b.status == b.OK
+
+    var c = Qt.include("exception.js", function(o) { test6 = o.status == o.EXCEPTION });
+    test5 = c.status == c.EXCEPTION
+}
+

tests/auto/declarative/qdeclarativeecmascript/data/include_callback.qml
@@ -1,0 +1,15 @@
+import Qt 4.7
+import "include_callback.js" as IncludeTest
+
+QtObject {
+    property bool test1: false
+    property bool test2: false
+    property bool test3: false
+    property bool test4: false
+    property bool test5: false
+    property bool test6: false
+
+    Component.onCompleted: { 
+        IncludeTest.go(); 
+    }
+}

tests/auto/declarative/qdeclarativeecmascript/data/include_remote.js
@@ -1,0 +1,26 @@
+var myvar = 10;
+
+function go()
+{
+    var a = Qt.include("http://127.0.0.1:8111/remote_file.js", 
+                       function(o) { 
+                            test2 = o.status == o.OK
+                            test3 = a.status == a.OK
+                            test4 = myvar == 13
+
+                            done = true;
+                       });
+    test1 = a.status == a.LOADING
+
+
+    var b = Qt.include("http://127.0.0.1:8111/exception.js", 
+                       function(o) { 
+                            test7 = o.status == o.EXCEPTION
+                            test8 = b.status == a.EXCEPTION
+                            test9 = b.exception.toString() == "Whoops!";
+                            test10 = o.exception.toString() == "Whoops!";
+
+                            done2 = true;
+                       });
+    test6 = b.status == b.LOADING
+}

commit c25737a44fb22668c67568eefb61c72457166dfe

Fix compilation of QtDeclarative module (gcc)
Broke with commit 645b9ee9dd6

src/declarative/qml/qdeclarativeinclude.cpp
@@ -41,7 +41,7 @@
 
 #include "qdeclarativeinclude_p.h"
 
-#include <QtScript/qscriptengine>
+#include <QtScript/qscriptengine.h>
 #include <QtNetwork/qnetworkrequest.h>
 #include <QtNetwork/qnetworkreply.h>
 #include <QtCore/qfile.h>