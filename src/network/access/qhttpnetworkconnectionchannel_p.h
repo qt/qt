1564d012dd12e891a8e9112b288c94fae00dd745
Aleksandar Sasha Babic <aleksandar.babic@nokia.com>
2010-05-21 09:44:51 +0200

    Fixing the compile issue.
    Private headers have to be included in consistent and proper way:
    not like "name_p.h" or "../name_p.h" but <private/name_p.h>

--- src/network/access/qhttpnetworkconnectionchannel_p.h
+++ src/network/access/qhttpnetworkconnectionchannel_p.h
@@ -65,7 +65,7 @@
 #include <private/qhttpnetworkrequest_p.h>
 #include <private/qhttpnetworkreply_p.h>
 
-#include "qhttpnetworkconnection_p.h"
+#include <private/qhttpnetworkconnection_p.h>
 
 #ifndef QT_NO_HTTP
 
