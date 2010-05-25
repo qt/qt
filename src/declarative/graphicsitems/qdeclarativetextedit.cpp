3501614b3797272dcbd683adcca4fdacc5b319e9
Martin Jones <martin.jones@nokia.com>
2010-05-26 09:19:10 +1000

    Add inherits Item to TextEdit and TextInput docs.
    Task-number: QTBUG-10969

--- src/declarative/graphicsitems/qdeclarativetextedit.cpp
+++ src/declarative/graphicsitems/qdeclarativetextedit.cpp
@@ -61,8 +61,9 @@ QT_BEGIN_NAMESPACE
 
 /*!
     \qmlclass TextEdit QDeclarativeTextEdit
-  \since 4.7
+    \since 4.7
     \brief The TextEdit item allows you to add editable formatted text to a scene.
+    \inherits Item
 
     It can display both plain and rich text. For example:
 
