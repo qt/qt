0fc84aa48ef03c1910dc344e204e7267470305c4
Simon Hausmann <simon.hausmann@nokia.com>
2010-06-02 13:23:35 +0200

    Fix failing bypassShaping autotest on the Mac OS X
    This test relies on integer metrics, so we need to force them for Mac OS X.
    
    Reviewed-by: Jocelyn Turcotte

--- tests/auto/qfontmetrics/tst_qfontmetrics.cpp
+++ tests/auto/qfontmetrics/tst_qfontmetrics.cpp
@@ -223,6 +223,7 @@ void tst_QFontMetrics::averageCharWidth()
 void tst_QFontMetrics::bypassShaping()
 {
     QFont f;
+    f.setStyleStrategy(QFont::ForceIntegerMetrics);
     QFontMetrics fm(f);
     QString text = " A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
     int textWidth = fm.width(text, -1, Qt::TextBypassShaping);
