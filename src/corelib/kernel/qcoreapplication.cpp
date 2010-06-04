7ffb1c2e821cc4d508dcedcdb7363756f9f693ff
Geir Vattekar <geir.vattekar@nokia.com>
2010-06-04 15:06:45 +0200

    Doc: Said to not access events after they are posted(postEvent).
        Task-number: QTBUG-10210

--- src/corelib/kernel/qcoreapplication.cpp
+++ src/corelib/kernel/qcoreapplication.cpp
@@ -1073,7 +1073,7 @@ void QCoreApplication::exit(int returnCode)
 
     The event must be allocated on the heap since the post event queue
     will take ownership of the event and delete it once it has been
-    posted.  It is \e {not safe} to modify or delete the event after
+    posted.  It is \e {not safe} to access the event after
     it has been posted.
 
     When control returns to the main event loop, all events that are
@@ -1104,7 +1104,7 @@ void QCoreApplication::postEvent(QObject *receiver, QEvent *event)
 
     The event must be allocated on the heap since the post event queue
     will take ownership of the event and delete it once it has been
-    posted.  It is \e {not safe} to modify or delete the event after
+    posted.  It is \e {not safe} to access the event after
     it has been posted.
 
     When control returns to the main event loop, all events that are
