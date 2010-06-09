qt/qt

commit 09c6a81109d3978c1583c556202c01c1e774f11f

Make qbswap() use glibc's fast bswap_*() functions if available.
Reviewed-by: Thiago Macieira

src/corelib/global/qendian.h
@@ -42,6 +42,16 @@
 #ifndef QENDIAN_H
 #define QENDIAN_H
 
+#ifdef Q_OS_LINUX
+QT_BEGIN_INCLUDE_NAMESPACE
+# include <features.h>
+QT_END_INCLUDE_NAMESPACE
+#endif
+
+#ifdef __GLIBC__
+#include <byteswap.h>
+#endif
+
 #include <QtCore/qglobal.h>
 
 QT_BEGIN_HEADER
@@ -264,6 +274,21 @@ template <> inline qint16 qFromBigEndian<qint16>(const uchar *src)
  * and it is therefore a bit more convenient and in most cases more efficient.
 */
 template <typename T> T qbswap(T source);
+
+#ifdef __GLIBC__
+template <> inline quint64 qbswap<quint64>(quint64 source)
+{
+    return bswap_64(source);
+}
+template <> inline quint32 qbswap<quint32>(quint32 source)
+{
+    return bswap_32(source);
+}
+template <> inline quint16 qbswap<quint16>(quint16 source)
+{
+    return bswap_16(source);
+}
+#else
 template <> inline quint64 qbswap<quint64>(quint64 source)
 {
     return 0
@@ -292,6 +317,7 @@ template <> inline quint16 qbswap<quint16>(quint16 source)
                     | ((source & 0x00ff) << 8)
                     | ((source & 0xff00) >> 8) );
 }
+#endif // __GLIBC__
 
 // signed specializations
 template <> inline qint64 qbswap<qint64>(qint64 source)

commit e6f601ce80726bf2d5c2fdb1b87a62b563cb471b

Attempt to fix build failure on Linux introduced by 09c6a81109

src/corelib/global/qendian.h
@@ -42,8 +42,9 @@
 #ifndef QENDIAN_H
 #define QENDIAN_H
 
+#include <QtCore/qglobal.h>
+
 #ifdef Q_OS_LINUX
-# include <qglobal.h>
 QT_BEGIN_INCLUDE_NAMESPACE
 # include <features.h>
 QT_END_INCLUDE_NAMESPACE
@@ -52,8 +53,6 @@ QT_END_INCLUDE_NAMESPACE
 #ifdef __GLIBC__
 #include <byteswap.h>
 #endif
-
-#include <QtCore/qglobal.h>
 
 QT_BEGIN_HEADER
 