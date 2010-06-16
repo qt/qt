qt/qt

commit 71ba2b0973d291e991e1498c266e69d6640c8531

Reduce the memory consumption of QtFontStyle
QtFontSize::pixelSize() was allocating 8 slots for QtFontSize
while most fonts only require one.
This patch add a special case for the first QtFontSize in order to
reduce memory consumption.

The size of QtFontSize in memory has also been reduced.

Overall, the memory consumtion of QtFontStyle instances go down
from 100kb to 10kb.

Reviewed-by: Eskil Abrahamsen Blomfeldt

src/gui/text/qfontdatabase.cpp
@@ -145,18 +145,18 @@ struct QtFontEncoding
 
 struct QtFontSize
 {
-    unsigned short pixelSize;
-
 #ifdef Q_WS_X11
-    int count;
     QtFontEncoding *encodings;
     QtFontEncoding *encodingID(int id, uint xpoint = 0, uint xres = 0,
                                 uint yres = 0, uint avgwidth = 0, bool add = false);
+    unsigned short count : 16;
 #endif // Q_WS_X11
 #if defined(Q_WS_QWS) || defined(Q_OS_SYMBIAN)
     QByteArray fileName;
     int fileIndex;
 #endif // defined(Q_WS_QWS) || defined(Q_OS_SYMBIAN)
+
+    unsigned short pixelSize : 16;
 };
 
 
@@ -284,7 +284,12 @@ QtFontSize *QtFontStyle::pixelSize(unsigned short size, bool add)
     if (!add)
         return 0;
 
-    if (!(count % 8)) {
+    if (!pixelSizes) {
+        // Most style have only one font size, we avoid waisting memory
+        QtFontSize *newPixelSizes = (QtFontSize *)malloc(sizeof(QtFontSize));
+        Q_CHECK_PTR(newPixelSizes);
+        pixelSizes = newPixelSizes;
+    } else if (!(count % 8)) {
         QtFontSize *newPixelSizes = (QtFontSize *)
                      realloc(pixelSizes,
                               (((count+8) >> 3) << 3) * sizeof(QtFontSize));

commit 6dffaee6ab0bbeddd9e0cb16ff4545c3cb09bfe9

Allocate the memory for QtFontSize when count > 1
This fix a regression introduced by
71ba2b0973d291e991e1498c266e69d6640c8531.
In the case count >= 1 && count < 8, no memory is allocated.

Reviewed-by: Andreas Kling

src/gui/text/qfontdatabase.cpp
@@ -289,7 +289,7 @@ QtFontSize *QtFontStyle::pixelSize(unsigned short size, bool add)
         QtFontSize *newPixelSizes = (QtFontSize *)malloc(sizeof(QtFontSize));
         Q_CHECK_PTR(newPixelSizes);
         pixelSizes = newPixelSizes;
-    } else if (!(count % 8)) {
+    } else if (!(count % 8) || count == 1) {
         QtFontSize *newPixelSizes = (QtFontSize *)
                      realloc(pixelSizes,
                               (((count+8) >> 3) << 3) * sizeof(QtFontSize));