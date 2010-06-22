qt/qt

commit f321a3f2b1e0d5bc7ab0ec9be1af59981c3a9679

doc: Added more DITA output to the XML generator
cxxEnumeration stuff for enum types.

Task-number:  	 QTBUG-11391

tools/qdoc3/ditaxmlgenerator.cpp
@@ -4563,6 +4563,11 @@ void DitaXmlGenerator::writeLocation(const Node* n, CodeMarker* marker)
         s2 = CXXFUNCTIONDECLARATIONFILE;
         s3 = CXXFUNCTIONDECLARATIONFILELINE;
     }
+    else if (n->type() == Node::Enum) {
+        s1 = CXXENUMERATIONAPIITEMLOCATION;
+        s2 = CXXENUMERATIONDECLARATIONFILE;
+        s3 = CXXENUMERATIONDECLARATIONFILELINE;
+    }
     writer.writeStartElement(s1);
     writer.writeStartElement(s2);
     writer.writeAttribute("name","filePath");
@@ -4727,6 +4732,67 @@ void DitaXmlGenerator::writeEnumerations(const Section& s,
                                          const ClassNode* cn, 
                                          CodeMarker* marker)
 {
+    NodeList::ConstIterator m = s.members.begin();
+    while (m != s.members.end()) {
+        if ((*m)->type() == Node::Enum) {
+            EnumNode* en = static_cast<const EnumNode*>(*m);
+            writer.writeStartElement(CXXENUMERATION);
+            writer.writeAttribute("id",en->guid());
+            writer.writeStartElement(APINAME);
+            writer.writeCharacters(en->name());
+            writer.writeEndElement(); // </apiName>
+            generateBrief(en,marker);
+            writer.writeStartElement(CXXENUMERATIONDETAIL);
+            writer.writeStartElement(CXXENUMERATIONDEFINITION);
+            writer.writeStartElement(CXXENUMERATIONACCESSSPECIFIER);
+            writer.writeAttribute("value",en->accessString());
+            writer.writeEndElement(); // <cxxEnumerationAccessSpecifier>
+
+            QString fq = fullQualification(en);
+            if (!fq.isEmpty()) {
+                writer.writeStartElement(CXXENUMERATIONSCOPEDNAME);
+                writer.writeCharacters(fq);
+                writer.writeEndElement(); // <cxxEnumerationScopedName>
+            }
+            const QList<EnumItem>& items = en->items();
+            if (!items.isEmpty()) {
+                writer.writeStartElement(CXXENUMERATIONPROTOTYPE);
+                writer.writeCharacters(en->name());
+                writer.writeCharacters(" = { ");
+                QList<EnumItem>::ConstIterator i = items.begin();
+                while (i != items.end()) {
+                    writer.writeCharacters((*i).name());
+                    if (!(*i).value().isEmpty()) {
+                        writer.writeCharacters(" = ");
+                        writer.writeCharacters((*i).value());
+                    }
+                    ++i;
+                    if (i != items.end())
+                        writer.writeCharacters(", ");
+                }
+                writer.writeCharacters(" }");
+                writer.writeEndElement(); // <cxxEnumerationPrototype>
+            }
+
+            writer.writeStartElement(CXXENUMERATIONNAMELOOKUP);
+            writer.writeCharacters(en->parent()->name() + "::" + en->name());
+            writer.writeEndElement(); // <cxxEnumerationNameLookup>
+
+            writeLocation(en, marker);
+            writer.writeEndElement(); // <cxxEnumerationDefinition>
+            writer.writeStartElement(APIDESC);
+
+            if (!en->doc().isEmpty()) {
+                generateBody(en, marker);
+                //        generateAlsoList(inner, marker);
+            }
+
+            writer.writeEndElement(); // </apiDesc>
+            writer.writeEndElement(); // </cxxEnumerationDetail>
+            writer.writeEndElement(); // </cxxEnumeration>
+        }
+        ++m;
+    }
 }
 
 void DitaXmlGenerator::writeTypedefs(const Section& s, 

tools/qdoc3/node.cpp
@@ -968,6 +968,8 @@ QString FakeNode::subTitle() const
  */
 
 /*!
+  The constructor for the node representing an enum type
+  has a \a parent class and an enum type \a name.
  */
 EnumNode::EnumNode(InnerNode *parent, const QString& name)
     : LeafNode(Enum, parent, name), ft(0)
@@ -975,6 +977,7 @@ EnumNode::EnumNode(InnerNode *parent, const QString& name)
 }
 
 /*!
+  Add \a item to the enum type's item list.
  */
 void EnumNode::addItem(const EnumItem& item)
 {
@@ -983,15 +986,15 @@ void EnumNode::addItem(const EnumItem& item)
 }
 
 /*!
+  Returns the access level of the enumeration item named \a name.
+  Apparently it is private if it has been omitted by qdoc's
+  omitvalue command. Otherwise it is public.
  */
 Node::Access EnumNode::itemAccess(const QString &name) const
 {
-    if (doc().omitEnumItemNames().contains(name)) {
+    if (doc().omitEnumItemNames().contains(name))
         return Private;
-    }
-    else {
-        return Public;
-    }
+    return Public;
 }
 
 /*!

commit 9c7d10686f73aeddcff73a88c47aac9735f5b3ad

qdoc3: Build fix for regression in f321a3f2b1e

tools/qdoc3/ditaxmlgenerator.cpp
@@ -4588,7 +4588,7 @@ void DitaXmlGenerator::writeFunctions(const Section& s,
     NodeList::ConstIterator m = s.members.begin();
     while (m != s.members.end()) {
         if ((*m)->type() == Node::Function) {
-            FunctionNode* fn = static_cast<const FunctionNode*>(*m);
+            const FunctionNode* fn = static_cast<const FunctionNode*>(*m);
             writer.writeStartElement(CXXFUNCTION);
             writer.writeAttribute("id",fn->guid());
             writer.writeStartElement(APINAME);
@@ -4696,7 +4696,7 @@ void DitaXmlGenerator::writeFunctions(const Section& s,
     }
 }
 
-void DitaXmlGenerator::writeParameters(FunctionNode* fn, CodeMarker* marker)
+void DitaXmlGenerator::writeParameters(const FunctionNode* fn, CodeMarker* marker)
 {
     const QList<Parameter>& parameters = fn->parameters();
     if (!parameters.isEmpty()) {
@@ -4735,7 +4735,7 @@ void DitaXmlGenerator::writeEnumerations(const Section& s,
     NodeList::ConstIterator m = s.members.begin();
     while (m != s.members.end()) {
         if ((*m)->type() == Node::Enum) {
-            EnumNode* en = static_cast<const EnumNode*>(*m);
+            const EnumNode* en = static_cast<const EnumNode*>(*m);
             writer.writeStartElement(CXXENUMERATION);
             writer.writeAttribute("id",en->guid());
             writer.writeStartElement(APINAME);

tools/qdoc3/ditaxmlgenerator.h
@@ -117,7 +117,7 @@ class DitaXmlGenerator : public PageGenerator
     void writeFunctions(const Section& s, 
                         const ClassNode* cn, 
                         CodeMarker* marker);
-    void writeParameters(FunctionNode* fn, CodeMarker* marker);
+    void writeParameters(const FunctionNode* fn, CodeMarker* marker);
     void writeNestedClasses(const Section& s, 
                             const ClassNode* cn, 
                             CodeMarker* marker);