5cfa764466be6ec2b987e0694b99f1d343d55048
Benjamin Poulain <benjamin.poulain@nokia.com>
2010-06-23 22:22:56 +0200

    Add an implementation of comp_func_solid_SourceOver_neon() with Neon.
    The function comp_func_solid_SourceOver_neon() is use extensively by
    WebKit via the calls to fillRect() of QPainter().
    
    Implementing the function with Neon provides some performance
    improvement (around 175% of the previous speed).
    
    Reviewed-by: Samuel Rødal

--- src/gui/painting/qdrawhelper.cpp
+++ src/gui/painting/qdrawhelper.cpp
@@ -7898,6 +7898,7 @@ void qInitDrawhelperAsm()
             qDrawHelper[QImage::Format_RGB16].alphamapBlit = qt_alphamapblit_quint16_neon;
 
             functionForMode_C[QPainter::CompositionMode_SourceOver] = qt_blend_argb32_on_argb32_scanline_neon;
+            functionForModeSolid_C[QPainter::CompositionMode_SourceOver] = comp_func_solid_SourceOver_neon;
             destFetchProc[QImage::Format_RGB16] = qt_destFetchRGB16_neon;
             destStoreProc[QImage::Format_RGB16] = qt_destStoreRGB16_neon;
         }
