bdb90df5d51d071c5b105a36654184a2c0f6ed65
mread <qt-info@nokia.com>
2010-07-01 10:54:29 +0100

    fix for broken input method loading
    Changed use of wrong flag "Q_WS_SYMBIAN" back to "Q_WS_S60" so that
    input methods should work correctly in S60 builds of Qt.

--- src/gui/kernel/qapplication.cpp
+++ src/gui/kernel/qapplication.cpp
@@ -5338,7 +5338,7 @@ QInputContext *QApplication::inputContext() const
             qic = QInputContextFactory::create(QLatin1String("xim"), that);
         that->d_func()->inputContext = qic;
     }
-#elif defined(Q_WS_SYMBIAN)
+#elif defined(Q_WS_S60)
     if (!d->inputContext) {
         QApplication *that = const_cast<QApplication *>(this);
         const QStringList keys = QInputContextFactory::keys();
