953eeb0eeebb27d440e002cf4c3620b87bbe43d5
Bea Lam <bea.lam@nokia.com>
2010-07-09 14:07:31 +1000

    Make test pass and fix docs following removal of SpringFollow (cherry picked from commit 31cd8c15c0fdb2f08c2652609a57413a715d1b13)

--- src/declarative/util/qdeclarativespringanimation.cpp
+++ src/declarative/util/qdeclarativespringanimation.cpp
@@ -232,7 +232,7 @@ qreal QDeclarativeSpringAnimation::to() const
 }
 
 /*!
-    \qmlproperty real SpringFollow::to
+    \qmlproperty real SpringAnimation::to
 */
 
 void QDeclarativeSpringAnimation::setTo(qreal value)
@@ -254,7 +254,7 @@ qreal QDeclarativeSpringAnimation::from() const
 }
 
 /*!
-    \qmlproperty real SpringFollow::from
+    \qmlproperty real SpringAnimation::from
 */
 
 void QDeclarativeSpringAnimation::setFrom(qreal value)
