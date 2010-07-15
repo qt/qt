40d2b2e30a526b46b17072ba8bdc38f30cd3c624
Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
2010-07-15 18:44:18 +0200

    remove nasty hack by using a stricter regexp (for language codes)

--- translations/check-ts.pl
+++ translations/check-ts.pl
@@ -43,8 +43,7 @@
 
 use strict;
 
-# "qt" must come last to avoid prefix matching.
-my @groups = ("assistant", "designer", "linguist", "qt_help", "qtconfig", "qvfb", "qt");
+my @groups = ("qt", "assistant", "designer", "linguist", "qt_help", "qtconfig", "qvfb");
 
 my %scores = ();
 my %langs = ();
@@ -55,7 +54,7 @@ for my $i (split(/ /, $res)) {
   $i =~ /^([^.]+)\.ts:(.*)$/;
   my ($fn, $pc) = ($1, $2);
   for my $g (@groups) {
-    if ($fn =~ /^${g}_(.*)$/) {
+    if ($fn =~ /^${g}_((.._)?..)$/) {
       my $lang = $1;
       $scores{$g}{$lang} = $pc;
       $langs{$lang} = 1;
@@ -63,10 +62,6 @@ for my $i (split(/ /, $res)) {
     }
   }
 }
-
-# now we move "qt" to the front, as it should be the first column.
-pop @groups;
-unshift @groups, "qt";
 
 my $code = "";
 
