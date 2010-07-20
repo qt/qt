a7182f757e5414d7baf21fdc098f3f6649e6a1d6
Jørgen Lind <jorgen.lind@nokia.com>
2010-07-20 13:16:09 +0200

    Cleanup directfb input handling in lighthouse

--- src/plugins/platforms/directfb/qdirectfbinput.h
+++ src/plugins/platforms/directfb/qdirectfbinput.h
@@ -1,9 +1,7 @@
 #ifndef QDIRECTFBINPUT_H
 #define QDIRECTFBINPUT_H
 
-#include <QThread>
-#include <QMutex>
-#include <QWaitCondition>
+#include <QSemaphore>
 #include <QObject>
 #include <QHash>
 #include <QPoint>
@@ -13,53 +11,35 @@
 
 #include <directfb.h>
 
-class InputSocketWaiter : public QThread
-{
-    Q_OBJECT
-public:
-    InputSocketWaiter(IDirectFBEventBuffer *eventBuffer, QObject *parent);
-    virtual ~InputSocketWaiter();
-    void continueWaitingForEvents();
-protected:
-    void run();
-signals:
-    void newEvent();
-private:
-     IDirectFBEventBuffer *m_eventBuffer;
-     bool m_shouldStop;
-     QMutex m_cleanupMutex;
-     QWaitCondition m_finishedProcessingEvents;
-};
-
 class QDirectFbInput : public QObject
 {
     Q_OBJECT
 public:
-    static QDirectFbInput *instance();
+    QDirectFbInput(QObject *parent);
     void addWindow(DFBWindowID id, QWidget *tlw);
     void removeWindow(WId wId);
 
 public slots:
+    void runInputEventLoop();
+    void stopInputEventLoop();
     void handleEvents();
-    void applicationEnd();
 
 private:
-    QDirectFbInput();
-
     void handleMouseEvents(const DFBEvent &event);
     void handleWheelEvent(const DFBEvent &event);
     void handleKeyEvents(const DFBEvent &event);
     void handleEnterLeaveEvents(const DFBEvent &event);
-    IDirectFB *dfbInterface;
-    IDirectFBDisplayLayer *dfbDisplayLayer;
-    IDirectFBEventBuffer *eventBuffer;
-
-    QHash<DFBWindowID,QWidget *>tlwMap;
-
     inline QPoint globalPoint(const DFBEvent &event) const;
 
-    InputSocketWaiter *m_inputHandler;
 
+    IDirectFB *m_dfbInterface;
+    IDirectFBDisplayLayer *m_dfbDisplayLayer;
+    IDirectFBEventBuffer *m_eventBuffer;
+
+    bool m_shouldStop;
+    QSemaphore m_waitStop;
+
+    QHash<DFBWindowID,QWidget *>m_tlwMap;
 };
 
 #endif // QDIRECTFBINPUT_H
