4e26c800f2274754a670b0353e3fb4afd55514c3
Robert Loehning <robert.loehning@nokia.com>
2010-07-21 17:36:55 +0200

    Compile fix for MSVC

--- examples/tutorials/modelview/2_formatting/mymodel.cpp
+++ examples/tutorials/modelview/2_formatting/mymodel.cpp
@@ -89,7 +89,8 @@ QVariant MyModel::data(const QModelIndex &index, int role) const
 
         if (row == 1 && col == 2)  //change background only for cell(1,2)
         {
-            QBrush redBackground(QColor(Qt::red));
+            QColor redColor(Qt::red);
+            QBrush redBackground(redColor);
             return redBackground;
         }
         break;
