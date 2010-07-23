24262b2bf82c2e3e5854326e8fc3c77e3b053162
Martin Smith <martin.smith@nokia.com>
2010-07-23 09:54:44 +0200

    doc: Added missing \since 4.7 to effect propertyu.
    Task-number: QTBUG-12132
    (cherry picked from commit d9f5b8742f84124c3c0e2bdbd7b860dce2bc2225)

--- src/gui/graphicsview/qgraphicsitem.cpp
+++ src/gui/graphicsview/qgraphicsitem.cpp
@@ -7926,6 +7926,7 @@ void QGraphicsItemPrivate::resetHeight()
 
 /*!
   \property QGraphicsObject::effect
+  \since 4.7
   \brief the effect attached to this item
 
   \sa QGraphicsItem::setGraphicsEffect(), QGraphicsItem::graphicsEffect()
