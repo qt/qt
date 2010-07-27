f1257358b4432abf874460f58cba73bce17981ab
Gareth Stockwell <ext-gareth.stockwell@nokia.com>
2010-07-27 16:23:52 +0100

    Ensure that window rectangle is updated when CBA visibility changes
    In S60, there are two pieces of screen furniture, changes in whose
    visibility or size can affect the size of the Qt application window.
    These are the status pane, at the top of the screen, and the CBA at
    the bottom.
    
    QSymbianControl listens for changes in status pane visibility and
    size by implementing MEikStatusPaneObserver.  Notifications
    received via this interface trigger a call to
    QSymbianControl::handleClientAreaChange() which resizes the control.
    
    There is no corresponding interface through which to receive
    notifications of changes in CBA visibility.
    
    This patch introduces a utility function for setting the visibility
    of both the status pane and CBA, which ensures that the control's
    rectangle is updated when either one changes.
    
    Task-number: QTBUG-5320
    Reviewed-by: Jason Barron

--- src/gui/kernel/qt_s60_p.h
+++ src/gui/kernel/qt_s60_p.h
@@ -148,6 +148,7 @@ public:
     static inline CAknTitlePane* titlePane();
     static inline CAknContextPane* contextPane();
     static inline CEikButtonGroupContainer* buttonGroupContainer();
+    static void setStatusPaneAndButtonGroupVisibility(bool statusPaneVisible, bool buttonGroupVisible);
 
     TTrapHandler *s60InstalledTrapHandler;
 #endif
@@ -226,6 +227,8 @@ private:
 #ifdef QT_SYMBIAN_SUPPORTS_ADVANCED_POINTER
     void translateAdvancedPointerEvent(const TAdvancedPointerEvent *event);
 #endif
+
+public:
     void handleClientAreaChange();
 
 private:
