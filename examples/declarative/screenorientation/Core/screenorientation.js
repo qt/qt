b2735d19d2d2873f52f1db9469ea6105e23e67e9
Joona Petrell <joona.t.petrell@nokia.com>
2010-07-28 18:45:23 +1000

    Make it possible to manually set the orientation of QML Viewer on Symbian
    Task-number: QTBUG-12142
    Reviewed-by: Warwick Allison
    
    This patch brings ability to switch QML Viewer's orientation between auto-orientation (=follow sensor),
    portrait and landscape orientations (lock orientation) on Symbian. It provides same orientation options as
    Qt Creator 2.1's Qt QML Standalone Application creation wizard. Also, menu item rotateOrientation now
    works on Symbian, but it's hidden when orientation mode is set to auto-orientation.
    
    Property runtime.orientation has been switched back to supporting four-way orientation on Symbian, previously
    it only updated values between portrait and landscape. If your application only supports landscape or portrait
    modes, just don't react to the inverted orientations.
    
    Added orientation example screenorientation under examples/declarative.
    
    The patch includes a fix for calculator example, which rotated to wrong direction when switching from portrait
    to landscape. Also, improved qdeclarativeviewer unit tests. Changes have been tested to work on Windows,
    Linux and Symbian^3.
    (cherry picked from commit e62f266a7642e675e9d235a1f54a6b5746500d48)

---
+++ examples/declarative/screenorientation/Core/screenorientation.js
@@ -0,0 +1,95 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtDeclarative module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+function printOrientation(orientation) {
+    var orientationString;
+    if (orientation == Orientation.Portrait) {
+        orientationString = "Portrait";
+    } else if (orientation == Orientation.Landscape) {
+        orientationString = "Landscape";
+    } else if (orientation == Orientation.PortraitInverted) {
+        orientationString = "Portrait inverted";
+    } else if (orientation == Orientation.LandscapeInverted) {
+        orientationString = "Landscape inverted";
+    } else {
+        orientationString = "UnknownOrientation";
+    }
+    return orientationString;
+}
+ 
+function getAngle(orientation) {
+    var angle;
+    if (orientation == Orientation.Portrait) {
+        angle = 0;
+    } else if (orientation == Orientation.Landscape) {
+        angle = 90;
+    } else if (orientation == Orientation.PortraitInverted) {
+        angle = 180;
+    } else if (orientation == Orientation.LandscapeInverted) {
+        angle = 270;
+    } else {
+        angle = 0;
+    }
+    return angle;
+}
+
+function parallel(firstOrientation, secondOrientation) {
+    var difference = getAngle(firstOrientation) - getAngle(secondOrientation)
+    return difference % 180 == 0;
+}
+
+function calculateGravityPoint(firstOrientation, secondOrientation) {
+    var position = Qt.point(0, 0);
+    var difference = getAngle(firstOrientation) - getAngle(secondOrientation)
+    if (difference < 0) {
+        difference = 360 + difference;
+    }
+    if (difference == 0) {
+        position = Qt.point(0, -10);
+    } else if (difference == 90) {
+        position = Qt.point(-10, 0);
+    } else if (difference == 180) {
+        position = Qt.point(0, 1000);
+    } else if (difference == 270) {
+        position = Qt.point(1000, 0);
+    }
+    return position;
+}
