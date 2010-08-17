qt/qt

commit ebbab30af417dfbf3df47dec15c0e2f8d6a30fa6

Fix the rendering of lines with the X11 paint engine
On the X11 paint engine, when rendering lines with float coordinates,
the lines were one pixel off if the decimal was > 0.5.

This fixes the WebKit bug https://bugs.webkit.org/show_bug.cgi?id=42248

Autotest by Yoann Lopes.

Reviewed-by: Simon Hausmann
Reviewed-by: Yoann Lopes
Reviewed-by: Andreas Kling

src/gui/painting/qpaintengine_x11.cpp
@@ -696,11 +696,10 @@ void QX11PaintEngine::drawLines(const QLine *lines, int lineCount)
                 linef = d->matrix.map(QLineF(lines[i]));
             }
             if (clipLine(&linef, d->polygonClipper.boundingRect())) {
-                int x1 = qRound(linef.x1() + aliasedCoordinateDelta);
-                int y1 = qRound(linef.y1() + aliasedCoordinateDelta);
-                int x2 = qRound(linef.x2() + aliasedCoordinateDelta);
-                int y2 = qRound(linef.y2() + aliasedCoordinateDelta);
-
+                int x1 = qFloor(linef.x1() + aliasedCoordinateDelta);
+                int y1 = qFloor(linef.y1() + aliasedCoordinateDelta);
+                int x2 = qFloor(linef.x2() + aliasedCoordinateDelta);
+                int y2 = qFloor(linef.y2() + aliasedCoordinateDelta);
                 XDrawLine(d->dpy, d->hd, d->gc, x1, y1, x2, y2);
             }
         }
@@ -730,11 +729,10 @@ void QX11PaintEngine::drawLines(const QLineF *lines, int lineCount)
         for (int i = 0; i < lineCount; ++i) {
             QLineF linef = d->matrix.map(lines[i]);
             if (clipLine(&linef, d->polygonClipper.boundingRect())) {
-                int x1 = qRound(linef.x1() + aliasedCoordinateDelta);
-                int y1 = qRound(linef.y1() + aliasedCoordinateDelta);
-                int x2 = qRound(linef.x2() + aliasedCoordinateDelta);
-                int y2 = qRound(linef.y2() + aliasedCoordinateDelta);
-
+                int x1 = qFloor(linef.x1() + aliasedCoordinateDelta);
+                int y1 = qFloor(linef.y1() + aliasedCoordinateDelta);
+                int x2 = qFloor(linef.x2() + aliasedCoordinateDelta);
+                int y2 = qFloor(linef.y2() + aliasedCoordinateDelta);
                 XDrawLine(d->dpy, d->hd, d->gc, x1, y1, x2, y2);
             }
         }
@@ -1690,8 +1688,8 @@ void QX11PaintEnginePrivate::strokePolygon_dev(const QPointF *polygonPoints, int
     if (clippedCount > 0) {
         QVarLengthArray<XPoint> xpoints(clippedCount);
         for (int i = 0; i < clippedCount; ++i) {
-            xpoints[i].x = qRound(clippedPoints[i].x + aliasedCoordinateDelta);
-            xpoints[i].y = qRound(clippedPoints[i].y + aliasedCoordinateDelta);
+            xpoints[i].x = qFloor(clippedPoints[i].x + aliasedCoordinateDelta);
+            xpoints[i].y = qFloor(clippedPoints[i].y + aliasedCoordinateDelta);
         }
         uint numberPoints = qMin(clippedCount, xlibMaxLinePoints);
         XPoint *pts = xpoints.data();

tests/auto/qpainter/tst_qpainter.cpp
@@ -118,10 +118,12 @@ private slots:
     void drawLine_task190634();
     void drawLine_task229459();
     void drawLine_task234891();
+    void drawHorizontalLineF();
 
     void drawRect_data() { fillData(); }
     void drawRect();
     void drawRect2();
+    void drawRectFHorizontalLine();
 
     void fillRect();
     void fillRect2();
@@ -251,6 +253,7 @@ private slots:
     void setPenColorOnPixmap();
 
     void QTBUG5939_attachPainterPrivate();
+    void drawHorizontalLine();
 
 private:
     void fillData();
@@ -1218,6 +1221,26 @@ void tst_QPainter::drawLine_task234891()
     QCOMPARE(expected, img);
 }
 
+void tst_QPainter::drawHorizontalLineF()
+{
+    QPixmap pixmap(100, 3);
+    pixmap.fill();
+
+    {
+        QPainter painter(&pixmap);
+        painter.drawLine(QLineF(1.5f, 1.5f, 98.5f, 1.5f));
+    }
+
+    QImage refImage(100, 3, QImage::Format_ARGB32);
+    refImage.fill(0xFFFFFFFF);
+    {
+        QPainter painter(&refImage);
+        painter.drawLine(QLineF(1.5f, 1.5f, 98.5f, 1.5f));
+    }
+
+    QCOMPARE(pixmap.toImage().convertToFormat(QImage::Format_ARGB32), refImage);
+}
+
 void tst_QPainter::drawLine_task216948()
 {
     QImage img(1, 10, QImage::Format_ARGB32_Premultiplied);
@@ -1300,6 +1323,26 @@ void tst_QPainter::drawRect2()
         QRect stroke = getPaintedSize(image, Qt::white);
         QCOMPARE(stroke.adjusted(1, 1, 0, 0), fill.adjusted(0, 0, 1, 1));
     }
+}
+
+void tst_QPainter::drawRectFHorizontalLine()
+{
+    QPixmap pixmap(100, 3);
+    pixmap.fill();
+
+    {
+        QPainter painter(&pixmap);
+        painter.drawRect(QRectF(1.5f, 1.5f, 98.5f, 1.5f));
+    }
+
+    QImage refImage(100, 3, QImage::Format_ARGB32);
+    refImage.fill(0xFFFFFFFF);
+    {
+        QPainter painter(&refImage);
+        painter.drawRect(QRectF(1.5f, 1.5f, 98.5f, 1.5f));
+    }
+
+    QCOMPARE(pixmap.toImage().convertToFormat(QImage::Format_ARGB32), refImage);
 }
 
 void tst_QPainter::fillRect()
@@ -4520,6 +4563,28 @@ void tst_QPainter::QTBUG5939_attachPainterPrivate()
 
     QVERIFY(widget->worldTransform.isIdentity());
     QCOMPARE(widget->deviceTransform, proxy->deviceTransform);
+}
+
+void tst_QPainter::drawHorizontalLine()
+{
+    QPixmap pixmap(100, 3);
+    pixmap.fill();
+
+    {
+        QPainter painter(&pixmap);
+        painter.translate(0.3, 0.3);
+        painter.drawLine(QLine(1, 1, 99, 1));
+    }
+
+    QImage refImage(100, 3, QImage::Format_ARGB32);
+    refImage.fill(0xFFFFFFFF);
+    {
+        QPainter painter(&refImage);
+        painter.translate(0.3, 0.3);
+        painter.drawLine(QLine(1, 1, 99, 1));
+    }
+
+    QCOMPARE(pixmap.toImage().convertToFormat(QImage::Format_ARGB32), refImage);
 }
 
 QTEST_MAIN(tst_QPainter)

commit ff405f5623d7ed18c881c097368e3e9afd2e9443

Outline / fill inconsistency in X11 paint engine.
Change ebbab30af417dfbf3df47dec15c0e2f8d6a30fa6 broke outline / fill
consistency in the X11 engine. Since the positioning of lines is more
important we'll round the fill coordinates the same way.

The bug was visible in the filltest.qps arthur test.

Reviewed-by: Gunnar Sletta

src/gui/painting/qpaintengine_x11.cpp
@@ -1516,8 +1516,8 @@ void QX11PaintEnginePrivate::fillPolygon_translated(const QPointF *polygonPoints
     for (int i = 0; i < pointCount; ++i) {
         translated_points[i] = polygonPoints[i] + offset;
 
-        translated_points[i].rx() = qRound(translated_points[i].x()) + offs;
-        translated_points[i].ry() = qRound(translated_points[i].y()) + offs;
+        translated_points[i].rx() = qFloor(translated_points[i].x()) + offs;
+        translated_points[i].ry() = qFloor(translated_points[i].y()) + offs;
     }
 
     fillPolygon_dev(translated_points.data(), pointCount, gcMode, mode);
@@ -1754,8 +1754,8 @@ void QX11PaintEnginePrivate::fillPath(const QPainterPath &path, QX11PaintEngineP
         for (int j = 0; j < polys.at(i).size(); ++j) {
             translated_points[j] = polys.at(i).at(j);
             if (!X11->use_xrender || !(render_hints & QPainter::Antialiasing)) {
-                translated_points[j].rx() = qRound(translated_points[j].rx() + aliasedCoordinateDelta) + offs;
-                translated_points[j].ry() = qRound(translated_points[j].ry() + aliasedCoordinateDelta) + offs;
+                translated_points[j].rx() = qFloor(translated_points[j].rx() + aliasedCoordinateDelta) + offs;
+                translated_points[j].ry() = qFloor(translated_points[j].ry() + aliasedCoordinateDelta) + offs;
             }
         }
 