931d47db10ca8b8ebf564c4b6537e31370b7f53c
Thomas Zander <t.zander@nokia.com>
2010-08-25 13:19:32 +0200

    Fix the unit test on Windows

--- src/corelib/io/qfilesystementry.cpp
+++ src/corelib/io/qfilesystementry.cpp
@@ -121,7 +121,14 @@ QString QFileSystemEntry::completeSuffix() const
 bool QFileSystemEntry::isAbsolute() const
 {
     resolveFilePath();
-    return !m_filePath.isEmpty() && (m_filePath[0].unicode() == '/' /*|| hasScheme()*/);
+    return (!m_filePath.isEmpty() && (m_filePath[0].unicode() == '/')
+#if defined(Q_FS_FAT) || defined(Q_OS_OS2EMX) || defined(Q_OS_SYMBIAN)
+        || (m_filePath.length() >= 2
+                && ((m_filePath[0].isLetter() && m_filePath[1] == QLatin1Char(':'))
+                    || (m_filePath.at(0) == QLatin1Char('/') && m_filePath.at(1) == QLatin1Char('/'))))
+#endif
+    );
+
 }
 
 // private methods
