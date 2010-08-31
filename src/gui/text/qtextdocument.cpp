af1af7a14c7d89a34267d955ca75ddd7abf5a370
Nils Jeisecke <jeisecke@saltation.de>
2010-08-31 15:39:43 +0200

    Implemented suffix and prefix strings for the numbering in ordered lists as supported by ODF. Works also with HTML import/export by using Qt CSS properties extensions.
    Merge-request: 1642
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@nokia.com>

--- src/gui/text/qtextdocument.cpp
+++ src/gui/text/qtextdocument.cpp
@@ -2591,14 +2591,40 @@ void QTextHtmlExporter::emitBlock(const QTextBlock &block)
                 default: html += QLatin1String("<ul"); // ### should not happen
             }
 
-            html += QLatin1String(" style=\"margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px;");
+            QString styleString = QString::fromLatin1("margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px;");
 
             if (format.hasProperty(QTextFormat::ListIndent)) {
-                html += QLatin1String(" -qt-list-indent: ");
-                html += QString::number(format.indent());
-                html += QLatin1Char(';');
+                styleString += QLatin1String(" -qt-list-indent: ");
+                styleString += QString::number(format.indent());
+                styleString += QLatin1Char(';');
             }
 
+            if (format.hasProperty(QTextFormat::ListNumberPrefix)) {
+                QString numberPrefix = format.numberPrefix();
+                numberPrefix.replace('"', "\\22");
+                numberPrefix.replace('\'', "\\27"); // FIXME: There's a problem in the CSS parser the prevents this from being correctly restored
+                styleString += QLatin1String(" -qt-list-number-prefix: ");
+                styleString += QLatin1Char('\'');
+                styleString += numberPrefix;
+                styleString += QLatin1Char('\'');
+                styleString += QLatin1Char(';');
+            }
+
+            if (format.hasProperty(QTextFormat::ListNumberSuffix)) {
+                if (format.numberSuffix() != QLatin1String(".")) { // this is our default
+                    QString numberSuffix = format.numberSuffix();
+                    numberSuffix.replace('"', "\\22");
+                    numberSuffix.replace('\'', "\\27"); // see above
+                    styleString += QLatin1String(" -qt-list-number-suffix: ");
+                    styleString += QLatin1Char('\'');
+                    styleString += numberSuffix;
+                    styleString += QLatin1Char('\'');
+                    styleString += QLatin1Char(';');
+                }
+            }
+
+            html += QLatin1String(" style=\"");
+            html += styleString;
             html += QLatin1String("\">");
         }
 
