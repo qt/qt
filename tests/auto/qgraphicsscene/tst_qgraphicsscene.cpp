qt/qt

commit dda8a57c085216db609f822837c50bae38006b4e

QGraphicsWidget is painted twice on the first show.
We want to discard all update requests when we there's a full update
pending. The problem was that 'updateAll' was reset too early causing
update requests to fall through. To prevent this from happening we
reset 'updateAll' right before the items are actually painted.

Auto-test included.

Task-number: QTBUG-6956

src/gui/graphicsview/qgraphicsscene.cpp
@@ -373,7 +373,10 @@ void QGraphicsScenePrivate::_q_emitUpdated()
             }
         }
     } else {
-        updateAll = false;
+        if (views.isEmpty()) {
+            updateAll = false;
+            return;
+        }
         for (int i = 0; i < views.size(); ++i)
             views.at(i)->d_func()->processPendingUpdates();
         // It's important that we update all views before we dispatch, hence two for-loops.
@@ -4604,6 +4607,7 @@ void QGraphicsScenePrivate::drawItems(QPainter *painter, const QTransform *const
     if (!unpolishedItems.isEmpty())
         _q_polishItems();
 
+    updateAll = false;
     QRectF exposedSceneRect;
     if (exposedRegion && indexMethod != QGraphicsScene::NoIndex) {
         exposedSceneRect = exposedRegion->boundingRect().adjusted(-1, -1, 1, 1);
@@ -5166,6 +5170,7 @@ void QGraphicsScene::drawItems(QPainter *painter,
     if (!d->unpolishedItems.isEmpty())
         d->_q_polishItems();
 
+    d->updateAll = false;
     QTransform viewTransform = painter->worldTransform();
     Q_UNUSED(options);
 

tests/auto/qgraphicswidget/tst_qgraphicswidget.cpp
@@ -164,6 +164,7 @@ private slots:
     void polishEvent();
     void polishEvent2();
     void initialShow();
+    void initialShow2();
 
     // Task fixes
     void task236127_bspTreeIndexFails();
@@ -2877,6 +2878,32 @@ void tst_QGraphicsWidget::initialShow()
     QTest::qWait(100);
     scene.addItem(widget);
     QTest::qWait(100);
+
+    QCOMPARE(widget->repaints, 1);
+}
+
+void tst_QGraphicsWidget::initialShow2()
+{
+    class MyGraphicsWidget : public QGraphicsWidget
+    { public:
+        MyGraphicsWidget() : repaints(0) {}
+        int repaints;
+        void paint(QPainter *, const QStyleOptionGraphicsItem *, QWidget*) { ++repaints; }
+        void polishEvent() { update(); }
+    };
+
+    MyGraphicsWidget *widget = new MyGraphicsWidget;
+    widget->resize(100, 100);
+
+    QGraphicsScene scene(0, 0, 200, 200);
+    scene.addItem(widget);
+
+    QGraphicsView view(&scene);
+    view.show();
+    // Not using QTest::qWaitForWindowShown(&view); on purpose, because there's
+    // a bug in qt_x11_wait_for_window_manager that prevents this test
+    // to pass. Denis is looking into it.
+    QTest::qWait(300);
 
     QCOMPARE(widget->repaints, 1);
 }

commit 548155fec3a8b575215625d4fd767e63c20f6b84

Assertion calledEmitUpdated fails in QGraphicsScene.
Problem was that the 'updateAll' boolean was reset to 'false' when it really
should have been unchanged. This happened when rendering the scene from outside
the view's paint event (i.e. from QGraphicsView/Scene::render). We only
want to reset 'updateAll' when triggering drawItems() from
QGraphicsView::paintEvent, i.e. when the 'view' pointer != 0.

Broke after commit: dda8a57c.
Auto test included.

Task-number: QT-3674

src/gui/graphicsview/qgraphicsscene.cpp
@@ -5270,7 +5270,6 @@ void QGraphicsScene::drawItems(QPainter *painter,
     if (!d->unpolishedItems.isEmpty())
         d->_q_polishItems();
 
-    d->updateAll = false;
     QTransform viewTransform = painter->worldTransform();
     Q_UNUSED(options);
 
@@ -5279,6 +5278,7 @@ void QGraphicsScene::drawItems(QPainter *painter,
     QRegion *expose = 0;
     const quint32 oldRectAdjust = d->rectAdjust;
     if (view) {
+        d->updateAll = false;
         expose = &view->d_func()->exposedRegion;
         if (view->d_func()->optimizationFlags & QGraphicsView::DontAdjustForAntialiasing)
             d->rectAdjust = 1;

tests/auto/qgraphicsscene/tst_qgraphicsscene.cpp
@@ -287,6 +287,7 @@ private slots:
     void taskQTBUG_5904_crashWithDeviceCoordinateCache();
     void taskQT657_paintIntoCacheWithTransparentParts();
     void taskQTBUG_7863_paintIntoCacheWithTransparentParts();
+    void taskQT_3674_doNotCrash();
 };
 
 void tst_QGraphicsScene::initTestCase()
@@ -4563,6 +4564,25 @@ void tst_QGraphicsScene::taskQTBUG_7863_paintIntoCacheWithTransparentParts()
 
         delete view;
     }
+}
+
+void tst_QGraphicsScene::taskQT_3674_doNotCrash()
+{
+    QGraphicsScene scene;
+
+    QGraphicsView view(&scene);
+    view.resize(200, 200);
+
+    QPixmap pixmap(view.size());
+    QPainter painter(&pixmap);
+    view.render(&painter);
+    painter.end();
+
+    scene.addItem(new QGraphicsWidget);
+    scene.setBackgroundBrush(Qt::green);
+
+    QApplication::processEvents();
+    QApplication::processEvents();
 }
 
 void tst_QGraphicsScene::zeroScale()