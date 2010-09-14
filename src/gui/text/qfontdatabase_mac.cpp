qt/qt

commit f2f6330d915cbe3d0989ad280738ed0a6954cd35

Fix Core Text font loading for certain Mac Fonts
Font names enumerated by Core Text cannot be found by
ATSFontFamilyFindFromName because ATS is expecting native names
instead of the names returned by Core Text. This patch get rid
of ATS font matching code in Cocoa code path to simplify the code,
avoid deprecation warnings (in the future) and fix this issue.

Task-number: QTBUG-11145
Reviewed-by: Eskil

src/gui/text/qfontdatabase_mac.cpp
@@ -293,8 +293,12 @@ void QFontDatabase::load(const QFontPrivate *d, int script)
     // previous versions
     family_list << QApplication::font().defaultFamily();
 
+#if defined(QT_MAC_USE_COCOA)
+    QCFString fontName = NULL, familyName = NULL;
+#else
     ATSFontFamilyRef familyRef = 0;
     ATSFontRef fontRef = 0;
+#endif
 
     QMutexLocker locker(fontDatabaseMutex());
     QFontDatabasePrivate *db = privateDb();
@@ -304,26 +308,21 @@ void QFontDatabase::load(const QFontPrivate *d, int script)
         for (int k = 0; k < db->count; ++k) {
             if (db->families[k]->name.compare(family_list.at(i), Qt::CaseInsensitive) == 0) {
                 QByteArray family_name = db->families[k]->name.toUtf8();
+#if defined(QT_MAC_USE_COCOA)
+                CTFontRef ctFont = CTFontCreateWithName(QCFString(db->families[k]->name), 12, NULL);
+                if (ctFont) {
+                    fontName = CTFontCopyFullName(ctFont);
+                    familyName = CTFontCopyFamilyName(ctFont);
+                    CFRelease(ctFont);
+                    goto FamilyFound;
+                }
+#else
                 familyRef = ATSFontFamilyFindFromName(QCFString(db->families[k]->name), kATSOptionFlagsDefault);
                 if (familyRef) {
                     fontRef = ATSFontFindFromName(QCFString(db->families[k]->name), kATSOptionFlagsDefault);
                     goto FamilyFound;
-                } else {
-#if defined(QT_MAC_USE_COCOA)
-                    // ATS and CT disagrees on what the family name should be,
-                    // use CT to look up the font if ATS fails.
-                    QCFString familyName = QString::fromAscii(family_name);
-                    QCFType<CTFontRef> CTfontRef = CTFontCreateWithName(familyName, 12, NULL);
-                    QCFType<CTFontDescriptorRef> fontDescriptor = CTFontCopyFontDescriptor(CTfontRef);
-                    QCFString displayName = (CFStringRef)CTFontDescriptorCopyAttribute(fontDescriptor, kCTFontDisplayNameAttribute);
-
-                    familyRef = ATSFontFamilyFindFromName(displayName, kATSOptionFlagsDefault);
-                    if (familyRef) {
-                        fontRef = ATSFontFindFromName(displayName, kATSOptionFlagsDefault);
-                        goto FamilyFound;
-                    }
-#endif
                 }
+#endif
             }
         }
     }
@@ -331,18 +330,18 @@ FamilyFound:
     //fill in the engine's font definition
     QFontDef fontDef = d->request; //copy..
     if(fontDef.pointSize < 0)
-	fontDef.pointSize = qt_mac_pointsize(fontDef, d->dpi);
+        fontDef.pointSize = qt_mac_pointsize(fontDef, d->dpi);
     else
-	fontDef.pixelSize = qt_mac_pixelsize(fontDef, d->dpi);
-    {
-        QCFString actualName;
-        if(ATSFontFamilyGetName(familyRef, kATSOptionFlagsDefault, &actualName) == noErr)
-            fontDef.family = actualName;
-    }
+        fontDef.pixelSize = qt_mac_pixelsize(fontDef, d->dpi);
 
 #ifdef QT_MAC_USE_COCOA
-    QFontEngine *engine = new QCoreTextFontEngineMulti(familyRef, fontRef, fontDef, d->kerning);
+    fontDef.family = familyName;
+    QFontEngine *engine = new QCoreTextFontEngineMulti(fontName, fontDef, d->kerning);
+    CFRelease(fontName);
 #else
+    QCFString actualName;
+    if (ATSFontFamilyGetName(familyRef, kATSOptionFlagsDefault, &actualName) == noErr)
+        fontDef.family = actualName;
     QFontEngine *engine = new QFontEngineMacMulti(familyRef, fontRef, fontDef, d->kerning);
 #endif
     d->engineData->engine = engine;

src/gui/text/qfontengine_mac.mm
@@ -143,7 +143,7 @@ void qmacfontengine_gamma_correct(QImage *image)
 
 
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5
-QCoreTextFontEngineMulti::QCoreTextFontEngineMulti(const ATSFontFamilyRef &, const ATSFontRef &atsFontRef, const QFontDef &fontDef, bool kerning)
+QCoreTextFontEngineMulti::QCoreTextFontEngineMulti(const QCFString &name, const QFontDef &fontDef, bool kerning)
     : QFontEngineMulti(0)
 {
     this->fontDef = fontDef;
@@ -158,9 +158,6 @@ QCoreTextFontEngineMulti::QCoreTextFontEngineMulti(const ATSFontFamilyRef &, con
         symbolicTraits |= kCTFontItalicTrait;
         break;
     }
-
-    QCFString name;
-    ATSFontGetName(atsFontRef, kATSOptionFlagsDefault, &name);
 
     transform = CGAffineTransformIdentity;
     if (fontDef.stretch != 100) {

src/gui/text/qfontengine_p.h
@@ -477,8 +477,7 @@ private:
 class QCoreTextFontEngineMulti : public QFontEngineMulti
 {
 public:
-    QCoreTextFontEngineMulti(const ATSFontFamilyRef &atsFamily, const ATSFontRef &atsFontRef,
-                             const QFontDef &fontDef, bool kerning);
+    QCoreTextFontEngineMulti(const QCFString &name, const QFontDef &fontDef, bool kerning);
     ~QCoreTextFontEngineMulti();
 
     virtual bool stringToCMap(const QChar *str, int len, QGlyphLayout *glyphs, int *nglyphs,

tests/auto/qfontdatabase/tst_qfontdatabase.cpp
@@ -70,6 +70,11 @@ private slots:
     void fixedPitch_data();
     void fixedPitch();
 
+#ifdef Q_WS_MAC
+    void trickyFonts_data();
+    void trickyFonts();
+#endif
+
     void widthTwoTimes_data();
     void widthTwoTimes();
 
@@ -131,10 +136,16 @@ void tst_QFontDatabase::fixedPitch_data()
 
     QTest::newRow( "Times New Roman" ) << QString( "Times New Roman" ) << false;
     QTest::newRow( "Arial" ) << QString( "Arial" ) << false;
-    QTest::newRow( "Script" ) << QString( "Script" ) << false;
+    QTest::newRow( "Andale Mono" ) << QString( "Andale Mono" ) << true;
     QTest::newRow( "Courier" ) << QString( "Courier" ) << true;
     QTest::newRow( "Courier New" ) << QString( "Courier New" ) << true;
+#ifndef Q_WS_MAC
+    QTest::newRow( "Script" ) << QString( "Script" ) << false;
     QTest::newRow( "Lucida Console" ) << QString( "Lucida Console" ) << true;
+#else
+    QTest::newRow( "Menlo" ) << QString( "Menlo" ) << true;
+    QTest::newRow( "Monaco" ) << QString( "Monaco" ) << true;
+#endif
 }
 
 void tst_QFontDatabase::fixedPitch()
@@ -155,6 +166,28 @@ void tst_QFontDatabase::fixedPitch()
     QFontInfo fi(qfont);
     QCOMPARE(fi.fixedPitch(), fixedPitch);
 }
+
+#ifdef Q_WS_MAC
+void tst_QFontDatabase::trickyFonts_data()
+{
+    QTest::addColumn<QString>("font");
+
+    QTest::newRow( "Geeza Pro" ) << QString( "Geeza Pro" );
+}
+
+void tst_QFontDatabase::trickyFonts()
+{
+    QFETCH(QString, font);
+
+    QFontDatabase fdb;
+    if (!fdb.families().contains(font))
+        QSKIP( "Font not installed", SkipSingle);
+
+    QFont qfont(font);
+    QFontInfo fi(qfont);
+    QCOMPARE(fi.family(), font);
+}
+#endif
 
 void tst_QFontDatabase::widthTwoTimes_data()
 {

tests/auto/qglyphs/tst_qglyphs.cpp
@@ -347,10 +347,6 @@ void tst_QGlyphs::drawMultiScriptText1()
 
 void tst_QGlyphs::drawMultiScriptText2()
 {
-#if defined(Q_WS_MAC)
-    QSKIP("Unstable because of QTBUG-11145", SkipAll);
-#endif
-
     QString text;
     text += QChar(0x0621); // Arabic, Hamza
     text += QChar(0x03D0); // Greek, beta
@@ -530,10 +526,6 @@ void tst_QGlyphs::drawUnderlinedText()
 
 void tst_QGlyphs::drawRightToLeft()
 {
-#if defined(Q_WS_MAC)
-    QSKIP("Unstable because of QTBUG-11145", SkipAll);
-#endif
-
     QString s;
     s.append(QChar(1575));
     s.append(QChar(1578));

commit 0ac41424ef143d00461e55087ccceb4b9dc11745

Fix crash in Mac font loading
Commit f2f6330d915cbe3d0989ad280738ed0a6954cd35 introduced an
extra CFRelease of QCFString, which will cause crash. Also,
since we are not refering to ATS font in Cocoa code anymore,
conversion of CTFontRef to CGFontRef should not rely on
platform fonts, we use CTFontCopyGraphicsFont to get the
corresponding CGFontRef directly.

Reviewed-by: Eskil Abrahamsen Blomfeldt

src/gui/text/qfontdatabase_mac.cpp
@@ -309,11 +309,10 @@ void QFontDatabase::load(const QFontPrivate *d, int script)
             if (db->families[k]->name.compare(family_list.at(i), Qt::CaseInsensitive) == 0) {
                 QByteArray family_name = db->families[k]->name.toUtf8();
 #if defined(QT_MAC_USE_COCOA)
-                CTFontRef ctFont = CTFontCreateWithName(QCFString(db->families[k]->name), 12, NULL);
+                QCFType<CTFontRef> ctFont = CTFontCreateWithName(QCFString(db->families[k]->name), 12, NULL);
                 if (ctFont) {
                     fontName = CTFontCopyFullName(ctFont);
                     familyName = CTFontCopyFamilyName(ctFont);
-                    CFRelease(ctFont);
                     goto FamilyFound;
                 }
 #else
@@ -337,7 +336,6 @@ FamilyFound:
 #ifdef QT_MAC_USE_COCOA
     fontDef.family = familyName;
     QFontEngine *engine = new QCoreTextFontEngineMulti(fontName, fontDef, d->kerning);
-    CFRelease(fontName);
 #else
     QCFString actualName;
     if (ATSFontFamilyGetName(familyRef, kATSOptionFlagsDefault, &actualName) == noErr)

src/gui/text/qfontengine_mac.mm
@@ -419,8 +419,7 @@ QCoreTextFontEngine::QCoreTextFontEngine(CTFontRef font, const QFontDef &def,
     synthesisFlags = 0;
     ctfont = font;
     CFRetain(ctfont);
-    ATSFontRef atsfont = CTFontGetPlatformFont(ctfont, 0);
-    cgFont = CGFontCreateWithPlatformFont(&atsfont);
+    cgFont = CTFontCopyGraphicsFont(ctfont, NULL);
     CTFontSymbolicTraits traits = CTFontGetSymbolicTraits(ctfont);
     if (fontDef.weight >= QFont::Bold && !(traits & kCTFontBoldTrait)) {
          synthesisFlags |= SynthesizedBold;
@@ -440,8 +439,8 @@ QCoreTextFontEngine::QCoreTextFontEngine(CTFontRef font, const QFontDef &def,
 
 QCoreTextFontEngine::~QCoreTextFontEngine()
 {
-    CFRelease(ctfont);
     CFRelease(cgFont);
+    CFRelease(ctfont);
 }
 
 bool QCoreTextFontEngine::stringToCMap(const QChar *, int, QGlyphLayout *, int *, QTextEngine::ShaperFlags) const
@@ -693,8 +692,6 @@ QImage QCoreTextFontEngine::imageForGlyph(glyph_t glyph, QFixed subPixelPosition
     CGContextSetRGBFillColor(ctx, 1, 1, 1, 1);
     CGContextSetTextDrawingMode(ctx, kCGTextFill);
 
-    ATSFontRef atsfont = CTFontGetPlatformFont(ctfont, 0);
-    QCFType<CGFontRef> cgFont = CGFontCreateWithPlatformFont(&atsfont);
     CGContextSetFont(ctx, cgFont);
 
     qreal pos_x = -br.x.toReal() + subPixelPosition.toReal();