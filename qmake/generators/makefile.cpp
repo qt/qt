qt/qt

commit 019032c93b7397c7239ec99ec44b4923df31533b

run depend_command even if the binary has no absolute path
this magic is a bit braindead anyway, but whatever.

Reviewed-by: joerg
Task-number: QTBUG-13366

qmake/generators/makefile.cpp
@@ -1794,10 +1794,10 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                 const QString c = Option::fixPathToLocalOS(cmdline.at(argv0), true);
                 if(exists(c)) {
                     cmdline[argv0] = escapeFilePath(Option::fixPathToLocalOS(cmdline.at(argv0), false));
-                    tmp_dep_cmd = cmdline.join(" ");
                 } else {
                     cmdline[argv0] = escapeFilePath(cmdline.at(argv0));
                 }
+                tmp_dep_cmd = cmdline.join(" ");
             }
             dep_cd_cmd = QLatin1String("cd ")
                  + escapeFilePath(Option::fixPathToLocalOS(Option::output_dir, false))

commit 1ca554e1744a8e430483a5df3aa9716c266d0c2c

fix error messages when configuring Qt
Commit 019032c93b7397c7239ec99ec44b4923df31533b introduced a lot of
annoying error messages when configuring Qt. It makes qmake complain
that it cannot find rcc and uic, which aren't yet built at this point.

We're now checking for the existence of the depend_command if it is
given as absolute path. This is the case for uic and rcc.
Yeah this is a weak solution for sick code.

Task-number: QTBUG-13366
Reviewed-by: ossi

qmake/generators/makefile.cpp
@@ -1797,7 +1797,9 @@ MakefileGenerator::writeExtraCompilerTargets(QTextStream &t)
                 } else {
                     cmdline[argv0] = escapeFilePath(cmdline.at(argv0));
                 }
-                tmp_dep_cmd = cmdline.join(" ");
+                QFileInfo cmdFileInfo(cmdline[argv0]);
+                if (!cmdFileInfo.isAbsolute() || cmdFileInfo.exists())
+                    tmp_dep_cmd = cmdline.join(" ");
             }
             dep_cd_cmd = QLatin1String("cd ")
                  + escapeFilePath(Option::fixPathToLocalOS(Option::output_dir, false))