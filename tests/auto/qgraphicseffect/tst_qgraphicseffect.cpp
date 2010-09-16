qt/qt

commit f5c5e20ab20f016c07351330d03432e4912f20b2

Fixed missing clip when computing the graphics item effect source rect.
If item clips children to shape we should take that into account when
computing the source rect, to avoid creating unnecessary large source
pixmaps which might have a significant performance impact.

Task-number: QT-3551
Reviewed-by: Gunnar
Reviewed-by: Trond

src/gui/graphicsview/qgraphicsitem.cpp
@@ -1205,8 +1205,14 @@ void QGraphicsItemPrivate::setParentItemHelper(QGraphicsItem *newParent, const Q
 
     Returns the bounding rect of this item's children (excluding itself).
 */
-void QGraphicsItemPrivate::childrenBoundingRectHelper(QTransform *x, QRectF *rect)
+void QGraphicsItemPrivate::childrenBoundingRectHelper(QTransform *x, QRectF *rect, bool doClip)
 {
+    Q_Q(QGraphicsItem);
+
+    QRectF childrenRect;
+    QRectF *result = rect;
+    rect = &childrenRect;
+
     for (int i = 0; i < children.size(); ++i) {
         QGraphicsItem *child = children.at(i);
         QGraphicsItemPrivate *childd = child->d_ptr.data();
@@ -1228,6 +1234,15 @@ void QGraphicsItemPrivate::childrenBoundingRectHelper(QTransform *x, QRectF *rec
                 childd->childrenBoundingRectHelper(x, rect);
         }
     }
+
+    if (doClip && (flags & QGraphicsItem::ItemClipsChildrenToShape)){
+        if (x)
+            *rect &= x->mapRect(q->boundingRect());
+        else
+            *rect &= q->boundingRect();
+    }
+
+    *result |= *rect;
 }
 
 void QGraphicsItemPrivate::initStyleOption(QStyleOptionGraphicsItem *option, const QTransform &worldTransform,
@@ -10816,8 +10831,14 @@ QRectF QGraphicsItemEffectSourcePrivate::boundingRect(Qt::CoordinateSystem syste
     }
 
     QRectF rect = item->boundingRect();
-    if (!item->d_ptr->children.isEmpty())
-        rect |= item->childrenBoundingRect();
+    if (!item->d_ptr->children.isEmpty()) {
+        if (dirtyChildrenBoundingRect) {
+            childrenBoundingRect = QRectF();
+            item->d_ptr->childrenBoundingRectHelper(0, &childrenBoundingRect, true);
+            dirtyChildrenBoundingRect = false;
+        }
+        rect |= childrenBoundingRect;
+    }
 
     if (deviceCoordinates) {
         Q_ASSERT(info->painter);

src/gui/graphicsview/qgraphicsitem_p.h
@@ -239,7 +239,7 @@ public:
     void removeChild(QGraphicsItem *child);
     void setParentItemHelper(QGraphicsItem *parent, const QVariant *newParentVariant,
                              const QVariant *thisPointerVariant);
-    void childrenBoundingRectHelper(QTransform *x, QRectF *rect);
+    void childrenBoundingRectHelper(QTransform *x, QRectF *rect, bool doClip = true);
     void initStyleOption(QStyleOptionGraphicsItem *option, const QTransform &worldTransform,
                          const QRegion &exposedRegion, bool allItems = false) const;
     QRectF effectiveBoundingRect() const;
@@ -580,7 +580,7 @@ class QGraphicsItemEffectSourcePrivate : public QGraphicsEffectSourcePrivate
 {
 public:
     QGraphicsItemEffectSourcePrivate(QGraphicsItem *i)
-        : QGraphicsEffectSourcePrivate(), item(i), info(0)
+        : QGraphicsEffectSourcePrivate(), dirtyChildrenBoundingRect(true), item(i), info(0)
     {}
 
     inline void detach()
@@ -630,6 +630,9 @@ public:
                    QPoint *offset,
                    QGraphicsEffect::PixmapPadMode mode) const;
     QRect paddedEffectRect(Qt::CoordinateSystem system, QGraphicsEffect::PixmapPadMode mode, const QRectF &sourceRect, bool *unpadded = 0) const;
+
+    mutable bool dirtyChildrenBoundingRect;
+    mutable QRectF childrenBoundingRect;
 
     QGraphicsItem *item;
     QGraphicsItemPaintInfo *info;
@@ -788,9 +791,12 @@ inline void QGraphicsItemPrivate::markParentDirty(bool updateBoundingRect)
 #ifndef QT_NO_GRAPHICSEFFECT
         if (parentp->graphicsEffect) {
             if (updateBoundingRect) {
+                QGraphicsItemEffectSourcePrivate *sourcep =
+                    static_cast<QGraphicsItemEffectSourcePrivate *>(parentp->graphicsEffect->d_func()
+                                                                    ->source->d_func());
+                parentp->dirtyChildrenBoundingRect = 1;
                 parentp->notifyInvalidated = 1;
-                static_cast<QGraphicsItemEffectSourcePrivate *>(parentp->graphicsEffect->d_func()
-                                                                ->source->d_func())->invalidateCache();
+                sourcep->invalidateCache();
             }
             if (parentp->graphicsEffect->isEnabled()) {
                 parentp->dirty = 1;

tests/auto/qgraphicseffectsource/tst_qgraphicseffectsource.cpp
@@ -161,6 +161,7 @@ private slots:
     void draw();
     void update();
     void boundingRect();
+    void clippedBoundingRect();
     void deviceRect();
     void pixmap();
 
@@ -280,6 +281,20 @@ void tst_QGraphicsEffectSource::boundingRect()
     QTRY_COMPARE(effect->source()->boundingRect(Qt::LogicalCoordinates), itemBoundingRect);
     // Make sure default value is Qt::LogicalCoordinates.
     QTRY_COMPARE(effect->source()->boundingRect(), itemBoundingRect);
+}
+
+void tst_QGraphicsEffectSource::clippedBoundingRect()
+{
+    QRectF itemBoundingRect = item->boundingRect();
+    item->setFlag(QGraphicsItem::ItemClipsChildrenToShape);
+
+    QGraphicsRectItem *child = new QGraphicsRectItem(-1000, -1000, 2000, 2000);
+    child->setBrush(Qt::red);
+    child->setParentItem(item);
+
+    effect->storeDeviceDependentStuff = true;
+    effect->source()->update();
+    QTRY_COMPARE(effect->source()->boundingRect(Qt::LogicalCoordinates), itemBoundingRect);
 }
 
 void tst_QGraphicsEffectSource::deviceRect()

commit e76564e8a3b305f4fe69135bebb409e357f29f57

Wrong bounding rect returned by QGraphicsEffect::boundingRect().
Regression after: f5c5e20a
Problem was that the cached bounding rect was never invalidated. We can
also remove the cached bounding rect in QGraphicsEffectSource because
QGraphicsItem::childrenBoundingRect now clips by default. This basically
means partially reverting above commit and invalidate whenever
ItemClipsChildrenToShape flag changes.

Auto test included.

Task-number: Discovered while investigating QT-3633
Reviewed-by: samuel

src/gui/graphicsview/qgraphicsitem.cpp
@@ -1272,7 +1272,7 @@ void QGraphicsItemPrivate::setParentItemHelper(QGraphicsItem *newParent, const Q
 
     Returns the bounding rect of this item's children (excluding itself).
 */
-void QGraphicsItemPrivate::childrenBoundingRectHelper(QTransform *x, QRectF *rect, bool doClip)
+void QGraphicsItemPrivate::childrenBoundingRectHelper(QTransform *x, QRectF *rect)
 {
     Q_Q(QGraphicsItem);
 
@@ -1302,7 +1302,7 @@ void QGraphicsItemPrivate::childrenBoundingRectHelper(QTransform *x, QRectF *rec
         }
     }
 
-    if (doClip && (flags & QGraphicsItem::ItemClipsChildrenToShape)){
+    if (flags & QGraphicsItem::ItemClipsChildrenToShape){
         if (x)
             *rect &= x->mapRect(q->boundingRect());
         else
@@ -1870,6 +1870,10 @@ void QGraphicsItem::setFlags(GraphicsItemFlags flags)
         // Item children clipping changes. Propagate the ancestor flag to
         // all children.
         d_ptr->updateAncestorFlag(ItemClipsChildrenToShape);
+        // The childrenBoundingRect is clipped to the boundingRect in case of ItemClipsChildrenToShape,
+        // which means we have to invalidate the cached childrenBoundingRect whenever this flag changes.
+        d_ptr->dirtyChildrenBoundingRect = 1;
+        d_ptr->markParentDirty(true);
     }
 
     if ((flags & ItemIgnoresTransformations) != (oldFlags & ItemIgnoresTransformations)) {
@@ -11168,14 +11172,8 @@ QRectF QGraphicsItemEffectSourcePrivate::boundingRect(Qt::CoordinateSystem syste
     }
 
     QRectF rect = item->boundingRect();
-    if (!item->d_ptr->children.isEmpty()) {
-        if (dirtyChildrenBoundingRect) {
-            childrenBoundingRect = QRectF();
-            item->d_ptr->childrenBoundingRectHelper(0, &childrenBoundingRect, true);
-            dirtyChildrenBoundingRect = false;
-        }
-        rect |= childrenBoundingRect;
-    }
+    if (!item->d_ptr->children.isEmpty())
+        rect |= item->childrenBoundingRect();
 
     if (deviceCoordinates) {
         Q_ASSERT(info->painter);

src/gui/graphicsview/qgraphicsitem_p.h
@@ -300,7 +300,7 @@ public:
     QDeclarativeListProperty<QGraphicsObject> childrenList();
     void setParentItemHelper(QGraphicsItem *parent, const QVariant *newParentVariant,
                              const QVariant *thisPointerVariant);
-    void childrenBoundingRectHelper(QTransform *x, QRectF *rect, bool doClip = true);
+    void childrenBoundingRectHelper(QTransform *x, QRectF *rect);
     void initStyleOption(QStyleOptionGraphicsItem *option, const QTransform &worldTransform,
                          const QRegion &exposedRegion, bool allItems = false) const;
     QRectF effectiveBoundingRect() const;
@@ -661,7 +661,7 @@ class QGraphicsItemEffectSourcePrivate : public QGraphicsEffectSourcePrivate
 {
 public:
     QGraphicsItemEffectSourcePrivate(QGraphicsItem *i)
-        : QGraphicsEffectSourcePrivate(), dirtyChildrenBoundingRect(true), item(i), info(0)
+        : QGraphicsEffectSourcePrivate(), item(i), info(0)
     {}
 
     inline void detach()
@@ -711,9 +711,6 @@ public:
                    QPoint *offset,
                    QGraphicsEffect::PixmapPadMode mode) const;
     QRect paddedEffectRect(Qt::CoordinateSystem system, QGraphicsEffect::PixmapPadMode mode, const QRectF &sourceRect, bool *unpadded = 0) const;
-
-    mutable bool dirtyChildrenBoundingRect;
-    mutable QRectF childrenBoundingRect;
 
     QGraphicsItem *item;
     QGraphicsItemPaintInfo *info;
@@ -872,12 +869,9 @@ inline void QGraphicsItemPrivate::markParentDirty(bool updateBoundingRect)
 #ifndef QT_NO_GRAPHICSEFFECT
         if (parentp->graphicsEffect) {
             if (updateBoundingRect) {
-                QGraphicsItemEffectSourcePrivate *sourcep =
-                    static_cast<QGraphicsItemEffectSourcePrivate *>(parentp->graphicsEffect->d_func()
-                                                                    ->source->d_func());
-                parentp->dirtyChildrenBoundingRect = 1;
+                static_cast<QGraphicsItemEffectSourcePrivate *>(parentp->graphicsEffect->d_func()
+                                                                ->source->d_func())->invalidateCache();
                 parentp->notifyInvalidated = 1;
-                sourcep->invalidateCache();
             }
             if (parentp->scene && parentp->graphicsEffect->isEnabled()) {
                 parentp->dirty = 1;

tests/auto/qgraphicseffect/tst_qgraphicseffect.cpp
@@ -66,6 +66,7 @@ private slots:
     void source();
     void boundingRectFor();
     void boundingRect();
+    void boundingRect2();
     void draw();
     void opacity();
     void grayscale();
@@ -262,6 +263,31 @@ void tst_QGraphicsEffect::boundingRect()
     QVERIFY(!ptr);
 
     delete item;
+}
+
+void tst_QGraphicsEffect::boundingRect2()
+{
+    CustomEffect *effect = new CustomEffect;
+    QGraphicsRectItem *root = new QGraphicsRectItem;
+    root->setGraphicsEffect(effect);
+
+    QGraphicsRectItem *child = new QGraphicsRectItem;
+    QRectF childRect(0, 0, 100, 100);
+    child->setFlag(QGraphicsItem::ItemClipsChildrenToShape);
+    child->setRect(childRect);
+    child->setParentItem(root);
+
+    QGraphicsRectItem *grandChild = new QGraphicsRectItem;
+    QRectF grandChildRect(0, 0, 200, 200);
+    grandChild->setRect(grandChildRect);
+    grandChild->setParentItem(child);
+
+    // Make sure the effect's bounding rect is clipped to the child's bounding rect.
+    QCOMPARE(effect->boundingRect(), effect->boundingRectFor(childRect));
+
+    // Disable ItemClipsChildrenToShape; effect's bounding rect is no longer clipped.
+    child->setFlag(QGraphicsItem::ItemClipsChildrenToShape, false);
+    QCOMPARE(effect->boundingRect(), effect->boundingRectFor(childRect | grandChildRect));
 }
 
 void tst_QGraphicsEffect::draw()