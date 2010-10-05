e319e931e84a4ba9db50729ffc926197a407d78c
Jedrzej Nowacki <jedrzej.nowacki@nokia.com>
2010-10-05 16:44:03 +0200

    Fix regression in QML, console.log wasn't exposed.
    The right soultion needs more investigation but for now it works.

--- src/script/qscriptengine.cpp
+++ src/script/qscriptengine.cpp
@@ -1517,7 +1517,8 @@ static v8::Handle<v8::Value> QtGlobalObjectNamedPropertyGetter(v8::Local<v8::Str
     v8::Local<v8::Object> customGlobalObject = v8::Local<v8::Object>::Cast(data->Get(0));
     QScriptEnginePrivate *engine = static_cast<QScriptEnginePrivate*>(v8::Local<v8::Object>::Cast(data->Get(1))->GetPointerFromInternalField(0));
     // always intercepts
-    v8::Local<v8::Value> result = customGlobalObject->GetRealNamedProperty(property);
+    // FIXME I think it should be GetRealProperty, we don't want to go to the old global object
+    v8::Local<v8::Value> result = customGlobalObject->Get(property);
     if (result.IsEmpty())
         return handleScope.Close(engine->makeJSValue());
     return handleScope.Close(result);
