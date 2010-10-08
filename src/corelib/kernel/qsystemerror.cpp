a780773292c3a59e1500f5e36c3de729a8f45f1c
Shane Kearns <shane.kearns@accenture.com>
2010-10-08 10:43:40 +0100

    Create QSystemError class
    This class is for holding errors returned from system calls, which can
    be later converted to a string when a string is needed.
    This will eventually replace the setError(int,string) used in file
    engines so that strings are only created on demand.
    
    The scope of an error is used to distinguish error codes from the C standard
    library vs error codes from native calls. On some OS (e.g.windows) these
    error ranges overlap.
    
    Reviewed-By: joao

---
+++ src/corelib/kernel/qsystemerror.cpp
@@ -0,0 +1,190 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtCore module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qsystemerror_p.h"
+#include <errno.h>
+
+static QString standardLibraryErrorString(int errorCode)
+{
+    const char *s = 0;
+    QString ret;
+    switch (errorCode) {
+    case 0:
+        break;
+    case EACCES:
+        s = QT_TRANSLATE_NOOP("QIODevice", "Permission denied");
+        break;
+    case EMFILE:
+        s = QT_TRANSLATE_NOOP("QIODevice", "Too many open files");
+        break;
+    case ENOENT:
+        s = QT_TRANSLATE_NOOP("QIODevice", "No such file or directory");
+        break;
+    case ENOSPC:
+        s = QT_TRANSLATE_NOOP("QIODevice", "No space left on device");
+        break;
+    default: {
+    #if !defined(QT_NO_THREAD) && defined(_POSIX_THREAD_SAFE_FUNCTIONS) && _POSIX_VERSION >= 200112L && !defined(Q_OS_INTEGRITY) && !defined(Q_OS_QNX)
+        QByteArray buf(1024, '\0');
+        ret = fromstrerror_helper(strerror_r(errorCode, buf.data(), buf.size()), buf);
+    #else
+        ret = QString::fromLocal8Bit(strerror(errorCode));
+    #endif
+    break; }
+    }
+    if (s) {
+        // ######## this breaks moc build currently
+        // ret = QCoreApplication::translate("QIODevice", s);
+        ret = QString::fromLatin1(s);
+    }
+    return ret.trimmed();
+}
+
+#ifdef Q_OS_WIN
+static QString windowsErrorString(int errorCode)
+{
+    QString ret;
+    wchar_t *string = 0;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+                  NULL,
+                  errorCode,
+                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                  (LPWSTR)&string,
+                  0,
+                  NULL);
+    ret = QString::fromWCharArray(string);
+    LocalFree((HLOCAL)string);
+
+    if (ret.isEmpty() && errorCode == ERROR_MOD_NOT_FOUND)
+        ret = QString::fromLatin1("The specified module could not be found.");
+    return ret;
+}
+#endif
+
+#ifdef Q_OS_SYMBIAN
+static QString symbianErrorString(int errorCode)
+{
+    switch (errorCode) {
+    case KErrNotFound:
+        return QLatin1String("not found");
+    case KErrCancel:
+        return QLatin1String("cancelled");
+    case KErrNoMemory:
+        return QLatin1String("out of memory");
+    case KErrNotSupported:
+        return QLatin1String("not supported");
+    case KErrBadHandle:
+        return QLatin1String("bad handle"); //KERN-EXEC 0 panic is more likely
+    case KErrAlreadyExists:
+        return QLatin1String("already exists");
+    case KErrPathNotFound:
+        return QLatin1String("path not found");
+    case KErrInUse:
+        return QLatin1String("in use");
+    case KErrNotReady:
+        return QLatin1String("not ready (e.g. FS dismounted, no memory card)");
+    case KErrCorrupt:
+        return QLatin1String("corrupt");
+    case KErrAccessDenied:
+        return QLatin1String("access denied");
+    case KErrLocked:
+        return QLatin1String("locked");
+    case KErrWrite:
+        return QLatin1String("incomplete write error");
+    case KErrDisMounted:
+        return QLatin1String("file system dismounted during operation"); //i.e. a forcible dismount was done while we had files open
+    case KErrEof:
+        return QLatin1String("end of file");
+    case KErrDiskFull:
+        return QLatin1String("no space in file system");
+    case KErrBadName:
+        return QLatin1String("invalid filename");
+    case KErrTimedOut:
+        return QLatin1String("timed out");
+    case KErrBadDescriptor:
+        return QLatin1String("bad descriptor (passed address on stack to async call?)");
+    case KErrAbort:
+        return QLatin1String("aborted");
+    case KErrTooBig:
+        return QLatin1String("too big"); //e.g. trying to open a >2GB file with 32 bit API
+    case KErrBadPower:
+        return QLatin1String("insufficient power");
+    case KErrDirFull:
+        return QLatin1String("no space in directory table");
+    case KErrHardwareNotAvailable:
+        return QLatin1String("hardware not available");
+    case KErrSessionClosed:
+        return QLatin1String("session closed");
+    case KErrPermissionDenied:
+        return QLatin1String("permission denied");
+    default:
+        return QString(QLatin1String("symbian error %d")).arg(errorCode);
+    }    
+}
+#endif
+
+QString QSystemError::toString()
+{
+    switch(errorScope) {
+    case NativeError:
+#if defined (Q_OS_WIN)
+        errorString = windowsErrorString(errorCode);
+        break;
+#elif defined (Q_OS_SYMBIAN)
+        return symbianErrorString(errorCode);
+        break;
+#else
+        //unix: fall through as native and standard library are the same
+#endif
+    case StandardLibraryError:
+        return standardLibraryErrorString(errorCode);
+        break;
+    default:
+        qWarning("invalid error scope");
+        //fall through
+    case NoError:
+        return QLatin1String("No error");
+        break;
+    }
+}
+
+QT_END_NAMESPACE
+
