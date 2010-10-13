qt/qt

commit 319b0262418d74cc416a7dd1f620b54ba45bad22

Obsolete QDirModel and remove it from the doc.
QFileSystemModel provides the same features as QDirModel but in a
efficient way so no need to keep QDirModel around.
This commit mark as obsolete QDirModel. Examples has been ported to
use QFileSystemModel instead. QCompleter also gain the support of
QFileSystemModel to complete nicely a line edit.

This commit also add a new signal in QFileSystemModel when the directory
is loaded, i.e. the thread has finished to read the current dir.

Task-number:QTBUG-3884
Reviewed-by:ogoffart
Reviewed-by:gabi
Reviewed-by:mbm

src/gui/util/qcompleter.cpp
@@ -62,7 +62,7 @@
 
     \snippet doc/src/snippets/code/src_gui_util_qcompleter.cpp 0
 
-    A QDirModel can be used to provide auto completion of file names.
+    A QFileSystemModel can be used to provide auto completion of file names.
     For example:
 
     \snippet doc/src/snippets/code/src_gui_util_qcompleter.cpp 1
@@ -120,7 +120,7 @@
     completion is then performed one level at a time.
 
     Let's take the example of a user typing in a file system path.
-    The model is a (hierarchical) QDirModel. The completion
+    The model is a (hierarchical) QFileSystemModel. The completion
     occurs for every element in the path. For example, if the current
     text is \c C:\Wind, QCompleter might suggest \c Windows to
     complete the current path element. Similarly, if the current text
@@ -130,12 +130,12 @@
     split the path into a list of strings that are matched at each level.
     For \c C:\Windows\Sy, it needs to be split as "C:", "Windows" and "Sy".
     The default implementation of splitPath(), splits the completionPrefix
-    using QDir::separator() if the model is a QDirModel.
+    using QDir::separator() if the model is a QFileSystemModel.
 
     To provide completions, QCompleter needs to know the path from an index.
     This is provided by pathFromIndex(). The default implementation of
     pathFromIndex(), returns the data for the \l{Qt::EditRole}{edit role}
-    for list models and the absolute file path if the mode is a QDirModel.
+    for list models and the absolute file path if the mode is a QFileSystemModel.
 
     \sa QAbstractItemModel, QLineEdit, QComboBox, {Completer Example}
 */
@@ -147,6 +147,7 @@
 #include "QtGui/qscrollbar.h"
 #include "QtGui/qstringlistmodel.h"
 #include "QtGui/qdirmodel.h"
+#include "QtGui/qfilesystemmodel.h"
 #include "QtGui/qheaderview.h"
 #include "QtGui/qlistview.h"
 #include "QtGui/qapplication.h"
@@ -470,9 +471,13 @@ QMatchData QCompletionEngine::filterHistory()
     QAbstractItemModel *source = c->proxy->sourceModel();
     if (curParts.count() <= 1 || c->proxy->showAll || !source)
         return QMatchData();
-    bool dirModel = false;
+    bool isDirModel = false;
+    bool isFsModel = false;
 #ifndef QT_NO_DIRMODEL
-    dirModel = (qobject_cast<QDirModel *>(source) != 0);
+    isDirModel = (qobject_cast<QDirModel *>(source) != 0);
+#endif
+#ifndef QT_NO_FILESYSTEMMODEL
+    isFsModel = (qobject_cast<QFileSystemModel *>(source) != 0);
 #endif
     QVector<int> v;
     QIndexMapper im(v);
@@ -482,7 +487,7 @@ QMatchData QCompletionEngine::filterHistory()
         QString str = source->index(i, c->column).data().toString();
         if (str.startsWith(c->prefix, c->cs)
 #if (!defined(Q_OS_WIN) || defined(Q_OS_WINCE)) && !defined(Q_OS_SYMBIAN)
-            && (!dirModel || QDir::toNativeSeparators(str) != QDir::separator())
+            && ((!isFsModel && !isDirModel) || QDir::toNativeSeparators(str) != QDir::separator())
 #endif
             )
             m.indices.append(i);
@@ -838,6 +843,13 @@ void QCompleterPrivate::_q_complete(QModelIndex index, bool highlighted)
                 completion += QDir::separator();
         }
 #endif
+#ifndef QT_NO_FILESYSTEMMODEL
+        // add a trailing separator in inline
+        if (mode == QCompleter::InlineCompletion) {
+            if (qobject_cast<QFileSystemModel *>(proxy->sourceModel()) && QFileInfo(completion).isDir())
+                completion += QDir::separator();
+        }
+#endif
     }
 
     if (highlighted) {
@@ -889,6 +901,14 @@ void QCompleterPrivate::showPopup(const QRect& rect)
 
     if (!popup->isVisible())
         popup->show();
+}
+
+
+void QCompleterPrivate::_q_fileSystemModelDirectoryLoaded(const QString &path)
+{
+    Q_Q(QCompleter);
+    Q_UNUSED(path);
+    q->complete();
 }
 
 /*!
@@ -971,7 +991,7 @@ QWidget *QCompleter::widget() const
     be list model or a tree model. If a model has been already previously set
     and it has the QCompleter as its parent, it is deleted.
 
-    For convenience, if \a model is a QDirModel, QCompleter switches its
+    For convenience, if \a model is a QFileSystemModel, QCompleter switches its
     caseSensitivity to Qt::CaseInsensitive on Windows and Qt::CaseSensitive
     on other platforms.
 
@@ -995,6 +1015,17 @@ void QCompleter::setModel(QAbstractItemModel *model)
 #endif
     }
 #endif // QT_NO_DIRMODEL
+#ifndef QT_NO_FILESYSTEMMODEL
+    QFileSystemModel *fsModel = qobject_cast<QFileSystemModel *>(model);
+    if (fsModel) {
+#if (defined(Q_OS_WIN) && !defined(Q_OS_WINCE)) || defined(Q_OS_SYMBIAN)
+        setCaseSensitivity(Qt::CaseInsensitive);
+#else
+        setCaseSensitivity(Qt::CaseSensitive);
+#endif
+        connect(fsModel, SIGNAL(directoryLoaded(QString)), this, SLOT(_q_fileSystemModelDirectoryLoaded(QString)));
+    }
+#endif // QT_NO_FILESYSTEMMODEL
 }
 
 /*!
@@ -1626,10 +1657,11 @@ QAbstractItemModel *QCompleter::completionModel() const
 
     The default implementation returns the \l{Qt::EditRole}{edit role} of the
     item for list models. It returns the absolute file path if the model is a
-    QDirModel.
+    QFileSystemModel.
 
     \sa splitPath()
 */
+
 QString QCompleter::pathFromIndex(const QModelIndex& index) const
 {
     Q_D(const QCompleter);
@@ -1639,10 +1671,15 @@ QString QCompleter::pathFromIndex(const QModelIndex& index) const
     QAbstractItemModel *sourceModel = d->proxy->sourceModel();
     if (!sourceModel)
         return QString();
+    bool isDirModel = false;
+    bool isFsModel = false;
 #ifndef QT_NO_DIRMODEL
-    QDirModel *dirModel = qobject_cast<QDirModel *>(sourceModel);
-    if (!dirModel)
+    isDirModel = qobject_cast<QDirModel *>(d->proxy->sourceModel()) != 0;
 #endif
+#ifndef QT_NO_FILESYSTEMMODEL
+    isFsModel = qobject_cast<QFileSystemModel *>(d->proxy->sourceModel()) != 0;
+#endif
+    if (!isDirModel && !isFsModel)
         return sourceModel->data(index, d->role).toString();
 
     QModelIndex idx = index;
@@ -1668,7 +1705,7 @@ QString QCompleter::pathFromIndex(const QModelIndex& index) const
     in the model().
 
     The default implementation of splitPath() splits a file system path based on
-    QDir::separator() when the sourceModel() is a QDirModel.
+    QDir::separator() when the sourceModel() is a QFileSystemModel.
 
     When used with list models, the first item in the returned list is used for
     matching.
@@ -1678,12 +1715,19 @@ QString QCompleter::pathFromIndex(const QModelIndex& index) const
 QStringList QCompleter::splitPath(const QString& path) const
 {
     bool isDirModel = false;
+    bool isFsModel = false;
 #ifndef QT_NO_DIRMODEL
     Q_D(const QCompleter);
     isDirModel = qobject_cast<QDirModel *>(d->proxy->sourceModel()) != 0;
 #endif
+#ifndef QT_NO_FILESYSTEMMODEL
+#ifdef QT_NO_DIRMODEL
+    Q_D(const QCompleter);
+#endif
+    isFsModel = qobject_cast<QFileSystemModel *>(d->proxy->sourceModel()) != 0;
+#endif
 
-    if (!isDirModel || path.isEmpty())
+    if ((!isDirModel && !isFsModel) || path.isEmpty())
         return QStringList(completionPrefix());
 
     QString pathCopy = QDir::toNativeSeparators(path);

src/gui/util/qcompleter_p.h
@@ -98,6 +98,7 @@ public:
     void _q_complete(QModelIndex, bool = false);
     void _q_completionSelected(const QItemSelection&);
     void _q_autoResizePopup();
+    void _q_fileSystemModelDirectoryLoaded(const QString &path);
     void setCurrentIndex(QModelIndex, bool = true);
 };
 

examples/itemviews/dirview/main.cpp
@@ -45,7 +45,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QDirModel model;
+    QFileSystemModel model;
+    model.setRootPath("");
     QTreeView tree;
     tree.setModel(&model);
 

examples/tools/completer/dirmodel.cpp
@@ -42,14 +42,14 @@
 #include "dirmodel.h"
 
 //! [0]
-DirModel::DirModel(QObject *parent)
-    : QDirModel(parent)
+FileSystemModel::FileSystemModel(QObject *parent)
+    : QFileSystemModel(parent)
 {
 }
 //! [0]
 
 //! [1]
-QVariant DirModel::data(const QModelIndex &index, int role) const
+QVariant FileSystemModel::data(const QModelIndex &index, int role) const
 {
     if (role == Qt::DisplayRole && index.column() == 0) {
         QString path  = QDir::toNativeSeparators(filePath(index));
@@ -58,6 +58,7 @@ QVariant DirModel::data(const QModelIndex &index, int role) const
         return path;
     }
 
-    return QDirModel::data(index, role);
+    return QFileSystemModel::data(index, role);
 }
+
 //! [1]

examples/tools/completer/dirmodel.h
@@ -39,21 +39,21 @@
 **
 ****************************************************************************/
 
-#ifndef DIRMODEL_H
-#define DIRMODEL_H
+#ifndef FILESYSTEMMODEL_H
+#define FILESYSTEMMODEL_H
 
-#include <QDirModel>
+#include <QFileSystemModel>
 
-// With a QDirModel, set on a view, you will see "Program Files" in the view
+// With a QFileSystemModel, set on a view, you will see "Program Files" in the view
 // But with this model, you will see "C:\Program Files" in the view.
 // We acheive this, by having the data() return the entire file path for
 // the display role. Note that the Qt::EditRole over which the QCompleter
 // looks for matches is left unchanged
 //! [0]
-class DirModel : public QDirModel
+class FileSystemModel : public QFileSystemModel
 {
 public:
-    DirModel(QObject *parent = 0);
+    FileSystemModel(QObject *parent = 0);
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 };
 //! [0]

examples/tools/completer/mainwindow.cpp
@@ -55,8 +55,8 @@ MainWindow::MainWindow(QWidget *parent)
     modelLabel->setText(tr("Model"));
 
     modelCombo = new QComboBox;
-    modelCombo->addItem(tr("QDirModel"));
-    modelCombo->addItem(tr("QDirModel that shows full path"));
+    modelCombo->addItem(tr("QFileSytemModel"));
+    modelCombo->addItem(tr("QFileSytemModel that shows full path"));
     modelCombo->addItem(tr("Country list"));
     modelCombo->addItem(tr("Word list"));
     modelCombo->setCurrentIndex(0);
@@ -218,17 +218,19 @@ void MainWindow::changeModel()
     switch (modelCombo->currentIndex()) {
     default:
     case 0:
-        { // Unsorted QDirModel
-            QDirModel *dirModel = new QDirModel(completer);
-            completer->setModel(dirModel);
+        { // Unsorted QFileSystemModel
+            QFileSystemModel *fsModel = new QFileSystemModel(completer);
+            fsModel->setRootPath("");
+            completer->setModel(fsModel);
             contentsLabel->setText(tr("Enter file path"));
         }
         break;
 //! [11] //! [12]
     case 1:
-        { // DirModel that shows full paths
-            DirModel *dirModel = new DirModel(completer);
-            completer->setModel(dirModel);
+        {   // FileSystemModel that shows full paths
+            FileSystemModel *fsModel = new FileSystemModel(completer);
+            completer->setModel(fsModel);
+            fsModel->setRootPath("");
             contentsLabel->setText(tr("Enter file path"));
         }
         break;

src/gui/dialogs/qfileinfogatherer.cpp
@@ -354,6 +354,7 @@ void QFileInfoGatherer::getFileInfos(const QString &path, const QStringList &fil
     }
     if (!updatedFiles.isEmpty())
         emit updates(path, updatedFiles);
+    emit directoryLoaded(path);
 }
 
 void QFileInfoGatherer::fetch(const QFileInfo &fileInfo, QTime &base, bool &firstTime, QList<QPair<QString, QFileInfo> > &updatedFiles, const QString &path) {

src/gui/dialogs/qfileinfogatherer_p.h
@@ -155,6 +155,7 @@ Q_SIGNALS:
     void updates(const QString &directory, const QList<QPair<QString, QFileInfo> > &updates);
     void newListOfFiles(const QString &directory, const QStringList &listOfFiles) const;
     void nameResolved(const QString &fileName, const QString &resolvedName) const;
+    void directoryLoaded(const QString &path);
 
 public:
     QFileInfoGatherer(QObject *parent = 0);

commit c416ec00e7c859a844a5bcb24c7a31147aed974c

QCompleter: do not auto complete when directory is changed.
Fixed regression for QCompleter associated with a QFileSystemModel.
It would popup a popup each time the directory is changed.

This was a regression in Qt 4.7.0, introduced with 319b0262418d74cc416a7d

Task-number: QTBUG-14292
Reviewed-by: Thierry

src/gui/util/qcompleter.cpp
@@ -782,7 +782,8 @@ QMatchData QUnsortedModelEngine::filter(const QString& part, const QModelIndex& 
 ///////////////////////////////////////////////////////////////////////////////
 QCompleterPrivate::QCompleterPrivate()
 : widget(0), proxy(0), popup(0), cs(Qt::CaseSensitive), role(Qt::EditRole), column(0),
-  maxVisibleItems(7), sorting(QCompleter::UnsortedModel), wrap(true), eatFocusOut(true)
+  maxVisibleItems(7), sorting(QCompleter::UnsortedModel), wrap(true), eatFocusOut(true),
+  hiddenBecauseNoMatch(false)
 {
 }
 
@@ -921,12 +922,14 @@ void QCompleterPrivate::showPopup(const QRect& rect)
 void QCompleterPrivate::_q_fileSystemModelDirectoryLoaded(const QString &path)
 {
     Q_Q(QCompleter);
-#ifndef QT_NO_LINEEDIT
-    QLineEdit *lineEdit = qobject_cast<QLineEdit *>(widget);
-    //the path given by QFileSystemModel does not end with /
-    if (lineEdit && !lineEdit->text().isEmpty() && !q->completionPrefix().isEmpty() && q->completionPrefix() != path + QLatin1Char('/'))
+    // Slot called when QFileSystemModel has finished loading.
+    // If we hide the popup because there was no match because the model was not loaded yet,
+    // we re-start the completion when we get the results
+    if (hiddenBecauseNoMatch
+        && prefix.startsWith(path) && prefix != (path + '/')
+        && widget) {
         q->complete();
-#endif
+    }
 }
 
 /*!
@@ -1200,6 +1203,7 @@ bool QCompleter::eventFilter(QObject *o, QEvent *e)
     Q_D(QCompleter);
 
     if (d->eatFocusOut && o == d->widget && e->type() == QEvent::FocusOut) {
+        d->hiddenBecauseNoMatch = false;
         if (d->popup && d->popup->isVisible())
             return true;
     }
@@ -1378,6 +1382,7 @@ void QCompleter::complete(const QRect& rect)
 {
     Q_D(QCompleter);
     QModelIndex idx = d->proxy->currentIndex(false);
+    d->hiddenBecauseNoMatch = false;
     if (d->mode == QCompleter::InlineCompletion) {
         if (idx.isValid())
             d->_q_complete(idx, true);
@@ -1389,6 +1394,7 @@ void QCompleter::complete(const QRect& rect)
         || (d->mode == QCompleter::UnfilteredPopupCompletion && d->proxy->rowCount() == 0)) {
         if (d->popup)
             d->popup->hide(); // no suggestion, hide
+        d->hiddenBecauseNoMatch = true;
         return;
     }
 

src/gui/util/qcompleter_p.h
@@ -93,6 +93,7 @@ public:
 
     bool eatFocusOut;
     QRect popupRect;
+    bool hiddenBecauseNoMatch;
 
     void showPopup(const QRect&);
     void _q_complete(QModelIndex, bool = false);

tests/auto/qcompleter/tst_qcompleter.cpp
@@ -151,6 +151,7 @@ private slots:
     void task253125_lineEditCompletion_data();
     void task253125_lineEditCompletion();
     void task247560_keyboardNavigation();
+    void QTBUG_14292_filesystem();
 
 private:
     void filter(bool assync = false);
@@ -571,6 +572,7 @@ void tst_QCompleter::csMatchingOnCiSortedModel()
 void tst_QCompleter::directoryModel_data()
 {
     delete completer;
+
     completer = new CsvCompleter;
     completer->setModelSorting(QCompleter::CaseSensitivelySortedModel);
     setSourceModel(DIRECTORY_MODEL);
@@ -1449,6 +1451,78 @@ void tst_QCompleter::task247560_keyboardNavigation()
     QTest::keyClick(edit.completer()->popup(), Qt::Key_Enter);
 
     QCOMPARE(edit.text(), QString("row 3 column 1"));
+}
+
+void tst_QCompleter::QTBUG_14292_filesystem()
+{
+    QDir tmpDir = QDir::temp();
+    qsrand(QTime::currentTime().msec());
+    QString d = "tst_QCompleter_" + QString::number(qrand());
+    QVERIFY(tmpDir.mkdir(d));
+
+#if 0
+    struct Cleanup {
+        QString dir;
+        ~Cleanup() {
+            qDebug() << dir <<
+            QFile::remove(dir); }
+    } cleanup;
+    cleanup.dir = tmpDir.absolutePath()+"/" +d;
+#endif
+
+    QVERIFY(tmpDir.cd(d));
+    QVERIFY(tmpDir.mkdir("hello"));
+    QVERIFY(tmpDir.mkdir("holla"));
+
+    QLineEdit edit;
+    QCompleter comp;
+    QFileSystemModel model;
+    model.setRootPath(tmpDir.path());
+    comp.setModel(&model);
+    edit.setCompleter(&comp);
+
+    edit.show();
+    QApplication::setActiveWindow(&edit);
+    QTest::qWaitForWindowShown(&edit);
+    QTRY_VERIFY(QApplication::activeWindow() == &edit);
+
+    QVERIFY(!comp.popup()->isVisible());
+    edit.setText(tmpDir.path());
+    QTest::keyClick(&edit, '/');
+    QTRY_VERIFY(comp.popup()->isVisible());
+    QCOMPARE(comp.popup()->model()->rowCount(), 2);
+    QTest::keyClick(&edit, 'h');
+    QCOMPARE(comp.popup()->model()->rowCount(), 2);
+    QTest::keyClick(&edit, 'e');
+    QCOMPARE(comp.popup()->model()->rowCount(), 1);
+    QTest::keyClick(&edit, 'r');
+    QTRY_VERIFY(!comp.popup()->isVisible());
+    QVERIFY(tmpDir.mkdir("hero"));
+    QTRY_VERIFY(comp.popup()->isVisible());
+    QCOMPARE(comp.popup()->model()->rowCount(), 1);
+    QTest::keyClick(comp.popup(), Qt::Key_Escape);
+    QTRY_VERIFY(!comp.popup()->isVisible());
+    QVERIFY(tmpDir.mkdir("nothingThere"));
+    //there is no reason creating a file should open a popup, it did in Qt 4.7.0
+    QTest::qWait(60);
+    QVERIFY(!comp.popup()->isVisible());
+
+    QTest::keyClick(&edit, Qt::Key_Backspace);
+    QTRY_VERIFY(comp.popup()->isVisible());
+    QCOMPARE(comp.popup()->model()->rowCount(), 2);
+    QTest::keyClick(&edit, 'm');
+    QTRY_VERIFY(!comp.popup()->isVisible());
+
+    QWidget w;
+    w.show();
+    QApplication::setActiveWindow(&w);
+    QTest::qWaitForWindowShown(&w);
+    QTRY_VERIFY(!edit.hasFocus() && !comp.popup()->hasFocus());
+
+    QVERIFY(tmpDir.mkdir("hemo"));
+    //there is no reason creating a file should open a popup, it did in Qt 4.7.0
+    QTest::qWait(60);
+    QVERIFY(!comp.popup()->isVisible());
 }
 
 QTEST_MAIN(tst_QCompleter)