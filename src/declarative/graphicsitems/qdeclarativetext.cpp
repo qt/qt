82159dbbfe7e36bc8f67ad1637a2285fb002eb11
Yann Bodson <yann.bodson@nokia.com>
2010-10-28 18:18:41 +1000

    Remove unnecessary layouting code from QDeclarativeText
    Task-number: QTBUG-14795
    Reviewed-by: Michael Brasser

--- src/declarative/graphicsitems/qdeclarativetext.cpp
+++ src/declarative/graphicsitems/qdeclarativetext.cpp
@@ -287,8 +287,7 @@ QSize QDeclarativeTextPrivate::setupTextLayout()
     Q_Q(QDeclarativeText);
     layout.setCacheEnabled(true);
 
-    int height = 0;
-    qreal widthUsed = 0;
+    qreal height = 0;
     qreal lineWidth = 0;
 
     //set manual width
@@ -297,6 +296,7 @@ QSize QDeclarativeTextPrivate::setupTextLayout()
 
     QTextOption textOption = layout.textOption();
     textOption.setWrapMode(QTextOption::WrapMode(wrapMode));
+    textOption.setAlignment((Qt::Alignment)hAlign);
     layout.setTextOption(textOption);
 
     layout.beginLayout();
@@ -305,37 +305,15 @@ QSize QDeclarativeTextPrivate::setupTextLayout()
         if (!line.isValid())
             break;
 
-        if ((wrapMode != QDeclarativeText::NoWrap || elideMode != QDeclarativeText::ElideNone) && q->widthValid()) 
+        if ((wrapMode != QDeclarativeText::NoWrap || elideMode != QDeclarativeText::ElideNone) && q->widthValid()) {
             line.setLineWidth(lineWidth);
+            line.setPosition(QPointF(0, height));
+            height += line.height();
+        }
     }
     layout.endLayout();
 
-    for (int i = 0; i < layout.lineCount(); ++i) {
-        QTextLine line = layout.lineAt(i);
-        widthUsed = qMax(widthUsed, line.naturalTextWidth());
-    }
-
-    qreal layoutWidth = q->widthValid()?q->width():widthUsed;
-
-    int x = 0;
-    for (int i = 0; i < layout.lineCount(); ++i) {
-        QTextLine line = layout.lineAt(i);
-        line.setPosition(QPointF(0, height));
-        height += int(line.height());
-
-        if (!cacheAllTextAsImage) {
-            if (hAlign == QDeclarativeText::AlignLeft) {
-                x = 0;
-            } else if (hAlign == QDeclarativeText::AlignRight) {
-                x = layoutWidth - (int)line.naturalTextWidth();
-            } else if (hAlign == QDeclarativeText::AlignHCenter) {
-                x = (layoutWidth - (int)line.naturalTextWidth()) / 2;
-            }
-            line.setPosition(QPoint(x, (int)line.y()));
-        }
-    }
-
-    return QSize(qCeil(widthUsed), height);
+    return QSize(qCeil(layout.boundingRect().width()), layout.boundingRect().height());
 }
 
 /*!
@@ -346,19 +324,6 @@ QPixmap QDeclarativeTextPrivate::textLayoutImage(bool drawStyle)
 {
     //do layout
     QSize size = layedOutTextSize;
-
-    int x = 0;
-    for (int i = 0; i < layout.lineCount(); ++i) {
-        QTextLine line = layout.lineAt(i);
-        if (hAlign == QDeclarativeText::AlignLeft) {
-            x = 0;
-        } else if (hAlign == QDeclarativeText::AlignRight) {
-            x = size.width() - (int)line.naturalTextWidth();
-        } else if (hAlign == QDeclarativeText::AlignHCenter) {
-            x = (size.width() - (int)line.naturalTextWidth()) / 2;
-        }
-        line.setPosition(QPoint(x, (int)line.y()));
-    }
 
     //paint text
     QPixmap img(size);
@@ -1147,7 +1112,7 @@ void QDeclarativeText::geometryChanged(const QRectF &newGeometry, const QRectF &
     if ((!d->internalWidthUpdate && newGeometry.width() != oldGeometry.width())
             && (d->wrapMode != QDeclarativeText::NoWrap
                 || d->elideMode != QDeclarativeText::ElideNone
-                || d->hAlign != Qt::AlignLeft)) {
+                || d->hAlign != QDeclarativeText::AlignLeft)) {
         if (d->singleline && d->elideMode != QDeclarativeText::ElideNone && widthValid()) {
             // We need to re-elide
             d->updateLayout();
