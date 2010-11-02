qt/qt

commit b81b8e43ad57183ed66086ec90cabef5906ab9a2

Fix the int validator not handling the locale
Task-number: QTBUG-3179
Reviewed-by: ogoffart

src/gui/widgets/qvalidator.cpp
@@ -400,8 +400,10 @@ QValidator::State QIntValidator::validate(QString & input, int&) const
     qlonglong entered = QLocalePrivate::bytearrayToLongLong(buff.constData(), 10, &ok, &overflow);
     if (overflow || !ok)
         return Invalid;
-    if (entered >= b && entered <= t)
-        return Acceptable;
+    if (entered >= b && entered <= t) {
+        locale().toInt(input, &ok);
+        return ok ? Acceptable : Intermediate;
+    }
 
     if (entered >= 0) {
         // the -entered < b condition is necessary to allow people to type
@@ -412,6 +414,20 @@ QValidator::State QIntValidator::validate(QString & input, int&) const
     }
 }
 
+/*! \reimp */
+void QIntValidator::fixup(QString &input) const
+{
+    QByteArray buff;
+    if (!locale().d()->validateChars(input, QLocalePrivate::IntegerMode, &buff)) {
+        QLocale cl(QLocale::C);
+        if (!cl.d()->validateChars(input, QLocalePrivate::IntegerMode, &buff))
+            return;
+    }
+    bool ok, overflow;
+    qlonglong entered = QLocalePrivate::bytearrayToLongLong(buff.constData(), 10, &ok, &overflow);
+    if (ok && !overflow)
+        input = locale().toString(entered);
+}
 
 /*!
     Sets the range of the validator to only accept integers between \a

src/corelib/tools/qlocale.cpp
@@ -4293,6 +4293,7 @@ bool QLocalePrivate::validateChars(const QString &str, NumberMode numMode, QByte
 
     const bool scientific = numMode == DoubleScientificMode;
     bool lastWasE = false;
+    bool lastWasDigit = false;
     int eCnt = 0;
     int decPointCnt = 0;
     bool dec = false;
@@ -4307,6 +4308,7 @@ bool QLocalePrivate::validateChars(const QString &str, NumberMode numMode, QByte
                 if (dec && decDigits != -1 && decDigits < ++decDigitCnt)
                     return false;
             }
+            lastWasDigit = true;
         } else {
             switch (c) {
                 case '.':
@@ -4344,7 +4346,10 @@ bool QLocalePrivate::validateChars(const QString &str, NumberMode numMode, QByte
                     break;
 
                 case ',':
-                    return false;
+                    //it can only be placed after a digit which is before the decimal point
+                    if (!lastWasDigit || decPointCnt > 0)
+                        return false;
+                    break;
 
                 case 'e':
                     if (scientific) {
@@ -4362,10 +4367,12 @@ bool QLocalePrivate::validateChars(const QString &str, NumberMode numMode, QByte
                     // If it's not a valid digit, it shall be Invalid.
                     return false;
             }
+            lastWasDigit = false;
         }
 
         lastWasE = c == 'e';
-        buff->append(c);
+        if (c != ',')
+            buff->append(c);
     }
 
     return true;

src/gui/widgets/qvalidator.h
@@ -105,6 +105,7 @@ public:
     ~QIntValidator();
 
     QValidator::State validate(QString &, int &) const;
+    void fixup(QString &input) const;
 
     void setBottom(int);
     void setTop(int);

tests/auto/qintvalidator/tst_qintvalidator.cpp
@@ -50,6 +50,7 @@ private slots:
     void validate_data();
     void validate();
     void validateArabic();
+    void validateFrench();
 };
 
 Q_DECLARE_METATYPE(QValidator::State);
@@ -182,6 +183,30 @@ void tst_QIntValidator::validateArabic()
 
 }
 
+
+void tst_QIntValidator::validateFrench()
+{
+    QIntValidator validator(-2000, 2000, 0);
+    validator.setLocale(QLocale::French);
+    int i;
+
+    QString s = QLatin1String("1 ");
+    QCOMPARE(validator.validate(s, i), QValidator::Acceptable);
+    validator.fixup(s);
+    QCOMPARE(s, s);
+
+    s = QLatin1String("1 000");
+    QCOMPARE(validator.validate(s, i), QValidator::Acceptable);
+    validator.fixup(s);
+    QCOMPARE(s, s);
+
+
+    s = QLatin1String("1 0 00");
+    QCOMPARE(validator.validate(s, i), QValidator::Intermediate);
+    validator.fixup(s);
+    QCOMPARE(s, validator.locale().toString(1000));
+}
+
 void tst_QIntValidator::validate()
 {
     QFETCH(int, minimum);
@@ -190,6 +215,7 @@ void tst_QIntValidator::validate()
     QFETCH(QValidator::State, state);
 
     QIntValidator iv(minimum, maximum, 0);
+    iv.setLocale(QLocale::C);
     int dummy;
     QCOMPARE((int)iv.validate(value, dummy), (int)state);
 }

commit 445ef8847979dab72893aab1924d46d0fe1a8a3e

With some locales, QDoubleValidator would not accept "C" locale valid numbers
Locales using dot as thousands delimiter and comma as decimal
separator are prone to this error.

This is a regression introduced by commit b81b8e43ad57183ed66.

Auto-tests included.

Reviewed-by: Olivier
Task-number: QTBUG_14935

src/corelib/tools/qlocale.h
@@ -114,7 +114,7 @@ class Q_CORE_EXPORT QLocale
     friend class QString;
     friend class QByteArray;
     friend class QIntValidator;
-    friend class QDoubleValidator;
+    friend class QDoubleValidatorPrivate;
     friend class QTextStream;
     friend class QTextStreamPrivate;
 

src/gui/widgets/qvalidator.cpp
@@ -499,6 +499,8 @@ public:
     }
 
     QDoubleValidator::Notation notation;
+
+    QValidator::State validateWithLocale(QString & input, QLocalePrivate::NumberMode numMode, const QLocale &locale) const;
 };
 
 
@@ -654,42 +656,49 @@ QValidator::State QDoubleValidator::validate(QString & input, int &) const
             break;
     }
 
+    State currentLocaleValidation = d->validateWithLocale(input, numMode, locale());
+    if (currentLocaleValidation == Acceptable || locale().language() == QLocale::C)
+        return currentLocaleValidation;
+    State cLocaleValidation = d->validateWithLocale(input, numMode, QLocale(QLocale::C));
+    return qMax(currentLocaleValidation, cLocaleValidation);
+}
+
+QValidator::State QDoubleValidatorPrivate::validateWithLocale(QString &input, QLocalePrivate::NumberMode numMode, const QLocale &locale) const
+{
+    Q_Q(const QDoubleValidator);
     QByteArray buff;
-    if (!locale().d()->validateChars(input, numMode, &buff, dec)) {
-        QLocale cl(QLocale::C);
-        if (!cl.d()->validateChars(input, numMode, &buff, dec))
-            return Invalid;
-    }
+    if (!locale.d()->validateChars(input, numMode, &buff, q->dec))
+        return QValidator::Invalid;
 
     if (buff.isEmpty())
-        return Intermediate;
+        return QValidator::Intermediate;
 
-    if (b >= 0 && buff.startsWith('-'))
-        return Invalid;
+    if (q->b >= 0 && buff.startsWith('-'))
+        return QValidator::Invalid;
 
-    if (t < 0 && buff.startsWith('+'))
-        return Invalid;
+    if (q->t < 0 && buff.startsWith('+'))
+        return QValidator::Invalid;
 
     bool ok, overflow;
     double i = QLocalePrivate::bytearrayToDouble(buff.constData(), &ok, &overflow);
     if (overflow)
-        return Invalid;
+        return QValidator::Invalid;
     if (!ok)
-        return Intermediate;
+        return QValidator::Intermediate;
 
-    if (i >= b && i <= t)
-        return Acceptable;
+    if (i >= q->b && i <= q->t)
+        return QValidator::Acceptable;
 
-    if (d->notation == StandardNotation) {
-        double max = qMax(qAbs(b), qAbs(t));
+    if (notation == QDoubleValidator::StandardNotation) {
+        double max = qMax(qAbs(q->b), qAbs(q->t));
         if (max < LLONG_MAX) {
             qlonglong n = pow10(numDigits(qlonglong(max))) - 1;
             if (qAbs(i) > n)
-                return Invalid;
+                return QValidator::Invalid;
         }
     }
 
-    return Intermediate;
+    return QValidator::Intermediate;
 }
 
 

tests/auto/qdoublevalidator/tst_qdoublevalidator.cpp
@@ -214,6 +214,10 @@ void tst_QDoubleValidator::validate_data()
     arabicNum += QChar(1643);
     arabicNum += QChar(1636);
     QTest::newRow("arabic") << "ar" << 0.0 << 20.0 << 2 << arabicNum << ACC << ACC;
+
+    QTest::newRow("data_QTBUG_14935-1") << "de" << 0.0 << 1.0 << 5 << QString("0.31") << ACC << ACC;
+    QTest::newRow("data_QTBUG_14935-2") << "de" << 0.0 << 1000000.0 << 5 << QString("3.123") << ACC << ACC;
+    QTest::newRow("data_QTBUG_14935-3") << "de" << 0.0 << 1000000.0 << 5 << QString("123,345.678") << ACC << ACC;
 }
 
 void tst_QDoubleValidator::validate()