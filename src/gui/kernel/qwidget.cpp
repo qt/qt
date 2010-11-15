qt/qt

commit 54865d47cfd859a0e84ba8e1bbff3b56c93d4e0d

Fix restore of maximized window geometry on Windows
Task-number: QTBUG-2064

src/gui/kernel/qwidget.cpp
@@ -7053,7 +7053,11 @@ bool QWidget::restoreGeometry(const QByteArray &geometry)
     if (maximized || fullScreen) {
         // set geomerty before setting the window state to make
         // sure the window is maximized to the right screen.
+        // Skip on windows: the window is restored into a broken
+        // half-maximized state.
+#ifndef Q_WS_WIN
         setGeometry(restoredNormalGeometry);
+#endif
         Qt::WindowStates ws = windowState();
         if (maximized)
             ws |= Qt::WindowMaximized;

commit abba37cd0eff959edf2d011842cbf2b36cce8467

Improve restoring of miximized window geomertry.
Task-number: QTBUG-2064

Follow-up commit to 54865d47cfd, which caused
a regression on multi-display systems on Windows.

src/gui/kernel/qwidget.cpp
@@ -7051,13 +7051,16 @@ bool QWidget::restoreGeometry(const QByteArray &geometry)
     restoredNormalGeometry.moveTop(qMax(restoredNormalGeometry.top(), availableGeometry.top() + frameHeight));
 
     if (maximized || fullScreen) {
-        // set geomerty before setting the window state to make
-        // sure the window is maximized to the right screen.
-        // Skip on windows: the window is restored into a broken
-        // half-maximized state.
-#ifndef Q_WS_WIN
-        setGeometry(restoredNormalGeometry);
-#endif
+        // Set the geomerty before setting the maximized/fullscreen
+        // window state to make sure the window is maximized to the
+        // correct screen. Clear the window state before setting the
+        // geometry to prevent a bug where the window is restored to
+        // an inconsitent state on Windows.
+        if (desktop->screenNumber(restoredNormalGeometry.center()) != desktop->screenNumber(this)) {
+            setWindowState(Qt::WindowNoState);
+            setGeometry(restoredNormalGeometry);
+        }
+
         Qt::WindowStates ws = windowState();
         if (maximized)
             ws |= Qt::WindowMaximized;