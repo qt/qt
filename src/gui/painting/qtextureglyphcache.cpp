qt/qt

commit 72f161739b270b01807f97cd853030440f0fd430

Fix possible corrupted text when gl glyph cache becomes full
When the OpenGL glyph cache filled up (the max texture size on the
hardware was exceeded) the characters would be drawn as black blocks
instead. As a work-around for this, the cache will now be cleared and
repopulated whenever this happens, meaning that once in a while (when
a lot of different glyphs have been drawn in a font) there will be a
performance hit. A more complete solution is described in QTBUG-13784,
but this requires so much refactoring that it was deemed too risky for
a patch release. This patch fixes the problem with a small penalty
and low risk.

Task-number: QT-3971
Reviewed-by: Samuel

src/gui/painting/qtextureglyphcache.cpp
@@ -65,7 +65,7 @@ static inline int qt_next_power_of_two(int v)
     return v;
 }
 
-void QTextureGlyphCache::populate(QFontEngine *fontEngine, int numGlyphs, const glyph_t *glyphs,
+bool QTextureGlyphCache::populate(QFontEngine *fontEngine, int numGlyphs, const glyph_t *glyphs,
                                   const QFixedPoint *)
 {
 #ifdef CACHE_DEBUG
@@ -119,7 +119,7 @@ void QTextureGlyphCache::populate(QFontEngine *fontEngine, int numGlyphs, const 
         rowHeight = qMax(rowHeight, glyph_height);
     }
     if (listItemCoordinates.isEmpty())
-        return;
+        return true;
 
     rowHeight += margin * 2 + paddingDoubled;
     if (isNull())
@@ -150,6 +150,13 @@ void QTextureGlyphCache::populate(QFontEngine *fontEngine, int numGlyphs, const 
             int new_height = m_h*2;
             while (new_height < m_cy + c.h)
                 new_height *= 2;
+
+	    if (new_height > maxTextureHeight()) {
+	        // We can't make a new texture of the required size, so 
+	        // bail out
+	        return false;
+	    }
+
             // if no room in the current texture - realloc a larger texture
             resizeTextureData(m_w, new_height);
             m_h = new_height;
@@ -165,7 +172,7 @@ void QTextureGlyphCache::populate(QFontEngine *fontEngine, int numGlyphs, const 
         ++iter;
     }
 
-
+    return true;
 }
 
 QImage QTextureGlyphCache::textureMapForGlyph(glyph_t g) const

src/gui/painting/qtextureglyphcache_p.h
@@ -96,7 +96,7 @@ public:
         int baseLineY;
     };
 
-    void populate(QFontEngine *fontEngine, int numGlyphs, const glyph_t *glyphs,
+    bool populate(QFontEngine *fontEngine, int numGlyphs, const glyph_t *glyphs,
                   const QFixedPoint *positions);
 
     virtual void createTextureData(int width, int height) = 0;

src/opengl/gl2paintengineex/qpaintengineex_opengl2.cpp
@@ -1499,8 +1499,13 @@ void QGL2PaintEngineExPrivate::drawCachedGlyphs(QFontEngineGlyphCache::Type glyp
     // cache so this text is performed before we test if the cache size has changed.
     if (recreateVertexArrays) {
         cache->setPaintEnginePrivate(this);
-        cache->populate(staticTextItem->fontEngine(), staticTextItem->numGlyphs,
-                        staticTextItem->glyphs, staticTextItem->glyphPositions);
+        if (!cache->populate(staticTextItem->fontEngine(), staticTextItem->numGlyphs,
+			     staticTextItem->glyphs, staticTextItem->glyphPositions)) {
+	    // No space in cache. We need to clear the cache and try again
+            cache->clear();
+            cache->populate(staticTextItem->fontEngine(), staticTextItem->numGlyphs,
+			    staticTextItem->glyphs, staticTextItem->glyphPositions);
+	}
     }
 
     if (cache->width() == 0 || cache->height() == 0)

src/opengl/gl2paintengineex/qtextureglyphcache_gl.cpp
@@ -78,7 +78,7 @@ void QGLTextureGlyphCache::setContext(QGLContext *context)
             SLOT(contextDestroyed(const QGLContext*)));
 }
 
-QGLTextureGlyphCache::~QGLTextureGlyphCache()
+void QGLTextureGlyphCache::clear() 
 {
     if (ctx) {
         QGLShareContextScope scope(ctx);
@@ -88,7 +88,24 @@ QGLTextureGlyphCache::~QGLTextureGlyphCache()
 
         if (m_width || m_height)
             glDeleteTextures(1, &m_texture);
+
+        m_fbo = 0;
+        m_texture = 0;
+        m_width = 0;
+        m_height = 0;
+        m_w = 0;
+        m_h = 0;
+        m_cx = 0;
+        m_cy = 0;
+        m_currentRowHeight = 0;
+        coords.clear();
     }
+  
+}
+
+QGLTextureGlyphCache::~QGLTextureGlyphCache()
+{
+    clear();
 }
 
 void QGLTextureGlyphCache::createTextureData(int width, int height)

src/opengl/gl2paintengineex/qtextureglyphcache_gl_p.h
@@ -124,6 +124,8 @@ public Q_SLOTS:
         }
     }
 
+    void clear();
+
 private:
     QGLContext *ctx;
 

commit 649719c29bc3b33ab6a5eb9577d53bd0ba897550

Fix possible missing glyphs in raster engine glyph cache
Two possible failures when using the glyph cache on raster engine and
populating the cache with very many glyphs:
1. Change 72f161739b270b01807f97cd853030440f0fd430 caused the maximum
   height of the glyph cache to be 32768, which was not sufficient for
   large fonts with very many characters (e.g. Chinese text)
2. Since we are using QPainter to draw into the glyph cache for RGB32
   glyphcaches, and QPainter does not support very high coordinates,
   we need to create a reference image that references a section of the
   glyph cache and paint into that instead.

Task-number: QT-3971
Reviewed-by: Samuel

src/gui/painting/qtextureglyphcache.cpp
@@ -151,11 +151,11 @@ bool QTextureGlyphCache::populate(QFontEngine *fontEngine, int numGlyphs, const 
             while (new_height < m_cy + c.h)
                 new_height *= 2;
 
-	    if (new_height > maxTextureHeight()) {
-	        // We can't make a new texture of the required size, so 
-	        // bail out
-	        return false;
-	    }
+            if (maxTextureHeight() > 0 && new_height > maxTextureHeight()) {
+                // We can't make a new texture of the required size, so
+                // bail out
+                return false;
+            }
 
             // if no room in the current texture - realloc a larger texture
             resizeTextureData(m_w, new_height);
@@ -266,11 +266,14 @@ void QImageTextureGlyphCache::fillTexture(const Coord &c, glyph_t g)
     }
 #endif
 
-    if (m_type == QFontEngineGlyphCache::Raster_RGBMask) {
-        QPainter p(&m_image);
+    if (m_type == QFontEngineGlyphCache::Raster_RGBMask) {        
+        QImage ref(m_image.bits() + (c.x * 4 + c.y * m_image.bytesPerLine()),
+                   qMax(mask.width(), c.w), qMax(mask.height(), c.h), m_image.bytesPerLine(),
+                   m_image.format());
+        QPainter p(&ref);
         p.setCompositionMode(QPainter::CompositionMode_Source);
-        p.fillRect(c.x, c.y, c.w, c.h, QColor(0,0,0,0)); // TODO optimize this
-        p.drawImage(c.x, c.y, mask);
+        p.fillRect(0, 0, c.w, c.h, QColor(0,0,0,0)); // TODO optimize this
+        p.drawImage(0, 0, mask);
         p.end();
     } else if (m_type == QFontEngineGlyphCache::Raster_Mono) {
         if (mask.depth() > 1) {

src/gui/painting/qtextureglyphcache_p.h
@@ -118,7 +118,7 @@ public:
 
     QImage textureMapForGlyph(glyph_t g) const;
     virtual int maxTextureWidth() const { return QT_DEFAULT_TEXTURE_GLYPH_CACHE_WIDTH; }
-    virtual int maxTextureHeight() const { return 32768; }
+    virtual int maxTextureHeight() const { return -1; }
 
 protected:
     QFontEngine *m_current_fontengine;