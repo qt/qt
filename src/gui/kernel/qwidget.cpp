dd8ae3d39b9181bebcba6ea60fb9edc2aea0a4e2
Morten Johan Sørvig <morten.sorvig@nokia.com>
2010-11-22 14:40:30 +0100

    Revert "Resolve two save/restore geometry issues on X11."
    This reverts commit 3b80b70d1464320aee2b38ddeb1ec0d257f6bd1c.

--- src/gui/kernel/qwidget.cpp
+++ src/gui/kernel/qwidget.cpp
@@ -7066,8 +7066,13 @@ bool QWidget::restoreGeometry(const QByteArray &geometry)
        setWindowState(ws);
        d_func()->topData()->normalGeometry = restoredNormalGeometry;
     } else {
+        QPoint offset;
+#ifdef Q_WS_X11
+        if (isFullScreen())
+            offset = d_func()->topData()->fullScreenOffset;
+#endif
         setWindowState(windowState() & ~(Qt::WindowMaximized | Qt::WindowFullScreen));
-        move(restoredFrameGeometry.topLeft());
+        move(restoredFrameGeometry.topLeft() + offset);
         resize(restoredNormalGeometry.size());
     }
     return true;
