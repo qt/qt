qt/qt

commit ed8f3b6c98f1b305f0d183bc70c5f810a9c45ef2

QThreadStorage: fix memory leak if thread storage are added while destroying
The destructor(q) function could use itself and create new QThreadStorage.
This might be the case for example, when using qDebug in a destructor.

Task-number: QTBUG-14579
Reveiwed-by: Joao
Reviewed-by: Brad

src/corelib/thread/qthreadstorage.cpp
@@ -178,11 +178,12 @@ void QThreadStorageData::finish(void **p)
         return; // nothing to do
 
     DEBUG_MSG("QThreadStorageData: Destroying storage for thread %p", QThread::currentThread());
-
-    for(int i = tls->size() - 1; i >= 0; i--) {
-        void *&value = (*tls)[i];
+    while (!tls->isEmpty()) {
+        void *&value = tls->last();
         void *q = value;
         value = 0;
+        int i = tls->size() - 1;
+        tls->resize(i);
 
         if (!q) {
             // data already deleted

tests/auto/qthreadstorage/tst_qthreadstorage.cpp
@@ -77,6 +77,7 @@ private slots:
     void adoptedThreads();
     void ensureCleanupOrder();
     void QTBUG13877_crashOnExit();
+    void QTBUG14579_leakInDestructor();
 };
 
 class Pointer
@@ -308,6 +309,77 @@ void tst_QThreadStorage::QTBUG13877_crashOnExit()
 #endif
     QVERIFY(process.waitForFinished());
     QVERIFY(process.exitStatus() != QProcess::CrashExit);
+}
+
+// S stands for thread Safe.
+class SPointer
+{
+public:
+    static QBasicAtomicInt count;
+    inline SPointer() { count.ref(); }
+    inline ~SPointer() { count.deref(); }
+};
+QBasicAtomicInt SPointer::count = Q_BASIC_ATOMIC_INITIALIZER(0);
+
+Q_GLOBAL_STATIC(QThreadStorage<SPointer *>, QTBUG14579_pointers1)
+Q_GLOBAL_STATIC(QThreadStorage<SPointer *>, QTBUG14579_pointers2)
+
+class QTBUG14579_class
+{
+public:
+    SPointer member;
+    inline ~QTBUG14579_class() {
+        QVERIFY(!QTBUG14579_pointers1()->hasLocalData());
+        QVERIFY(!QTBUG14579_pointers2()->hasLocalData());
+        QTBUG14579_pointers2()->setLocalData(new SPointer);
+        QTBUG14579_pointers1()->setLocalData(new SPointer);
+        QVERIFY(QTBUG14579_pointers1()->hasLocalData());
+        QVERIFY(QTBUG14579_pointers2()->hasLocalData());
+    }
+};
+
+
+void tst_QThreadStorage::QTBUG14579_leakInDestructor()
+{
+    class Thread : public QThread
+    {
+    public:
+        QThreadStorage<QTBUG14579_class *> &tls;
+
+        Thread(QThreadStorage<QTBUG14579_class *> &t) : tls(t) { }
+
+        void run()
+        {
+            QVERIFY(!tls.hasLocalData());
+            tls.setLocalData(new QTBUG14579_class);
+            QVERIFY(tls.hasLocalData());
+        }
+    };
+    int c = SPointer::count;
+
+    QThreadStorage<QTBUG14579_class *> tls;
+
+    QVERIFY(!QTBUG14579_pointers1()->hasLocalData());
+    QThreadStorage<int *> tls2; //add some more tls to make sure ids are not following each other too much
+    QThreadStorage<int *> tls3;
+    QVERIFY(!tls2.hasLocalData());
+    QVERIFY(!tls3.hasLocalData());
+    QVERIFY(!tls.hasLocalData());
+
+    Thread t1(tls);
+    Thread t2(tls);
+    Thread t3(tls);
+
+    t1.start();
+    t2.start();
+    t3.start();
+
+    QVERIFY(t1.wait());
+    QVERIFY(t2.wait());
+    QVERIFY(t3.wait());
+
+    //check all the constructed things have been destructed
+    QCOMPARE(int(SPointer::count), c);
 }
 
 QTEST_MAIN(tst_QThreadStorage)

commit 68e5673bb1ca080bbaf5cf7198fcf2deafa60772

QThreadStorage: fix crash if thread local destructor reset himself
Regression in ed8f3b6c98f1b305f0d183bc70c5f810a9c45ef2 (QTBUG-14579)

WebKit ThreadSpecific implementation does that.
See the comment in ThreadSpecific<T>::destroy(void*)

Reveiwed-by: Brad

src/corelib/thread/qthreadstorage.cpp
@@ -201,6 +201,11 @@ void QThreadStorageData::finish(void **p)
             continue;
         }
         destructor(q); //crash here might mean the thread exited after qthreadstorage was destroyed
+
+        if (tls->size() > i) {
+            //re reset the tls in case it has been recreated by its own destructor.
+            (*tls)[i] = 0;
+        }
     }
     tls->clear();
 }

tests/auto/qthreadstorage/tst_qthreadstorage.cpp
@@ -78,6 +78,7 @@ private slots:
     void ensureCleanupOrder();
     void QTBUG13877_crashOnExit();
     void QTBUG14579_leakInDestructor();
+    void QTBUG14579_resetInDestructor();
     void valueBased();
 };
 
@@ -383,6 +384,49 @@ void tst_QThreadStorage::QTBUG14579_leakInDestructor()
     //check all the constructed things have been destructed
     QCOMPARE(int(SPointer::count), c);
 }
+
+
+class QTBUG14579_reset;
+Q_GLOBAL_STATIC(QThreadStorage<QTBUG14579_reset *>, QTBUG14579_resetTls)
+
+class QTBUG14579_reset {
+public:
+    SPointer member;
+    ~QTBUG14579_reset() {
+        //Quite stupid, but WTF::ThreadSpecific<T>::destroy does it.
+        QTBUG14579_resetTls()->setLocalData(this);
+    }
+};
+
+
+void tst_QThreadStorage::QTBUG14579_resetInDestructor()
+{
+    class Thread : public QThread
+    {
+    public:
+        void run()
+        {
+            QVERIFY(!QTBUG14579_resetTls()->hasLocalData());
+            QTBUG14579_resetTls()->setLocalData(new QTBUG14579_reset);
+            QVERIFY(QTBUG14579_resetTls()->hasLocalData());
+        }
+    };
+    int c = SPointer::count;
+
+    Thread t1;
+    Thread t2;
+    Thread t3;
+    t1.start();
+    t2.start();
+    t3.start();
+    QVERIFY(t1.wait());
+    QVERIFY(t2.wait());
+    QVERIFY(t3.wait());
+
+    //check all the constructed things have been destructed
+    QCOMPARE(int(SPointer::count), c);
+}
+
 
 void tst_QThreadStorage::valueBased()
 {