qt/qt

commit 4d0c4b9f09b35d707d437611519d0024f6f87a8c

QLocalSocket/Win: handle ERROR_MORE_DATA after read operation
If we're connected to a name pipe which is in message mode, we have to
handle the following case: ReadFile() or GetOverlappedResult() return
FALSE and GetLastError() returns ERROR_MORE_DATA.

This just means, that the message didn't fit into the pipe's internal
buffer. We must not handle this as error.

Task-number: QTBUG-11490
Reviewed-by: ossi

src/network/socket/qlocalsocket_win.cpp
@@ -280,6 +280,12 @@ void QLocalSocketPrivate::startAsyncRead()
                 case ERROR_IO_PENDING:
                     // This is not an error. We're getting notified, when data arrives.
                     return;
+                case ERROR_MORE_DATA:
+                    // This is not an error. The synchronous read succeeded.
+                    // We're connected to a message mode pipe and the message
+                    // didn't fit into the pipe's system buffer.
+                    completeAsyncRead();
+                    break;
                 case ERROR_PIPE_NOT_CONNECTED:
                     {
                         // It may happen, that the other side closes the connection directly
@@ -309,9 +315,18 @@ bool QLocalSocketPrivate::completeAsyncRead()
 
     DWORD bytesRead;
     if (!GetOverlappedResult(handle, &overlapped, &bytesRead, TRUE)) {
-        if (GetLastError() != ERROR_PIPE_NOT_CONNECTED)
+        switch (GetLastError()) {
+        case ERROR_MORE_DATA:
+            // This is not an error. We're connected to a message mode
+            // pipe and the message didn't fit into the pipe's system
+            // buffer. We will read the remaining data in the next call.
+            break;
+        case ERROR_PIPE_NOT_CONNECTED:
             setErrorString(QLatin1String("QLocalSocketPrivate::completeAsyncRead"));
-        return false;
+            // fall through
+        default:
+            return false;
+        }
     }
 
     actualReadBufferSize += bytesRead;

commit 0bf8f70f85a24ab2864775bdd38f5ced43a7d4de

QLocalSocket/Win: do not emit error() when no error actually occurred
This fixes a regression in 4d0c4b9f09b35d707d437611519d0024f6f87a8c.
Previously the error string was set if the error is not
ERROR_PIPE_NOT_CONNECTED but the commit changed this behaviour to set
the error string if it is ERROR_PIPE_NOT_CONNECTED.

Task-number: QTBUG-13646
Merge-request: 941
Reviewed-by: Joerg Bornemann <joerg.bornemann@nokia.com>

src/network/socket/qlocalsocket_win.cpp
@@ -322,9 +322,9 @@ bool QLocalSocketPrivate::completeAsyncRead()
             // buffer. We will read the remaining data in the next call.
             break;
         case ERROR_PIPE_NOT_CONNECTED:
-            setErrorString(QLatin1String("QLocalSocketPrivate::completeAsyncRead"));
-            // fall through
+            return false;
         default:
+            setErrorString(QLatin1String("QLocalSocketPrivate::completeAsyncRead"));
             return false;
         }
     }