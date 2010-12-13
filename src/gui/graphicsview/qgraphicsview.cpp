0af1f5508e165f3b494ab236cb1cdf1b0edcffa5
Gabriel de Dietrich <gabriel.dietrich-de@nokia.com>
2010-12-13 11:29:22 +0100

    Fixes text jitter in QtQuick components when using the virtual keyboard
    QGraphicsView::mapFromScene() returns a QRect instead of a
    QRectF. Thus, we were prematurely rounding the micro focus rectangle
    coordinates in QGraphicsView::inputMethodQuery(). (The
    Qt::ImMicroFocus query returns a QRectF from QGraphicsScene, which is
    the result of querying QDeclarativeTextEdit and mapping it to the
    scene.)
    
    Auto-test included.
    
    Reviewed-by: leo
    Reviewed-by: bnilsen
    Task-number: QTCOMPONENTS-288
    Task-number: QTBUG-16063

--- src/gui/graphicsview/qgraphicsview.cpp
+++ src/gui/graphicsview/qgraphicsview.cpp
@@ -2494,7 +2494,7 @@ QVariant QGraphicsView::inputMethodQuery(Qt::InputMethodQuery query) const
 
     QVariant value = d->scene->inputMethodQuery(query);
     if (value.type() == QVariant::RectF)
-        value = mapFromScene(value.toRectF()).boundingRect();
+        value = d->mapRectFromScene(value.toRectF());
     else if (value.type() == QVariant::PointF)
         value = mapFromScene(value.toPointF());
     else if (value.type() == QVariant::Rect)
