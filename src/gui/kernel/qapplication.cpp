qt/qt

commit 8dd8db250d92521fda619bdcf3e1c859b37b2da0

Crash in QWidgetPrivate::init on QApplication::quit() using a modal dialog on Mac
The problem was the order of deletion in the destructor. To autotest
this I create an application, start a modal dialog and then quit the
application.
The fix was contributed by a customer.

Task-number: QTBUG-12673
Reviewed-by: Olivier Goffart

src/gui/kernel/qapplication.cpp
@@ -1055,6 +1055,18 @@ QApplication::~QApplication()
     QApplicationPrivate::is_app_closing = true;
     QApplicationPrivate::is_app_running = false;
 
+    // delete all widgets
+    if (QWidgetPrivate::allWidgets) {
+        QWidgetSet *mySet = QWidgetPrivate::allWidgets;
+        QWidgetPrivate::allWidgets = 0;
+        for (QWidgetSet::ConstIterator it = mySet->constBegin(); it != mySet->constEnd(); ++it) {
+            register QWidget *w = *it;
+            if (!w->parent())                        // window
+                w->destroy(true, true);
+        }
+        delete mySet;
+    }
+
     delete qt_desktopWidget;
     qt_desktopWidget = 0;
 
@@ -1074,18 +1086,6 @@ QApplication::~QApplication()
 
     delete QWidgetPrivate::mapper;
     QWidgetPrivate::mapper = 0;
-
-    // delete all widgets
-    if (QWidgetPrivate::allWidgets) {
-        QWidgetSet *mySet = QWidgetPrivate::allWidgets;
-        QWidgetPrivate::allWidgets = 0;
-        for (QWidgetSet::ConstIterator it = mySet->constBegin(); it != mySet->constEnd(); ++it) {
-            register QWidget *w = *it;
-            if (!w->parent())                        // window
-                w->destroy(true, true);
-        }
-        delete mySet;
-    }
 
     delete QApplicationPrivate::app_pal;
     QApplicationPrivate::app_pal = 0;

tests/auto/qapplication/modal/main.cpp
@@ -1,0 +1,11 @@
+#include <QtGui>
+
+#include <QApplication>
+#include "base.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    base *b = new base();
+    return app.exec();
+}

tests/auto/qapplication/modal/base.cpp
@@ -1,0 +1,21 @@
+#include "base.h"
+
+base::base(QWidget *parent) :
+    QWidget(parent)
+{
+    m_timer = new QTimer(this);
+    m_modalStarted = false;
+    m_timer->setSingleShot(false);
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(periodicTimer()));
+    m_timer->start(5000);
+}
+
+void base::periodicTimer()
+{
+    if(m_modalStarted)
+        exit(0);
+    m_modalDialog = new QDialog(this);
+    m_modalDialog->setModal(true);
+    m_modalDialog->show();
+    m_modalStarted = true;
+}

tests/auto/qapplication/modal/base.h
@@ -1,0 +1,23 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <QWidget>
+#include <QTimer>
+#include <QDialog>
+
+class base : public QWidget
+{
+Q_OBJECT
+    QTimer *m_timer;
+    bool m_modalStarted;
+    QDialog *m_modalDialog;
+public:
+    explicit base(QWidget *parent = 0);
+
+signals:
+
+public slots:
+    void periodicTimer();
+};
+
+#endif // BASE_H

tests/auto/qapplication/modal/modal.pro
@@ -1,0 +1,8 @@
+TEMPLATE = app
+TARGET = 
+DEPENDPATH += .
+INCLUDEPATH += .
+SOURCES += main.cpp \
+    base.cpp
+DESTDIR = ./
+HEADERS += base.h

tests/auto/qapplication/qapplication.pro
@@ -1,6 +1,7 @@
 TEMPLATE = subdirs
 SUBDIRS = test \
 	  desktopsettingsaware \
+          modal \
       wincmdline
 
 

tests/auto/qapplication/tst_qapplication.cpp
@@ -146,6 +146,7 @@ private slots:
 
     void symbianNeedForTraps();
     void symbianLeaveThroughMain();
+    void qtbug_12673();
 };
 
 class EventSpy : public QObject
@@ -2237,6 +2238,23 @@ void tst_QApplication::symbianLeaveThroughMain()
 
     QCOMPARE(numDestroyed, 2);
 #endif
+}
+
+void tst_QApplication::qtbug_12673()
+{
+#ifdef Q_OS_SYMBIAN
+    QSKIP("This might not make sense in Symbian, but since I do not know how to test it I'll just skip it for now.", SkipAll);
+#else
+    QProcess testProcess;
+    QStringList arguments;
+#ifdef Q_OS_MAC
+    testProcess.start("modal/modal.app", arguments);
+#else
+    testProcess.start("modal/modal", arguments);
+#endif
+    QVERIFY(testProcess.waitForFinished(20000));
+    QCOMPARE(testProcess.exitStatus(), QProcess::NormalExit);
+#endif //  Q_OS_SYMBIAN
 }
 
 //QTEST_APPLESS_MAIN(tst_QApplication)

commit 736b8205158dd7b98af862a029b273dff6fb716d

QApplication: fix crash exit whith native windows
Regression introduced in 8dd8db250d92521fda619bdcf3e1c859b37b2da0
Patch comes from the task

Task-number: QTBUG-15774

src/gui/kernel/qapplication.cpp
@@ -1101,6 +1101,9 @@ QApplication::~QApplication()
     QApplicationPrivate::is_app_closing = true;
     QApplicationPrivate::is_app_running = false;
 
+    delete QWidgetPrivate::mapper;
+    QWidgetPrivate::mapper = 0;
+
     // delete all widgets
     if (QWidgetPrivate::allWidgets) {
         QWidgetSet *mySet = QWidgetPrivate::allWidgets;
@@ -1129,9 +1132,6 @@ QApplication::~QApplication()
 #if defined(Q_WS_WIN)
     delete d->ignore_cursor; d->ignore_cursor = 0;
 #endif
-
-    delete QWidgetPrivate::mapper;
-    QWidgetPrivate::mapper = 0;
 
     delete QApplicationPrivate::app_pal;
     QApplicationPrivate::app_pal = 0;

tests/auto/qapplication/modal/main.cpp
@@ -47,6 +47,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QApplication::setAttribute(Qt::AA_NativeWindows); //QTBUG-15774
     base *b = new base();
     return app.exec();
 }