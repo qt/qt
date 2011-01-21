0e2a3a7db889ea2fe7148fd076babd926dc44f9f
Jedrzej Nowacki <jedrzej.nowacki@nokia.com>
2011-01-21 09:09:28 +0200

    Build fix (missing include).
    QScriptContextInfo implementation depends indirectly on QScriptString
    and QScriptContext declarations.

--- src/script/qscriptcontextinfo.cpp
+++ src/script/qscriptcontextinfo.cpp
@@ -31,6 +31,12 @@
 
 #include <qdebug.h>
 #include "qscriptisolate_p.h"
+#include "qscriptengine_p.h"
+#include "qscriptengine_impl_p.h"
+#include "qscriptcontext_p.h"
+#include "qscriptcontext_impl_p.h"
+#include "qscriptstring_p.h"
+#include "qscriptstring_impl_p.h"
 
 QT_BEGIN_NAMESPACE
 
