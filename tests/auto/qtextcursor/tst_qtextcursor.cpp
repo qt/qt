qt/qt

commit 0ba1b4d05629643131c0c7461b2a483d8a56ed39

Fix exponential behavior of QTextCursor::removeSelectedText
This is an improved version of 62db6c18c7f1f60819783ed5e1340e9fc09e072e

src/gui/text/qtextdocument_p.cpp
@@ -205,6 +205,7 @@ QTextDocumentPrivate::QTextDocumentPrivate()
 
     undoEnabled = true;
     inContentsChange = false;
+    blockCursorAdjustment = false;
 
     defaultTextOption.setTabStop(80); // same as in qtextengine.cpp
     defaultTextOption.setWrapMode(QTextOption::WrapAtWordBoundaryOrAnywhere);
@@ -669,7 +670,14 @@ void QTextDocumentPrivate::remove(int pos, int length, QTextUndoCommand::Operati
 {
     if (length == 0)
         return;
+    blockCursorAdjustment = true;
     move(pos, -1, length, op);
+    blockCursorAdjustment = false;
+    foreach (QTextCursorPrivate *curs, cursors) {
+        if (curs->adjustPosition(pos, -length, op) == QTextCursorPrivate::CursorMoved) {
+            curs->changed = true;
+        }
+    }
 }
 
 void QTextDocumentPrivate::setCharFormat(int pos, int length, const QTextCharFormat &newFormat, FormatChangeMode mode)
@@ -1268,9 +1276,13 @@ void QTextDocumentPrivate::adjustDocumentChangesAndCursors(int from, int addedOr
     if (!editBlock)
         ++revision;
 
-    foreach (QTextCursorPrivate *curs, cursors) {
-        if (curs->adjustPosition(from, addedOrRemoved, op) == QTextCursorPrivate::CursorMoved) {
-            curs->changed = true;
+    if (blockCursorAdjustment)  {
+        ; // postpone, will be called again from QTextDocumentPrivate::remove()
+    } else {
+        foreach (QTextCursorPrivate *curs, cursors) {
+            if (curs->adjustPosition(from, addedOrRemoved, op) == QTextCursorPrivate::CursorMoved) {
+                curs->changed = true;
+            }
         }
     }
 

src/gui/text/qtextdocument_p.h
@@ -345,6 +345,7 @@ public:
     int maximumBlockCount;
     uint needsEnsureMaximumBlockCount : 1;
     uint inContentsChange : 1;
+    uint blockCursorAdjustment : 1;
     QSizeF pageSize;
     QString title;
     QString url;

commit 388c57c0a1f53cd4a98aa12841eac603332dfe54

Fix cursor position adjustment when removing strings
Commit 0ba1b4d0 introduced a regression to QTextDocument: it postponed
cursor position adjustment until the move operation is done, but
contentsChanged will be triggered by finishEdit() in this move
operation, thus cursor positions in this signal handler will be in
inconsistent states (normally we should first update cursor position
then trigger contentsChanged). In this case we should also postpone
finishEdit() handling after cursor positions have been adjusted, then
the states expose to applications will be consistent.

Task-number: QTBUG-15857
Reviewed-by: Eskil

src/gui/text/qtextdocument_p.cpp
@@ -663,7 +663,8 @@ void QTextDocumentPrivate::move(int pos, int to, int length, QTextUndoCommand::O
 
     Q_ASSERT(blocks.length() == fragments.length());
 
-    finishEdit();
+    if (!blockCursorAdjustment)
+        finishEdit();
 }
 
 void QTextDocumentPrivate::remove(int pos, int length, QTextUndoCommand::Operation op)
@@ -678,6 +679,7 @@ void QTextDocumentPrivate::remove(int pos, int length, QTextUndoCommand::Operati
             curs->changed = true;
         }
     }
+    finishEdit();
 }
 
 void QTextDocumentPrivate::setCharFormat(int pos, int length, const QTextCharFormat &newFormat, FormatChangeMode mode)

tests/auto/qtextcursor/tst_qtextcursor.cpp
@@ -134,6 +134,7 @@ private slots:
     void endOfLine();
 
     void editBlocksDuringRemove();
+    void selectAllDuringRemove();
 
     void update_data();
     void update();
@@ -1388,6 +1389,17 @@ public slots:
         ++recordingCount;
     }
 
+    void selectAllContents()
+    {
+        // Only test the first time
+        if (!recordingCount) {
+            recordingCount++;
+            cursor->select(QTextCursor::Document);
+            lastRecordedPosition = cursor->position();
+            lastRecordedAnchor = cursor->anchor();
+        }
+    }
+
 private:
     QTextCursor *cursor;
 };
@@ -1409,6 +1421,22 @@ void tst_QTextCursor::editBlocksDuringRemove()
     QCOMPARE(listener.lastRecordedAnchor, 0);
 
     QVERIFY(doc->toPlainText().isEmpty());
+}
+
+void tst_QTextCursor::selectAllDuringRemove()
+{
+    CursorListener listener(&cursor);
+
+    cursor.insertText("Hello World");
+    cursor.movePosition(QTextCursor::End);
+
+    connect(doc, SIGNAL(contentsChanged()), &listener, SLOT(selectAllContents()));
+    listener.recordingCount = 0;
+    QTextCursor localCursor = cursor;
+    localCursor.deletePreviousChar();
+
+    QCOMPARE(listener.lastRecordedPosition, 10);
+    QCOMPARE(listener.lastRecordedAnchor, 0);
 }
 
 void tst_QTextCursor::update_data()