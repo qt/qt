281ff52c266e54030ca48326e79eab2d452c2e83
Bea Lam <bea.lam@nokia.com>
2011-01-27 18:22:15 +1000

    Test for passing functions to createObject() for property bindings

--- tests/auto/declarative/qdeclarativecomponent/tst_qdeclarativecomponent.cpp
+++ tests/auto/declarative/qdeclarativecomponent/tst_qdeclarativecomponent.cpp
@@ -62,7 +62,7 @@ private slots:
     void null();
     void loadEmptyUrl();
     void qmlCreateObject();
-    void qmlCreatObjectWithScript();
+    void qmlCreateObjectWithProperties();
 
 private:
     QDeclarativeEngine engine;
@@ -121,10 +121,11 @@ void tst_qdeclarativecomponent::qmlCreateObject()
     QCOMPARE(testObject3->metaObject()->className(), "QDeclarativeGraphicsWidget");
 }
 
-void tst_qdeclarativecomponent::qmlCreatObjectWithScript()
+void tst_qdeclarativecomponent::qmlCreateObjectWithProperties()
 {
     QDeclarativeEngine engine;
     QDeclarativeComponent component(&engine, QUrl::fromLocalFile(SRCDIR "/data/createObjectWithScript.qml"));
+    QVERIFY2(component.errorString().isEmpty(), component.errorString().toUtf8());
     QObject *object = component.create();
     QVERIFY(object != 0);
 
@@ -134,6 +135,7 @@ void tst_qdeclarativecomponent::qmlCreatObjectWithScript()
     QCOMPARE(testObject1->property("x").value<int>(), 17);
     QCOMPARE(testObject1->property("y").value<int>(), 17);
     QCOMPARE(testObject1->property("color").value<QColor>(), QColor(255,255,255));
+    delete testObject1;
 
     QObject *testObject2 = object->property("declarativeitem").value<QObject*>();
     QVERIFY(testObject2);
@@ -144,6 +146,23 @@ void tst_qdeclarativecomponent::qmlCreatObjectWithScript()
     QCOMPARE(testObject2->property("testBool").value<bool>(), true);
     QCOMPARE(testObject2->property("testInt").value<int>(), 17);
     QCOMPARE(testObject2->property("testObject").value<QObject*>(), object);
+    delete testObject2;
+
+    QObject *testBindingObj = object->property("bindingTestObject").value<QObject*>();
+    QVERIFY(testBindingObj);
+    QCOMPARE(testBindingObj->parent(), object);
+    QCOMPARE(testBindingObj->property("testValue").value<int>(), 300);
+    object->setProperty("width", 150);
+    QCOMPARE(testBindingObj->property("testValue").value<int>(), 150 * 3);
+    delete testBindingObj;
+
+    QObject *testBindingThisObj = object->property("bindingThisTestObject").value<QObject*>();
+    QVERIFY(testBindingThisObj);
+    QCOMPARE(testBindingThisObj->parent(), object);
+    QCOMPARE(testBindingThisObj->property("testValue").value<int>(), 900);
+    testBindingThisObj->setProperty("width", 200);
+    QCOMPARE(testBindingThisObj->property("testValue").value<int>(), 200 * 3);
+    delete testBindingThisObj;
 }
 
 QTEST_MAIN(tst_qdeclarativecomponent)
