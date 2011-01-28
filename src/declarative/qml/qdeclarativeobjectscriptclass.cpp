qt/qt

commit 43b8305367156c1ceb09eb4a056bdae3f325b5eb

Allow property bindings to be easily created from JavaScript
Properties can now be assigned a function that returns the binding
value.

Task-number: QTBUG-14964
Reviewed-by: Aaron Kennedy

src/declarative/qml/qdeclarativeobjectscriptclass.cpp
@@ -359,8 +359,20 @@ void QDeclarativeObjectScriptClass::setProperty(QObject *obj,
         }
     }
 
+    QDeclarativeBinding *newBinding = 0;
+    if (value.isFunction() && !value.isRegExp()) {
+        QScriptContextInfo ctxtInfo(context);
+        QDeclarativePropertyCache::ValueTypeData valueTypeData;
+
+        newBinding = new QDeclarativeBinding(value, obj, evalContext);
+        newBinding->setSourceLocation(ctxtInfo.fileName(), ctxtInfo.functionStartLineNumber());
+        newBinding->setTarget(QDeclarativePropertyPrivate::restore(*lastData, valueTypeData, obj, evalContext));
+        if (newBinding->expression().contains("this"))
+            newBinding->setEvaluateFlags(newBinding->evaluateFlags() | QDeclarativeBinding::RequiresThisObject);
+    }
+
     QDeclarativeAbstractBinding *delBinding =
-        QDeclarativePropertyPrivate::setBinding(obj, lastData->coreIndex, -1, 0);
+        QDeclarativePropertyPrivate::setBinding(obj, lastData->coreIndex, -1, newBinding);
     if (delBinding)
         delBinding->destroy();
 
@@ -379,10 +391,7 @@ void QDeclarativeObjectScriptClass::setProperty(QObject *obj,
         QString error = QLatin1String("Cannot assign [undefined] to ") +
                         QLatin1String(QMetaType::typeName(lastData->propType));
         context->throwError(error);
-    } else if (!value.isRegExp() && value.isFunction()) {
-        QString error = QLatin1String("Cannot assign a function to a property.");
-        context->throwError(error);
-    } else {
+    } else if (!value.isFunction()) {
         QVariant v;
         if (lastData->flags & QDeclarativePropertyCache::Data::IsQList)
             v = enginePriv->scriptValueToVariant(value, qMetaTypeId<QList<QObject *> >());

doc/src/declarative/javascriptblocks.qdoc
@@ -200,37 +200,12 @@ Likewise, the \l {Component::onDestruction} attached property is triggered on
 component destruction.
 
 
-\section1 Property Assignment vs Property Binding
+\section1 JavaScript and Property Binding
 
-When working with both QML and JavaScript, it is important to differentiate between
-QML \l {Property Binding} and JavaScript value assignment. In QML, a property 
-binding is created using the \e {property: value} syntax:
+Property bindings can be created in JavaScript by assigning the property with a \c function
+that returns the required value.
 
-\code
-Rectangle {
-    width: otherItem.width
-}
-\endcode
-
-The \c width of the above \l Rectangle is updated whenever \c otherItem.width changes. On the other
-hand, take the following JavaScript code snippet, that runs when the \l Rectangle is created:
-
-\code
-Rectangle {
-
-    Component.onCompleted: {
-        width = otherItem.width;
-    }
-}
-\endcode
-
-The \c width of this \l Rectangle is \e assigned the value of \c otherItem.width using the
-\e {property = value} syntax in JavaScript. Unlike the QML \e {property: value} syntax, this
-does not invoke QML property binding; the \c rectangle.width property is set to the value
-of \c otherItem.width at the time of the assignment and will not be updated if that value
-changes.
-
-See \l {Property Binding} for more information.
+See \l {Binding Properties from JavaScript} for details.
 
 
 \section1 Receiving QML Signals in JavaScript
@@ -315,10 +290,13 @@ This restriction exists as the QML environment is not yet fully established.
 To run code after the environment setup has completed, refer to
 \l {Running JavaScript at Startup}.
 
-\o The value of \c this is currently undefined in QML
+\o The value of \c this is currently undefined in QML in the majority of contexts
 
-The value of \c this is undefined in QML.  To refer to any element, provide
-an \c id.  For example:
+The \c this keyword is supported when \l {Binding Properties from JavaScript}
+{binding properties from JavaScript}. In all other situations, the value of
+\c this is undefined in QML.
+
+To refer to any element, provide an \c id.  For example:
 
 \qml
 Item {

doc/src/declarative/propertybinding.qdoc
@@ -95,44 +95,109 @@ Rectangle {
 \endcode
 
 
-\section1 Effects of Property Assignment in JavaScript
+\section1 Binding Properties from JavaScript
 
-Assigning a property value from JavaScript does \e not create a property binding.
-For example:
+When working with both QML and JavaScript, it is important to differentiate between
+\l {Property Binding} syntax in QML and simple \e {property assignment} in JavaScript. Take
+the example below, which uses property binding to ensure the item's \c height is always twice
+its \c width:
+
+\qml
+Item {
+    width: 100
+    height: width * 2
+}
+\endqml
+
+On the other hand, take the following JavaScript code snippet, which \e assigns, rather
+than \e binds, the value of the \c height property:
 
 \code
-Rectangle {
+Item {
+    width: 100
 
     Component.onCompleted: {
-        width = otherItem.width;
+        height = width * 2  // if width changes later, height is not updated!
     }
 }
 \endcode
 
-Instead of creating a property binding, this simply sets the \c width of the \l Rectangle
-to the value of \c other.width at the time the JavaScript code is invoked.  See 
-\l {Property Assignment vs Property Binding} for more details.
+Instead of creating a property binding, this simply sets the \c height property to the correct
+value \e {at the time that} the JavaScript code is invoked. Unlike the first example, the
+\c height will never change if \c width changes.
 
-Also note that assigning a value to a property that is currently bound will remove the binding.
-A property can only have one value at a time, and if any code explicitly sets
-this value, the binding is removed.  The \l Rectangle in the example below will have
-a width of 13, regardless of the \c otherItem's width.
+The \e {property : value} syntax for property binding is QML-specific and cannot be used in
+JavaScript. Instead, to bind a property from JavaScript, assign a \e function to the property
+that returns the required value. The following code correctly sets the property binding
+created in the first example, but creates the binding in JavaScript rather than QML:
 
-\code
-Rectangle {
-    width: otherItem.width
+\qml
+Item {
+    width: 100
 
     Component.onCompleted: {
-        width = 13;
+        height = (function() { return width * 2 })
+    }
+}
+\endqml
+
+
+\section2 Using \c this to create a binding
+
+When creating a property binding from JavaScript, QML allows the use of the \c this keyword to
+refer to the object to which the property binding will be assigned. This allows one to
+explicitly refer to a property within an object when there may be ambiguity about the exact
+property that should be used for the binding.
+
+For example, the \c Component.onCompleted handler below is defined within the scope of the
+\l Item, and references to \c width within this scope would refer to the \l Item's width, rather
+than that of the \l Rectangle. To bind the \l Rectangle's \c height to its own \c width, the
+function needs to explicitly refer to \c this.width rather than just \c width. Otherwise, the
+height of the \l Rectangle would be bound to the width of the \l Item and not the \l Rectangle.
+
+\qml
+Item {
+    width: 500
+    height: 500
+
+    Rectangle {
+        id: rect
+        width: 100
+        color: "yellow"
+    }
+
+    Component.onCompleted: {
+        rect.height = (function() { return this.width * 2 })
+    }
+}
+\endqml
+
+(In this case, the function could also have referred to \c rect.width rather than \c this.width.)
+
+Note that the value of \c this is not defined outside of its use in property binding.
+See \l {QML JavaScript Restrictions} for details.
+
+
+\section2 Effects of property assignment
+
+Note that assigning a value to a property that is currently bound will remove the binding.
+A property can only have one value at a time, and if any code explicitly sets this value, the
+binding is removed. In the following example, although \c width has been bound to \c height,
+the binding is removed by the JavaScript code that assigns \c width to 50:
+
+\code
+Item {
+    width: height * 2
+    height: 100
+
+    Component.onCompleted: {
+        width = 50;
     }
 }
 \endcode
 
-There is no way to create a property binding directly from imperative JavaScript code,
-although it is possible to set up a \l Binding object (shown below).
 
-
-\section1 Binding Element
+\section1 The Binding Element
 
 The implicit binding syntax shown previously is easy to use and works perfectly for most uses
 of bindings.  In some advanced cases, it is necessary to create bindings explicitly using the 

src/declarative/qml/qdeclarativebinding.cpp
@@ -274,6 +274,13 @@ QDeclarativeBinding::QDeclarativeBinding(const QString &str, QObject *obj, QDecl
     setNotifyOnValueChanged(true);
 }
 
+QDeclarativeBinding::QDeclarativeBinding(const QScriptValue &func, QObject *obj, QDeclarativeContextData *ctxt, QObject *parent)
+: QDeclarativeExpression(ctxt, obj, func, *new QDeclarativeBindingPrivate)
+{
+    setParent(parent);
+    setNotifyOnValueChanged(true);
+}
+
 QDeclarativeBinding::~QDeclarativeBinding()
 {
 }
@@ -291,6 +298,19 @@ QDeclarativeProperty QDeclarativeBinding::property() const
    Q_D(const QDeclarativeBinding);
    return d->property; 
 }
+
+void QDeclarativeBinding::setEvaluateFlags(EvaluateFlags flags)
+{
+    Q_D(QDeclarativeBinding);
+    d->setEvaluateFlags(QDeclarativeQtScriptExpression::EvaluateFlags(static_cast<int>(flags)));
+}
+
+QDeclarativeBinding::EvaluateFlags QDeclarativeBinding::evaluateFlags() const
+{
+    Q_D(const QDeclarativeBinding);
+    return QDeclarativeBinding::EvaluateFlags(static_cast<int>(d->evaluateFlags()));
+}
+
 
 class QDeclarativeBindingProfiler {
 public:

src/declarative/qml/qdeclarativebinding_p.h
@@ -147,13 +147,20 @@ class Q_DECLARATIVE_PRIVATE_EXPORT QDeclarativeBinding : public QDeclarativeExpr
 {
 Q_OBJECT
 public:
+    enum EvaluateFlag { RequiresThisObject = 0x01 };
+    Q_DECLARE_FLAGS(EvaluateFlags, EvaluateFlag)
+
     QDeclarativeBinding(const QString &, QObject *, QDeclarativeContext *, QObject *parent=0);
     QDeclarativeBinding(const QString &, QObject *, QDeclarativeContextData *, QObject *parent=0);
     QDeclarativeBinding(void *, QDeclarativeRefCount *, QObject *, QDeclarativeContextData *, 
                         const QString &, int, QObject *parent);
+    QDeclarativeBinding(const QScriptValue &, QObject *, QDeclarativeContextData *, QObject *parent=0);
 
     void setTarget(const QDeclarativeProperty &);
     QDeclarativeProperty property() const;
+
+    void setEvaluateFlags(EvaluateFlags flags);
+    EvaluateFlags evaluateFlags() const;
 
     bool enabled() const;
 
@@ -176,6 +183,8 @@ protected:
 private:
     Q_DECLARE_PRIVATE(QDeclarativeBinding)
 };
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(QDeclarativeBinding::EvaluateFlags)
 
 QT_END_NAMESPACE
 

src/declarative/qml/qdeclarativeexpression.cpp
@@ -103,6 +103,19 @@ void QDeclarativeExpressionPrivate::init(QDeclarativeContextData *ctxt, const QS
     expressionFunctionValid = false;
 }
 
+void QDeclarativeExpressionPrivate::init(QDeclarativeContextData *ctxt, const QScriptValue &func,
+                                         QObject *me)
+{
+    expression = func.toString();
+
+    QDeclarativeAbstractExpression::setContext(ctxt);
+    scopeObject = me;
+
+    expressionFunction = func;
+    expressionFunctionMode = ExplicitContext;
+    expressionFunctionValid = true;
+}
+
 void QDeclarativeExpressionPrivate::init(QDeclarativeContextData *ctxt, void *expr, 
                                          QDeclarativeRefCount *rc, 
                                          QObject *me, const QString &srcUrl, int lineNumber)
@@ -304,6 +317,19 @@ QDeclarativeExpression::QDeclarativeExpression(QDeclarativeContextData *ctxt, QO
     d->setNotifyObject(this, QDeclarativeExpression_notifyIdx);
 }
 
+/*!  \internal */
+QDeclarativeExpression::QDeclarativeExpression(QDeclarativeContextData *ctxt, QObject *scope, const QScriptValue &func,
+                       QDeclarativeExpressionPrivate &dd)
+: QObject(dd, 0)
+{
+    Q_D(QDeclarativeExpression);
+    d->init(ctxt, func, scope);
+
+    if (QDeclarativeExpression_notifyIdx == -1)
+        QDeclarativeExpression_notifyIdx = QDeclarativeExpression::staticMetaObject.indexOfMethod("_q_notify()");
+    d->setNotifyObject(this, QDeclarativeExpression_notifyIdx);
+}
+
 /*!
     Destroy the QDeclarativeExpression instance.
 */
@@ -412,6 +438,16 @@ void QDeclarativeQtScriptExpression::setNotifyObject(QObject *object, int notify
     }
 }
 
+void QDeclarativeQtScriptExpression::setEvaluateFlags(EvaluateFlags flags)
+{
+    evalFlags = flags;
+}
+
+QDeclarativeQtScriptExpression::EvaluateFlags QDeclarativeQtScriptExpression::evaluateFlags() const
+{
+    return evalFlags;
+}
+
 QScriptValue QDeclarativeQtScriptExpression::scriptValue(QObject *secondaryScope, bool *isUndefined)
 {
     Q_ASSERT(context() && context()->engine);
@@ -476,7 +512,10 @@ QScriptValue QDeclarativeQtScriptExpression::eval(QObject *secondaryScope, bool 
         oldOverride = ep->contextClass->setOverrideObject(expressionContext, secondaryScope);
     }
 
-    QScriptValue svalue = expressionFunction.call(); // This could cause this to be deleted
+    QScriptValue thisObject;
+    if (evalFlags & RequiresThisObject)
+        thisObject = ep->objectClass->newQObject(scopeObject);
+    QScriptValue svalue = expressionFunction.call(thisObject); // This could cause this c++ object to be deleted
 
     if (isShared) {
         ep->sharedContext = oldSharedContext;

src/declarative/qml/qdeclarativeexpression.h
@@ -59,6 +59,7 @@ class QDeclarativeEngine;
 class QDeclarativeContext;
 class QDeclarativeExpressionPrivate;
 class QDeclarativeContextData;
+class QScriptValue;
 class Q_DECLARATIVE_EXPORT QDeclarativeExpression : public QObject
 {
     Q_OBJECT
@@ -93,6 +94,8 @@ Q_SIGNALS:
 
 protected:
     QDeclarativeExpression(QDeclarativeContextData *, QObject *, const QString &,
+                           QDeclarativeExpressionPrivate &dd);
+    QDeclarativeExpression(QDeclarativeContextData *, QObject *, const QScriptValue &,
                            QDeclarativeExpressionPrivate &dd);
     QDeclarativeExpression(QDeclarativeContextData *, void *, QDeclarativeRefCount *rc, 
                            QObject *me, const QString &, int, QDeclarativeExpressionPrivate &dd);

src/declarative/qml/qdeclarativeexpression_p.h
@@ -113,6 +113,9 @@ class QDeclarativeQtScriptExpression : public QDeclarativeAbstractExpression,
 public:
     enum Mode { SharedContext, ExplicitContext };
 
+    enum EvaluateFlag { RequiresThisObject = 0x01 };
+    Q_DECLARE_FLAGS(EvaluateFlags, EvaluateFlag)
+
     QDeclarativeQtScriptExpression();
     virtual ~QDeclarativeQtScriptExpression();
 
@@ -130,6 +133,9 @@ public:
     void setNotifyOnValueChange(bool);
     void resetNotifyOnChange();
     void setNotifyObject(QObject *, int );
+
+    void setEvaluateFlags(EvaluateFlags flags);
+    EvaluateFlags evaluateFlags() const;
 
     QScriptValue scriptValue(QObject *secondaryScope, bool *isUndefined);
 
@@ -157,7 +163,12 @@ private:
     QObject *guardObject;
     int guardObjectNotifyIndex;
     bool *deleted;
+
+    EvaluateFlags evalFlags;
 };
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(QDeclarativeQtScriptExpression::EvaluateFlags)
+
 
 class QDeclarativeExpression;
 class QString;
@@ -169,6 +180,7 @@ public:
     ~QDeclarativeExpressionPrivate();
 
     void init(QDeclarativeContextData *, const QString &, QObject *);
+    void init(QDeclarativeContextData *, const QScriptValue &, QObject *);
     void init(QDeclarativeContextData *, void *, QDeclarativeRefCount *, QObject *, const QString &, int);
 
     QVariant value(QObject *secondaryScope = 0, bool *isUndefined = 0);

commit de223987715ad747496099165121aabdc38c8b0a

Fix test breakage for qdeclarativeworkerscript
Broken by 43b8305367156c1ceb09eb4a056bdae3f325b5eb.

src/declarative/qml/qdeclarativeobjectscriptclass.cpp
@@ -392,7 +392,9 @@ void QDeclarativeObjectScriptClass::setProperty(QObject *obj,
         QString error = QLatin1String("Cannot assign [undefined] to ") +
                         QLatin1String(QMetaType::typeName(lastData->propType));
         context->throwError(error);
-    } else if (!value.isFunction()) {
+    } else if (value.isFunction() && !value.isRegExp()) {
+        // this is handled by the binding creation above
+    } else {
         QVariant v;
         if (lastData->flags & QDeclarativePropertyCache::Data::IsQList)
             v = enginePriv->scriptValueToVariant(value, qMetaTypeId<QList<QObject *> >());