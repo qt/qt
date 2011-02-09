e533f58be48ea28695aed8fc1f07d9a04e71d33c
Olivier Goffart <olivier.goffart@nokia.com>
2011-02-09 10:31:31 +0100

    QScriptEngine::setGlobalObject set the prototype
    setGlobalObject will actually set the prototype of the global object
    
    The reason we do that is that if we set an interceptor, all the properteis
    will be intercepted.
    
    But we have to work around the fact that for v8, the real global
    object is the prototype of the global object.  So we use the prototype
    of the global object everywhere.

--- tests/auto/qscriptengine/tst_qscriptengine.cpp
+++ tests/auto/qscriptengine/tst_qscriptengine.cpp
@@ -1517,10 +1517,6 @@ void tst_QScriptEngine::globalObjectGetterSetterProperty()
 
 void tst_QScriptEngine::customGlobalObjectWithPrototype()
 {
-    QSKIP("Does not work on v8", SkipAll);
-    //hasOwnProperty will fail because setGlobalObject install interceptor on the global object.
-    //And then the test crash because we change the prototype of a global object.
-    //http://code.google.com/p/v8/issues/detail?id=1078
     for (int x = 0; x < 2; ++x) {
         QScriptEngine engine;
         QScriptValue wrap = engine.newObject();
@@ -1550,11 +1546,13 @@ void tst_QScriptEngine::customGlobalObjectWithPrototype()
         {
             QScriptValue ret = engine.evaluate("hasOwnProperty('print')");
             QVERIFY(ret.isBool());
+            if (x) QEXPECT_FAIL("", "Why?", Continue);
             QVERIFY(!ret.toBool());
         }
         {
             QScriptValue ret = engine.evaluate("this.hasOwnProperty('print')");
             QVERIFY(ret.isBool());
+            if (x) QEXPECT_FAIL("", "Why?", Continue);
             QVERIFY(!ret.toBool());
         }
 
@@ -1595,7 +1593,7 @@ void tst_QScriptEngine::customGlobalObjectWithPrototype()
         {
             QScriptValue ret = engine.evaluate("anotherProtoProperty");
             QVERIFY(ret.isError());
-            QCOMPARE(ret.toString(), QString::fromLatin1("ReferenceError: anotherProtoProperty is not defined"));
+            QVERIFY(ret.toString().startsWith("ReferenceError: "));
         }
         {
             QScriptValue ret = engine.evaluate("print");
@@ -1608,10 +1606,6 @@ void tst_QScriptEngine::customGlobalObjectWithPrototype()
 
 void tst_QScriptEngine::globalObjectWithCustomPrototype()
 {
-    QSKIP("Does not work on v8", SkipAll);
-    //test crash because we change the prototype of a global object.
-    //http://code.google.com/p/v8/issues/detail?id=1078
-
     QScriptEngine engine;
     QScriptValue proto = engine.newObject();
     proto.setProperty("protoProperty", 123);
@@ -1643,7 +1637,6 @@ void tst_QScriptEngine::globalObjectWithCustomPrototype()
     {
         QScriptValue ret = engine.evaluate("this.__proto__ = { 'a': 123 }; a");
         QVERIFY(ret.isNumber());
-        QEXPECT_FAIL("", "QTBUG-9737", Continue);
         QVERIFY(ret.strictlyEquals(global.property("a")));
     }
 }
