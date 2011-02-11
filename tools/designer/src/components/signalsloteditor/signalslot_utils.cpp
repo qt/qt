qt/qt

commit dd7873cd7e02e42b384709f33a34f69bc06fb25d

Designer: Fix a crash in the Signal-Slot-Editor.
Object search found deleted widgets by name when
searching on the form window. Search main container
instead, do not assert.

Reviewed-by: Jarek Kobus <jaroslaw.kobus@nokia.com>
Task-number: QTBUG-17179

tools/designer/src/components/signalsloteditor/signalslot_utils.cpp
@@ -72,7 +72,6 @@ static void memberList(QDesignerFormEditorInterface *core,
 {
     if (!object)
         return;
-
     // 1) member sheet
     const QDesignerMemberSheetExtension *members = qt_extension<QDesignerMemberSheetExtension*>(core->extensionManager(), object);
     Q_ASSERT(members != 0);
@@ -118,15 +117,15 @@ static void memberList(QDesignerFormEditorInterface *core,
     if (!metaDataBase)
         return;
 
-    const qdesigner_internal::MetaDataBaseItem *mdbItem = metaDataBase->metaDataBaseItem(object);
-    Q_ASSERT(mdbItem);
-    const QStringList mdbFakeMethods =  member_type == qdesigner_internal::SlotMember ? mdbItem->fakeSlots() : mdbItem->fakeSignals();
-    if (!mdbFakeMethods.empty())
-        foreach (const QString &fakeMethod, mdbFakeMethods)
-            if (predicate(fakeMethod)) {
-                *it = ClassNameSignaturePair(className, fakeMethod);
-                ++it;
-            }
+    if (const qdesigner_internal::MetaDataBaseItem *mdbItem = metaDataBase->metaDataBaseItem(object)) {
+        const QStringList mdbFakeMethods =  member_type == qdesigner_internal::SlotMember ? mdbItem->fakeSlots() : mdbItem->fakeSignals();
+        if (!mdbFakeMethods.empty())
+            foreach (const QString &fakeMethod, mdbFakeMethods)
+                if (predicate(fakeMethod)) {
+                    *it = ClassNameSignaturePair(className, fakeMethod);
+                    ++it;
+                }
+    }
 }
 
 namespace {
@@ -245,10 +244,9 @@ namespace qdesigner_internal {
     ClassesMemberFunctions reverseClassesMemberFunctions(const QString &obj_name, MemberType member_type,
                                                          const QString &peer, QDesignerFormWindowInterface *form)
     {
-        QObject *object = qFindChild<QObject*>(form, obj_name);
+        QObject *object = qFindChild<QObject*>(form->mainContainer(), obj_name);
         if (!object)
             return ClassesMemberFunctions();
-
         QDesignerFormEditorInterface *core = form->core();
 
         ClassesMemberFunctions rc;

tools/designer/src/components/signalsloteditor/signalsloteditorwindow.cpp
@@ -658,7 +658,7 @@ QWidget *ConnectionDelegate::createEditor(QWidget *parent,
 
         const qdesigner_internal::ClassesMemberFunctions class_list = qdesigner_internal::reverseClassesMemberFunctions(obj_name, type, peer, m_form);
 
-        QObject *object = qFindChild<QObject*>(m_form, obj_name);
+        QObject *object = qFindChild<QObject*>(m_form->mainContainer(), obj_name);
 
         inline_editor->addText(type == qdesigner_internal::SignalMember ? tr("<signal>") : tr("<slot>"));
         foreach (const qdesigner_internal::ClassMemberFunctions &class_info, class_list) {

commit f7e75244ab62b1fe0d1d73b84a6089d6b2a81d1a

Designer: Slots of main container not visible in signal/slot editor.
Fix a regression introduced by dd7873cd7e02e42b384709f33a34f69bc06fb25d
of QTBUG-17179.

Reviewed-by: Kai Koehne <kai.koehne@nokia.com>
Task-number: QTBUG-17358

tools/designer/src/components/signalsloteditor/signalslot_utils.cpp
@@ -244,7 +244,12 @@ namespace qdesigner_internal {
     ClassesMemberFunctions reverseClassesMemberFunctions(const QString &obj_name, MemberType member_type,
                                                          const QString &peer, QDesignerFormWindowInterface *form)
     {
-        QObject *object = qFindChild<QObject*>(form->mainContainer(), obj_name);
+        QObject *object = 0;
+        if (obj_name == form->mainContainer()->objectName()) {
+            object = form->mainContainer();
+        } else {
+            object = qFindChild<QObject*>(form->mainContainer(), obj_name);
+        }
         if (!object)
             return ClassesMemberFunctions();
         QDesignerFormEditorInterface *core = form->core();

tools/designer/src/components/signalsloteditor/signalsloteditorwindow.cpp
@@ -658,8 +658,12 @@ QWidget *ConnectionDelegate::createEditor(QWidget *parent,
 
         const qdesigner_internal::ClassesMemberFunctions class_list = qdesigner_internal::reverseClassesMemberFunctions(obj_name, type, peer, m_form);
 
-        QObject *object = qFindChild<QObject*>(m_form->mainContainer(), obj_name);
-
+        QObject *object = 0;
+        if (obj_name == m_form->mainContainer()->objectName()) {
+            object = m_form->mainContainer();
+        } else {
+            object = qFindChild<QObject*>(m_form->mainContainer(), obj_name);
+        }
         inline_editor->addText(type == qdesigner_internal::SignalMember ? tr("<signal>") : tr("<slot>"));
         foreach (const qdesigner_internal::ClassMemberFunctions &class_info, class_list) {
             if (class_info.m_className.isEmpty() || class_info.m_memberList.isEmpty())