3997977cdb712042c540b143f2ce2f5376e9d1e0
Andrew den Exter <andrew.den-exter@nokia.com>
2011-02-15 16:14:29 +1000

    Use QML notation for documenting TextInput::accepted() signal.
    Use the name onAccepted() and refer to it as a handler rather than
    a signal.
    
    Change-Id: Ibd4144aa809e6d9db136187a07e9a660afa5df1b

--- src/declarative/graphicsitems/qdeclarativetextinput.cpp
+++ src/declarative/graphicsitems/qdeclarativetextinput.cpp
@@ -776,12 +776,12 @@ bool QDeclarativeTextInput::hasAcceptableInput() const
 }
 
 /*!
-    \qmlsignal TextInput::accepted()
+    \qmlsignal TextInput::onAccepted()
 
-    This signal is emitted when the Return or Enter key is pressed.
+    This handler is called when the Return or Enter key is pressed.
     Note that if there is a \l validator or \l inputMask set on the text
-    input, the accepted signal will only be emitted if the input
-    is in an acceptable state.
+    input, the handler will only be emitted if the input is in an acceptable
+    state.
 */
 
 /*!
