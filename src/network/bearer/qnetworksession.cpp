7ef73c9b9eb445796b6a9296618daf3b085841c4
Thiago Macieira <thiago.macieira@nokia.com>
2011-02-20 15:03:03 +0100

    Fix warning about QLatin1String::operator==(const char*) deprecated
    Use qstrcmp instead.

--- src/network/bearer/qnetworksession.cpp
+++ src/network/bearer/qnetworksession.cpp
@@ -680,7 +680,7 @@ void QNetworkSession::connectNotify(const char *signal)
 
     //check for preferredConfigurationChanged() signal connect notification
     //This is not required on all platforms
-    if (QLatin1String(signal) == SIGNAL(preferredConfigurationChanged(QNetworkConfiguration,bool)))
+    if (qstrcmp(signal, SIGNAL(preferredConfigurationChanged(QNetworkConfiguration,bool))) == 0)
         d->setALREnabled(true);
 }
 
@@ -701,7 +701,7 @@ void QNetworkSession::disconnectNotify(const char *signal)
 
     //check for preferredConfigurationChanged() signal disconnect notification
     //This is not required on all platforms
-    if (QLatin1String(signal) == SIGNAL(preferredConfigurationChanged(QNetworkConfiguration,bool)))
+    if (qstrcmp(signal, SIGNAL(preferredConfigurationChanged(QNetworkConfiguration,bool))) == 0)
         d->setALREnabled(false);
 }
 
