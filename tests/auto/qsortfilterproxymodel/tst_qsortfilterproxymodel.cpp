qt/qt

commit d149a3faca9b97ce806249bc7ef73fe2f59589d5

Don't call invalidate when resetting the QSortFilterProxyModel.
The invalidate connection is made before connecting the source model,
which means it gets called first.

If a second proxy model is used with it, the sourceLayoutAboutToBeChanged
of the second proxy is therefore called before its sourceReset slot, but
after the persistent indexes of the first model have been invalidated.

The invalidate call is not needed because clear_mappings is called
when the source model is reset anyway.

Merge-request: 416
Reviewed-by: Olivier Goffart <ogoffart@trolltech.com>

src/gui/itemviews/qsortfilterproxymodel.cpp
@@ -1481,7 +1481,6 @@ QSortFilterProxyModel::QSortFilterProxyModel(QObject *parent)
     d->filter_column = 0;
     d->filter_role = Qt::DisplayRole;
     d->dynamic_sortfilter = false;
-    connect(this, SIGNAL(modelReset()), this, SLOT(invalidate()));
 }
 
 /*!

tests/auto/modeltest/dynamictreemodel.cpp
@@ -160,10 +160,11 @@ QVariant DynamicTreeModel::data(const QModelIndex &index, int role) const
 
 void DynamicTreeModel::clear()
 {
+  beginResetModel();
   m_items.clear();
   m_childItems.clear();
   nextId = 1;
-  reset();
+  endResetModel();
 }
 
 

tests/auto/modeltest/tst_modeltest.cpp
@@ -67,6 +67,7 @@ private slots:
     void standardItemModel();
     void testInsertThroughProxy();
     void moveSourceItems();
+    void testResetThroughProxy();
 };
 
 
@@ -225,8 +226,9 @@ public slots:
 
     void storePersistent()
     {
-        m_persistentSourceIndexes.clear();
-        m_persistentProxyIndexes.clear();
+      foreach(const QModelIndex &idx, m_persistentProxyIndexes)
+        Q_ASSERT(idx.isValid()); // This is called from layoutAboutToBeChanged. Persistent indexes should be valid
+
         Q_ASSERT(m_proxy->persistent().isEmpty());
         storePersistent(QModelIndex());
         Q_ASSERT(!m_proxy->persistent().isEmpty());
@@ -243,6 +245,8 @@ public slots:
             QModelIndex updatedSource = m_persistentSourceIndexes.at(row);
             QCOMPARE(m_proxy->mapToSource(updatedProxy), updatedSource);
         }
+        m_persistentSourceIndexes.clear();
+        m_persistentProxyIndexes.clear();
     }
 
 private:
@@ -276,6 +280,28 @@ void tst_ModelTest::moveSourceItems()
     moveCommand->setDestAncestors(QList<int>() << 1);
     moveCommand->setDestRow(0);
     moveCommand->doCommand();
+}
+
+void tst_ModelTest::testResetThroughProxy()
+{
+    DynamicTreeModel *model = new DynamicTreeModel(this);
+
+    ModelInsertCommand *insertCommand = new ModelInsertCommand(model, this);
+    insertCommand->setStartRow(0);
+    insertCommand->setEndRow(2);
+    insertCommand->doCommand();
+
+    QPersistentModelIndex persistent = model->index(0, 0);
+
+    AccessibleProxyModel *proxy = new AccessibleProxyModel(this);
+    proxy->setSourceModel(model);
+
+    ObservingObject observer(proxy);
+    observer.storePersistent();
+
+    ModelResetCommand *resetCommand = new ModelResetCommand(model, this);
+    resetCommand->setNumCols(0);
+    resetCommand->doCommand();
 }
 
 

commit e77e820191919aaf54c85f599cf71cfbecd1ee14

QSortFilterProxyModel::reset() should invalidate.
This was broken by: d149a3faca9b97ce806249bc7ef73fe2f59589d5

The connection was removed in that commit because we did not want
the layoutChanged signal to be emitted.  So instead of calling
invalidate(), we call clearMapping() directly.
In order to do that, clear_mapping has been turned into a private slot

Task-number: QTBUG-17812
Reviewed-by: Gabriel

src/gui/itemviews/qsortfilterproxymodel.cpp
@@ -227,7 +227,7 @@ public:
     void _q_sourceColumnsRemoved(const QModelIndex &source_parent,
                                  int start, int end);
 
-    void clear_mapping();
+    void _q_clearMapping();
 
     void sort();
     bool update_source_sort_column();
@@ -281,7 +281,7 @@ typedef QHash<QModelIndex, QSortFilterProxyModelPrivate::Mapping *> IndexMap;
 void QSortFilterProxyModelPrivate::_q_sourceModelDestroyed()
 {
     QAbstractProxyModelPrivate::_q_sourceModelDestroyed();
-    clear_mapping();
+    _q_clearMapping();
 }
 
 void QSortFilterProxyModelPrivate::remove_from_mapping(const QModelIndex &source_parent)
@@ -293,7 +293,7 @@ void QSortFilterProxyModelPrivate::remove_from_mapping(const QModelIndex &source
     }
 }
 
-void QSortFilterProxyModelPrivate::clear_mapping()
+void QSortFilterProxyModelPrivate::_q_clearMapping()
 {
     // store the persistent indexes
     QModelIndexPairList source_indexes = store_persistent_indexes();
@@ -1223,7 +1223,7 @@ void QSortFilterProxyModelPrivate::_q_sourceReset()
 {
     Q_Q(QSortFilterProxyModel);
     invalidatePersistentIndexes();
-    clear_mapping();
+    _q_clearMapping();
     // All internal structures are deleted in clear()
     q->endResetModel();
     update_source_sort_column();
@@ -1519,6 +1519,7 @@ QSortFilterProxyModel::QSortFilterProxyModel(QObject *parent)
     d->filter_column = 0;
     d->filter_role = Qt::DisplayRole;
     d->dynamic_sortfilter = false;
+    connect(this, SIGNAL(modelReset()), this, SLOT(_q_clearMapping()));
 }
 
 /*!
@@ -1620,7 +1621,7 @@ void QSortFilterProxyModel::setSourceModel(QAbstractItemModel *sourceModel)
     connect(d->model, SIGNAL(modelAboutToBeReset()), this, SLOT(_q_sourceAboutToBeReset()));
     connect(d->model, SIGNAL(modelReset()), this, SLOT(_q_sourceReset()));
 
-    d->clear_mapping();
+    d->_q_clearMapping();
     endResetModel();
     if (d->update_source_sort_column() && d->dynamic_sortfilter)
         d->sort();
@@ -2311,7 +2312,7 @@ void QSortFilterProxyModel::clear()
 {
     Q_D(QSortFilterProxyModel);
     emit layoutAboutToBeChanged();
-    d->clear_mapping();
+    d->_q_clearMapping();
     emit layoutChanged();
 }
 
@@ -2326,7 +2327,7 @@ void QSortFilterProxyModel::invalidate()
 {
     Q_D(QSortFilterProxyModel);
     emit layoutAboutToBeChanged();
-    d->clear_mapping();
+    d->_q_clearMapping();
     emit layoutChanged();
 }
 

src/gui/itemviews/qsortfilterproxymodel.h
@@ -189,6 +189,7 @@ private:
     Q_PRIVATE_SLOT(d_func(), void _q_sourceColumnsInserted(const QModelIndex &source_parent, int start, int end))
     Q_PRIVATE_SLOT(d_func(), void _q_sourceColumnsAboutToBeRemoved(const QModelIndex &source_parent, int start, int end))
     Q_PRIVATE_SLOT(d_func(), void _q_sourceColumnsRemoved(const QModelIndex &source_parent, int start, int end))
+    Q_PRIVATE_SLOT(d_func(), void _q_clearMapping())
 };
 
 QT_END_NAMESPACE

tests/auto/qsortfilterproxymodel/tst_qsortfilterproxymodel.cpp
@@ -144,6 +144,8 @@ private slots:
     void taskQTBUG_7537_appearsAndSort();
     void taskQTBUG_7716_unnecessaryDynamicSorting();
     void taskQTBUG_10287_unnecessaryMapCreation();
+    void taskQTBUG_17812_resetInvalidate_data();
+    void taskQTBUG_17812_resetInvalidate();
 
     void testMultipleProxiesWithSelection();
     void mapSelectionFromSource();
@@ -3211,6 +3213,61 @@ void tst_QSortFilterProxyModel::filteredColumns()
     // Parent is QModelIndex()
     insertCommand->doCommand();
 
+}
+
+void tst_QSortFilterProxyModel::taskQTBUG_17812_resetInvalidate_data()
+{
+    QTest::addColumn<int>("test");
+    QTest::addColumn<bool>("works");
+
+    QTest::newRow("nothing") << 0 << false;
+    QTest::newRow("reset") << 1 << true;
+    QTest::newRow("invalidate") << 2 << true;
+    QTest::newRow("invalidate_filter") << 3 << true;
+}
+
+void tst_QSortFilterProxyModel::taskQTBUG_17812_resetInvalidate()
+{
+    QFETCH(int, test);
+    QFETCH(bool, works);
+
+    struct Proxy : QSortFilterProxyModel {
+        QString pattern;
+        virtual bool filterAcceptsRow(int source_row, const QModelIndex&) const {
+            return sourceModel()->data(sourceModel()->index(source_row, 0)).toString().contains(pattern);
+        }
+        void notifyChange(int test) {
+            switch (test) {
+            case 0: break;
+            case 1: reset(); break;
+            case 2: invalidate(); break;
+            case 3: invalidateFilter(); break;
+            }
+        }
+    };
+
+    QStringListModel sourceModel(QStringList() << "Poisson" << "Vache" << "Brebis"
+                                               << "Elephant" << "Cochon" << "Serpent"
+                                               << "Mouton" << "Ecureuil" << "Mouche");
+    Proxy proxy;
+    proxy.pattern = QString::fromLatin1("n");
+    proxy.setSourceModel(&sourceModel);
+
+    QCOMPARE(proxy.rowCount(), 5);
+    for (int i = 0; i < proxy.rowCount(); i++) {
+        QVERIFY(proxy.data(proxy.index(i,0)).toString().contains('n'));
+    }
+
+    proxy.pattern = QString::fromLatin1("o");
+    proxy.notifyChange(test);
+
+    QCOMPARE(proxy.rowCount(), works ? 4 : 5);
+    bool ok = true;
+    for (int i = 0; i < proxy.rowCount(); i++) {
+        if (!proxy.data(proxy.index(i,0)).toString().contains('o'))
+            ok = false;
+    }
+    QCOMPARE(ok, works);
 }
 
 QTEST_MAIN(tst_QSortFilterProxyModel)