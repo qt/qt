qt/qt

commit a3a79fefe65ec12c4c34a69885c2d23d79538a97

Fail in a nicer way when QPixmap is used in a non-GUI application.
Show same fatal message as when trying to instantiate a QWidget.

Task-number: QTBUG-17873
Reviewed-by: Gunnar Sletta

src/gui/image/qpixmap.cpp
@@ -112,8 +112,13 @@ void QPixmap::init(int w, int h, Type type)
     init(w, h, int(type));
 }
 
+extern QApplication::Type qt_appType;
+
 void QPixmap::init(int w, int h, int type)
 {
+    if (qt_appType == QApplication::Tty)
+        qFatal("QPixmap: Cannot create a QPixmap when no GUI is being used");
+
     if ((w > 0 && h > 0) || type == QPixmapData::BitmapType)
         data = QPixmapData::create(w, h, (QPixmapData::PixelType) type);
     else

commit 39f2c09d9154e00409c73c6f6db90a4ddb06b8f0

Fixed auto-test failures caused by a3a79fefe65ec12.
QPixmaps might be unintentionally created through QVariant streaming for
example, so to prevent breaking existing applications we should just use
a warning instead, when creating a QPixmap in a non-GUI application.

Task-number: QTBUG-17873
Reviewed-by: aavit

src/gui/image/qpixmap.cpp
@@ -116,8 +116,11 @@ extern QApplication::Type qt_appType;
 
 void QPixmap::init(int w, int h, int type)
 {
-    if (qt_appType == QApplication::Tty)
-        qFatal("QPixmap: Cannot create a QPixmap when no GUI is being used");
+    if (qt_appType == QApplication::Tty) {
+        qWarning("QPixmap: Cannot create a QPixmap when no GUI is being used");
+        data = 0;
+        return;
+    }
 
     if ((w > 0 && h > 0) || type == QPixmapData::BitmapType)
         data = QPixmapData::create(w, h, (QPixmapData::PixelType) type);