qt/qt

commit 64852122ba71bbb297b4f1e440f6fabee16ca2fe

Fix crash in QTextBlock::next()/previous()
We should check not just p but also n in next()/previous(),
which is what isValid() does. Otherwise n == 0 will cause
crash in QFragmentMap.

Task-number: QTBUG-16279
Reviewed-by: Eskil

src/gui/text/qtextobject.cpp
@@ -1488,7 +1488,7 @@ QTextBlock::iterator QTextBlock::end() const
 */
 QTextBlock QTextBlock::next() const
 {
-    if (!p)
+    if (!isValid())
         return QTextBlock();
 
     return QTextBlock(p, p->blockMap().next(n));
@@ -1504,7 +1504,7 @@ QTextBlock QTextBlock::next() const
 */
 QTextBlock QTextBlock::previous() const
 {
-    if (!p)
+    if (!isValid())
         return QTextBlock();
 
     return QTextBlock(p, p->blockMap().previous(n));

commit 1b326932d48c0e584f9d5173fa5f892c5086fee8

Not requiring valid QTextBlock in previous()
Commit 64852122ba7 introduced a regression in QTextBlock::previous(),
programs that do doc.end().previous() will not be able to retrieve
the last valid block. Revert this change so that we can keep the
behavior consistent with previous versions.

Task-number: QTBUG-18026
Reviewed-by: Eskil

src/gui/text/qtextobject.cpp
@@ -1504,7 +1504,7 @@ QTextBlock QTextBlock::next() const
 */
 QTextBlock QTextBlock::previous() const
 {
-    if (!isValid())
+    if (!p)
         return QTextBlock();
 
     return QTextBlock(p, p->blockMap().previous(n));

tests/auto/qtextblock/tst_qtextblock.cpp
@@ -75,6 +75,7 @@ private slots:
     void fragmentOverBlockBoundaries();
     void excludeParagraphSeparatorFragment();
     void backwardsBlockIterator();
+    void previousBlock_qtbug18026();
 
 private:
     QTextDocument *doc;
@@ -172,6 +173,12 @@ void tst_QTextBlock::backwardsBlockIterator()
     QCOMPARE(it.fragment().position(), 1);
     --it;
     QCOMPARE(it.fragment().position(), 0);
+}
+
+void tst_QTextBlock::previousBlock_qtbug18026()
+{
+    QTextBlock last = doc->end().previous();
+    QVERIFY(last.isValid());
 }
 
 QTEST_MAIN(tst_QTextBlock)