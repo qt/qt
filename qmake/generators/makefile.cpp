c74f29f28f2bfee8335820a67598d16e850e9444
Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
2011-03-29 10:28:27 +0200

    simplify: absolute fixification ignores the base dir arguments
    Reviewed-by: mariusSO

--- qmake/generators/makefile.cpp
+++ qmake/generators/makefile.cpp
@@ -2460,7 +2460,7 @@ MakefileGenerator::writeSubTargets(QTextStream &t, QList<MakefileGenerator::SubT
         //qmake it
         if(!subtarget->profile.isEmpty()) {
             QString out = subtarget->makefile;
-            QString in = fileFixify(in_directory + subtarget->profile, out_directory, QString(), FileFixifyAbsolute);
+            QString in = fileFixify(in_directory + subtarget->profile, FileFixifyAbsolute);
             if(out.startsWith(in_directory))
                 out = out.mid(in_directory.length());
             t << mkfile << ": " << "\n\t";
