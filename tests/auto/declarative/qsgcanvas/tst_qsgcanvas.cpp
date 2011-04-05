83c8db56e99a4da587323ae594dad87e22469e1e
Michael Brasser <michael.brasser@nokia.com>
2011-04-05 14:08:30 +1000

    Prevent crash on QSGCanvas destruction.
    Change-Id: I26ce7f0747c6279124bc4f592d229dc3728c2531
    Reviewed-by: Aaron Kennedy

--- tests/auto/declarative/qsgcanvas/tst_qsgcanvas.cpp
+++ tests/auto/declarative/qsgcanvas/tst_qsgcanvas.cpp
@@ -166,6 +166,8 @@ private slots:
     void touchEvent_basic();
     void touchEvent_propagation();
     void touchEvent_propagation_data();
+
+    void clearCanvas();
 };
 
 tst_qsgcanvas::tst_qsgcanvas()
@@ -411,6 +413,21 @@ void tst_qsgcanvas::touchEvent_propagation_data()
     QTest::newRow("disable events") << false << true << 1.0;
     QTest::newRow("disable item") << true << false << 1.0;
     QTest::newRow("opacity of 0") << true << true << 0.0;
+}
+
+void tst_qsgcanvas::clearCanvas()
+{
+    QSGCanvas *canvas = new QSGCanvas;
+    QSGItem *item = new QSGItem;
+    item->setParentItem(canvas->rootItem());
+
+    QVERIFY(item->canvas() == canvas);
+
+    delete canvas;
+
+    QVERIFY(item->canvas() == 0);
+
+    delete item;
 }
 
 
