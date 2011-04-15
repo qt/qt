40d2e6a407406cf7c5e35d197359fc9b47a13761
Michael Brasser <michael.brasser@nokia.com>
2011-04-15 11:22:10 +1000

    Fix crash in qsggridview autotest.
    Change-Id: I06af76a77ee28a3e94df1fc439f28e961d2cc248

--- src/declarative/items/qsgitem.cpp
+++ src/declarative/items/qsgitem.cpp
@@ -1157,6 +1157,7 @@ void QSGItemPrivate::initCanvas(InitializationState *state, QSGCanvas *c)
     Q_Q(QSGItem);
 
     if (canvas) {
+        removeFromDirtyList();
         QSGCanvasPrivate *c = QSGCanvasPrivate::get(canvas);
         if (polishScheduled)
             c->itemsToPolish.remove(q);
