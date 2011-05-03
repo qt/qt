6f082a3fdd368cd7a969b7f324354583d081da82
Jason McDonald <jason.mcdonald@nokia.com>
2011-05-03 11:44:16 +1000

    Remove Q_ASSERT from QMetaObject autotest.
    Issue a helpful warning rather than ignoring the error in release mode
    builds.
    
    Change-Id: Iea7bf4d63f8227abaf7dcf1700c5fef62763afad
    Task-number: QTBUG-17582
    Reviewed-by: Rohan McGovern

--- tests/auto/qmetaobject/tst_qmetaobject.cpp
+++ tests/auto/qmetaobject/tst_qmetaobject.cpp
@@ -241,7 +241,12 @@ public:
     QObject *child;
 
 public slots:
-    void on_child1_destroyed(QObject *obj = 0) { ++invokeCount1; Q_ASSERT(obj && obj == child); }
+    void on_child1_destroyed(QObject *obj = 0)
+    {
+        ++invokeCount1;
+        if (!obj || obj != child)
+            qWarning() << "on_child1_destroyed invoked with wrong child object";
+    }
     void on_child2_destroyed() { ++invokeCount2; }
 };
 
@@ -265,7 +270,12 @@ public:
     }
 
 private slots:
-    void on_child1_destroyed(QObject *obj) { ++invokeCount1; Q_ASSERT(obj && obj == child); }
+    void on_child1_destroyed(QObject *obj)
+    {
+        ++invokeCount1;
+        if (!obj || obj != child)
+            qWarning() << "on_child1_destroyed invoked with wrong child object";
+    }
     void on_child1_destroyed() { ++invokeCount2; }
 };
 
