qt/qt

commit 35faeb205843c4f0b921d2b878d2d24962c64664

Moved the QML Observer Service and related functionality into Qt
This code was previously developed as part of Qt Creator in
share/qtcreator/qml/qmljsdebugger/

Moving it into Qt will allow us to simplify the setup required before
you can debug QML applications.

To avoid adding too much weight to the QtDeclarative module, a
declarativeobserver plugin was introduced that contains the
QDeclarativeViewObserver and related classes. The
QDeclarativeObserverService is just a stub service that loads this
plugin once a QML debugging client connects. The plugin implements the
QDeclarativeObserverInterface

A QJSDebugService was separated out of QJSDebuggerAgent, so
that the service can be active while the agent is instantiated lazily.
Each QDeclarativeEngine adds itself to the QJSDebugService. Currently
only the first one is used when instantiating the agent.

QDeclarativeObserverService is hooked into QDeclarativeView, with the
view registering itself to the service, allowing the
QDeclarativeViewObserver to be created for the view once somebody
connects to the service. Again, only the first view is used at the
moment.

Change-Id: Ib50579c6d24361c2b39528e5556410d3446c2e90
Reviewed-by: Martin Jones
Reviewed-by: Michael Brasser

src/plugins/qmltooling/declarativeobserver/editor/abstractliveedittool.cpp
@@ -1,0 +1,200 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtDeclarative module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "abstractliveedittool_p.h"
+#include "qdeclarativeviewobserver_p_p.h"
+
+#include <QDeclarativeEngine>
+
+#include <QtDebug>
+#include <QGraphicsItem>
+#include <QDeclarativeItem>
+
+QT_BEGIN_NAMESPACE
+
+AbstractLiveEditTool::AbstractLiveEditTool(QDeclarativeViewObserver *editorView)
+    : QObject(editorView), m_observer(editorView)
+{
+}
+
+
+AbstractLiveEditTool::~AbstractLiveEditTool()
+{
+}
+
+QDeclarativeViewObserver *AbstractLiveEditTool::observer() const
+{
+    return m_observer;
+}
+
+QDeclarativeView *AbstractLiveEditTool::view() const
+{
+    return m_observer->declarativeView();
+}
+
+QGraphicsScene* AbstractLiveEditTool::scene() const
+{
+    return view()->scene();
+}
+
+void AbstractLiveEditTool::updateSelectedItems()
+{
+    selectedItemsChanged(items());
+}
+
+QList<QGraphicsItem*> AbstractLiveEditTool::items() const
+{
+    return observer()->selectedItems();
+}
+
+void AbstractLiveEditTool::enterContext(QGraphicsItem *itemToEnter)
+{
+    observer()->data->enterContext(itemToEnter);
+}
+
+bool AbstractLiveEditTool::topItemIsMovable(const QList<QGraphicsItem*> & itemList)
+{
+    QGraphicsItem *firstSelectableItem = topMovableGraphicsItem(itemList);
+    if (firstSelectableItem == 0)
+        return false;
+    if (toQDeclarativeItem(firstSelectableItem) != 0)
+        return true;
+
+    return false;
+
+}
+
+bool AbstractLiveEditTool::topSelectedItemIsMovable(const QList<QGraphicsItem*> &itemList)
+{
+    QList<QGraphicsItem*> selectedItems = observer()->selectedItems();
+
+    foreach (QGraphicsItem *item, itemList) {
+        QDeclarativeItem *declarativeItem = toQDeclarativeItem(item);
+        if (declarativeItem
+                && selectedItems.contains(declarativeItem)
+                /*&& (declarativeItem->qmlItemNode().hasShowContent() || selectNonContentItems)*/)
+            return true;
+    }
+
+    return false;
+
+}
+
+bool AbstractLiveEditTool::topItemIsResizeHandle(const QList<QGraphicsItem*> &/*itemList*/)
+{
+    return false;
+}
+
+QDeclarativeItem *AbstractLiveEditTool::toQDeclarativeItem(QGraphicsItem *item)
+{
+    return qobject_cast<QDeclarativeItem*>(item->toGraphicsObject());
+}
+
+QGraphicsItem *AbstractLiveEditTool::topMovableGraphicsItem(const QList<QGraphicsItem*> &itemList)
+{
+    foreach (QGraphicsItem *item, itemList) {
+        if (item->flags().testFlag(QGraphicsItem::ItemIsMovable))
+            return item;
+    }
+    return 0;
+}
+
+QDeclarativeItem *AbstractLiveEditTool::topMovableDeclarativeItem(const QList<QGraphicsItem*>
+                                                                  &itemList)
+{
+    foreach (QGraphicsItem *item, itemList) {
+        QDeclarativeItem *declarativeItem = toQDeclarativeItem(item);
+        if (declarativeItem /*&& (declarativeItem->qmlItemNode().hasShowContent())*/)
+            return declarativeItem;
+    }
+
+    return 0;
+}
+
+QList<QGraphicsObject*> AbstractLiveEditTool::toGraphicsObjectList(const QList<QGraphicsItem*>
+                                                                   &itemList)
+{
+    QList<QGraphicsObject*> gfxObjects;
+    foreach (QGraphicsItem *item, itemList) {
+        QGraphicsObject *obj = item->toGraphicsObject();
+        if (obj)
+            gfxObjects << obj;
+    }
+
+    return gfxObjects;
+}
+
+QString AbstractLiveEditTool::titleForItem(QGraphicsItem *item)
+{
+    QString className(QLatin1String("QGraphicsItem"));
+    QString objectStringId;
+
+    QString constructedName;
+
+    QGraphicsObject *gfxObject = item->toGraphicsObject();
+    if (gfxObject) {
+        className = QLatin1String(gfxObject->metaObject()->className());
+
+        className.remove(QRegExp(QLatin1String("_QMLTYPE_\\d+")));
+        className.remove(QRegExp(QLatin1String("_QML_\\d+")));
+        if (className.startsWith(QLatin1String("QDeclarative")))
+            className = className.remove(QLatin1String("QDeclarative"));
+
+        QDeclarativeItem *declarativeItem = qobject_cast<QDeclarativeItem*>(gfxObject);
+        if (declarativeItem) {
+            objectStringId = m_observer->idStringForObject(declarativeItem);
+        }
+
+        if (!objectStringId.isEmpty()) {
+            constructedName = objectStringId + QLatin1String(" (") + className + QLatin1Char(')');
+        } else {
+            if (!gfxObject->objectName().isEmpty()) {
+                constructedName = gfxObject->objectName() + QLatin1String(" (") + className + QLatin1Char(')');
+            } else {
+                constructedName = className;
+            }
+        }
+    }
+
+    return constructedName;
+}
+
+QT_END_NAMESPACE

commit 894515429397be20670806825099b4b9896d50d6

Fixed compile on Windows
This compiled fine on Linux, but on Windows the project root is not
automatically included in the INCLUDEPATH. Use the correct relative path
to make the include work.

Problem introduced with 35faeb205843c4f0b921d2b878d2d24962c64664

src/plugins/qmltooling/declarativeobserver/editor/abstractliveedittool.cpp
@@ -40,7 +40,7 @@
 ****************************************************************************/
 
 #include "abstractliveedittool_p.h"
-#include "qdeclarativeviewobserver_p_p.h"
+#include "../qdeclarativeviewobserver_p_p.h"
 
 #include <QDeclarativeEngine>
 