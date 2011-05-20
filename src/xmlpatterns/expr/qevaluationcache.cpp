11084503c711f862baa4d4027a7b97cedae1034e
Rohan McGovern <rohan.mcgovern@nokia.com>
2011-05-20 18:02:58 +1000

    Fixed compilation.
    The same bug was fixed two different ways in Qt 4.7 and 4.8, and not
    merged correctly.  Use the fix from 4.7.

--- src/xmlpatterns/expr/qevaluationcache.cpp
+++ src/xmlpatterns/expr/qevaluationcache.cpp
@@ -49,7 +49,7 @@ template<bool IsForGlobal>
 EvaluationCache<IsForGlobal>::EvaluationCache(const Expression::Ptr &op,
                                               const VariableDeclaration *varDecl,
                                               const VariableSlotID aSlot) : SingleContainer(op)
-                                                                          , m_declaration(varDecl.constData())
+                                                                          , m_declaration(varDecl)
                                                                           , m_varSlot(aSlot)
 {
     Q_ASSERT(m_declaration);
