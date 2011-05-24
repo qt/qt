qt/qt

commit b727e5b95c1f0bd308b228fcf0445b42847ae583

Squashed commit of the changes from the mobile-examples repository (4.7-generated-declarative branch). (cherry picked from commit 539311f7b2687e3148ea695ce06fee768abe7b44)

src/gui/kernel/qcocoasharedwindowmethods_mac_p.h
@@ -199,6 +199,19 @@ QT_END_NAMESPACE
     [super setInitialFirstResponder:view];
 }
 
+- (void)setInitialFirstResponder:(NSView *)view
+{
+    // This method is called the first time the window is placed on screen and
+    // is the earliest point in time we can connect OpenGL contexts to NSViews.
+    QWidget *qwidget = [[QT_MANGLE_NAMESPACE(QCocoaWindowDelegate) sharedDelegate] qt_qwidgetForWindow:self];
+    if (qwidget) {
+        qt_event_request_window_change(qwidget);
+        qt_mac_send_posted_gl_updates(qwidget);
+    }
+
+    [super setInitialFirstResponder:view];
+}
+
 - (BOOL)makeFirstResponder:(NSResponder *)responder
 {
     // For some reason Cocoa wants to flip the first responder

commit 16b11332931caef92bd103d854fac4dff3ff771e

Fixed breakage caused by b727e5b95c1f0bd308b228fcf0445b42847ae583.

src/gui/kernel/qcocoasharedwindowmethods_mac_p.h
@@ -199,19 +199,6 @@ QT_END_NAMESPACE
     [super setInitialFirstResponder:view];
 }
 
-- (void)setInitialFirstResponder:(NSView *)view
-{
-    // This method is called the first time the window is placed on screen and
-    // is the earliest point in time we can connect OpenGL contexts to NSViews.
-    QWidget *qwidget = [[QT_MANGLE_NAMESPACE(QCocoaWindowDelegate) sharedDelegate] qt_qwidgetForWindow:self];
-    if (qwidget) {
-        qt_event_request_window_change(qwidget);
-        qt_mac_send_posted_gl_updates(qwidget);
-    }
-
-    [super setInitialFirstResponder:view];
-}
-
 - (BOOL)makeFirstResponder:(NSResponder *)responder
 {
     // For some reason Cocoa wants to flip the first responder