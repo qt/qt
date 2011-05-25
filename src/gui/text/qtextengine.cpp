3f82ecbd0e2cdf477e57c7fe41b63c09d7e84787
Jiang Jiang <jiang.jiang@nokia.com>
2011-05-25 18:24:30 +0200

    Fix boundry conditions for cursor hit test
    Clicking at the edge of a glyph means lookup for the left glyph.
    
    Reviewed-by: TrustMe
    (cherry picked from commit 31110bf84bb06d57983501fa65fe0db3f7c61927)

--- src/gui/text/qtextengine.cpp
+++ src/gui/text/qtextengine.cpp
@@ -2739,7 +2739,7 @@ int QTextEngine::positionInLigature(const QScriptItem *si, int end,
     if (glyph_pos == -1 && end > 0)
         glyph_pos = logClusters[end - 1];
     else {
-        if (x < edge)
+        if (x <= edge)
             glyph_pos--;
     }
 
