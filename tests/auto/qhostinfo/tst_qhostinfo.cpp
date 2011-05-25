a7cf6e27c9e31833991d61685d4dc5b8b5f777b7
Shane Kearns <shane.kearns@accenture.com>
2011-05-25 14:46:01 +0100

    Update bearer startup code in network autotests
    The bearer startup code I added to some of the network autotests to
    ensure the network was up before testing caused test failures on some
    linux configurations due to there being no default network defined.
    
    I've changed it to follow these steps:
    1. update the configuration list & wait for update completed signal
     - due to the polling engines not having any config defined on the first
       run before the initial poll happens
    2. check default configuration is valid before attempting to start it
    3a. for valid configuration, start it and wait for started as before
    3b. for invalid configuration, fail if bearer is mandatory, otherwise ignore
     - on symbian bearer use is "mandatory", on desktop platforms it is optional
    
    Reviewed-by: Markus Goetz

--- tests/auto/qhostinfo/tst_qhostinfo.cpp
+++ tests/auto/qhostinfo/tst_qhostinfo.cpp
@@ -197,13 +197,19 @@ tst_QHostInfo::~tst_QHostInfo()
 void tst_QHostInfo::initTestCase()
 {
 #ifndef QT_NO_BEARERMANAGEMENT
-    //start the default network
     netConfMan = new QNetworkConfigurationManager(this);
+    netConfMan->updateConfigurations();
+    connect(netConfMan, SIGNAL(updateCompleted()), &QTestEventLoop::instance(), SLOT(exitLoop()));
+    QTestEventLoop::instance().enterLoop(10);
     networkConfiguration = netConfMan->defaultConfiguration();
-    networkSession.reset(new QNetworkSession(networkConfiguration));
-    if (!networkSession->isOpen()) {
-        networkSession->open();
-        QVERIFY(networkSession->waitForOpened(30000));
+    if (networkConfiguration.isValid()) {
+        networkSession.reset(new QNetworkSession(networkConfiguration));
+        if (!networkSession->isOpen()) {
+            networkSession->open();
+            QVERIFY(networkSession->waitForOpened(30000));
+        }
+    } else {
+        QVERIFY(!(netConfMan->capabilities() & QNetworkConfigurationManager::NetworkSessionRequired));
     }
 #endif
 
