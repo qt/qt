10f16bc55b9e5535bc3353260f97a32e18d70cf1
Christian Kamm <christian.d.kamm@nokia.com>
2011-05-30 10:20:29 +0200

    qmlplugindump: Bump QtQuick.tooling version to 1.1.
    Mirrors a change to qmlplugindump in
    qtcreator/715cee76a9e46efb7f8245004aaa8a1c47b1618d
    
    Reviewed-by: Kai Koehne

--- tools/qmlplugindump/main.cpp
+++ tools/qmlplugindump/main.cpp
@@ -510,7 +510,7 @@ int main(int argc, char *argv[])
         engine->addImportPath(pluginImportPath);
 
     // find all QMetaObjects reachable from the builtin module
-    QByteArray importCode("import QtQuick 1.0\n");
+    QByteArray importCode("import QtQuick 1.1\n");
     QSet<const QMetaObject *> defaultReachable = collectReachableMetaObjects(importCode, engine);
 
     // this will hold the meta objects we want to dump information of
