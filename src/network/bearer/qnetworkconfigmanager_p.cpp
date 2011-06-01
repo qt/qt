5f879c55e531165cc2569b03c3796d0f33d0a0b7
Shane Kearns <shane.kearns@accenture.com>
2011-06-01 16:35:43 +0100

    bearer: run the bearer engines in their own worker thread
    The original architecture of the QtNetwork bearer support hosted the
    engines in the application's main thread, but this causes some problems.
    If the QNetworkConfigurationManager is constructed in a worker thread,
    then it is populated asynchronously without any notification when it is
    done (the app gets incomplete or missing results)
    Fixing that by restoring the earlier behaviour of using blocking queued
    connections to wait for the lists to be populated caused a regression,
    as some applications deadlock because the main thread is waiting on the
    worker thread at this time.
    
    By introducing a dedicated worker thread for the bearer engines,
    QNetworkConfigurationManager can be safely constructed in any thread
    while using blocking queued connections internally.
    
    Task-number: QTBUG-18795
    Reviewed-by: mread

--- src/network/bearer/qnetworkconfigmanager_p.cpp
+++ src/network/bearer/qnetworkconfigmanager_p.cpp
@@ -66,11 +66,31 @@ QNetworkConfigurationManagerPrivate::QNetworkConfigurationManagerPrivate()
     qRegisterMetaType<QNetworkConfigurationPrivatePointer>("QNetworkConfigurationPrivatePointer");
 }
 
+void QNetworkConfigurationManagerPrivate::initialize()
+{
+    //Two stage construction, because we only want to do this heavyweight work for the winner of the Q_GLOBAL_STATIC race.
+    bearerThread = new QThread();
+    bearerThread->moveToThread(QCoreApplicationPrivate::mainThread()); // because cleanup() is called in main thread context.
+    moveToThread(bearerThread);
+    bearerThread->start();
+    updateConfigurations();
+}
+
 QNetworkConfigurationManagerPrivate::~QNetworkConfigurationManagerPrivate()
 {
     QMutexLocker locker(&mutex);
 
     qDeleteAll(sessionEngines);
+    if (bearerThread)
+        bearerThread->quit();
+}
+
+void QNetworkConfigurationManagerPrivate::cleanup()
+{
+    QThread* thread = bearerThread;
+    deleteLater();
+    if(thread->wait(5000))
+        delete thread;
 }
 
 QNetworkConfiguration QNetworkConfigurationManagerPrivate::defaultConfiguration()
@@ -356,13 +376,6 @@ void QNetworkConfigurationManagerPrivate::updateConfigurations()
         if (sender())
             return;
 
-        if (thread() != QCoreApplicationPrivate::mainThread()) {
-            if (thread() != QThread::currentThread())
-                return;
-
-            moveToThread(QCoreApplicationPrivate::mainThread());
-        }
-
         updating = false;
 
 #ifndef QT_NO_LIBRARY
@@ -382,7 +395,7 @@ void QNetworkConfigurationManagerPrivate::updateConfigurations()
                 else
                     sessionEngines.append(engine);
 
-                engine->moveToThread(QCoreApplicationPrivate::mainThread());
+                engine->moveToThread(bearerThread);
 
                 connect(engine, SIGNAL(updateCompleted()),
                         this, SLOT(updateConfigurations()));
@@ -424,14 +437,9 @@ void QNetworkConfigurationManagerPrivate::updateConfigurations()
     if (firstUpdate) {
         firstUpdate = false;
         QList<QBearerEngine*> enginesToInitialize = sessionEngines; //shallow copy the list in case it is modified when we unlock mutex
-        Qt::ConnectionType connectionType;
-        if (QCoreApplicationPrivate::mainThread() == QThread::currentThread())
-            connectionType = Qt::DirectConnection;
-        else
-            connectionType = Qt::BlockingQueuedConnection;
         locker.unlock();
         foreach (QBearerEngine* engine, enginesToInitialize) {
-                QMetaObject::invokeMethod(engine, "initialize", connectionType);
+            QMetaObject::invokeMethod(engine, "initialize", Qt::BlockingQueuedConnection);
         }
     }
 }
