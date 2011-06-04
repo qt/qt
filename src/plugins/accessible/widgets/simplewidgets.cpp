636b7088eb3740800f54a7c1634d3e041e688270
José Millán Soto <fid@gpul.org>
2011-06-04 18:06:47 +0200

    Do not expose text when echo mode is not Normal.
    Reviewed-by: Frederik Gladhorn

--- src/plugins/accessible/widgets/simplewidgets.cpp
+++ src/plugins/accessible/widgets/simplewidgets.cpp
@@ -808,6 +808,10 @@ QString QAccessibleLineEdit::text(int startOffset, int endOffset)
 {
     if (startOffset > endOffset)
         return QString();
+
+    if (lineEdit()->echoMode() != QLineEdit::Normal)
+        return QString();
+
     return lineEdit()->text().mid(startOffset, endOffset - startOffset);
 }
 
