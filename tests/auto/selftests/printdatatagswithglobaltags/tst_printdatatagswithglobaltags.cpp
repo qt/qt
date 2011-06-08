dd0e653981cfbba2d7ba6e74422bc8b206fab202
jasplin <jo.asplin@nokia.com>
2011-06-08 17:13:13 +0200

    Added -datatags option to QTestLib
    Passing the -datatags option to a QTestLib program prints the available
    data tags to standard output.
    
    Data tags for each test function (f() in this case) are printed in
    four different ways depending on the presence of local and global
    data tags:
    
    Case 1: No tags:
    f()
    
    Case 2: Local tags only:
    f() local tag 1
    f() local tag 2
    ...
    
    Case 3: Global tags only:
    f() __global__ global tag 1
    f() __global__ global tag 2
    ...
    
    Case 4: Local and global tags:
    f() local tag 1 __global__ global tag 1
    f() local tag 2 __global__ global tag 1
    ...
    f() local tag 1 __global__ global tag 2
    f() local tag 2 __global__ global tag 2
    ...
    ...
    
    Reviewed-by: Rohan McGovern
    Task-number: QTQAINFRA-226
    Change-Id: I14de203b586a0085b8efda8e62772711e44677d2

---
+++ tests/auto/selftests/printdatatagswithglobaltags/tst_printdatatagswithglobaltags.cpp
@@ -0,0 +1,64 @@
+#include <QtTest/QtTest>
+
+class tst_Foo: public QObject
+{
+    Q_OBJECT
+private slots:
+    void initTestCase_data() const;
+    void initTestCase() const;
+
+    void a_data() const;
+    void a() const;
+
+    void b() const;
+
+    void c_data() const;
+    void c() const;
+};
+
+void tst_Foo::initTestCase_data() const
+{
+    QTest::addColumn<int>("f");
+    QTest::addColumn<int>("g");
+
+    QTest::newRow("global data tag 1 ") << 1 << 2;
+    QTest::newRow("global data tag 2") << 1 << 2;
+}
+
+void tst_Foo::initTestCase() const
+{
+}
+
+void tst_Foo::a_data() const
+{
+    QTest::addColumn<int>("x");
+    QTest::addColumn<int>("y");
+
+    QTest::newRow("data tag a1 ") << 1 << 2;
+    QTest::newRow("data tag a2") << 1 << 2;
+}
+
+void tst_Foo::a() const
+{
+}
+
+void tst_Foo::b() const
+{
+}
+
+void tst_Foo::c_data() const
+{
+    QTest::addColumn<int>("x");
+
+    QTest::newRow("data tag c1") << 1;
+    QTest::newRow("data tag c2") << 1;
+    QTest::newRow("data tag c3") << 1;
+}
+
+void tst_Foo::c() const
+{
+}
+
+QTEST_MAIN(tst_Foo)
+
+#include "tst_printdatatagswithglobaltags.moc"
