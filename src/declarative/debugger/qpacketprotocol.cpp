qt/qt

commit a5926145407103ed2e3e6ee0369358709a50c551

QmlDebugger: parsing packets iteratively in the communication protocol
Reviewed-by: Kai Koehne
(reapplied commit fea13a449c59690ae7b7f43aa64f50c0a290a2cf from qtcreator)

Change-Id: I0c78188994029b2ac7daa3d59182566c5ec6c8b5

src/declarative/debugger/qpacketprotocol.cpp
@@ -164,47 +164,44 @@ public Q_SLOTS:
 
     void readyToRead()
     {
-        if(-1 == inProgressSize) {
-            // We need a size header of sizeof(qint32)
-            if(sizeof(qint32) > (uint)dev->bytesAvailable())
-                return;
-
-            // Read size header
-            int read = dev->read((char *)&inProgressSize, sizeof(qint32));
-            Q_ASSERT(read == sizeof(qint32));
-            Q_UNUSED(read);
-
-            // Check sizing constraints
-            if(inProgressSize > maxPacketSize) {
-                QObject::disconnect(dev, SIGNAL(readyRead()),
-                                    this, SLOT(readyToRead()));
-                QObject::disconnect(dev, SIGNAL(aboutToClose()),
-                                    this, SLOT(aboutToClose()));
-                QObject::disconnect(dev, SIGNAL(bytesWritten(qint64)),
-                                    this, SLOT(bytesWritten(qint64)));
-                dev = 0;
-                emit invalidPacket();
-                return;
-            }
-
-            inProgressSize -= sizeof(qint32);
-
+        while (true) {
             // Need to get trailing data
-            readyToRead();
-        } else {
-            inProgress.append(dev->read(inProgressSize - inProgress.size()));
+            if (-1 == inProgressSize) {
+                // We need a size header of sizeof(qint32)
+                if (sizeof(qint32) > (uint)dev->bytesAvailable())
+                    return;
 
-            if(inProgressSize == inProgress.size()) {
-                // Packet has arrived!
-                packets.append(inProgress);
-                inProgressSize = -1;
-                inProgress.clear();
+                // Read size header
+                int read = dev->read((char *)&inProgressSize, sizeof(qint32));
+                Q_ASSERT(read == sizeof(qint32));
+                Q_UNUSED(read);
 
-                emit readyRead();
-                waitingForPacket = false;
+                // Check sizing constraints
+                if (inProgressSize > maxPacketSize) {
+                    QObject::disconnect(dev, SIGNAL(readyRead()),
+                                        this, SLOT(readyToRead()));
+                    QObject::disconnect(dev, SIGNAL(aboutToClose()),
+                                        this, SLOT(aboutToClose()));
+                    QObject::disconnect(dev, SIGNAL(bytesWritten(qint64)),
+                                        this, SLOT(bytesWritten(qint64)));
+                    dev = 0;
+                    emit invalidPacket();
+                    return;
+                }
 
-                // Need to get trailing data
-                readyToRead();
+                inProgressSize -= sizeof(qint32);
+            } else {
+                inProgress.append(dev->read(inProgressSize - inProgress.size()));
+
+                if (inProgressSize == inProgress.size()) {
+                    // Packet has arrived!
+                    packets.append(inProgress);
+                    inProgressSize = -1;
+                    inProgress.clear();
+
+                    emit readyRead();
+                } else
+                    return;
             }
         }
     }

commit e3b5de8651586cf5484fd8ab217cddf17f0fe01e

QDeclarativeDebug: Don't hang if started with ',block' argument
Fixes regression introduced in a59261454071

Reviewed-by: Christiaan Janssen

src/declarative/debugger/qpacketprotocol.cpp
@@ -199,6 +199,7 @@ public Q_SLOTS:
                     inProgressSize = -1;
                     inProgress.clear();
 
+                    waitingForPacket = false;
                     emit readyRead();
                 } else
                     return;