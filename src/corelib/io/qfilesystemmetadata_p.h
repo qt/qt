9d674794b60a07850e8c3123ebb18f023f7134e9
Jiang Jiang <jiang.jiang@nokia.com>
2011-07-22 16:52:35 +0200

    Revert "fix QFileInfo::isSymLink() for NTFS mount points"
    This reverts commit 70a434dc1c24f306805900b5ad7a45c7272702c6.

--- src/corelib/io/qfilesystemmetadata_p.h
+++ src/corelib/io/qfilesystemmetadata_p.h
@@ -369,7 +369,8 @@ inline void QFileSystemMetaData::fillFromFindData(WIN32_FIND_DATA &findData, boo
         entryFlags &= ~LinkType;
 #if !defined(Q_OS_WINCE)
         if ((fileAttribute_ & FILE_ATTRIBUTE_REPARSE_POINT)
-            && (findData.dwReserved0 == IO_REPARSE_TAG_SYMLINK)) {
+            && (findData.dwReserved0 == IO_REPARSE_TAG_SYMLINK
+                || findData.dwReserved0 == IO_REPARSE_TAG_MOUNT_POINT)) {
             entryFlags |= LinkType;
         }
 #endif
