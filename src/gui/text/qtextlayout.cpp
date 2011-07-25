qt/qt

commit fd8183129d0efe99e0d28f524264c59fb9155b80

Adjust right bearing when breaking with line separators
If we found a forced line break with line separator (e.g. '\n'),
take the right bearing of previous glyph into account, otherwise
the resulting text width will be slightly smaller than the one
without a line separator.

Task-number: QTBUG-17020
Reviewed-by: Eskil

src/gui/text/qtextlayout.cpp
@@ -1682,6 +1682,7 @@ namespace {
         int glyphCount;
         int maxGlyphs;
         int currentPosition;
+        glyph_t previousGlyph;
 
         QFixed minw;
         QFixed softHyphenWidth;
@@ -1709,6 +1710,15 @@ namespace {
             return glyphs.glyphs[logClusters[currentPosition - 1]];
         }
 
+        inline void saveCurrentGlyph()
+        {
+            previousGlyph = 0;
+            if (currentPosition > 0 &&
+                logClusters[currentPosition - 1] < glyphs.numGlyphs) {
+                previousGlyph = currentGlyph(); // needed to calculate right bearing later
+            }
+        }
+
         inline void adjustRightBearing(glyph_t glyph)
         {
             qreal rb;
@@ -1721,6 +1731,12 @@ namespace {
             if (currentPosition <= 0)
                 return;
             adjustRightBearing(currentGlyph());
+        }
+
+        inline void adjustPreviousRightBearing()
+        {
+            if (previousGlyph > 0)
+                adjustRightBearing(previousGlyph);
         }
 
         inline void resetRightBearing()
@@ -1798,22 +1814,7 @@ void QTextLine::layout_helper(int maxGlyphs)
     lbh.manualWrap = (wrapMode == QTextOption::ManualWrap || wrapMode == QTextOption::NoWrap);
 
     int item = -1;
-    int newItem = -1;
-    int left = 0;
-    int right = eng->layoutData->items.size()-1;
-    while(left <= right) {
-        int middle = ((right-left)/2)+left;
-        if (line.from > eng->layoutData->items[middle].position)
-            left = middle+1;
-        else if(line.from < eng->layoutData->items[middle].position)
-            right = middle-1;
-        else {
-            newItem = middle;
-            break;
-        }
-    }
-    if (newItem == -1)
-        newItem = right;
+    int newItem = eng->findItem(line.from);
 
     LB_DEBUG("from: %d: item=%d, total %d, width available %f", line.from, newItem, eng->layoutData->items.size(), line.width.toReal());
 
@@ -1825,6 +1826,7 @@ void QTextLine::layout_helper(int maxGlyphs)
     lbh.currentPosition = line.from;
     int end = 0;
     lbh.logClusters = eng->layoutData->logClustersPtr;
+    lbh.previousGlyph = 0;
 
     while (newItem < eng->layoutData->items.size()) {
         lbh.resetRightBearing();
@@ -1885,6 +1887,7 @@ void QTextLine::layout_helper(int maxGlyphs)
                                current, lbh.logClusters, lbh.glyphs);
             } else {
                 lbh.tmpData.length++;
+                lbh.adjustPreviousRightBearing();
             }
             line += lbh.tmpData;
             goto found;
@@ -1915,9 +1918,7 @@ void QTextLine::layout_helper(int maxGlyphs)
         } else {
             lbh.whiteSpaceOrObject = false;
             bool sb_or_ws = false;
-            glyph_t previousGlyph = 0;
-            if (lbh.currentPosition > 0 && lbh.logClusters[lbh.currentPosition - 1] <lbh.glyphs.numGlyphs)
-                previousGlyph = lbh.currentGlyph(); // needed to calculate right bearing later
+            lbh.saveCurrentGlyph();
             do {
                 addNextCluster(lbh.currentPosition, end, lbh.tmpData, lbh.glyphCount,
                                current, lbh.logClusters, lbh.glyphs);
@@ -1942,7 +1943,7 @@ void QTextLine::layout_helper(int maxGlyphs)
                 //  b) if we are so short of available width that the
                 //     soft hyphen is the first breakable position, then
                 //     we don't want to show it. However we initially
-                //     have to take the width for it into accoun so that
+                //     have to take the width for it into account so that
                 //     the text document layout sees the overflow and
                 //     switch to break-anywhere mode, in which we
                 //     want the soft-hyphen to slip into the next line
@@ -1970,8 +1971,9 @@ void QTextLine::layout_helper(int maxGlyphs)
                     // we are too wide, fix right bearing
                     if (rightBearing <= 0)
                         lbh.rightBearing = rightBearing; // take from cache
-                    else if (previousGlyph > 0)
-                        lbh.adjustRightBearing(previousGlyph);
+                    else
+                        lbh.adjustPreviousRightBearing();
+
                     if (!breakany) {
                         line.textWidth += lbh.softHyphenWidth;
                     }
@@ -1979,6 +1981,7 @@ void QTextLine::layout_helper(int maxGlyphs)
                     goto found;
                 }
             }
+            lbh.saveCurrentGlyph();
         }
         if (lbh.currentPosition == end)
             newItem = item + 1;

tests/auto/qtextlayout/tst_qtextlayout.cpp
@@ -124,6 +124,7 @@ private slots:
     void lineWidthFromBOM();
     void textWidthVsWIdth();
     void textWidthWithStackedTextEngine();
+    void textWidthWithLineSeparator();
 
 private:
     QFont testFont;
@@ -1397,6 +1398,22 @@ void tst_QTextLayout::textWidthWithStackedTextEngine()
     layout.endLayout();
     QFontMetricsF fm(layout.font());
     QCOMPARE(line.naturalTextWidth(), fm.width(text));
+}
+
+void tst_QTextLayout::textWidthWithLineSeparator()
+{
+    QString s1("Save Project"), s2("Save Project\ntest");
+    s2.replace('\n', QChar::LineSeparator);
+
+    QTextLayout layout1(s1), layout2(s2);
+    layout1.beginLayout();
+    layout2.beginLayout();
+
+    QTextLine line1 = layout1.createLine();
+    QTextLine line2 = layout2.createLine();
+    line1.setLineWidth(0x1000);
+    line2.setLineWidth(0x1000);
+    QCOMPARE(line1.naturalTextWidth(), line2.naturalTextWidth());
 }
 
 QTEST_MAIN(tst_QTextLayout)

commit 06f079e528bf135e5d407b15d1bd50a91ce4a888

Reset previousGlyph once we reached a new text item
The bug was introduced in fd818312. Before that, previousGlyph
is only saved in the same text item. After we moved it to
LineBreakHelper struct, it will cause crash if the font engine
in the new text item no longer contains the sub engine required
by previousGlyph.

Task-number: QTBUG-20243
Reviewed-by: Eskil

src/gui/text/qtextlayout.cpp
@@ -1869,6 +1869,7 @@ void QTextLine::layout_helper(int maxGlyphs)
             lbh.currentPosition = qMax(line.from, current.position);
             end = current.position + eng->length(item);
             lbh.glyphs = eng->shapedGlyphs(&current);
+            lbh.previousGlyph = 0;
             QFontEngine *fontEngine = eng->fontEngine(current);
             if (lbh.fontEngine != fontEngine) {
                 lbh.fontEngine = fontEngine;