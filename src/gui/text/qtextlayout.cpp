2fa986aa670bee0fafb30241f40bb3556594e32b
Jiang Jiang <jiang.jiang@nokia.com>
2011-07-26 16:03:37 +0200

    Include trailing space width in RTL text line width
    Task-number: QTBUG-15511
    Reviewed-by: Eskil

--- src/gui/text/qtextlayout.cpp
+++ src/gui/text/qtextlayout.cpp
@@ -1917,8 +1917,12 @@ found:
 
     if (line.textWidth > 0 && item < eng->layoutData->items.size())
         eng->maxWidth += lbh.spaceData.textWidth;
-    if (eng->option.flags() & QTextOption::IncludeTrailingSpaces)
+    // In the latter case, text are drawn with trailing spaces at the beginning
+    // of a line, so the naturalTextWidth should contain the space width
+    if ((eng->option.flags() & QTextOption::IncludeTrailingSpaces) ||
+        (line.width == QFIXED_MAX && eng->isRightToLeft())) {
         line.textWidth += lbh.spaceData.textWidth;
+    }
     if (lbh.spaceData.length) {
         line.length += lbh.spaceData.length;
         line.hasTrailingSpaces = true;
