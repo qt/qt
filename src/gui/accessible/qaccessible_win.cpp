qt/qt

commit d289e54f2d2aa066cb383d8c8249bd7594bdf7b0

Make accessibility work on Windows with alien
This means that there will be no implicit conversion to windows handles
anymore!
Enabler for making QML accessible on windows.
(cherry picked from commit a3ac7deb5dfe48c5fdd0e170c20b6852c3bb41de)

Reviewed-by: Frederik Gladhorn

src/gui/accessible/qaccessible_win.cpp
@@ -47,6 +47,8 @@
 #include "qt_windows.h"
 #include "qwidget.h"
 #include "qsettings.h"
+#include <QtCore/qmap.h>
+#include <QtCore/qpair.h>
 
 #include <winuser.h>
 #if !defined(WINABLEAPI)
@@ -159,6 +161,12 @@ void showDebug(const char* funcName, const QAccessibleInterface *iface)
 # define showDebug(f, iface)
 #endif
 
+// This stuff is used for widgets/items with no window handle:
+typedef QMap<int, QPair<QObject*,int> > NotifyMap;
+Q_GLOBAL_STATIC(NotifyMap, qAccessibleRecentSentEvents)
+static int eventNum = 0;
+
+
 void QAccessible::initialize()
 {
 
@@ -251,17 +259,15 @@ void QAccessible::updateAccessibility(QObject *o, int who, Event reason)
     // An event has to be associated with a window,
     // so find the first parent that is a widget.
     QWidget *w = 0;
-    if (o->isWidgetType()) {
-        w = (QWidget*)o;
-    } else {
-        QObject *p = o;
-        while ((p = p->parent()) != 0) {
-            if (p->isWidgetType()) {
-                w = (QWidget*)p;
+    QObject *p = o;
+    do {
+        if (p->isWidgetType()) {
+            w = static_cast<QWidget*>(p);
+            if (w->internalWinId())
                 break;
-            }
         }
-    }
+        p = p->parent();
+    } while (p);
 
     if (!w) {
         if (reason != QAccessible::ContextHelpStart &&
@@ -282,11 +288,80 @@ void QAccessible::updateAccessibility(QObject *o, int who, Event reason)
         }
     }
 
+    WId wid = w->internalWinId();
     if (reason != MenuCommand) { // MenuCommand is faked
-        ptrNotifyWinEvent(reason, w->winId(), OBJID_CLIENT, who);
+        if (w != o) {
+            // See comment "SENDING EVENTS TO OBJECTS WITH NO WINDOW HANDLE"
+            eventNum %= 50;              //[0..49]
+            int eventId = - eventNum - 1;
+
+            qAccessibleRecentSentEvents()->insert(eventId, qMakePair(o,who));
+            ptrNotifyWinEvent(reason, wid, OBJID_CLIENT, eventId );
+
+            ++eventNum;
+        } else {
+            ptrNotifyWinEvent(reason, wid, OBJID_CLIENT, who);
+        }
     }
 #endif // Q_WS_WINCE
 }
+
+/*  == SENDING EVENTS TO OBJECTS WITH NO WINDOW HANDLE ==
+
+    If the user requested to send the event to a widget with no window,
+    we need to send an event to an object with no hwnd.
+    The way we do that is to send it to the *first* ancestor widget
+    with a window.
+    Then we'll need a way of identifying the child:
+    We'll just keep a list of the most recent events that we have sent,
+    where each entry in the list is identified by a negative value
+    between [-50,-1]. This negative value we will pass on to
+    NotifyWinEvent() as the child id. When the negative value have
+    reached -50, it will wrap around to -1. This seems to be enough
+
+    Now, when the client receives that event, he will first call
+    AccessibleObjectFromEvent() where dwChildID is the special
+    negative value. AccessibleObjectFromEvent does two steps:
+    1. It will first sent a WM_GETOBJECT to the server, asking
+       for the IAccessible interface for the HWND.
+    2. With the IAccessible interface it got hold of it will call
+       acc_getChild where the child id argument is the special
+       negative identifier. In our reimplementation of get_accChild
+       we check for this if the child id is negative. If it is, then
+       we'll look up in our table for the entry that is associated
+       with that value.
+       The entry will then contain a pointer to the QObject /QWidget
+       that we can use to call queryAccessibleInterface() on.
+
+
+    The following figure shows how the interaction between server and
+    client is in the case when the server is sending an event.
+
+SERVER (Qt)                                 | CLIENT                                |
+--------------------------------------------+---------------------------------------+
+                                            |
+acc->updateAccessibility(obj,  childIndex)  |
+                                            |
+recentEvents()->insert(- 1 - eventNum,      |
+            qMakePair(obj, childIndex)      |
+NotifyWinEvent(hwnd, childId) =>            |
+                                            |   AccessibleObjectFromEvent(event, hwnd, OBJID_CLIENT, childId )
+                                            |   will do:
+                                          <===  1. send WM_GETOBJECT(hwnd, OBJID_CLIENT)
+widget ~= hwnd
+iface = queryAccessibleInteface(widget)
+(create IAccessible interface wrapper for
+ iface)
+ return iface                              ===> IAccessible* iface; (for hwnd)
+                                            |
+                                          <===  call iface->get_accChild(childId)
+get_accChild() {                            |
+    if (varChildID.lVal < 0) {
+        QPair ref = recentEvents().value(varChildID.lVal);
+        [...]
+    }
+*/
+
 
 void QAccessible::setRootObject(QObject *o)
 {
@@ -418,15 +493,18 @@ public:
         delete accessible;
     }
 
+    /* IUnknown */
     HRESULT STDMETHODCALLTYPE QueryInterface(REFIID, LPVOID *);
     ULONG STDMETHODCALLTYPE AddRef();
     ULONG STDMETHODCALLTYPE Release();
 
+    /* IDispatch */
     HRESULT STDMETHODCALLTYPE GetTypeInfoCount(unsigned int *);
     HRESULT STDMETHODCALLTYPE GetTypeInfo(unsigned int, unsigned long, ITypeInfo **);
     HRESULT STDMETHODCALLTYPE GetIDsOfNames(const _GUID &, wchar_t **, unsigned int, unsigned long, long *);
     HRESULT STDMETHODCALLTYPE Invoke(long, const _GUID &, unsigned long, unsigned short, tagDISPPARAMS *, tagVARIANT *, tagEXCEPINFO *, unsigned int *);
 
+    /* IAccessible */
     HRESULT STDMETHODCALLTYPE accHitTest(long xLeft, long yTop, VARIANT *pvarID);
     HRESULT STDMETHODCALLTYPE accLocation(long *pxLeft, long *pyTop, long *pcxWidth, long *pcyHeight, VARIANT varID);
     HRESULT STDMETHODCALLTYPE accNavigate(long navDir, VARIANT varStart, VARIANT *pvarEnd);
@@ -451,6 +529,7 @@ public:
     HRESULT STDMETHODCALLTYPE get_accFocus(VARIANT *pvarID);
     HRESULT STDMETHODCALLTYPE get_accSelection(VARIANT *pvarChildren);
 
+    /* IOleWindow */
     HRESULT STDMETHODCALLTYPE GetWindow(HWND *phwnd);
     HRESULT STDMETHODCALLTYPE ContextSensitiveHelp(BOOL fEnterMode);
 
@@ -896,9 +975,30 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accChild(VARIANT varChildID, I
     if (varChildID.vt == VT_EMPTY)
         return E_INVALIDARG;
 
+
+    int childIndex = varChildID.lVal;
     QAccessibleInterface *acc = 0;
-    RelationFlag rel = varChildID.lVal ? Child : Self;
-    accessible->navigate(rel, varChildID.lVal, &acc);
+
+    if (childIndex < 0) {
+        const int entry = childIndex;
+        QPair<QObject*, int> ref = qAccessibleRecentSentEvents()->value(entry);
+        if (ref.first) {
+            acc = queryAccessibleInterface(ref.first);
+            if (acc && ref.second) {
+                if (ref.second) {
+                    QAccessibleInterface *res;
+                    int index = acc->navigate(Child, ref.second, &res);
+                    delete acc;
+                    if (index == -1)
+                        return E_INVALIDARG;
+                    acc = res;
+                }
+            }
+        }
+    } else {
+        RelationFlag rel = childIndex ? Child : Self;
+        accessible->navigate(rel, childIndex, &acc);
+    }
 
     if (acc) {
         QWindowsAccessible* wacc = new QWindowsAccessible(acc);
@@ -1203,7 +1303,7 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::GetWindow(HWND *phwnd)
     if (!o || !o->isWidgetType())
         return E_FAIL;
 
-    *phwnd = static_cast<QWidget*>(o)->winId();
+    *phwnd = static_cast<QWidget*>(o)->effectiveWinId();
     return S_OK;
 }
 

commit 6f84e64f3500db9901bb518c8c5bd2ca03fbbf42

Fix a crash in a11y introduced by d289e54f2d2aa066cb3
The problem was that the patch assumed that all AT clients only called
get_accChild() after NotifyWinEvent was called with a negative child id
argument. However, some AT clients might call get_accRole() and
get_accState() with that negative value. Implementations of
QAccessibleInterface::role() should be able to make the assumption that
the child index is always valid.

The safe solution is to always look for the item in the cache if the
child index is negative for all IAccessible functions we implement.
If its positive or zero, execute as before.

Crash could be reproduced by:
1. Start MS narrator
2. launch examples\dialogs\tabdialog
3. Click the tab labeled "Permissions"
   *crash* assertion in QList, index out of range.

Reviewed-by: Frederik Gladhorn

src/gui/accessible/qaccessible_win.cpp
@@ -600,6 +600,35 @@ HRESULT STDMETHODCALLTYPE QWindowsEnumerate::Skip(unsigned long celt)
     return S_OK;
 }
 
+struct AccessibleElement {
+    AccessibleElement(int entryId, QAccessibleInterface *accessible) {
+        if (entryId < 0) {
+            QPair<QObject*, int> ref = qAccessibleRecentSentEvents()->value(entryId);
+            iface = QAccessible::queryAccessibleInterface(ref.first);
+            entry = ref.second;
+            cleanupInterface = true;
+        } else {
+            iface = accessible;
+            entry = entryId;
+            cleanupInterface = false;
+        }
+    }
+
+    QString text(QAccessible::Text t) const {
+        return iface ? iface->text(t, entry) : QString();
+    }
+
+    ~AccessibleElement() {
+        if (cleanupInterface)
+            delete iface;
+    }
+
+    QAccessibleInterface *iface;
+    int entry;
+private:
+    bool cleanupInterface;
+};
+
 /*
 */
 class QWindowsAccessible : public IAccessible, IOleWindow, QAccessible
@@ -998,7 +1027,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::accLocation(long *pxLeft, long *py
     if (!accessible->isValid())
         return E_FAIL;
 
-    QRect rect = accessible->rect(varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    QRect rect = elem.iface ? elem.iface->rect(elem.entry) : QRect();
     if (rect.isValid()) {
         *pxLeft = rect.x();
         *pyTop = rect.y();
@@ -1101,25 +1131,12 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accChild(VARIANT varChildID, I
     int childIndex = varChildID.lVal;
     QAccessibleInterface *acc = 0;
 
-    if (childIndex < 0) {
-        const int entry = childIndex;
-        QPair<QObject*, int> ref = qAccessibleRecentSentEvents()->value(entry);
-        if (ref.first) {
-            acc = queryAccessibleInterface(ref.first);
-            if (acc && ref.second) {
-                if (ref.second) {
-                    QAccessibleInterface *res;
-                    int index = acc->navigate(Child, ref.second, &res);
-                    delete acc;
-                    if (index == -1)
-                        return E_INVALIDARG;
-                    acc = res;
-                }
-            }
-        }
-    } else {
-        RelationFlag rel = childIndex ? Child : Self;
-        accessible->navigate(rel, childIndex, &acc);
+    AccessibleElement elem(childIndex, accessible);
+    if (elem.iface) {
+        RelationFlag rel = elem.entry ? Child : Self;
+        int index = elem.iface->navigate(rel, elem.entry, &acc);
+        if (index == -1)
+            return E_INVALIDARG;
     }
 
     if (acc) {
@@ -1171,7 +1188,9 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::accDoDefaultAction(VARIANT varID)
     if (!accessible->isValid())
         return E_FAIL;
 
-    return accessible->doAction(DefaultAction, varID.lVal, QVariantList()) ? S_OK : S_FALSE;
+    AccessibleElement elem(varID.lVal, accessible);
+    const bool res = elem.iface ? elem.iface->doAction(DefaultAction, elem.entry, QVariantList()) : false;
+    return res ? S_OK : S_FALSE;
 }
 
 HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accDefaultAction(VARIANT varID, BSTR* pszDefaultAction)
@@ -1180,7 +1199,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accDefaultAction(VARIANT varID
     if (!accessible->isValid())
         return E_FAIL;
 
-    QString def = accessible->actionText(DefaultAction, Name, varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    QString def = elem.iface ? elem.iface->actionText(DefaultAction, Name, elem.entry) : QString();
     if (def.isEmpty()) {
         *pszDefaultAction = 0;
         return S_FALSE;
@@ -1196,7 +1216,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accDescription(VARIANT varID, 
     if (!accessible->isValid())
         return E_FAIL;
 
-    QString descr = accessible->text(Description, varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    QString descr = elem.text(Description);
     if (descr.size()) {
         *pszDescription = QStringToBSTR(descr);
         return S_OK;
@@ -1212,7 +1233,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accHelp(VARIANT varID, BSTR *p
     if (!accessible->isValid())
         return E_FAIL;
 
-    QString help = accessible->text(Help, varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    QString help = elem.text(Help);
     if (help.size()) {
         *pszHelp = QStringToBSTR(help);
         return S_OK;
@@ -1233,7 +1255,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accKeyboardShortcut(VARIANT va
     if (!accessible->isValid())
         return E_FAIL;
 
-    QString sc = accessible->text(Accelerator, varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    QString sc = elem.text(Accelerator);
     if (sc.size()) {
         *pszKeyboardShortcut = QStringToBSTR(sc);
         return S_OK;
@@ -1249,7 +1272,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accName(VARIANT varID, BSTR* p
     if (!accessible->isValid())
         return E_FAIL;
 
-    QString n = accessible->text(Name, varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    QString n = elem.text(Name);
     if (n.size()) {
         *pszName = QStringToBSTR(n);
         return S_OK;
@@ -1271,7 +1295,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accRole(VARIANT varID, VARIANT
     if (!accessible->isValid())
         return E_FAIL;
 
-    Role role = accessible->role(varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    Role role = elem.iface ? elem.iface->role(elem.entry) : NoRole;
     if (role != NoRole) {
         if (role == LayeredPane)
             role = QAccessible::Pane;
@@ -1290,7 +1315,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accState(VARIANT varID, VARIAN
         return E_FAIL;
 
     (*pvarState).vt = VT_I4;
-    (*pvarState).lVal = accessible->state(varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    (*pvarState).lVal = elem.iface ? elem.iface->state(elem.entry) : 0;
     return S_OK;
 }
 
@@ -1300,7 +1326,8 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::get_accValue(VARIANT varID, BSTR* 
     if (!accessible->isValid())
         return E_FAIL;
 
-    QString value = accessible->text(Value, varID.lVal);
+    AccessibleElement elem(varID.lVal, accessible);
+    QString value = elem.text(Value);
     if (!value.isNull()) {
         *pszValue = QStringToBSTR(value);
         return S_OK;
@@ -1324,19 +1351,23 @@ HRESULT STDMETHODCALLTYPE QWindowsAccessible::accSelect(long flagsSelect, VARIAN
 
     bool res = false;
 
-    if (flagsSelect & SELFLAG_TAKEFOCUS)
-        res = accessible->doAction(SetFocus, varID.lVal, QVariantList());
-    if (flagsSelect & SELFLAG_TAKESELECTION) {
-        accessible->doAction(ClearSelection, 0, QVariantList());
-        res = accessible->doAction(AddToSelection, varID.lVal, QVariantList());
+    AccessibleElement elem(varID.lVal, accessible);
+    QAccessibleInterface *acc = elem.iface;
+    if (acc) {
+        const int entry = elem.entry;
+        if (flagsSelect & SELFLAG_TAKEFOCUS)
+            res = acc->doAction(SetFocus, entry, QVariantList());
+        if (flagsSelect & SELFLAG_TAKESELECTION) {
+            acc->doAction(ClearSelection, 0, QVariantList());   //### bug, 0 should be entry??
+            res = acc->doAction(AddToSelection, entry, QVariantList());
+        }
+        if (flagsSelect & SELFLAG_EXTENDSELECTION)
+            res = acc->doAction(ExtendSelection, entry, QVariantList());
+        if (flagsSelect & SELFLAG_ADDSELECTION)
+            res = acc->doAction(AddToSelection, entry, QVariantList());
+        if (flagsSelect & SELFLAG_REMOVESELECTION)
+            res = acc->doAction(RemoveSelection, entry, QVariantList());
     }
-    if (flagsSelect & SELFLAG_EXTENDSELECTION)
-        res = accessible->doAction(ExtendSelection, varID.lVal, QVariantList());
-    if (flagsSelect & SELFLAG_ADDSELECTION)
-        res = accessible->doAction(AddToSelection, varID.lVal, QVariantList());
-    if (flagsSelect & SELFLAG_REMOVESELECTION)
-        res = accessible->doAction(RemoveSelection, varID.lVal, QVariantList());
-
     return res ? S_OK : S_FALSE;
 }
 