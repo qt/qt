98f0e52547f6f840e386740dc6c2d99452965266
João Abecasis <joao.abecasis@nokia.com>
2011-08-17 14:38:27 +0200

    Cleanup code: removing empty stubs
    Reviewed-by: Shane Kearns

--- tests/auto/qtemporaryfile/tst_qtemporaryfile.cpp
+++ tests/auto/qtemporaryfile/tst_qtemporaryfile.cpp
@@ -69,13 +69,7 @@
 class tst_QTemporaryFile : public QObject
 {
     Q_OBJECT
-public:
-    tst_QTemporaryFile();
-    virtual ~tst_QTemporaryFile();
 public slots:
-    void init();
-    void cleanup();
-
     void initTestCase();
     void cleanupTestCase();
 
@@ -103,8 +97,6 @@ private slots:
 
     void QTBUG_4796_data();
     void QTBUG_4796();
-
-public:
 };
 
 void tst_QTemporaryFile::initTestCase()
@@ -137,27 +129,6 @@ void tst_QTemporaryFile::getSetCheck()
     QCOMPARE(false, obj1.autoRemove());
     obj1.setAutoRemove(true);
     QCOMPARE(true, obj1.autoRemove());
-}
-
-tst_QTemporaryFile::tst_QTemporaryFile()
-{
-}
-
-tst_QTemporaryFile::~tst_QTemporaryFile()
-{
-
-}
-
-void tst_QTemporaryFile::init()
-{
-// TODO: Add initialization code here.
-// This will be executed immediately before each test is run.
-}
-
-void tst_QTemporaryFile::cleanup()
-{
-// TODO: Add cleanup code here.
-// This will be executed immediately after each test is run.
 }
 
 void tst_QTemporaryFile::fileTemplate_data()
