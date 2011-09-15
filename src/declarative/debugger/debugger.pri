qt/qtdeclarative

Debugger: Move QT_DECLARATIVE_DEBUG handling out of qdeclarative.h
Apps don't have to (directly or indirectly) include qdeclarative.h.
Instead, move the static instance to qdeclarativeengine.h, and
qdeclarativeview.h, qsgview.h (which instantiate their own engine).

Change-Id: I8b3e63ad4f134969734a2cc712395145d90e0dfa
Reviewed-on: http://codereview.qt.nokia.com/3941
Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
Reviewed-by: Aurindam Jana <aurindam.jana@nokia.com>
Reviewed-by: Martin Jones <martin.jones@nokia.com>

src/declarative/debugger/debugger.pri
@@ -27,4 +27,5 @@ HEADERS += \
     $$PWD/qdeclarativeinspectorservice_p.h \
     $$PWD/qdeclarativeinspectorinterface_p.h \
     $$PWD/qv8debugservice_p.h \
-    $$PWD/qdeclarativeenginedebugservice_p.h
+    $$PWD/qdeclarativeenginedebugservice_p.h \
+    $$PWD/qdeclarativedebug.h

qt/qt

qdeclarativeview.h (which instantiates it's own engine).
(cherrypicked from commit 6cb39fb829b78b5f6e9751283c7cd50400821e2a)

src/declarative/debugger/debugger.pri
@@ -27,4 +27,5 @@ HEADERS += \
     $$PWD/qdeclarativeinspectorservice_p.h \
     $$PWD/qdeclarativeinspectorinterface_p.h \
     $$PWD/qjsdebuggeragent_p.h \
-    $$PWD/qjsdebugservice_p.h
+    $$PWD/qjsdebugservice_p.h \
+    $$PWD/qdeclarativedebug.h