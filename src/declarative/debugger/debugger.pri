qt/qtdeclarative

Rename QDeclarativeEngineDebugServer to ~Service
And fix the file names/location, too

Change-Id: If2d5ec0896332896ad11af748ec8f75c39e1555c
Reviewed-on: http://codereview.qt.nokia.com/3890
Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
Reviewed-by: Kai Koehne <kai.koehne@nokia.com>

src/declarative/debugger/debugger.pri
@@ -10,7 +10,8 @@ SOURCES += \
     $$PWD/qdeclarativedebughelper.cpp \
     $$PWD/qdeclarativedebugserver.cpp \
     $$PWD/qdeclarativeinspectorservice.cpp \
-    $$PWD/qv8debugservice.cpp
+    $$PWD/qv8debugservice.cpp \
+    $$PWD/qdeclarativeenginedebugservice.cpp
 
 HEADERS += \
     $$PWD/qdeclarativedebuggerstatus_p.h \
@@ -25,4 +26,5 @@ HEADERS += \
     $$PWD/qdeclarativedebugserverconnection_p.h \
     $$PWD/qdeclarativeinspectorservice_p.h \
     $$PWD/qdeclarativeinspectorinterface_p.h \
-    $$PWD/qv8debugservice_p.h
+    $$PWD/qv8debugservice_p.h \
+    $$PWD/qdeclarativeenginedebugservice_p.h

qt/qt

(cherrypicked from 8804ec49bda8672c5700ab843f2958c3d2bd8e41)

src/declarative/debugger/debugger.pri
@@ -11,7 +11,8 @@ SOURCES += \
     $$PWD/qdeclarativedebugserver.cpp \
     $$PWD/qdeclarativeinspectorservice.cpp \
     $$PWD/qjsdebuggeragent.cpp \
-    $$PWD/qjsdebugservice.cpp
+    $$PWD/qjsdebugservice.cpp \
+    $$PWD/qdeclarativeenginedebugservice.cpp
 
 HEADERS += \
     $$PWD/qdeclarativedebuggerstatus_p.h \
@@ -28,4 +29,5 @@ HEADERS += \
     $$PWD/qdeclarativeinspectorinterface_p.h \
     $$PWD/qjsdebuggeragent_p.h \
     $$PWD/qjsdebugservice_p.h \
-    $$PWD/qdeclarativedebug.h
+    $$PWD/qdeclarativedebug.h \
+    $$PWD/qdeclarativeenginedebugservice_p.h