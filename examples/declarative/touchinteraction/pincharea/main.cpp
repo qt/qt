7a98c9cca6dade7a2074d9dd19a44c8c23bd412b
artoka <arto.katajasalo@digia.com>
2011-11-17 15:50:02 +0100

    Touchintegration Examples Modifications
    touchintegration/ gestures, mousearea and pincharea example
    modifications
    
    Modified examples to use centralized qmlapplicationviewer,
    removed duplicate files and modified project files according
    to the changes.
    
    Merge-request: 2719
    Reviewed-by: Casper van Donderen <casper.vandonderen@nokia.com>

--- examples/declarative/touchinteraction/pincharea/main.cpp
+++ examples/declarative/touchinteraction/pincharea/main.cpp
@@ -38,39 +38,17 @@
 **
 ****************************************************************************/
 
-import QtQuick 1.0
-import Qt.labs.gestures 1.0
+#include "qmlapplicationviewer.h"
+#include <QtGui/QApplication>
 
-// Only works on platforms with Touch support.
+Q_DECL_EXPORT int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
 
-Rectangle {
-    id: rect
-    width: 320
-    height: 180
+    QmlApplicationViewer viewer;
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+    viewer.setMainQmlFile(QLatin1String("qml/pincharea/flickresize.qml"));
+    viewer.showExpanded();
 
-    Text {
-        anchors.centerIn: parent
-        text: "Tap / TapAndHold / Pan / Pinch / Swipe\nOnly works on platforms with Touch support."
-        horizontalAlignment: Text.Center
-    }
-
-    GestureArea {
-        anchors.fill: parent
-        focus: true
-
-        // Only some of the many gesture properties are shown. See Gesture documentation.
-
-        onTap:
-            console.log("tap pos = (",gesture.position.x,",",gesture.position.y,")")
-        onTapAndHold:
-            console.log("tap and hold pos = (",gesture.position.x,",",gesture.position.y,")")
-        onPan:
-            console.log("pan delta = (",gesture.delta.x,",",gesture.delta.y,") acceleration = ",gesture.acceleration)
-        onPinch:
-            console.log("pinch center = (",gesture.centerPoint.x,",",gesture.centerPoint.y,") rotation =",gesture.rotationAngle," scale =",gesture.scaleFactor)
-        onSwipe:
-            console.log("swipe angle=",gesture.swipeAngle)
-        onGesture:
-            console.log("gesture hot spot = (",gesture.hotSpot.x,",",gesture.hotSpot.y,")")
-    }
+    return app.exec();
 }
