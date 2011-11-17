608c4766a8d1a30c773c8b0f0701c8e746215c80
Pierre Rossi <pierre.rossi@nokia.com>
2011-11-17 16:15:16 +0100

    First batch of French translations updates

--- translations/qt_help_fr.ts
+++ translations/qt_help_fr.ts
@@ -4,27 +4,22 @@
 <context>
     <name>QCLuceneResultWidget</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpsearchresultwidget.cpp" line="+110"/>
         <source>Search Results</source>
         <translation>Résultats de la recherche</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>Note:</source>
         <translation>Note :</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>The search results may not be complete since the documentation is still being indexed!</source>
         <translation>Les résultats de la recherche risquent d&apos;être incomplets car l&apos;indexation de la documentation est en cours !</translation>
     </message>
     <message>
-        <location line="+11"/>
         <source>Your search did not match any documents.</source>
         <translation>Votre recherche ne correspond à aucun document.</translation>
     </message>
     <message>
-        <location line="+4"/>
         <source>(The reason for this might be that the documentation is still being indexed.)</source>
         <translation>(Il est possible que cela soit dû au fait que la documentation est en cours d&apos;indexation.)</translation>
     </message>
@@ -32,7 +27,6 @@
 <context>
     <name>QHelp</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelp_global.cpp" line="+64"/>
         <source>Untitled</source>
         <translation>Sans titre</translation>
     </message>
@@ -40,80 +34,64 @@
 <context>
     <name>QHelpCollectionHandler</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpcollectionhandler.cpp" line="+79"/>
         <source>The collection file &apos;%1&apos; is not set up yet!</source>
         <translation>Le fichier de collection &apos;%1&apos; n&apos;est pas encore chargé !</translation>
     </message>
     <message>
-        <location line="+23"/>
         <source>Cannot load sqlite database driver!</source>
         <translatorcomment>driver ?</translatorcomment>
         <translation>Chargement du pilote de base de données sqlite impossible !</translation>
     </message>
     <message>
-        <location line="+11"/>
-        <location line="+52"/>
         <source>Cannot open collection file: %1</source>
         <translatorcomment>collection ?</translatorcomment>
         <translation>Impossible d&apos;ouvrir le fichier collection : %1</translation>
     </message>
     <message>
-        <location line="-40"/>
         <source>Cannot create tables in file %1!</source>
         <translation>Impossible de créer les tables dans le fichier : %1 !</translation>
     </message>
     <message>
-        <location line="+16"/>
         <source>The collection file &apos;%1&apos; already exists!</source>
         <translation>Le fichier collection &apos;%1&apos; existe déjà !</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Cannot create directory: %1</source>
         <translation>Impossible de créer le répertoire : %1</translation>
     </message>
     <message>
-        <location line="+26"/>
         <source>Cannot copy collection file: %1</source>
         <translation>Impossible de copier le fichier collection : %1</translation>
     </message>
     <message>
-        <location line="+119"/>
         <source>Unknown filter &apos;%1&apos;!</source>
         <translation>Filtre &apos;%1&apos; inconnu !</translation>
     </message>
     <message>
-        <location line="+53"/>
         <source>Cannot register filter %1!</source>
         <translation>Impossible d&apos;enregistrer le filtre %1 !</translation>
     </message>
     <message>
-        <location line="+44"/>
         <source>Cannot open documentation file %1!</source>
         <translation>Impossible d&apos;ouvrir le fichier de documentation %1 !</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Invalid documentation file &apos;%1&apos;!</source>
         <translation>Fichier de documentation invalide : &apos;%1&apos; !</translation>
     </message>
     <message>
-        <location line="+34"/>
         <source>The namespace %1 was not registered!</source>
         <translation>L&apos;espace de noms &apos;%1&apos; n&apos;était pas référencé !</translation>
     </message>
     <message>
-        <location line="+120"/>
         <source>Namespace %1 already exists!</source>
         <translation>L&apos;espace de noms %1 existe déjà !</translation>
     </message>
     <message>
-        <location line="+13"/>
         <source>Cannot register namespace &apos;%1&apos;!</source>
         <translation>Impossible d&apos;enregistrer l&apos;espace de noms &apos;%1&apos; !</translation>
     </message>
     <message>
-        <location line="+24"/>
         <source>Cannot open database &apos;%1&apos; to optimize!</source>
         <translation>Impossible d&apos;ouvrir la base de données à optimiser &apos;%1&apos; !</translation>
     </message>
@@ -121,7 +99,6 @@
 <context>
     <name>QHelpDBReader</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpdbreader.cpp" line="+98"/>
         <source>Cannot open database &apos;%1&apos; &apos;%2&apos;: %3</source>
         <extracomment>The placeholders are: %1 - The name of the database which cannot be opened %2 - The unique id for the connection %3 - The actual error string</extracomment>
         <translation>Impossible d&apos;ouvrir la base de données &apos;%1&apos; &apos;%2&apos; : %3</translation>
@@ -130,12 +107,10 @@
 <context>
     <name>QHelpEngineCore</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpenginecore.cpp" line="+122"/>
         <source>Cannot open documentation file %1: %2!</source>
         <translation>Impossible d&apos;ouvrir le fichier de documentation %1 : %2 !</translation>
     </message>
     <message>
-        <location line="+404"/>
         <source>The specified namespace does not exist!</source>
         <translation>L&apos;espace de noms spécifié n&apos;existe pas !</translation>
     </message>
@@ -150,133 +125,107 @@
 <context>
     <name>QHelpGenerator</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpgenerator.cpp" line="+158"/>
         <source>Invalid help data!</source>
         <translation>Données d&apos;aide invalides !</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>No output file name specified!</source>
         <translation>Aucun nom de fichier de sortie spécifié !</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>The file %1 cannot be overwritten!</source>
         <translation>Le fichier %1 ne peut être écrasé !</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>Building up file structure...</source>
         <translation>Construction de la structure de fichiers en cours...</translation>
     </message>
     <message>
-        <location line="+11"/>
         <source>Cannot open data base file %1!</source>
         <translation>Impossible d&apos;ouvrir le fichier de base de données %1 !</translation>
     </message>
     <message>
-        <location line="+14"/>
         <source>Cannot register namespace %1!</source>
         <translation>Impossible d&apos;enregistrer l&apos;espace de noms %1 !</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Insert custom filters...</source>
         <translation>Insértion des filtres personnalisés...</translation>
     </message>
     <message>
-        <location line="+12"/>
         <source>Insert help data for filter section (%1 of %2)...</source>
         <translatorcomment>???</translatorcomment>
         <translation>Insertion des données d&apos;aide pour la section filtre (%1 de %2)...</translation>
     </message>
     <message>
-        <location line="+18"/>
         <source>Documentation successfully generated.</source>
         <translation>Documentation générée avec succès.</translation>
     </message>
     <message>
-        <location line="+76"/>
         <source>Some tables already exist!</source>
         <translation>Certaines tables existent déjà !</translation>
     </message>
     <message>
-        <location line="+61"/>
         <source>Cannot create tables!</source>
         <translation>Impossible de créer les tables !</translation>
     </message>
     <message>
-        <location line="+86"/>
         <source>Cannot register virtual folder!</source>
         <translation>Impossible d&apos;enregistrer le dossier virtuel !</translation>
     </message>
     <message>
-        <location line="+10"/>
         <source>Insert files...</source>
         <translation>Insertion des fichiers...</translation>
     </message>
     <message>
-        <location line="+42"/>
         <source>The referenced file %1 must be inside or within a subdirectory of (%2). Skipping it.</source>
         <translation>Le fichier référencé %1 doit être dans le dossier (%2) ou un de ses sous-dossiers. Fichier non pris en compte.</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>The file %1 does not exist! Skipping it.</source>
         <translation>Le fichier %1 n&apos;existe pas ! Fichier non pris en compte.</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Cannot open file %1! Skipping it.</source>
         <translation>Impossible d&apos;ouvrir le fichier %1 ! Fichier non pris en compte.</translation>
     </message>
     <message>
-        <location line="+133"/>
         <source>The filter %1 is already registered!</source>
         <translation>Le filtre %1 est déjà enregistré !</translation>
     </message>
     <message>
-        <location line="+5"/>
         <source>Cannot register filter %1!</source>
         <translation>Impossible d&apos;enregistrer le filtre %1 !</translation>
     </message>
     <message>
-        <location line="+24"/>
         <source>Insert indices...</source>
         <translation>Insertion des index...</translation>
     </message>
     <message>
-        <location line="+91"/>
         <source>Insert contents...</source>
         <translation>insertion du contenu...</translation>
     </message>
     <message>
-        <location line="+8"/>
         <source>Cannot insert contents!</source>
         <translation>Impossible d&apos;insérer le contenu !</translation>
     </message>
     <message>
-        <location line="+12"/>
         <source>Cannot register contents!</source>
         <translation>Impossible de référencer le contenu !</translation>
     </message>
     <message>
-        <location line="+56"/>
         <source>File &apos;%1&apos; does not exist.</source>
         <translation>Le fichier &apos;%1&apos; n&apos;existe pas.</translation>
     </message>
     <message>
-        <location line="+19"/>
         <source>File &apos;%1&apos; cannot be opened.</source>
         <translation>Le fichier &apos;%1&apos; ne peut être ouvert.</translation>
     </message>
     <message>
-        <location line="+19"/>
         <source>File &apos;%1&apos; contains an invalid link to file &apos;%2&apos;</source>
         <translation>Le fichier &apos;%1&apos; contient un lien invalide vers le fichier &apos;%2&apos;</translation>
     </message>
     <message>
-        <location line="+9"/>
         <source>Invalid links in HTML files.</source>
         <translation>Liens invalides dans les fichiers HTML.</translation>
     </message>
@@ -284,47 +233,38 @@
 <context>
     <name>QHelpProject</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpprojectdata.cpp" line="+88"/>
         <source>Unknown token.</source>
         <translation>Identificateur inconnu.</translation>
     </message>
     <message>
-        <location line="+13"/>
         <source>Unknown token. Expected &quot;QtHelpProject&quot;!</source>
         <translation>Identificateur inconnu. &quot;QtHelpProject&quot; attendu !</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Error in line %1: %2</source>
         <translation>Erreur à la ligne %1 : %2</translation>
     </message>
     <message>
-        <location line="+14"/>
         <source>Virtual folder has invalid syntax.</source>
         <translation>Syntaxe invalide pour le dossier virtuel.</translation>
     </message>
     <message>
-        <location line="+5"/>
         <source>Namespace has invalid syntax.</source>
         <translation>Syntaxe invalide pour l&apos;espace de noms.</translation>
     </message>
     <message>
-        <location line="+19"/>
         <source>Missing namespace in QtHelpProject.</source>
         <translation>Espace de noms manquant dans QtHelpProject.</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Missing virtual folder in QtHelpProject</source>
         <translation>Dossier virtuel manquant dans QtHelpProject</translation>
     </message>
     <message>
-        <location line="+89"/>
         <source>Missing attribute in keyword at line %1.</source>
         <translation>Attribut manquant pour le mot clé à la ligne %1.</translation>
     </message>
     <message>
-        <location line="+143"/>
         <source>The input file %1 could not be opened!</source>
         <translation>Le fichier source %1 n&apos;a pas pu être ouvert !</translation>
     </message>
@@ -332,52 +272,42 @@
 <context>
     <name>QHelpSearchQueryWidget</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpsearchquerywidget.cpp" line="+124"/>
         <source>Search for:</source>
         <translation>Rechercher :</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>Previous search</source>
         <translation>Recherche précédente</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>Next search</source>
         <translation>Recherche suivante</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>Search</source>
         <translation>Recherche</translation>
     </message>
     <message>
-        <location line="+2"/>
         <source>Advanced search</source>
         <translation>Recherche avancée</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>words &lt;B&gt;similar&lt;/B&gt; to:</source>
         <translation>mots &lt;B&gt;semblables&lt;/B&gt; à :</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>&lt;B&gt;without&lt;/B&gt; the words:</source>
         <translation>&lt;B&gt;sans&lt;/B&gt; les mots :</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>with &lt;B&gt;exact phrase&lt;/B&gt;:</source>
         <translation>avec la &lt;B&gt;phrase exacte&lt;/B&gt; :</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>with &lt;B&gt;all&lt;/B&gt; of the words:</source>
         <translation>avec &lt;B&gt;tous&lt;/B&gt; les mots :</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>with &lt;B&gt;at least one&lt;/B&gt; of the words:</source>
         <translation>avec &lt;B&gt;au moins un&lt;/B&gt; des mots :</translation>
     </message>
@@ -385,7 +315,6 @@
 <context>
     <name>QHelpSearchResultWidget</name>
     <message numerus="yes">
-        <location filename="../tools/assistant/lib/qhelpsearchresultwidget.cpp" line="+174"/>
         <source>%1 - %2 of %n Hits</source>
         <translation>
             <numerusform>%1 - %2 de %n résultat</numerusform>
@@ -393,7 +322,6 @@
         </translation>
     </message>
     <message>
-        <location line="+61"/>
         <source>0 - 0 of 0 Hits</source>
         <translation>0 - 0 de 0 résultats</translation>
     </message>
