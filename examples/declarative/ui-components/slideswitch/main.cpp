d405e49af2266debf618f0b6aaacf9dce103026f
artoka <arto.katajasalo@digia.com>
2011-11-17 15:50:08 +0100

    ui-components/slideswitch example modifications
    Modified the example to use centralized qmlapplicationviewer,
    removed duplicate files and modified project files according
    to the changes.
    
    Merge-request: 2719
    Reviewed-by: Casper van Donderen <casper.vandonderen@nokia.com>

--- examples/declarative/ui-components/slideswitch/main.cpp
+++ examples/declarative/ui-components/slideswitch/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationLockLandscape);
-    viewer.setMainQmlFile(QLatin1String("qml/qml/slideswitch.qml"));
+    viewer.setMainQmlFile(QLatin1String("qml/slideswitch/slideswitch.qml"));
     viewer.showExpanded();
 
     return app.exec();
