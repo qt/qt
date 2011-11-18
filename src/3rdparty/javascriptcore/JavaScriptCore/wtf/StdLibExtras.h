9912af2108398b8633f91300c455455f6e16e482
Janne Anttila <janne.anttila@digia.com>
2011-11-18 11:44:46 +0200

    WEC7 build fix - use ptrdiff_t from std namespace.
    WEC7 does not have ptrdiff_t outside std namespace, switched to use
    ptrdiff_t from std namespace correspondingly as OwnArrayPtr.h does in
    operator[].
    
    Task-number: QTBUG-22502
    Change-Id: I5c2ffd271cccd12c1fcdca3975ace3e8224e00bf
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@digia.com>
    Reviewed-by: Kent Hansen <kent.hansen@nokia.com>

--- src/3rdparty/javascriptcore/JavaScriptCore/wtf/StdLibExtras.h
+++ src/3rdparty/javascriptcore/JavaScriptCore/wtf/StdLibExtras.h
@@ -46,7 +46,7 @@
 // OBJECT_OFFSETOF: Like the C++ offsetof macro, but you can use it with classes.
 // The magic number 0x4000 is insignificant. We use it to avoid using NULL, since
 // NULL can cause compiler problems, especially in cases of multiple inheritance.
-#define OBJECT_OFFSETOF(class, field) (reinterpret_cast<ptrdiff_t>(&(reinterpret_cast<class*>(0x4000)->field)) - 0x4000)
+#define OBJECT_OFFSETOF(class, field) (reinterpret_cast<std::ptrdiff_t>(&(reinterpret_cast<class*>(0x4000)->field)) - 0x4000)
 
 // STRINGIZE: Can convert any value to quoted string, even expandable macros
 #define STRINGIZE(exp) #exp
