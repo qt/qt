qt/qt

commit ab0a2466d7e3998caad226197355b729f523764a

Cocoa: non-native toolbar shows in window when hidden
The reason is that we explicitly needs to tell it to hide which we do
in the init function. But since QToolBar overrides setVisible, this
fails. This patch makes sure we call the super version of the
function

src/gui/widgets/qmenubar.cpp
@@ -1073,8 +1073,11 @@ void QMenuBar::paintEvent(QPaintEvent *e)
 void QMenuBar::setVisible(bool visible)
 {
 #if defined(Q_WS_MAC) || defined(Q_OS_WINCE) || defined(Q_WS_S60)
-    if (isNativeMenuBar())
+    if (isNativeMenuBar()) {
+        if (!visible)
+            QWidget::setVisible(false);
         return;
+    }
 #endif
     QWidget::setVisible(visible);
 }

commit ba3f5ec3884aafb6c9761ef8719b82d4345245fa

Symbian: fix tst_QMenuBar::task256322_highlight() regression
This fixes Symbian-only regression caused by
ab0a2466d7e3998caad226197355b729f523764a.

Reviewed-by: Miikka Heikkinen

src/gui/widgets/qmenubar.cpp
@@ -1111,8 +1111,10 @@ void QMenuBar::setVisible(bool visible)
 #else
 #if defined(Q_WS_MAC) || defined(Q_OS_WINCE) || defined(Q_WS_S60)
     if (isNativeMenuBar()) {
+#ifndef Q_WS_S60
         if (!visible)
             QWidget::setVisible(false);
+#endif        
         return;
     }
 #endif