fe3f0a5003135cdeb7c8fc478fd63ec026a51963
Andreas Hartmetz <andreas.hartmetz@kdab.com>
2012-01-31 17:32:54 +0100

    Escape path names in qmake-generated Makefiles.
    qmake-generated Makefiles since 4.6 (according to bug report) contain
    recursive qmake invocations with absolute paths to .pro files, where the
    absolute paths are not escaped.
    
    This is a backport of ea2c9f764fed654a57972223449f41e208e05863
    from qtbase master.
    
    Task-number: QTBUG-11776
    Change-Id: If926fe4f2ee4c0e72e140c0fcc376e5a0fd20f21
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>

--- qmake/generators/makefile.cpp
+++ qmake/generators/makefile.cpp
@@ -2476,7 +2476,7 @@ MakefileGenerator::writeSubTargets(QTextStream &t, QList<MakefileGenerator::SubT
         //qmake it
         if(!subtarget->profile.isEmpty()) {
             QString out = subtarget->makefile;
-            QString in = fileFixify(in_directory + subtarget->profile, FileFixifyAbsolute);
+            QString in = escapeFilePath(fileFixify(in_directory + subtarget->profile, FileFixifyAbsolute));
             if(out.startsWith(in_directory))
                 out = out.mid(in_directory.length());
             t << mkfile << ": " << "\n\t";
