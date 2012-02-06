f41135a9f83d17b319ad050dfacc7a8e88598151
David Faure <david.faure@kdab.com>
2012-02-06 14:31:41 +0100

    Make QString::latin1() re-entrant, the global QHash needs a mutex.
    *Different* instances of QString used in different threads would often
    lead to crashes due to the global QHash used by latin1() and ascii().
    
    Basic autotest for latin1() added.
    
    Change-Id: If4fd450deb28f41b1d71f377cacb8815ddeffbee
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- tests/auto/qstring/qstring.pro
+++ tests/auto/qstring/qstring.pro
@@ -9,3 +9,5 @@ DEFINES += QT_NO_CAST_TO_ASCII
 CONFIG += parallel_test
 
 contains(QT_CONFIG,icu):DEFINES += QT_USE_ICU
+
+contains(QT_CONFIG, qt3support): DEFINES += QT3_SUPPORT
--- tests/auto/qstring/tst_qstring.cpp
+++ tests/auto/qstring/tst_qstring.cpp
@@ -152,6 +152,7 @@ private slots:
 //  void indexOf3();
     void sprintf();
     void copy();
+    void latin1();
     void fill();
     void truncate();
     void constructor();
@@ -914,6 +915,24 @@ void tst_QString::copy()
     e = "XXX";
     QCOMPARE(ce,(QString)"String E");
     QCOMPARE(e,(QString)"XXX");
+#endif
+}
+
+void tst_QString::latin1()
+{
+#ifdef QT3_SUPPORT
+    QString e = "String E";
+    const char* lat = e.latin1();
+    QCOMPARE(lat, "String E");
+    e += "F";
+    QCOMPARE(e.latin1(), "String EF");
+
+    QString null;
+    QString empty = "";
+    QCOMPARE(null.latin1(), ""); // was a null pointer in Qt3!
+    QCOMPARE(empty.latin1(), "");
+#else
+    QSKIP("This test requires QT3_SUPPORT", SkipSingle);
 #endif
 }
 
