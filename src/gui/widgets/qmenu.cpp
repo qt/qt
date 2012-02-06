7a24aa513279ea43d53fb52529d4024694b3b7bb
Andy Shaw <andy.shaw@digia.com>
2012-02-06 15:57:22 +0100

    Ensure the scrollers appears when there is too many menu items
    When there are too many items in a single column menu then scrollers
    will appear for the menu to enable scrolling to the other items.
    However, this broke in Qt 4.8 as the scrollers would not appear anymore
    due to the adjustment of the size of the menu to account for the size
    of the desktop.
    
    Task-number: QTBUG-23507
    Change-Id: Ib6bb7a5572bc0fbbf3de6dac03a08b69404804a3
    Reviewed-by: Pierre Rossi <pierre.rossi@nokia.com>

--- src/gui/widgets/qmenu.cpp
+++ src/gui/widgets/qmenu.cpp
@@ -1850,16 +1850,17 @@ void QMenu::popup(const QPoint &p, QAction *atAction)
     screen = d->popupGeometry(QApplication::desktop()->screenNumber(p));
     const int desktopFrame = style()->pixelMetric(QStyle::PM_MenuDesktopFrameWidth, 0, this);
     bool adjustToDesktop = !window()->testAttribute(Qt::WA_DontShowOnScreen);
-
-    // if the screens have very different geometries and the menu is too big, we have to recalculate
-    if (size.height() > screen.height() || size.width() > screen.width()) {
-        size = d->adjustMenuSizeForScreen(screen);
-        adjustToDesktop = true;
-    }
-    // Layout is not right, we might be able to save horizontal space
-    if (d->ncols >1 && size.height() < screen.height()) {
-        size = d->adjustMenuSizeForScreen(screen);
-        adjustToDesktop = true;
+    if (!d->scroll) {
+        // if the screens have very different geometries and the menu is too big, we have to recalculate
+        if (size.height() > screen.height() || size.width() > screen.width()) {
+            size = d->adjustMenuSizeForScreen(screen);
+            adjustToDesktop = true;
+        }
+        // Layout is not right, we might be able to save horizontal space
+        if (d->ncols >1 && size.height() < screen.height()) {
+            size = d->adjustMenuSizeForScreen(screen);
+            adjustToDesktop = true;
+        }
     }
 
 #ifdef QT_KEYPAD_NAVIGATION
