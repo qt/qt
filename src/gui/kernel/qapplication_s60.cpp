fd97c5a83f2baf9186fea39f000492099cba7644
Ari Lehtola <ext-ari.lehtola@nokia.com>
2012-02-07 14:58:10 +0200

    Fix for effects in Qt-applications startup
    This is part of the solution for reducing flickering
    in Qt-applications startup. Fix is effective together
    with changes in platform effects framework.
    
    Task-Number: ou1cimx1#951526
    Change-Id: Ia8d0544ec30741605efd6f7db035bcea2f36e67a
    Reviewed-by: Murray Read <ext-murray.2.read@nokia.com>
    Reviewed-by: Pasi Pentikäinen <ext-pasi.a.pentikainen@nokia.com>

--- src/gui/kernel/qapplication_s60.cpp
+++ src/gui/kernel/qapplication_s60.cpp
@@ -1946,6 +1946,13 @@ void qt_init(QApplicationPrivate * /* priv */, int)
     TSecureId securId = me.SecureId();
     S60->uid = securId.operator TUid();
 
+    // Notify uiaccelerator, that we are qt application. This info is used for
+    // decision making how startup effects are shown.
+    GfxTransEffect::BeginFullScreen(AknTransEffect::ENone,
+        TRect(0, 0, 0, 0),
+        AknTransEffect::EParameterAvkonInternal,
+        AknTransEffect::GfxTransParam(S60->uid, KQtAppExitFlag));
+
     // enable focus events - used to re-enable mouse after focus changed between mouse and non mouse app,
     // and for dimming behind modal windows
     S60->windowGroup().EnableFocusChangeEvents();
