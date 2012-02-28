qt/qt

commit 7c0d15a22266a425c9e9ac0120d6774e120fe01e

Don't rely on mapFromGlobal in QGraphicsScenePrivate::itemsAtPosition.
The old approach discovered a bug in QWidget::mapToGlobal in the context of
lighthouse, which in turn brought our eyes to
QGraphicsScenePrivate::itemsAtPosition. The implementation itself was
correct, but we can easily remove the-not-so-cheap mapToGlobal call
since we already have the scene position, which is all we need.

This functionality is heavily tested by existing auto tests, and all of
them pass.

Reviewed-by: mae

src/gui/graphicsview/qgraphicsscene.cpp
@@ -1074,7 +1074,7 @@ void QGraphicsScenePrivate::enableMouseTrackingOnViews()
 /*!
     Returns all items for the screen position in \a event.
 */
-QList<QGraphicsItem *> QGraphicsScenePrivate::itemsAtPosition(const QPoint &screenPos,
+QList<QGraphicsItem *> QGraphicsScenePrivate::itemsAtPosition(const QPoint &/*screenPos*/,
                                                               const QPointF &scenePos,
                                                               QWidget *widget) const
 {
@@ -1083,16 +1083,12 @@ QList<QGraphicsItem *> QGraphicsScenePrivate::itemsAtPosition(const QPoint &scre
     if (!view)
         return q->items(scenePos, Qt::IntersectsItemShape, Qt::DescendingOrder, QTransform());
 
-    const QRectF pointRect(QPointF(widget->mapFromGlobal(screenPos)), QSizeF(1, 1));
+    const QRectF pointRect(scenePos, QSizeF(1, 1));
     if (!view->isTransformed())
         return q->items(pointRect, Qt::IntersectsItemShape, Qt::DescendingOrder);
 
     const QTransform viewTransform = view->viewportTransform();
-    if (viewTransform.type() <= QTransform::TxScale) {
-        return q->items(viewTransform.inverted().mapRect(pointRect), Qt::IntersectsItemShape,
-                        Qt::DescendingOrder, viewTransform);
-    }
-    return q->items(viewTransform.inverted().map(pointRect), Qt::IntersectsItemShape,
+    return q->items(pointRect, Qt::IntersectsItemShape,
                     Qt::DescendingOrder, viewTransform);
 }
 

commit aaa46755dcc15b8baaa0d1b928828eb60e0babbc

Add additional QGraphicsView tests for regression
Add additional tests for graphics view tooltip regression introduced by
7c0d15a22266a425c9e9ac0120d6774e120fe01e.

Task-number: QTBUG-17517
Task-number: QTBUG-22663
Change-Id: I5e0d0e19504730a3e14ac84712a366dbebe688e6
Reviewed-by: Robin Burchell <robin+qt@viroteck.net>

tests/auto/qgraphicsview/tst_qgraphicsview.cpp
@@ -70,6 +70,7 @@
 #include <QtGui/QPushButton>
 #include <QtGui/QInputContext>
 #include <QtGui/QDesktopWidget>
+#include <private/qgraphicsscene_p.h>
 #include <private/qgraphicsview_p.h>
 #include "../../shared/util.h"
 #include "../platformquirks.h"
@@ -138,6 +139,14 @@ protected:
     QEvent::Type spied;
 };
 
+#if defined QT_BUILD_INTERNAL
+class FriendlyGraphicsScene : public QGraphicsScene
+{
+    friend class tst_QGraphicsView;
+    Q_DECLARE_PRIVATE(QGraphicsScene);
+};
+#endif
+
 class tst_QGraphicsView : public QObject
 {
     Q_OBJECT
@@ -168,6 +177,10 @@ private slots:
     void ensureVisibleRect();
     void fitInView();
     void itemsAtPoint();
+#if defined QT_BUILD_INTERNAL
+    void itemsAtPosition_data();
+    void itemsAtPosition();
+#endif
     void itemsInRect();
     void itemsInRect_cosmeticAdjust_data();
     void itemsInRect_cosmeticAdjust();
@@ -1345,6 +1358,54 @@ void tst_QGraphicsView::itemsAtPoint()
     QCOMPARE(items.takeFirst()->zValue(), qreal(0));
     QCOMPARE(items.takeFirst()->zValue(), qreal(-1));
 }
+
+#if defined QT_BUILD_INTERNAL
+void tst_QGraphicsView::itemsAtPosition_data()
+{
+    QTest::addColumn<float>("rotation");
+    QTest::addColumn<float>("scale");
+    QTest::addColumn<QPoint>("viewPos");
+    QTest::addColumn<bool>("ignoreTransform");
+    QTest::addColumn<bool>("hit");
+    QTest::newRow("scaled + ignore transform, no hit") << 0.0f << 1000.0f << QPoint(0, 0) << true << false;
+    QTest::newRow("scaled + ignore transform, hit") << 0.0f << 1000.0f << QPoint(100, 100) << true << true;
+    QTest::newRow("rotated + scaled, no hit") << 45.0f << 2.0f << QPoint(90, 90) << false << false;
+    QTest::newRow("rotated + scaled, hit") << 45.0f << 2.0f << QPoint(100, 100) << false << true;
+}
+
+void tst_QGraphicsView::itemsAtPosition()
+{
+    QFETCH(float, rotation);
+    QFETCH(float, scale);
+    QFETCH(QPoint, viewPos);
+    QFETCH(bool, ignoreTransform);
+    QFETCH(bool, hit);
+
+    FriendlyGraphicsScene scene;
+    scene.setSceneRect(QRect(-100, -100, 200, 200));
+    QGraphicsItem *item = scene.addRect(-5, -5, 10, 10);
+
+    if (ignoreTransform)
+        item->setFlag(QGraphicsItem::ItemIgnoresTransformations);
+
+    QGraphicsView view;
+    view.resize(200, 200);
+    view.scale(scale, scale);
+    view.rotate(rotation);
+    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.setScene(&scene);
+    view.show();
+    QTest::qWaitForWindowShown(&view);
+
+    QPoint screenPos = view.viewport()->mapToGlobal(viewPos);
+    QPointF scenePos = view.mapToScene(viewPos);
+    QGraphicsScenePrivate *viewPrivate = scene.d_func();
+    QList<QGraphicsItem *> items;
+    items = viewPrivate->itemsAtPosition(screenPos, scenePos, view.viewport());
+    QCOMPARE(!items.empty(), hit);
+}
+#endif
 
 void tst_QGraphicsView::itemsInRect()
 {