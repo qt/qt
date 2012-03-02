qt/qt

commit ac2818bef95f1347e80be8ce847f2557339b76e0

Make it possible to call QWidget::metric() from a different thread.
Under Windows.

src/gui/kernel/qwidget_win.cpp
@@ -1633,8 +1633,6 @@ void QWidgetPrivate::scroll_sys(int dx, int dy, const QRect &r)
     }
 }
 
-extern Q_GUI_EXPORT HDC qt_win_display_dc();
-
 int QWidget::metric(PaintDeviceMetric m) const
 {
     Q_D(const QWidget);
@@ -1644,7 +1642,7 @@ int QWidget::metric(PaintDeviceMetric m) const
     } else if (m == PdmHeight) {
         val = data->crect.height();
     } else {
-        HDC gdc = qt_win_display_dc();
+        HDC gdc = GetDC(0);
         switch (m) {
         case PdmDpiX:
         case PdmPhysicalDpiX:
@@ -1695,6 +1693,7 @@ int QWidget::metric(PaintDeviceMetric m) const
             val = 0;
             qWarning("QWidget::metric: Invalid metric command");
         }
+        ReleaseDC(0, gdc);
     }
     return val;
 }

commit ad1b5495a2a437d21435a9c321c9abf612cd210e

Fixed Windows performance regression introduced by ac2818bef95f134.
Only create a temporary DC if we're not on the main thread.

Task-number: QTBUG-24602
Change-Id: I36dad4c197c7cc1e019b19d42c81526fc8cfdcf4
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>

src/gui/kernel/qwidget_win.cpp
@@ -49,6 +49,7 @@
 #include "qlayout.h"
 #include "qpainter.h"
 #include "qstack.h"
+#include "qthread.h"
 #include "qt_windows.h"
 #include "qwidget.h"
 #include "qwidget_p.h"
@@ -1636,6 +1637,8 @@ void QWidgetPrivate::scroll_sys(int dx, int dy, const QRect &r)
     }
 }
 
+extern Q_GUI_EXPORT HDC qt_win_display_dc();
+
 int QWidget::metric(PaintDeviceMetric m) const
 {
     Q_D(const QWidget);
@@ -1645,7 +1648,8 @@ int QWidget::metric(PaintDeviceMetric m) const
     } else if (m == PdmHeight) {
         val = data->crect.height();
     } else {
-        HDC gdc = GetDC(0);
+        bool ownDC = QThread::currentThread() != qApp->thread();
+        HDC gdc = ownDC ? GetDC(0) : qt_win_display_dc();
         switch (m) {
         case PdmDpiX:
         case PdmPhysicalDpiX:
@@ -1696,7 +1700,8 @@ int QWidget::metric(PaintDeviceMetric m) const
             val = 0;
             qWarning("QWidget::metric: Invalid metric command");
         }
-        ReleaseDC(0, gdc);
+        if (ownDC)
+            ReleaseDC(0, gdc);
     }
     return val;
 }