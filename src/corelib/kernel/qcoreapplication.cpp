a2dfb824021ea01c1c9c81461b9f275643793542
Jeff Kehres <jkehres@rim.com>
2012-03-23 10:43:44 -0400

    Integrate Blackberry Platform Services (BPS) with Qt event loop.
    This ensures interoperability between the Blackbery C and C++ APIs
    and makes it easier to expose platform services in C++ that are
    exposed in BPS - since events from both APIs can be processed on
    the same thread.
    
    Backport of qtbase 0723e14699704c35d5d61fa7f5e9f3bdbb378afa
    
    Change-Id: Ie0d66e7f9b9984ad3acff61f01665a2b90ef40db
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    Reviewed-by: Bradley T. Hughes <bradley.hughes@nokia.com>

--- src/corelib/kernel/qcoreapplication.cpp
+++ src/corelib/kernel/qcoreapplication.cpp
@@ -74,10 +74,14 @@
 #  include "private/qfilesystemengine_p.h"
 #  include <apacmdln.h>
 #elif defined(Q_OS_UNIX)
-#  if !defined(QT_NO_GLIB)
-#    include "qeventdispatcher_glib_p.h"
+#  if defined(Q_OS_BLACKBERRY)
+#    include "qeventdispatcher_blackberry_p.h"
+#  else
+#    if !defined(QT_NO_GLIB)
+#      include "qeventdispatcher_glib_p.h"
+#    endif
+#    include "qeventdispatcher_unix_p.h"
 #  endif
-#  include "qeventdispatcher_unix_p.h"
 #endif
 
 #ifdef Q_OS_WIN
@@ -457,12 +461,16 @@ void QCoreApplicationPrivate::createEventDispatcher()
 #if defined(Q_OS_SYMBIAN)
     eventDispatcher = new QEventDispatcherSymbian(q);
 #elif defined(Q_OS_UNIX)
+#  if defined(Q_OS_BLACKBERRY)
+    eventDispatcher = new QEventDispatcherBlackberry(q);
+#  else
 #  if !defined(QT_NO_GLIB)
     if (qgetenv("QT_NO_GLIB").isEmpty() && QEventDispatcherGlib::versionSupported())
         eventDispatcher = new QEventDispatcherGlib(q);
     else
 #  endif
         eventDispatcher = new QEventDispatcherUNIX(q);
+#  endif
 #elif defined(Q_OS_WIN)
     eventDispatcher = new QEventDispatcherWin32(q);
 #else
