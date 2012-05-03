qt/qt

commit 33bb996c83e541c26df632b3e8883a1190cc97f0

Take into account input shaping in findRealWindow
In previous commits we took into account bound shaping, but X also
supports input shaping, so make sure it's inside both input and bounding
shaping to consider the position as inside a window

My tests show that when unset Bound Shaping and Input Shaping return the
rectangle of the window itself, so we need to be inside both of the
rectangle sets to consider the position as a dragabble position for the
window

Change-Id: Icb2204a50a97e4a5e02e75301c67287525b290ba
Reviewed-by: Denis Dzyubenko <denis.dzyubenko@nokia.com>
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>

src/gui/kernel/qdnd_x11.cpp
@@ -1423,6 +1423,21 @@ void QDragManager::cancel(bool deleteSource)
 }
 
 static
+bool windowInteractsWithPosition(const QPoint & pos, Window w, int shapeType)
+{
+    int nrectanglesRet, dummyOrdering;
+    XRectangle *rectangles = XShapeGetRectangles(QX11Info::display(), w, shapeType, &nrectanglesRet, &dummyOrdering);
+    bool interacts = true;
+    if (rectangles) {
+        interacts = false;
+        for (int i = 0; !interacts && i < nrectanglesRet; ++i)
+            interacts = QRect(rectangles[i].x, rectangles[i].y, rectangles[i].width, rectangles[i].height).contains(pos);
+        XFree(rectangles);
+    }
+    return interacts;
+}
+
+static
 Window findRealWindow(const QPoint & pos, Window w, int md)
 {
     if (xdnd_data.deco && w == xdnd_data.deco->effectiveWinId())
@@ -1448,23 +1463,10 @@ Window findRealWindow(const QPoint & pos, Window w, int md)
                                    AnyPropertyType, &type, &f,&n,&a,&data);
                 if (data) XFree(data);
                 if (type) {
-#ifndef QT_NO_XFIXES
-                    if (X11->use_xfixes && X11->ptrXFixesCreateRegionFromWindow && X11->ptrXFixesFetchRegion && X11->ptrXFixesDestroyRegion) {
-                        XserverRegion region = X11->ptrXFixesCreateRegionFromWindow(X11->display, w, WindowRegionBounding);
-                        int nrectanglesRet;
-                        XRectangle *rectangles = X11->ptrXFixesFetchRegion(X11->display, region, &nrectanglesRet);
-                        if (rectangles) {
-                            windowContainsMouse = false;
-                            for (int i = 0; !windowContainsMouse && i < nrectanglesRet; ++i)
-                                windowContainsMouse = QRect(rectangles[i].x, rectangles[i].y, rectangles[i].width, rectangles[i].height).contains(pos);
-                            XFree(rectangles);
-                        }
-                        X11->ptrXFixesDestroyRegion(X11->display, region);
-
-                        if (windowContainsMouse)
-                            return w;
-                    } else
-#endif
+                    // When ShapeInput and ShapeBounding are not set they return a single rectangle with the geometry of the window, this is why we
+                    // need an && here so that in the case one is set and the other is not we still get the correct result.
+                    windowContainsMouse = windowInteractsWithPosition(pos, w, ShapeInput) && windowInteractsWithPosition(pos, w, ShapeBounding);
+                    if (windowContainsMouse)
                         return w;
                 }
             }

src/gui/kernel/qapplication_x11.cpp
@@ -2062,9 +2062,6 @@ void qt_init(QApplicationPrivate *priv, int,
             X11->ptrXFixesQueryVersion    = XFIXES_LOAD_V1(XFixesQueryVersion);
             X11->ptrXFixesSetCursorName   = XFIXES_LOAD_V2(XFixesSetCursorName);
             X11->ptrXFixesSelectSelectionInput = XFIXES_LOAD_V2(XFixesSelectSelectionInput);
-            X11->ptrXFixesCreateRegionFromWindow = XFIXES_LOAD_V2(XFixesCreateRegionFromWindow);
-            X11->ptrXFixesFetchRegion = XFIXES_LOAD_V2(XFixesFetchRegion);
-            X11->ptrXFixesDestroyRegion = XFIXES_LOAD_V2(XFixesDestroyRegion);
 
             if(X11->ptrXFixesQueryExtension && X11->ptrXFixesQueryVersion
                && X11->ptrXFixesQueryExtension(X11->display, &X11->xfixes_eventbase,
@@ -2087,9 +2084,6 @@ void qt_init(QApplicationPrivate *priv, int,
             X11->ptrXFixesQueryVersion    = 0;
             X11->ptrXFixesSetCursorName   = 0;
             X11->ptrXFixesSelectSelectionInput = 0;
-            X11->ptrXFixesCreateRegionFromWindow = 0;
-            X11->ptrXFixesFetchRegion = 0;
-            X11->ptrXFixesDestroyRegion = 0;
         }
 #endif // QT_NO_XFIXES
 

src/gui/kernel/qt_x11_p.h
@@ -422,9 +422,6 @@ struct QX11Data
     PtrXFixesQueryVersion ptrXFixesQueryVersion;
     PtrXFixesSetCursorName ptrXFixesSetCursorName;
     PtrXFixesSelectSelectionInput ptrXFixesSelectSelectionInput;
-    PtrXFixesDestroyRegion ptrXFixesDestroyRegion;
-    PtrXFixesCreateRegionFromWindow ptrXFixesCreateRegionFromWindow;
-    PtrXFixesFetchRegion ptrXFixesFetchRegion;
 #endif
 
 #ifndef QT_NO_XINPUT

commit 00ac402870730592965a41567bb4d577fc441f82

Fix compilation error on old Unix systems caused by ShapeInput
Change 33bb996c83e541c26df632b3e8883a1190cc97f0 caused a build error
on Solaris/HP-UX (at least) as ShapeInput type is not present in
default system headers.

Change-Id: I6e19351651856ed71bc9ceaeae625ceac74b27b0
Reviewed-by: Albert Astals Cid <albert.astals@canonical.com>
Reviewed-by: Sergio Ahumada <sergio.ahumada@nokia.com>

src/gui/kernel/qdnd_x11.cpp
@@ -1465,8 +1465,10 @@ Window findRealWindow(const QPoint & pos, Window w, int md)
                 if (type) {
                     // When ShapeInput and ShapeBounding are not set they return a single rectangle with the geometry of the window, this is why we
                     // need an && here so that in the case one is set and the other is not we still get the correct result.
-#if !defined(Q_OS_SOLARIS)
+#if defined(ShapeInput) && defined(ShapeBounding)
                     windowContainsMouse = windowInteractsWithPosition(pos, w, ShapeInput) && windowInteractsWithPosition(pos, w, ShapeBounding);
+#elif defined(ShapeBounding)
+                    windowContainsMouse = windowInteractsWithPosition(pos, w, ShapeBounding);
 #endif
                     if (windowContainsMouse)
                         return w;