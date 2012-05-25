665355e0bab5a6e7d04596cac3ca169f2e458ca7
Morten Sorvig <morten.sorvig@nokia.com>
2012-05-25 13:27:14 +0200

    Add Mac OS 10.8 QSysInfo enums
    Change-Id: If52192c5dcd8ba55b7ae7d3e3c4aea75c32d0f99
    Reviewed-by: James Turner <james.turner@kdab.com>
    Reviewed-by: Christoph Schleifenbaum <christoph.schleifenbaum@kdab.com>
    Reviewed-by: Bradley T. Hughes <bradley.hughes@nokia.com>

--- src/corelib/global/qglobal.h
+++ src/corelib/global/qglobal.h
@@ -1622,6 +1622,7 @@ public:
         MV_10_5 = 0x0007,
         MV_10_6 = 0x0008,
         MV_10_7 = 0x0009,
+        MV_10_8 = 0x000A,
 
         /* codenames */
         MV_CHEETAH = MV_10_0,
@@ -1631,7 +1632,8 @@ public:
         MV_TIGER = MV_10_4,
         MV_LEOPARD = MV_10_5,
         MV_SNOWLEOPARD = MV_10_6,
-        MV_LION = MV_10_7
+        MV_LION = MV_10_7,
+        MV_MOUNTAINLION = MV_10_8
     };
     static const MacVersion MacintoshVersion;
 #endif
