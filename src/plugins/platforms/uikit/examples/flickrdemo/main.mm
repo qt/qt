485edb4ec1f231b70eb5e2fa5a6e1ef584011e36
Eike Ziller <eike.ziller@nokia.com>
2012-08-15 08:56:24 +0200

    uikit: Adapt readme and examples to recent changes
    Use the new configure option (4720003c8f52ad7ecb59be04e1b5aa319c2f0ff0)
    Add info about mkspecs (c13df9f2147e85e104ac105f975ef87914c908e9)
    
    Change-Id: I4eadd4807a36400de3cf261605e3e1a71d217bbc
    Reviewed-by: Ian Dean <ian@mediator-software.com>
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@nokia.com>

--- src/plugins/platforms/uikit/examples/flickrdemo/main.mm
+++ src/plugins/platforms/uikit/examples/flickrdemo/main.mm
@@ -65,16 +65,7 @@ int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-    // force uikit platform plugin
-    QByteArray platform("-platform");
-    QByteArray uikit("uikit");
-    int ac = 3;
-    char *av[3];
-    av[0] = argv[0];
-    av[1] = platform.data();
-    av[2] = uikit.data();
-
-    QApplication app(ac, av);
+    QApplication app(argc, argv);
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
 	viewer.engine()->setOfflineStoragePath(documentsDirectory());
