44f3fb1eec2041c6587d3347f103ca6fa0a17dad
Marc Mutz <marc.mutz@kdab.com>
2012-08-24 10:27:02 +0200

    QDBusPendingCall: add a missing QWaitCondition::wakeAll() call
    In QDBusConnectionPrivate::waitForFinished(), threads that see
    pcall->waitingForFinished == true go to sleep on
    pcall->waitForFinishedCondition, but there was no call to
    waitForFinishedCondition.wakeAll() anywhere in the code, so add it.
    
    Change-Id: I8d068dc0cc4f20786eb40fd7e2bb9840d8b70c7f
    (cherry-picked from qtbase commit 20d7763b19400c062a07f440cc601f486be4039b)
    Reviewed-by: Peter Seiderer <ps.report@gmx.net>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/dbus/qdbusintegrator.cpp
+++ src/dbus/qdbusintegrator.cpp
@@ -1782,6 +1782,7 @@ void QDBusConnectionPrivate::waitForFinished(QDBusPendingCallPrivate *pcall)
             // QDBusConnectionPrivate::processFinishedCall() is called automatically
         }
         pcall->mutex.lock();
+        pcall->waitForFinishedCondition.wakeAll();
     }
 }
 
