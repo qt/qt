763f063f3930e7180a87087996e99db84a6a2eda
Mark Brand <mabrand@mabrand.nl>
2012-08-29 11:00:13 +0200

    documentation: grammar nitpick
    Cherry-picked from qt5/qtbase 4692432b61b09f010a3a7e57e80b9599686fe438
    Reviewed-by: Andy Shaw <andy.shaw@digia.com>
    Reviewed-by: Bill King <bill.king@nokia.com>
    
    Change-Id: I1944946b5f17bbf69005d2da30cedff4970f9827
    Reviewed-by: Andy Shaw <andy.shaw@digia.com>
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

--- src/sql/models/qsqlquerymodel.cpp
+++ src/sql/models/qsqlquerymodel.cpp
@@ -136,7 +136,7 @@ void QSqlQueryModelPrivate::initColOffsets(int size)
     data before showing it to the user, and how to create a
     read-write model based on QSqlQueryModel.
 
-    If the database doesn't return the amount of selected rows in
+    If the database doesn't return the number of selected rows in
     a query, the model will fetch rows incrementally.
     See fetchMore() for more information.
 
@@ -212,8 +212,8 @@ bool QSqlQueryModel::canFetchMore(const QModelIndex &parent) const
     \since 4.1
 
     If the database supports returning the size of a query
-    (see QSqlDriver::hasFeature()), the amount of rows of the current
-    query is returned. Otherwise, returns the amount of rows
+    (see QSqlDriver::hasFeature()), the number of rows of the current
+    query is returned. Otherwise, returns the number of rows
     currently cached on the client.
 
     \a parent should always be an invalid QModelIndex.
