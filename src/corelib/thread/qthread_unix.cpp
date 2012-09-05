qt/qt

commit aa21fcac3672cc6f0dca064b34bbe02ca4f4def7

Display QThread name for debugging.
Backport of commits:
    fb62fdac1cdb4e26388b5bacc7422ec4579b217e
    fe7c600fabdfb1b4cbad624ce7cb736e5edce86d
    3ef51efbe75bfb9f1dfbe7df073e9eb745a72ad8
    47fd7128dba4c38ff1bfcc517fec0f063fb90e4c

Change-Id: Ibcb13ebb2e6cb05ab381328228eca5abdde52ad9
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/thread/qthread_unix.cpp
@@ -94,6 +94,10 @@
 # endif
 #endif
 
+#if defined(Q_OS_LINUX) && !defined(QT_LINUXBASE)
+#include <sys/prctl.h>
+#endif
+
 #if defined(Q_OS_LINUX) && !defined(SCHED_IDLE)
 // from linux/sched.h
 # define SCHED_IDLE    5
@@ -276,6 +280,21 @@ void QThreadPrivate::createEventDispatcher(QThreadData *data)
 
 #ifndef QT_NO_THREAD
 
+#if (defined(Q_OS_LINUX) || defined(Q_OS_MAC) || defined(Q_OS_QNX))
+static void setCurrentThreadName(pthread_t threadId, const char *name)
+{
+#  if defined(Q_OS_LINUX) && !defined(QT_LINUXBASE)
+    Q_UNUSED(threadId);
+    prctl(PR_SET_NAME, (unsigned long)name, 0, 0, 0);
+#  elif defined(Q_OS_MAC)
+    Q_UNUSED(threadId);
+    pthread_setname_np(name);
+#  elif defined(Q_OS_QNX)
+    pthread_setname_np(threadId, name);
+#  endif
+}
+#endif
+
 void *QThreadPrivate::start(void *arg)
 {
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
@@ -300,6 +319,17 @@ void *QThreadPrivate::start(void *arg)
 
     // ### TODO: allow the user to create a custom event dispatcher
     createEventDispatcher(data);
+
+#if (defined(Q_OS_LINUX) || defined(Q_OS_MAC) || defined(Q_OS_QNX))
+    // sets the name of the current thread.
+    QString objectName = thr->objectName();
+
+    if (Q_LIKELY(objectName.isEmpty()))
+        setCurrentThreadName(thr->d_func()->thread_id, thr->metaObject()->className());
+    else
+        setCurrentThreadName(thr->d_func()->thread_id, objectName.toLocal8Bit());
+
+#endif
 
     emit thr->started();
     pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);

commit b6d1a35d6cb464f643d7905ec731069e729d4ad4

Allow compilation on OS X 10.5
Fixes regression introduced by
    aa21fcac3672cc6f0dca064b34bbe02ca4f4def7

Change-Id: Iada496dd8fee390771d8fc12066e93ef086dee90
Reviewed-by: Teemu Katajisto <teemu.katajisto@digia.com>
Reviewed-by: Juhani Taipale <juhani.taipale@digia.com>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/thread/qthread_unix.cpp
@@ -280,15 +280,16 @@ void QThreadPrivate::createEventDispatcher(QThreadData *data)
 
 #ifndef QT_NO_THREAD
 
-#if (defined(Q_OS_LINUX) || defined(Q_OS_MAC) || defined(Q_OS_QNX))
+#if (defined(Q_OS_LINUX) || (defined(Q_OS_MAC) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6) || defined(Q_OS_QNX))
 static void setCurrentThreadName(pthread_t threadId, const char *name)
 {
 #  if defined(Q_OS_LINUX) && !defined(QT_LINUXBASE)
     Q_UNUSED(threadId);
     prctl(PR_SET_NAME, (unsigned long)name, 0, 0, 0);
-#  elif defined(Q_OS_MAC)
+#  elif (defined(Q_OS_MAC) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
     Q_UNUSED(threadId);
-    pthread_setname_np(name);
+    if (QSysInfo::MacintoshVersion >= QSysInfo::MV_10_6)
+        pthread_setname_np(name);
 #  elif defined(Q_OS_QNX)
     pthread_setname_np(threadId, name);
 #  endif
@@ -320,7 +321,7 @@ void *QThreadPrivate::start(void *arg)
     // ### TODO: allow the user to create a custom event dispatcher
     createEventDispatcher(data);
 
-#if (defined(Q_OS_LINUX) || defined(Q_OS_MAC) || defined(Q_OS_QNX))
+#if (defined(Q_OS_LINUX) || (defined(Q_OS_MAC) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6) || defined(Q_OS_QNX))
     // sets the name of the current thread.
     QString objectName = thr->objectName();
 