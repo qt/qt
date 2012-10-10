d23ee1a4b519a625c1978ebd78df416fc6fb3374
Rafael Roquetto <rafael.roquetto.qnx@kdab.com>
2012-10-10 14:38:05 -0300

    Do not skip tst_QClipboard::copy_exit_paste on QNX
    This test is valid on QNX platforms
    
    cherry-picked from qt5 703fa6c361682cab0d64baabd9256c651da34779
    
    Change-Id: I51c1d191cf8cdbde6239afc99019ca310bd2220a
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- tests/auto/qclipboard/tst_qclipboard.cpp
+++ tests/auto/qclipboard/tst_qclipboard.cpp
@@ -235,7 +235,7 @@ void tst_QClipboard::testSignals()
 void tst_QClipboard::copy_exit_paste()
 {
 #ifndef QT_NO_PROCESS
-#if defined Q_WS_X11 || defined Q_WS_QWS || defined (Q_WS_QPA)
+#if defined Q_WS_X11 || defined Q_WS_QWS || (!defined (Q_OS_QNX) && defined (Q_WS_QPA))
     QSKIP("This test does not make sense on X11 and embedded, copied data disappears from the clipboard when the application exits ", SkipAll);
     // ### It's still possible to test copy/paste - just keep the apps running
 #elif defined (Q_OS_SYMBIAN) && defined (Q_CC_NOKIAX86)
