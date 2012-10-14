qt/qtbase

Blackberry mkspecs: Refine compiler options
stack-protector-strong gives performance benefits over
stack-protector-all and is still checking more than -stack-protector,
so seems to be a good middle way and we want to use it when it is
there.

The -shared option for the compiler (not the linker) prevents a
RIM internal version of qcc from forcing -fPIE, and should not harm
in general when set.

In addition, add a method "compilerSupportsFlag" for Windows as is
present in the Unix configure script.

Change-Id: Iba300e9cb82f34043e7b36f8e45287a1aed2a1a5
Original-patch-by: Greg Bentz
Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

tools/configure/configureapp.h
@@ -175,6 +175,7 @@ private:
 #endif
 
     bool tryCompileProject(const QString &projectPath, const QString &extraOptions = QString());
+    bool compilerSupportsFlag(const QString &compilerAndArgs);
 
     void desc(const char *description, int startingAt = 0, int wrapIndent = 0);
     void desc(const char *option, const char *description, bool skipIndent = false, char fillChar = '.');

qt/qt

(cherry picked from commit 80f6d7862c0e2e41768620d5bd81b0e1d5e3f61f)
Change-Id: I5867fc03dde1ef6e2fbf3747bdb9aaf15518eb6a

tools/configure/configureapp.h
@@ -161,6 +161,8 @@ private:
     void saveCmdLine();
 #endif
 
+    bool compilerSupportsFlag(const QStringList &compilerAndArgs);
+
     void desc(const char *description, int startingAt = 0, int wrapIndent = 0);
     void desc(const char *option, const char *description, bool skipIndent = false, char fillChar = '.');
     void desc(const char *mark_option, const char *mark, const char *option, const char *description, char fillChar = '.');