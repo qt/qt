qt/qt

commit 439aa67582e715ddc0ca7e30305b0c9498a71f81

Fixes ignored composition mode in QPainter::fillRect with X11 engine.
It's not sufficient to check that the source brush has an alpha, there
are composition modes that depend on the destination instead.

Task-number: QTBUG-28499
Change-Id: I4b267544c607b6a68c4c52a6784d526bec604d75
Reviewed-by: Gunnar Sletta <gunnar.sletta@digia.com>

src/gui/painting/qpaintengine_x11.cpp
@@ -144,6 +144,11 @@ static inline int qpainterOpToXrender(QPainter::CompositionMode mode)
     Q_ASSERT(mode <= QPainter::CompositionMode_Xor);
     return compositionModeToRenderOp[mode];
 }
+
+static inline bool complexPictOp(int op)
+{
+    return op != PictOpOver && op != PictOpSrc;
+}
 #endif
 
 // hack, so we don't have to make QRegion::clipRectangles() public or include
@@ -771,7 +776,8 @@ void QX11PaintEngine::drawRects(const QRectF *rects, int rectCount)
         || d->has_alpha_brush
         || d->has_complex_xform
         || d->has_custom_pen
-        || d->cbrush.style() != Qt::SolidPattern)
+        || d->cbrush.style() != Qt::SolidPattern
+        || complexPictOp(d->composition_mode))
     {
         QPaintEngine::drawRects(rects, rectCount);
         return;
@@ -838,7 +844,7 @@ void QX11PaintEngine::drawRects(const QRect *rects, int rectCount)
     ::Picture pict = d->picture;
 
     if (X11->use_xrender && pict && d->has_brush && d->pdev_depth != 1
-        && (d->has_texture || d->has_alpha_brush))
+        && (d->has_texture || d->has_alpha_brush || complexPictOp(d->composition_mode)))
     {
         XRenderColor xc;
         if (!d->has_texture && !d->has_pattern)

commit a92234fc38a4f32d3c00bcf0adf9b9c54506b8f8

QPaintEngine: Fix build if compiled with QT_NO_XRENDER
composition_mode is only defined if QT_NO_XRENDER is not defined.
Bug was introduced with 439aa67582e715ddc0ca7e30305b0c9498a71f81
"Fixes ignored composition mode in QPainter::fillRect with X11 engine."

Change-Id: I2dc036ffa277cbf0f9327ede3fead9a407147fc0
Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>

src/gui/painting/qpaintengine_x11.cpp
@@ -777,7 +777,10 @@ void QX11PaintEngine::drawRects(const QRectF *rects, int rectCount)
         || d->has_complex_xform
         || d->has_custom_pen
         || d->cbrush.style() != Qt::SolidPattern
-        || complexPictOp(d->composition_mode))
+#if !defined(QT_NO_XRENDER)
+        || complexPictOp(d->composition_mode)
+#endif
+       )
     {
         QPaintEngine::drawRects(rects, rectCount);
         return;