5f0c513f919a8d0a539fca4b5eb50ce125af8514
Friedemann Kleint <Friedemann.Kleint@digia.com>
2013-01-18 16:39:33 +0100

    Qt Designer: Use multiline validation for dynamic string properties.
    Task-number: QTBUG-26394
    
    Change-Id: Idbaa4bad71125b74d8407bb56ee9fdbf83831fe0
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@digia.com>
    (cherry picked from qttools/a7d61534acdb90b925d7f250665dcd30c3f37668)

--- tools/designer/src/lib/shared/qdesigner_propertyeditor.cpp
+++ tools/designer/src/lib/shared/qdesigner_propertyeditor.cpp
@@ -43,6 +43,9 @@
 #include "pluginmanager_p.h"
 
 #include <QtDesigner/QDesignerFormEditorInterface>
+#include <QtDesigner/QDesignerDynamicPropertySheetExtension>
+#include <QtDesigner/QDesignerPropertySheetExtension>
+#include <QtDesigner/QExtensionManager>
 #include <widgetfactory_p.h>
 #include <QtGui/QAction>
 #include <QtGui/QLineEdit>
@@ -99,6 +102,20 @@ QDesignerPropertyEditor::QDesignerPropertyEditor(QWidget *parent, Qt::WindowFlag
     connect(this, SIGNAL(propertyChanged(QString,QVariant)), this, SLOT(slotPropertyChanged(QString,QVariant)));
 }
 
+static inline bool isDynamicProperty(QDesignerFormEditorInterface *core, QObject *object,
+                                     const QString &propertyName)
+{
+    if (const QDesignerDynamicPropertySheetExtension *dynamicSheet = qt_extension<QDesignerDynamicPropertySheetExtension*>(core->extensionManager(), object)) {
+        if (dynamicSheet->dynamicPropertiesAllowed()) {
+            if (QDesignerPropertySheetExtension *propertySheet = qt_extension<QDesignerPropertySheetExtension*>(core->extensionManager(), object)) {
+                const int index = propertySheet->indexOf(propertyName);
+                return index >= 0 && dynamicSheet->isDynamicProperty(index);
+            }
+        }
+    }
+    return false;
+}
+
 QDesignerPropertyEditor::StringPropertyParameters QDesignerPropertyEditor::textPropertyValidationMode(
         QDesignerFormEditorInterface *core, const QObject *object,
         const QString &propertyName, bool isMainContainer)
@@ -117,6 +134,9 @@ QDesignerPropertyEditor::StringPropertyParameters QDesignerPropertyEditor::textP
         if (customData.xmlStringPropertyType(propertyName, &customType))
             return customType;
     }
+
+    if (isDynamicProperty(core, const_cast<QObject *>(object), propertyName))
+        return StringPropertyParameters(ValidationMultiLine, true);
 
     // Check hardcoded property ames
    const PropertyNameTypeMap::const_iterator hit = stringPropertyTypes().constFind(propertyName);
