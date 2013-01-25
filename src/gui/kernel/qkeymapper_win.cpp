qt/qt

commit 68331c5436506b6f9b4c2a49692a09020e4eef27

Fixed dead keys on MS Windows
Since Qt4, there is a bug which causes Qt to drop dead key modifiers
(like graves and acutes) if the user types enough fast on MS Windows.

This happens because of an extrange behavior of Windows, which drops
dead keys on ToUnicode() calls.

This patch tries to workaround that.

Task-number: QTBUG-8764
Task-number: QTBUG-10032

Conflicts:
	src/plugins/platforms/windows/qwindowskeymapper.cpp

Change-Id: Ifdde25817743194fd5c0b7533c27f46a7a108ca4
(cherry picked from commit 5d2bb24cc90194a3458f8741e30ae7afe0b45f5c)
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

src/gui/kernel/qkeymapper_win.cpp
@@ -925,9 +925,15 @@ bool QKeyMapperPrivate::translateKeyEvent(QWidget *widget, const MSG &msg, bool 
         if (isNumpad && (nModifiers & AltAny)) {
             code = winceKeyBend(msg.wParam);
         } else if (!isDeadKey) {
-            unsigned char kbdBuffer[256]; // Will hold the complete keyboard state
-            GetKeyboardState(kbdBuffer);
-            code = toKeyOrUnicode(msg.wParam, scancode, kbdBuffer);
+            // QTBUG-8764, QTBUG-10032
+            // Can't call toKeyOrUnicode because that would call ToUnicode, and, if a dead key
+            // is pressed at the moment, Windows would NOT use it to compose a character for the next
+            // WM_CHAR event.
+
+            // Instead, use MapVirtualKey, which will provide adequate values.
+            code = MapVirtualKey(msg.wParam, MAPVK_VK_TO_CHAR);
+            if (code < 0x20 || code == 0x7f) // The same logic as in toKeyOrUnicode()
+                code = winceKeyBend(msg.wParam);
         }
 
         // Invert state logic:

commit 131ccbb445ae13402c35e957b64153644fd4d09c

qkeymapper_win.cpp: Define missing constant for MinGW.
Fix MinGW compile breakage introduced by
68331c5436506b6f9b4c2a49692a09020e4eef27 .

Task-number: QTBUG-8764
Task-number: QTBUG-10032

Change-Id: I26bcefc382010eee7d79b806b3d9f08b6cb2ff8d
Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>

src/gui/kernel/qkeymapper_win.cpp
@@ -74,6 +74,9 @@ extern Q_CORE_EXPORT QLocale qt_localeFromLCID(LCID id);
 #ifndef VK_OEM_3
 #define VK_OEM_3 0xC0
 #endif
+#ifndef MAPVK_VK_TO_CHAR
+#define MAPVK_VK_TO_CHAR (2)
+#endif
 
 #if defined(Q_OS_WINCE)
 bool GetKeyboardState(unsigned char* kbuffer)