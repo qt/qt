qt/qtbase

BB10 systemProxyForQuery: query system proxy also for non-URL requests
... like e.g. in QAbstractSocket::connectToHost().
We can set the scheme on the query URL based on well-known ports; even
if the port is not well-known, we can just query the URL anyhow and let
the netstatus API resolve the right proxy for us.

In addition, return early for local schemes like "file" and "qrc".

Task-number: QTBUG-29425
Change-Id: I900f1ecbac6dd380bb8a77470b39de2c07aa7f6e
Reviewed-by: Andrey Leonov <aleonov@rim.com>
Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>

src/network/kernel/qnetworkproxy_blackberry.cpp
@@ -63,14 +63,36 @@ QT_BEGIN_NAMESPACE
 
 QList<QNetworkProxy> QNetworkProxyFactory::systemProxyForQuery(const QNetworkProxyQuery &query)
 {
-    QNetworkProxy proxy;
+    if (query.url().scheme() == QLatin1String("file")
+            || query.url().scheme() == QLatin1String("qrc"))
+        return QList<QNetworkProxy>() << QNetworkProxy(QNetworkProxy::NoProxy);
 
-    if (query.queryType() != QNetworkProxyQuery::UrlRequest) {
+    if (query.queryType() != QNetworkProxyQuery::UrlRequest
+            && query.queryType() != QNetworkProxyQuery::TcpSocket) {
         qWarning("Unsupported query type: %d", query.queryType());
         return QList<QNetworkProxy>() << QNetworkProxy(QNetworkProxy::NoProxy);
     }
 
-    QUrl url  = query.url();
+    QUrl url;
+    if (query.queryType() == QNetworkProxyQuery::UrlRequest) {
+        url = query.url();
+    } else if (query.queryType() == QNetworkProxyQuery::TcpSocket
+               && !query.peerHostName().isEmpty()) {
+        url.setHost(query.peerHostName());
+        switch (query.peerPort()) {
+        case 443:
+            url.setScheme(QStringLiteral("https"));
+            break;
+        case 21:
+            url.setScheme(QStringLiteral("ftp"));
+            break;
+        default:
+            // for unknown ports, we just pretend we are dealing
+            // with a HTTP URL, otherwise we will not get a proxy
+            // from the netstatus API
+            url.setScheme(QStringLiteral("http"));
+        }
+    }
 
     if (!url.isValid()) {
         qWarning("Invalid URL: %s", qPrintable(url.toString()));
@@ -111,6 +133,8 @@ QList<QNetworkProxy> QNetworkProxyFactory::systemProxyForQuery(const QNetworkPro
         netstatus_free_proxy_details(&details);
         return QList<QNetworkProxy>() << QNetworkProxy(QNetworkProxy::NoProxy);
     }
+
+    QNetworkProxy proxy;
 
     QString protocol = query.protocolTag();
     if (protocol.startsWith(QLatin1String("http"), Qt::CaseInsensitive)) { // http, https

qt/qt

(cherry picked from commit 28d526db74c50977215133159e565e7c15fd18e3)
Change-Id: I40a4865aa7765aad9145e956c0069e78c54670e7

src/network/kernel/qnetworkproxy_blackberry.cpp
@@ -62,14 +62,36 @@ QT_BEGIN_NAMESPACE
 
 QList<QNetworkProxy> QNetworkProxyFactory::systemProxyForQuery(const QNetworkProxyQuery &query)
 {
-    QNetworkProxy proxy;
+    if (query.url().scheme() == QLatin1String("file")
+            || query.url().scheme() == QLatin1String("qrc"))
+        return QList<QNetworkProxy>() << QNetworkProxy(QNetworkProxy::NoProxy);
 
-    if (query.queryType() != QNetworkProxyQuery::UrlRequest) {
+    if (query.queryType() != QNetworkProxyQuery::UrlRequest
+            && query.queryType() != QNetworkProxyQuery::TcpSocket) {
         qWarning("Unsupported query type: %d", query.queryType());
         return QList<QNetworkProxy>() << QNetworkProxy(QNetworkProxy::NoProxy);
     }
 
-    QUrl url  = query.url();
+    QUrl url;
+    if (query.queryType() == QNetworkProxyQuery::UrlRequest) {
+        url = query.url();
+    } else if (query.queryType() == QNetworkProxyQuery::TcpSocket
+               && !query.peerHostName().isEmpty()) {
+        url.setHost(query.peerHostName());
+        switch (query.peerPort()) {
+        case 443:
+            url.setScheme(QLatin1String("https"));
+            break;
+        case 21:
+            url.setScheme(QLatin1String("ftp"));
+            break;
+        default:
+            // for unknown ports, we just pretend we are dealing
+            // with a HTTP URL, otherwise we will not get a proxy
+            // from the netstatus API
+            url.setScheme(QLatin1String("http"));
+        }
+    }
 
     if (!url.isValid()) {
         qWarning("Invalid URL: %s", qPrintable(url.toString()));
@@ -110,6 +132,8 @@ QList<QNetworkProxy> QNetworkProxyFactory::systemProxyForQuery(const QNetworkPro
         netstatus_free_proxy_details(&details);
         return QList<QNetworkProxy>() << QNetworkProxy(QNetworkProxy::NoProxy);
     }
+
+    QNetworkProxy proxy;
 
     QString protocol = query.protocolTag();
     if (protocol.startsWith(QLatin1String("http"), Qt::CaseInsensitive)) { // http, https