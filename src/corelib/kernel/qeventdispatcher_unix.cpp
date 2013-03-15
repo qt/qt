815d7f0ff36aa656ecfbf6a6d69fc9453d72e598
David Faure <david.faure@kdab.com>
2013-03-15 19:47:46 +0100

    QThreadDataPrivate: fix data race on canWait boolean.
    postEvent() accesses it with the postEventList mutex locked, but
    processEvent() was checking it without any mutex locked.
    
    Change-Id: I31bbb50f7a1c337067b8e3de16ee7cd11400b517
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (cherry picked from qtbase/bf3a5ccef13d568662f027be62280aba1f73bada)
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

--- src/corelib/kernel/qeventdispatcher_unix.cpp
+++ src/corelib/kernel/qeventdispatcher_unix.cpp
@@ -908,7 +908,7 @@ bool QEventDispatcherUNIX::processEvents(QEventLoop::ProcessEventsFlags flags)
     QCoreApplicationPrivate::sendPostedEvents(0, 0, d->threadData);
 
     int nevents = 0;
-    const bool canWait = (d->threadData->canWait
+    const bool canWait = (d->threadData->canWaitLocked()
                           && !d->interrupt
                           && (flags & QEventLoop::WaitForMoreEvents));
 
