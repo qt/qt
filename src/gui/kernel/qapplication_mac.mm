qt/qt

commit 7b7c321d5f35b6ee70db5a72d5d37e19e125d7cf

Fix Mac static builds so they get the qt_menu.nib from the resource file
Instead of loading the file from the bundle (if it existed) then we
create the qt_menu.nib from the resources instead. This ensures that
static builds no longer have to have the qt_menu.nib file manually
copied to be in the Resources folder for every application.

Change-Id: I7abb6fad6395d466e22e7a3b7ffb63b50ae82f65
Reviewed-by: Gabriel de Dietrich <gabriel.dedietrich@digia.com>

src/gui/kernel/qapplication_mac.mm
@@ -226,6 +226,40 @@ extern void qt_mac_update_cursor(); // qcursor_mac.mm
 void onApplicationWindowChangedActivation( QWidget*widget, bool activated );
 void onApplicationChangedActivation( bool activated );
 
+void qt_mac_loadMenuNib(QT_MANGLE_NAMESPACE(QCocoaMenuLoader) *qtMenuLoader)
+{
+    // Create qt_menu.nib dir in temp.
+    QDir temp = QDir::temp();
+    temp.mkdir("qt_menu.nib");
+    QString nibDir = temp.canonicalPath() + QLatin1String("/") + QLatin1String("qt_menu.nib/");
+    if (!QDir(nibDir).exists()) {
+        qWarning("qt_mac_loadMenuNib: could not create nib directory in temp");
+        return;
+    }
+
+    // Copy nib files from resources to temp.
+    QDir nibResource(":/trolltech/mac/qt_menu.nib/");
+    if (!nibResource.exists()) {
+        qWarning("qt_mac_loadMenuNib: could not load nib from resources");
+        return;
+    }
+    foreach (const QFileInfo &file, nibResource.entryInfoList())
+        QFile::copy(file.absoluteFilePath(), nibDir + QLatin1String("/") + file.fileName());
+
+    // Load and instantiate nib file from temp
+    NSURL *nibUrl = [NSURL fileURLWithPath : reinterpret_cast<const NSString *>(QCFString::toCFStringRef(nibDir))];
+    NSNib *nib = [[NSNib alloc] initWithContentsOfURL : nibUrl];
+    [nib autorelease];
+    if (!nib) {
+        qWarning("qt_mac_loadMenuNib: could not load nib from temp");
+        return;
+    }
+    bool ok = [nib instantiateNibWithOwner : qtMenuLoader topLevelObjects : nil];
+    if (!ok)
+        qWarning("qt_mac_loadMenuNib: could not instantiate nib");
+}
+
+
 static void qt_mac_read_fontsmoothing_settings()
 {
     qt_applefontsmoothing_enabled = true;
@@ -1258,15 +1292,10 @@ void qt_init(QApplicationPrivate *priv, int)
         [cocoaApp setDelegate:newDelegate];
 
         QT_MANGLE_NAMESPACE(QCocoaMenuLoader) *qtMenuLoader = [[QT_MANGLE_NAMESPACE(QCocoaMenuLoader) alloc] init];
-        if ([NSBundle loadNibNamed:@"qt_menu" owner:qtMenuLoader] == false) {
-            qFatal("Qt internal error: qt_menu.nib could not be loaded. The .nib file"
-                   " should be placed in QtGui.framework/Versions/Current/Resources/ "
-                   " or in the resources directory of your application bundle.");
-        }
+        qt_mac_loadMenuNib(qtMenuLoader);
 
         [cocoaApp setMenu:[qtMenuLoader menu]];
         [newDelegate setMenuLoader:qtMenuLoader];
-        [qtMenuLoader release];
     }
 #endif
     // Register for Carbon tablet proximity events on the event monitor target.

src/gui/mac/macresources.qrc
@@ -9,4 +9,9 @@
 <qresource prefix="/trolltech/mac/style">
 <file>images/leopard-unified-toolbar-on.png</file>
 </qresource>
+<qresource prefix="/trolltech/mac/">
+<file>qt_menu.nib/classes.nib</file>
+<file>qt_menu.nib/info.nib</file>
+<file>qt_menu.nib/keyedobjects.nib</file>
+</qresource>
 </RCC>

commit b100e49d76232080d782bc7e1e77c29a30c9d710

Fix build regression caused by 7b7c321d5f35b6ee70db5a72d5d37e19e125d7cf.
Change-Id: I54f4e0ca2ca677a41c22183263931c9d65896168
Reviewed-by: Andy Shaw <andy.shaw@digia.com>

src/gui/kernel/qapplication_mac.mm
@@ -247,7 +247,7 @@ void qt_mac_loadMenuNib(QT_MANGLE_NAMESPACE(QCocoaMenuLoader) *qtMenuLoader)
         QFile::copy(file.absoluteFilePath(), nibDir + QLatin1String("/") + file.fileName());
 
     // Load and instantiate nib file from temp
-    NSURL *nibUrl = [NSURL fileURLWithPath : reinterpret_cast<const NSString *>(QCFString::toCFStringRef(nibDir))];
+    NSURL *nibUrl = [NSURL fileURLWithPath : const_cast<NSString *>(reinterpret_cast<const NSString *>(QCFString::toCFStringRef(nibDir)))];
     NSNib *nib = [[NSNib alloc] initWithContentsOfURL : nibUrl];
     [nib autorelease];
     if (!nib) {