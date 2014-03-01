1f35d0783cc2a742d94d4f8e33819835dc990d87
Dmitry Shachnev <mitya57@gmail.com>
2014-03-01 13:13:27 +0400

    Add AArch64 detection support to JavaScriptCore.
    Cherry-picked from qtscript commit 2e049836ee16f4aedbe7ccc3335fc5.
    
    Change-Id: I8d9fdeceb62f4de4eb9bd74ae45bb33a641ef5cc
    Reviewed-by: Lisandro Damián Nicanor Pérez Meyer <perezmeyer@gmail.com>
    Reviewed-by: Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>

--- src/3rdparty/javascriptcore/JavaScriptCore/wtf/Platform.h
+++ src/3rdparty/javascriptcore/JavaScriptCore/wtf/Platform.h
@@ -209,6 +209,14 @@
 #define WTF_CPU_AIX64 1
 #endif
 
+/* CPU(AARCH64) - AArch64 */
+#if defined(__aarch64__)
+#define WTF_CPU_AARCH64 1
+#if defined(__AARCH64EB__)
+#define WTF_CPU_BIG_ENDIAN 1
+#endif
+#endif
+
 /* CPU(ARM) - ARM, any version*/
 #if   defined(arm) \
     || defined(__arm__) \
@@ -895,7 +903,7 @@
 #endif
 
 #if !defined(WTF_USE_JSVALUE64) && !defined(WTF_USE_JSVALUE32) && !defined(WTF_USE_JSVALUE32_64)
-#if (CPU(X86_64) && (OS(UNIX) || OS(WINDOWS) || OS(SOLARIS) || OS(HPUX))) || (CPU(IA64) && !CPU(IA64_32)) || CPU(ALPHA) || CPU(AIX64) || CPU(SPARC64) || CPU(MIPS64)
+#if (CPU(X86_64) && (OS(UNIX) || OS(WINDOWS) || OS(SOLARIS) || OS(HPUX))) || (CPU(IA64) && !CPU(IA64_32)) || CPU(ALPHA) || CPU(AIX64) || CPU(SPARC64) || CPU(MIPS64) || CPU(AARCH64)
 #define WTF_USE_JSVALUE64 1
 #elif CPU(ARM) || CPU(PPC64)
 #define WTF_USE_JSVALUE32 1
