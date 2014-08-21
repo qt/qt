5dc8b2bac4657827293cfc870ba49fa87ec96e13
Samuel Gaist <samuel.gaist@edeltech.ch>
2014-08-21 09:42:32 +0200

    QThread snippet: add missing & from connection statement
    Change-Id: I0409feec55ebd19f80b0c2396c49b4999798ef74
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>

--- doc/src/snippets/code/src_corelib_thread_qthread.cpp
+++ doc/src/snippets/code/src_corelib_thread_qthread.cpp
@@ -88,7 +88,7 @@ public:
     Controller() {
         Worker *worker = new Worker;
         worker->moveToThread(&workerThread);
-        connect(workerThread, SIGNAL(finished()), worker, SLOT(deleteLater()));
+        connect(&workerThread, SIGNAL(finished()), worker, SLOT(deleteLater()));
         connect(this, SIGNAL(operate(QString)), worker, SLOT(doWork(QString)));
         connect(worker, SIGNAL(resultReady(QString)), this, SLOT(handleResults(QString)));
         workerThread.start();
