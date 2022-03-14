dd310d70d569ca9add89223769909c2ce288e0b0
Leandro Nini <drfiemost@users.noreply.github.com>
2022-03-14 18:42:54 +0100

    Apply disable-sslv3.patch from Arch
    https://aur.archlinux.org/cgit/aur.git/tree/disable-sslv3.patch?h=qt4

--- src/network/ssl/qsslsocket_openssl.cpp
+++ src/network/ssl/qsslsocket_openssl.cpp
@@ -267,7 +267,11 @@ init_context:
 #endif
         break;
     case QSsl::SslV3:
+#ifndef OPENSSL_NO_SSL3
         ctx = q_SSL_CTX_new(client ? q_SSLv3_client_method() : q_SSLv3_server_method());
+#else
+        ctx = 0; // SSL 3 not supported by the system, but chosen deliberately -> error
+#endif
         break;
     case QSsl::SecureProtocols: // SslV2 will be disabled below
     case QSsl::TlsV1SslV3: // SslV2 will be disabled below
