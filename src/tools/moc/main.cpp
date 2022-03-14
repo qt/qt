cd9600ffa54227929a033dd7eddb11c137593bb1
Leandro Nini <drfiemost@users.noreply.github.com>
2022-03-14 18:39:57 +0100

    Apply moc-boost-workaround.patch from Arch
    https://aur.archlinux.org/cgit/aur.git/tree/moc-boost-workaround.patch?h=qt4

--- src/tools/moc/main.cpp
+++ src/tools/moc/main.cpp
@@ -190,6 +190,8 @@ int runMoc(int _argc, char **_argv)
 
     // Workaround a bug while parsing the boost/type_traits/has_operator.hpp header. See QTBUG-22829
     pp.macros["BOOST_TT_HAS_OPERATOR_HPP_INCLUDED"];
+    pp.macros["BOOST_LEXICAL_CAST_INCLUDED"];
+    pp.macros["BOOST_NEXT_PRIOR_HPP_INCLUDED"];
 
     QByteArray filename;
     QByteArray output;
