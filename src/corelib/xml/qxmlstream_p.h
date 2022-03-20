a323471e116194800cd0e78980d9a8b07271b87d
Ralf Habacker <ralf.habacker@freenet.de>
2022-03-20 23:25:43 +0100

    CVE 2018 15518

--- src/corelib/xml/qxmlstream_p.h
+++ src/corelib/xml/qxmlstream_p.h
@@ -1242,7 +1242,7 @@ bool QXmlStreamReaderPrivate::parse()
             state_stack[tos] = 0;
             return true;
         } else if (act > 0) {
-            if (++tos == stack_size-1)
+            if (++tos >= stack_size-1)
                 reallocateStack();
 
             Value &val = sym_stack[tos];
