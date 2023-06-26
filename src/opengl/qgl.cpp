qt/qt

commit 0b41638611cef3defd485f97777723f4be21d46d

More symbian cleanup

src/opengl/qgl.cpp
@@ -101,16 +101,11 @@
 #include <EGL/egl.h>
 #endif
 
-#ifdef Q_OS_SYMBIAN
-#include <private/qgltexturepool_p.h>
-#include <private/qeglcontext_p.h>
-#endif
-
 // #define QT_GL_CONTEXT_RESOURCE_DEBUG
 
 QT_BEGIN_NAMESPACE
 
-#if defined(Q_WS_X11) || defined(Q_WS_MAC) || defined(Q_WS_QWS) || defined(Q_WS_QPA) || defined(Q_OS_SYMBIAN)
+#if defined(Q_WS_X11) || defined(Q_WS_MAC) || defined(Q_WS_QWS) || defined(Q_WS_QPA)
 QGLExtensionFuncs QGLContextPrivate::qt_extensionFuncs;
 #endif
 
@@ -2315,7 +2310,7 @@ static void convertToGLFormatHelper(QImage &dst, const QImage &img, GLenum textu
     }
 }
 
-#if defined(Q_WS_X11) || defined(Q_WS_MAC) || defined(Q_WS_QWS) || defined(Q_WS_QPA) || defined(Q_OS_SYMBIAN)
+#if defined(Q_WS_X11) || defined(Q_WS_MAC) || defined(Q_WS_QWS) || defined(Q_WS_QPA)
 QGLExtensionFuncs& QGLContextPrivate::extensionFuncs(const QGLContext *)
 {
     return qt_extensionFuncs;
@@ -2559,19 +2554,8 @@ QGLTexture* QGLContextPrivate::bindTexture(const QImage &image, GLenum target, G
 #endif
 
     const QImage &constRef = img; // to avoid detach in bits()...
-#ifdef Q_OS_SYMBIAN
-    // On Symbian we always use texture pool to reserve the texture
-    QGLTexturePool::instance()->createPermanentTexture(tx_id,
-                                                        target,
-                                                        0, internalFormat,
-                                                        img.width(), img.height(),
-                                                        externalFormat,
-                                                        pixel_type,
-                                                        constRef.bits());
-#else
     glTexImage2D(target, 0, internalFormat, img.width(), img.height(), 0, externalFormat,
                  pixel_type, constRef.bits());
-#endif
 
 #if defined(QT_OPENGL_ES_2)
     if (genMipmap)
@@ -2595,15 +2579,7 @@ QGLTexture* QGLContextPrivate::bindTexture(const QImage &image, GLenum target, G
     int cost = img.width()*img.height()*4/1024;
     QGLTexture *texture = new QGLTexture(q, tx_id, target, options);
     QGLTextureCache::instance()->insert(q, key, texture, cost);
-
-#ifdef Q_OS_SYMBIAN
-    // Store the key so that QGLTexturePool
-    // is able to release this texture when needed.
-    texture->boundKey = key;
-    // And append to LRU list
-    QGLTexturePool::instance()->useTexture(texture);
-#endif
-    return texture;
+texture;
 }
 
 QGLTexture *QGLContextPrivate::textureCacheLookup(const qint64 key, GLenum target)
@@ -2683,20 +2659,6 @@ QGLTexture *QGLContextPrivate::bindTexture(const QPixmap &pixmap, GLenum target,
 #endif
 
     if (!texture) {
-#ifdef Q_OS_SYMBIAN
-        // On Symbian pixmaps are backed up by native CFbsBitmap
-        // which can be shared across processes. QVolatileImage wraps
-        // it and provides locking mechanism to pixel access.
-        QVolatileImage volatileImage = pd->toVolatileImage();
-        if (volatileImage.isNull()) { // TODO: raster graphics system don't provide volatile image (yet)
-            // NOTE! On Symbian raster graphics system QPixmap::toImage() makes deep copy
-            texture = bindTexture(pixmap.toImage(), target, format, key, options);
-        } else {
-            volatileImage.beginDataAccess();
-            texture = bindTexture(volatileImage.imageRef(), target, format, key, options);
-            volatileImage.endDataAccess(true);
-        }
-#else
         QImage image;
         QPaintEngine* paintEngine = pixmap.paintEngine();
         if (!paintEngine || paintEngine->type() != QPaintEngine::Raster)
@@ -2716,7 +2678,6 @@ QGLTexture *QGLContextPrivate::bindTexture(const QPixmap &pixmap, GLenum target,
         if (pixmap.depth() == 16 && !image.hasAlphaChannel() )
             image = image.convertToFormat(QImage::Format_RGB16);
         texture = bindTexture(image, target, format, key, options);
-#endif
     }
     // NOTE: bindTexture(const QImage&, GLenum, GLint, const qint64, bool) should never return null
     Q_ASSERT(texture);
@@ -3457,22 +3418,6 @@ const QGLContext* QGLContext::currentContext()
 #else
     QGLThreadContext *threadContext = qgl_context_storage.localData();
     if (threadContext) {
-#ifdef Q_OS_SYMBIAN
-        // Query the current context and return null if it is different.
-        // This is needed to support mixed VG-GL rendering.
-        // QtOpenVG is free to make a QEglContext current at any time and
-        // QGLContext gets no notification that its underlying QEglContext is
-        // not current anymore. We query directly from EGL to be thread-safe.
-        // QEglContext does not store all the contexts per-thread.
-        if (threadContext->context) {
-            QEglContext *eglcontext = threadContext->context->d_func()->eglContext;
-            if (eglcontext) {
-                EGLContext ctx = eglcontext->context();
-                if (ctx != eglGetCurrentContext())
-                    return 0;
-            }
-        }
-#endif
         return threadContext->context;
     }
     return 0;
@@ -4393,37 +4338,6 @@ bool QGLWidget::event(QEvent *e)
         d->glcx->d_ptr->clearDrawable();
 #  endif
     }
-#elif defined(Q_OS_SYMBIAN)
-    // prevents errors on some systems, where we get a flush to a
-    // hidden widget
-    if (e->type() == QEvent::Hide) {
-        makeCurrent();
-        glFinish();
-        doneCurrent();
-    } else if (e->type() == QEvent::ParentChange) {
-        // if we've reparented a window that has the current context
-        // bound, we need to rebind that context to the new window id
-        if (d->glcx == QGLContext::currentContext())
-            makeCurrent(); // Shouldn't happen but keep it here just for sure
-
-        if (testAttribute(Qt::WA_TranslucentBackground))
-            setContext(new QGLContext(d->glcx->requestedFormat(), this));
-    }
-
-    // A re-parent is likely to destroy the Symbian window and re-create it. It is important
-    // that we free the EGL surface _before_ the winID changes - otherwise we can leak.
-    if (e->type() == QEvent::ParentAboutToChange) {
-        if (d->glcx == QGLContext::currentContext())
-            d->glcx->doneCurrent();
-        d->glcx->d_func()->destroyEglSurfaceForDevice();
-    }
-
-    if ((e->type() == QEvent::ParentChange) || (e->type() == QEvent::WindowStateChange)) {
-        // The window may have been re-created during re-parent or state change - if so, the EGL
-        // surface will need to be re-created.
-        d->recreateEglSurface();
-    }
-
 #endif
 
     return QWidget::event(e);
@@ -4631,11 +4545,6 @@ void QGLWidget::glDraw()
     Q_D(QGLWidget);
     if (!isValid())
         return;
-#ifdef Q_OS_SYMBIAN
-    // Crashes on Symbian if trying to render to invisible surfaces
-    if (!isVisible() && d->glcx->device()->devType() == QInternal::Widget)
-        return;
-#endif
     makeCurrent();
 #ifndef QT_OPENGL_ES
     if (d->glcx->deviceIsPixmap())
@@ -6140,28 +6049,5 @@ QSize QGLTexture::bindCompressedTexturePVR(const char *buf, int len)
 }
 
 #undef ctx
-
-#ifdef Q_OS_SYMBIAN
-void QGLTexture::freeTexture()
-{
-    if (!id)
-        return;
-
-    if (inTexturePool)
-        QGLTexturePool::instance()->detachTexture(this);
-
-    if (boundPixmap)
-        boundPixmap->releaseNativeImageHandle();
-
-    if (options & QGLContext::MemoryManagedBindOption) {
-        Q_ASSERT(context);
-        context->d_ptr->texture_destroyer->emitFreeTexture(context, 0, id);
-    }
-
-    id = 0;
-    boundPixmap = 0;
-    boundKey = 0;
-}
-#endif
 
 QT_END_NAMESPACE

mkspecs/features/qt_functions.prf
@@ -45,31 +45,6 @@ defineTest(qtAddLibrary) {
            }
        }
     }
-    symbian {
-        isEqual(LIB_NAME, QtCore) {
-            #workaround for dependency from f32file.h on e32svr.h which has moved location in symbian3
-            INCLUDEPATH *= $$OS_LAYER_SYSTEMINCLUDE
-        } else:isEqual(LIB_NAME, QtGui) {
-            # Needed for #include <QtGui> because qs60mainapplication.h includes aknapp.h
-            INCLUDEPATH *= $$MW_LAYER_SYSTEMINCLUDE
-        } else:isEqual(LIB_NAME, QtWebKit) {
-            # Needed for because relative inclusion problem in toolchain
-            INCLUDEPATH *= $$QMAKE_INCDIR_QT/QtXmlPatterns
-            INCLUDEPATH *= $$QMAKE_INCDIR_QT/QtNetwork
-            isEmpty(TARGET.CAPABILITY): TARGET.CAPABILITY = NetworkServices
-            isEmpty(TARGET.EPOCHEAPSIZE):TARGET.EPOCHEAPSIZE = 0x20000 0x2000000
-        } else:isEqual(LIB_NAME, QtXmlPatterns) {
-            # Needed for #include <QtXmlPatterns/QtXmlPatterns> because relative inclusion problem in toolchain
-            INCLUDEPATH *= $$QMAKE_INCDIR_QT/QtNetwork
-        } else:isEqual(LIB_NAME, QtNetwork) {
-            isEmpty(TARGET.CAPABILITY): TARGET.CAPABILITY = NetworkServices
-        } else:isEqual(LIB_NAME, QtDeclarative) {
-            isEmpty(TARGET.CAPABILITY): TARGET.CAPABILITY = NetworkServices
-            isEmpty(TARGET.EPOCHEAPSIZE):TARGET.EPOCHEAPSIZE = 0x20000 0x2000000
-        }
-        export(TARGET.EPOCHEAPSIZE)
-        export(TARGET.CAPABILITY)
-    }
     isEmpty(LINKAGE) {
        if(!debug_and_release|build_pass):CONFIG(debug, debug|release) {
            win32:LINKAGE = -l$${LIB_NAME}d$${QT_LIBINFIX}

qmake/Makefile.unix
@@ -10,8 +10,7 @@ OBJS=project.o property.o main.o makefile.o unixmake2.o unixmake.o  \
      mingw_make.o option.o winmakefile.o projectgenerator.o \
      meta.o makefiledeps.o metamakefile.o xmloutput.o pbuilder_pbx.o \
      borland_bmake.o msvc_vcproj.o msvc_vcxproj.o msvc_nmake.o msvc_objectmodel.o msbuild_objectmodel.o \
-     symmake.o symmake_abld.o symmake_sbsv2.o \
-     registry.o epocroot.o gbuild.o
+     registry.o gbuild.o
 
 #qt code
 QOBJS=qtextcodec.o qutfcodec.o qstring.o qtextstream.o qiodevice.o qmalloc.o qglobal.o \
@@ -74,7 +73,7 @@ CPPFLAGS = -I. -Igenerators -Igenerators/unix -Igenerators/win32 \
 	   -I$(BUILD_PATH)/src/corelib/global -I$(BUILD_PATH)/src/corelib/xml \
 	   -I$(SOURCE_PATH)/tools/shared \
 	   -DQT_NO_PCRE \
-	   -DQT_BUILD_QMAKE -DQT_BOOTSTRAPPED -DQLIBRARYINFO_EPOCROOT \
+	   -DQT_BUILD_QMAKE -DQT_BOOTSTRAPPED \
 	   -DQT_NO_TEXTCODEC -DQT_NO_UNICODETABLES -DQT_NO_COMPONENT -DQT_NO_STL \
 	   -DQT_NO_COMPRESS -I$(QMAKESPEC) -DHAVE_QCONFIG_CPP -DQT_NO_THREAD -DQT_NO_QOBJECT \
            -DQT_NO_GEOM_VARIANT -DQT_NO_DEPRECATED $(OPENSOURCE_CXXFLAGS)

qmake/Makefile.win32
@@ -40,8 +40,7 @@ CFLAGS_BARE = -c -Fo./ \
               -I$(SOURCE_PATH)\tools\shared \
               -DQT_NO_TEXTCODEC -DQT_NO_UNICODETABLES -DQT_LITE_COMPONENT -DQT_NODLL -DQT_NO_STL \
               -DQT_NO_COMPRESS -DUNICODE -DHAVE_QCONFIG_CPP -DQT_BUILD_QMAKE -DQT_NO_THREAD \
-              -DQT_NO_QOBJECT -DQT_NO_GEOM_VARIANT -DQT_NO_DATASTREAM -DQT_NO_PCRE -DQT_BOOTSTRAPPED \
-              -DQLIBRARYINFO_EPOCROOT
+              -DQT_NO_QOBJECT -DQT_NO_GEOM_VARIANT -DQT_NO_DATASTREAM -DQT_NO_PCRE -DQT_BOOTSTRAPPED
 CFLAGS   = -Yuqmake_pch.h -FIqmake_pch.h -Fpqmake_pch.pch $(CFLAGS_BARE) $(CFLAGS)
 
 CXXFLAGS_BARE = $(CFLAGS_BARE)
@@ -61,8 +60,8 @@ OBJS        = project.obj main.obj makefile.obj unixmake.obj unixmake2.obj mingw
               option.obj winmakefile.obj projectgenerator.obj property.obj meta.obj \
               makefiledeps.obj metamakefile.obj xmloutput.obj pbuilder_pbx.obj \
               borland_bmake.obj msvc_nmake.obj msvc_vcproj.obj msvc_vcxproj.obj \
-              msvc_objectmodel.obj msbuild_objectmodel.obj symmake.obj \
-              symmake_abld.obj symmake_sbsv2.obj registry.obj epocroot.obj \
+              msvc_objectmodel.obj msbuild_objectmodel.obj \
+              registry.obj \
               gbuild.obj
 
 !IFDEF QMAKE_OPENSOURCE_EDITION

qmake/Makefile.win32-g++
@@ -25,7 +25,7 @@ CFLAGS	    =	-c -o$@ -O \
 		-DQT_NO_TEXTCODEC -DQT_NO_UNICODETABLES -DQT_LITE_COMPONENT -DQT_NO_PCRE \
 		-DQT_NODLL -DQT_NO_STL -DQT_NO_COMPRESS -DUNICODE -DHAVE_QCONFIG_CPP \
 		-DQT_BUILD_QMAKE -DQT_NO_THREAD -DQT_NO_QOBJECT -DQT_NO_GEOM_VARIANT -DQT_NO_DATASTREAM \
-		-DQT_BOOTSTRAPPED -DQLIBRARYINFO_EPOCROOT
+		-DQT_BOOTSTRAPPED
 CXXFLAGS    =   $(CFLAGS)
 LFLAGS      = -static-libgcc -s
 LIBS	    = -lole32 -luuid -ladvapi32 -lkernel32
@@ -38,8 +38,8 @@ OBJS        = project.o main.o makefile.o unixmake.o unixmake2.o mingw_make.o \
               option.o winmakefile.o projectgenerator.o property.o meta.o \
               makefiledeps.o metamakefile.o xmloutput.o pbuilder_pbx.o \
               borland_bmake.o msvc_nmake.o msvc_vcproj.o msvc_vcxproj.o \
-              msvc_objectmodel.o msbuild_objectmodel.o symmake.o  \
-              symmake_abld.o symmake_sbsv2.o registry.o epocroot.o gbuild.o
+              msvc_objectmodel.o msbuild_objectmodel.o  \
+              registry.o gbuild.o
 
 ifdef QMAKE_OPENSOURCE_EDITION
 CFLAGS      += -DQMAKE_OPENSOURCE_EDITION

qmake/Makefile.win32-g++-sh
@@ -25,7 +25,7 @@ CFLAGS	    =	-c -o$@ -O \
 		-DQT_NO_TEXTCODEC -DQT_NO_UNICODETABLES -DQT_LITE_COMPONENT -DQT_NO_PCRE \
 		-DQT_NODLL -DQT_NO_STL -DQT_NO_COMPRESS -DUNICODE -DHAVE_QCONFIG_CPP \
 		-DQT_BUILD_QMAKE -DQT_NO_THREAD -DQT_NO_QOBJECT -DQT_NO_GEOM_VARIANT -DQT_NO_DATASTREAM \
-		-DQT_BOOTSTRAPPED -DQLIBRARYINFO_EPOCROOT
+		-DQT_BOOTSTRAPPED
 CXXFLAGS    =   $(CFLAGS)
 LFLAGS	    = -static-libgcc -static-libstdc++ -s
 LIBS	    = -lole32 -luuid -ladvapi32 -lkernel32
@@ -38,8 +38,8 @@ OBJS        = project.o main.o makefile.o unixmake.o unixmake2.o mingw_make.o \
               option.o winmakefile.o projectgenerator.o property.o meta.o \
               makefiledeps.o metamakefile.o xmloutput.o pbuilder_pbx.o \
               borland_bmake.o msvc_nmake.o msvc_vcproj.o msvc_vcxproj.o \
-              msvc_objectmodel.o msbuild_objectmodel.o symmake.o initprojectdeploy_symbian.o \
-              symmake_abld.o symmake_sbsv2.o symbiancommon.o registry.o epocroot.o gbuild.o
+              msvc_objectmodel.o msbuild_objectmodel.o \
+              registry.o gbuild.o
 
 ifdef QMAKE_OPENSOURCE_EDITION
 CFLAGS      += -DQMAKE_OPENSOURCE_EDITION
@@ -293,26 +293,8 @@ msvc_vcxproj.o: $(SOURCE_PATH)/qmake/generators/win32/msvc_vcxproj.cpp
 msbuild_objectmodel.o: $(SOURCE_PATH)/qmake/generators/win32/msbuild_objectmodel.cpp
 	$(CXX) $(CXXFLAGS) generators/win32/msbuild_objectmodel.cpp
 
-symmake.o: $(SOURCE_PATH)/qmake/generators/symbian/symmake.cpp
-	$(CXX) $(CXXFLAGS) generators/symbian/symmake.cpp
-
-symmake_abld.o: $(SOURCE_PATH)/qmake/generators/symbian/symmake_abld.cpp
-	$(CXX) $(CXXFLAGS) generators/symbian/symmake_abld.cpp
-
-symmake_sbsv2.o: $(SOURCE_PATH)/qmake/generators/symbian/symmake_sbsv2.cpp
-	$(CXX) $(CXXFLAGS) generators/symbian/symmake_sbsv2.cpp
-
-symbiancommon.o: $(SOURCE_PATH)/qmake/generators/symbian/symbiancommon.cpp
-	$(CXX) $(CXXFLAGS) generators/symbian/symbiancommon.cpp
-
-initprojectdeploy_symbian.o: $(SOURCE_PATH)/qmake/generators/symbian/initprojectdeploy_symbian.cpp
-	$(CXX) $(CXXFLAGS) generators/symbian/initprojectdeploy_symbian.cpp
-
 registry.o: $(SOURCE_PATH)/tools/shared/windows/registry.cpp
 	$(CXX) $(CXXFLAGS) $(SOURCE_PATH)/tools/shared/windows/registry.cpp
-
-epocroot.o: $(SOURCE_PATH)/tools/shared/symbian/epocroot.cpp
-	$(CXX) $(CXXFLAGS) $(SOURCE_PATH)/tools/shared/symbian/epocroot.cpp
 
 gbuild.o: $(SOURCE_PATH)/qmake/generators/integrity/gbuild.cpp
 	$(CXX) $(CXXFLAGS) generators/integrity/gbuild.cpp

qmake/generators/metamakefile.cpp
@@ -445,8 +445,6 @@ QT_BEGIN_INCLUDE_NAMESPACE
 #include "borland_bmake.h"
 #include "msvc_vcproj.h"
 #include "msvc_vcxproj.h"
-#include "symmake_abld.h"
-#include "symmake_sbsv2.h"
 #include "gbuild.h"
 QT_END_INCLUDE_NAMESPACE
 

src/gui/kernel/qdnd_s60.cpp
@@ -1,359 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the QtGui module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** As a special exception, The Qt Company gives you certain additional
-** rights. These rights are described in The Qt Company LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#include "qapplication.h"
-
-#ifndef QT_NO_DRAGANDDROP
-
-#include "qwidget.h"
-#include "qdatetime.h"
-#include "qbitmap.h"
-#include "qcursor.h"
-#include "qevent.h"
-#include "qpainter.h"
-#include "qdnd_p.h"
-#include "qt_s60_p.h"
-
-#include <coecntrl.h>
-// pointer cursor
-#include <w32std.h>
-#include <gdi.h>
-#include <QCursor>
-
-QT_BEGIN_NAMESPACE
-//### artistic impression of Symbians default DnD cursor ?
-
-static QPixmap *defaultPm = 0;
-static const int default_pm_hotx = -50;
-static const int default_pm_hoty = -50;
-static const char *const default_pm[] = {
-"13 9 3 1",
-".      c None",
-"       c #000000",
-"X      c #FFFFFF",
-"X X X X X X X",
-" X X X X X X ",
-"X ......... X",
-" X.........X ",
-"X ......... X",
-" X.........X ",
-"X ......... X",
-" X X X X X X ",
-"X X X X X X X",
-};
-//### actions need to be redefined for S60
-// Shift/Ctrl handling, and final drop status
-static Qt::DropAction global_accepted_action = Qt::MoveAction;
-static Qt::DropActions possible_actions = Qt::IgnoreAction;
-
-
-// static variables in place of a proper cross-process solution
-static QDrag *drag_object;
-static bool qt_symbian_dnd_dragging = false;
-
-
-static Qt::KeyboardModifiers oldstate;
-
-void QDragManager::updatePixmap()
-{
-    QPixmap pm;
-    QPoint pm_hot(default_pm_hotx,default_pm_hoty);
-    if (drag_object) {
-        pm = drag_object->pixmap();
-        if (!pm.isNull())
-            pm_hot = drag_object->hotSpot();
-    }
-    if (pm.isNull()) {
-        if (!defaultPm)
-            defaultPm = new QPixmap(default_pm);
-        pm = *defaultPm;
-    }
-#ifndef QT_NO_CURSOR
-    QCursor cursor(pm, pm_hot.x(), pm_hot.y());
-    overrideCursor = cursor;
-#endif
-}
-
-void QDragManager::timerEvent(QTimerEvent *) { }
-
-void QDragManager::move(const QPoint&) {
-}
-
-void QDragManager::updateCursor()
-{
-#ifndef QT_NO_CURSOR
-    QCursor cursor = willDrop ? overrideCursor : Qt::ForbiddenCursor;
-    if (!restoreCursor) {
-        QApplication::setOverrideCursor(cursor);
-        restoreCursor = true;
-    }
-    else {
-        QApplication::changeOverrideCursor(cursor);
-    }
-#endif
-}
-
-
-bool QDragManager::eventFilter(QObject *o, QEvent *e)
-{
- if (beingCancelled) {
-        return false;
-    }
-    if (!o->isWidgetType())
-        return false;
-
-    switch(e->type()) {
-        case QEvent::MouseButtonPress:
-        {
-        }
-        case QEvent::MouseMove:
-        {
-            if (!object) { //#### this should not happen
-                qWarning("QDragManager::eventFilter: No object");
-                return true;
-            }
-            QDragManager *manager = QDragManager::self();
-            QMimeData *dropData = manager->object ? manager->dragPrivate()->data : manager->dropData;
-            if (manager->object)
-                possible_actions =  manager->dragPrivate()->possible_actions;
-            else
-                possible_actions = Qt::IgnoreAction;
-
-            QMouseEvent *me = (QMouseEvent *)e;
-
-            if (me->buttons()) {
-                Qt::DropAction prevAction = global_accepted_action;
-                QWidget *cw = QApplication::widgetAt(me->globalPos());
-                // map the Coords relative to the window.
-                if (!cw)
-                    return true;
-
-                while (cw && !cw->acceptDrops() && !cw->isWindow())
-                    cw = cw->parentWidget();
-
-                bool oldWillDrop = willDrop;
-                if (object->target() != cw) {
-                    if (object->target()) {
-                        QDragLeaveEvent dle;
-                        QApplication::sendEvent(object->target(), &dle);
-                        willDrop = false;
-                        global_accepted_action = Qt::IgnoreAction;
-                        if (oldWillDrop != willDrop)
-                            updateCursor();
-                        object->d_func()->target = 0;
-                    }
-                    if (cw && cw->acceptDrops()) {
-                        object->d_func()->target = cw;
-                        QDragEnterEvent dee(cw->mapFromGlobal(me->globalPos()), possible_actions, dropData,
-                                            me->buttons(), me->modifiers());
-                        QApplication::sendEvent(object->target(), &dee);
-                        willDrop = dee.isAccepted() && dee.dropAction() != Qt::IgnoreAction;
-                        global_accepted_action = willDrop ? dee.dropAction() : Qt::IgnoreAction;
-                        if (oldWillDrop != willDrop)
-                            updateCursor();
-                    }
-                } else if (cw) {
-                    QDragMoveEvent dme(cw->mapFromGlobal(me->globalPos()), possible_actions, dropData,
-                                       me->buttons(), me->modifiers());
-                    if (global_accepted_action != Qt::IgnoreAction) {
-                        dme.setDropAction(global_accepted_action);
-                        dme.accept();
-                    }
-                    QApplication::sendEvent(cw, &dme);
-                    willDrop = dme.isAccepted();
-                    global_accepted_action = willDrop ? dme.dropAction() : Qt::IgnoreAction;
-                    if (oldWillDrop != willDrop) {
-                        updatePixmap();
-                        updateCursor();
-                    }
-                }
-                if (global_accepted_action != prevAction)
-                    emitActionChanged(global_accepted_action);
-            }
-            return true; // Eat all mouse events
-        }
-
-        case QEvent::MouseButtonRelease:
-        {
-            qApp->removeEventFilter(this);
-#ifndef QT_NO_CURSOR
-            if (restoreCursor) {
-                QApplication::restoreOverrideCursor();
-                willDrop = false;
-                restoreCursor = false;
-            }
-#endif
-            if (object && object->target()) {
-
-                QMouseEvent *me = (QMouseEvent *)e;
-
-                QDragManager *manager = QDragManager::self();
-                QMimeData *dropData = manager->object ? manager->dragPrivate()->data : manager->dropData;
-
-                QDropEvent de(object->target()->mapFromGlobal(me->globalPos()), possible_actions, dropData,
-                              me->buttons(), me->modifiers());
-                QApplication::sendEvent(object->target(), &de);
-                if (de.isAccepted())
-                    global_accepted_action = de.dropAction();
-                else
-                    global_accepted_action = Qt::IgnoreAction;
-
-                if (object)
-                    object->deleteLater();
-                drag_object = object = 0;
-            }
-            eventLoop->exit();
-            return true; // Eat all mouse events
-        }
-
-        default:
-             break;
-    }
-    return false;
-}
-
-Qt::DropAction QDragManager::drag(QDrag *o)
-{
-    Q_ASSERT(!qt_symbian_dnd_dragging);
-    if (object == o || !o || !o->source())
-         return Qt::IgnoreAction;
-
-    if (object) {
-        cancel();
-        qApp->removeEventFilter(this);
-        beingCancelled = false;
-    }
-
-    object = drag_object = o;
-
-    oldstate = Qt::NoModifier; // #### Should use state that caused the drag
-    willDrop = false;
-    updatePixmap();
-    updateCursor();
-
-#ifndef QT_NO_CURSOR
-    qt_symbian_set_cursor_visible(true); //force cursor on even for touch phone
-#endif
-
-    object->d_func()->target = 0;
-
-    qApp->installEventFilter(this);
-
-    global_accepted_action = defaultAction(dragPrivate()->possible_actions, Qt::NoModifier);
-    qt_symbian_dnd_dragging = true;
-
-    eventLoop = new QEventLoop;
-    // block
-    (void) eventLoop->exec(QEventLoop::AllEvents);
-    delete eventLoop;
-    eventLoop = 0;
-
-#ifndef QT_NO_CURSOR
-    qt_symbian_set_cursor_visible(false);
-
-    overrideCursor = QCursor(); //deref the cursor data
-    qt_symbian_dnd_dragging = false;
-#endif
-
-    return global_accepted_action;
-}
-
-
-void QDragManager::cancel(bool deleteSource)
-{
-    beingCancelled = true;
-
-    if (object->target()) {
-        QDragLeaveEvent dle;
-        QApplication::sendEvent(object->target(), &dle);
-    }
-
-    if (drag_object) {
-        if (deleteSource)
-            object->deleteLater();
-        drag_object = object = 0;
-    }
-
-#ifndef QT_NO_CURSOR
-    if (restoreCursor) {
-        QApplication::restoreOverrideCursor();
-        restoreCursor = false;
-    }
-#endif
-
-    global_accepted_action = Qt::IgnoreAction;
-}
-
-
-void QDragManager::drop()
-{
-#ifndef QT_NO_CURSOR
-    if (restoreCursor) {
-        QApplication::restoreOverrideCursor();
-        restoreCursor = false;
-    }
-#endif
-}
-
-QVariant QDropData::retrieveData_sys(const QString &mimetype, QVariant::Type type) const
-{
-    if (!drag_object)
-        return QVariant();
-    QByteArray data =  drag_object->mimeData()->data(mimetype);
-    if (type == QVariant::String)
-        return QString::fromUtf8(data);
-    return data;
-}
-
-bool QDropData::hasFormat_sys(const QString &format) const
-{
-    return formats().contains(format);
-}
-
-QStringList QDropData::formats_sys() const
-{
-    if (drag_object)
-        return drag_object->mimeData()->formats();
-    return QStringList();
-}
-
-QT_END_NAMESPACE
-#endif // QT_NO_DRAGANDDROP

commit 033ce8c66ac79d865a1c721450ffac0f03303a12

Fix error introduced with 0b41638

src/opengl/qgl.cpp
@@ -2579,7 +2579,8 @@ QGLTexture* QGLContextPrivate::bindTexture(const QImage &image, GLenum target, G
     int cost = img.width()*img.height()*4/1024;
     QGLTexture *texture = new QGLTexture(q, tx_id, target, options);
     QGLTextureCache::instance()->insert(q, key, texture, cost);
-texture;
+
+    return texture;
 }
 
 QGLTexture *QGLContextPrivate::textureCacheLookup(const qint64 key, GLenum target)